a46970d3db3d52ae86c9b6da666475b99a2462c4
mark <mark@lussumo.com>
2009-08-05 09:50:37 -0600

    Increased autosave time to 60 seconds. Added spinner when comments are saved.

--- applications/vanilla/js/autosave.js
+++ applications/vanilla/js/autosave.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
    
 /* Autosave functionality for comment & discussion drafts */
    $.fn.autosave = function(opts) {
-      var options = $.extend({interval: 30000, button: false}, opts);
+      var options = $.extend({interval: 60000, button: false}, opts);
       var textarea = this;
       if (!options.button)
          return false;

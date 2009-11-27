53b71ce25c50e06b0ee9c1978547f1b63d6df298
unknown <Lincoln@.icrontic.ichq>
2009-11-27 02:45:14 -0500

    Substitutes spaces for tabs

--- library/core/class.smarty.php
+++ library/core/class.smarty.php
@@ -9,64 +9,64 @@ Contact Mark O'Sullivan at mark [at] lussumo [dot] com
 */
 
 class Gdn_Smarty {
-	/// Constructor ///
-	
-	/// Properties ///
-	
-	/**
-	 * @var Smarty The smarty object used for the template.
-	 */
-	protected $_Smarty = NULL;
-	
-	/// Methods ///
-	
-	/**
-	 * Render the given view.
-	 *
-	 * @param string $Path The path to the view's file.
-	 * @param Controller $Controller The controller that is rendering the view.
-	 */
-	public function Render($Path, $Controller) {
-		$Smarty = $this->Smarty();
-		
-		// Get a friendly name for the controller.
-		$ControllerName = get_class($Controller);
-		if(preg_match('/^(?:Gdn_)?(.*?)(?:Controller)?$/', $ControllerName, $Matches)) {
-			$ControllerName = $Matches[1];
-		}
-		$Smarty->assign('ControllerName', $ControllerName);
-		
-		// Get an ID for the body.
-		$BodyIdentifier = strtolower($Controller->ApplicationFolder.'_'.$ControllerName.'_'.Format::AlphaNumeric(strtolower($Controller->RequestMethod)));
-		$Smarty->assign('BodyIdentifier', $BodyIdentifier);
-		$Smarty->assign('Config', Gdn::Config());
-		
-		// Make sure that any datasets use arrays instead of objects.
-		foreach($Controller->Data as $Key => $Value) {
-			if($Value instanceof Gdn_DataSet) {
-				$Value->DefaultDatasetType = DATASET_TYPE_ARRAY;
-			}
-		}
-		
-		$Smarty->assign($Controller->Data);
-		$Smarty->assign('Controller', $Controller);
-		
-		$Smarty->display($Path);
-	}
-	
-	/**
-	 * @return Smarty The smarty object used for rendering.
-	 */
-	public function Smarty() {
-		if(is_null($this->_Smarty)) {	
-			$Smarty = Gdn::Factory('Smarty');
-			
-			$Smarty->cache_dir = PATH_CACHE . DS . 'Smarty' . DS . 'cache';
-			$Smarty->compile_dir = PATH_CACHE . DS . 'Smarty' . DS . 'compile';
-			$Smarty->plugins_dir[] = PATH_LIBRARY . DS . 'vendors' . DS . 'SmartyPlugins';
-			
-			$this->_Smarty = $Smarty;
-		}
-		return $this->_Smarty;
-	}
+   /// Constructor ///
+   
+   /// Properties ///
+   
+   /**
+    * @var Smarty The smarty object used for the template.
+    */
+   protected $_Smarty = NULL;
+   
+   /// Methods ///
+   
+   /**
+    * Render the given view.
+    *
+    * @param string $Path The path to the view's file.
+    * @param Controller $Controller The controller that is rendering the view.
+    */
+   public function Render($Path, $Controller) {
+      $Smarty = $this->Smarty();
+      
+      // Get a friendly name for the controller.
+      $ControllerName = get_class($Controller);
+      if(preg_match('/^(?:Gdn_)?(.*?)(?:Controller)?$/', $ControllerName, $Matches)) {
+         $ControllerName = $Matches[1];
+      }
+      $Smarty->assign('ControllerName', $ControllerName);
+      
+      // Get an ID for the body.
+      $BodyIdentifier = strtolower($Controller->ApplicationFolder.'_'.$ControllerName.'_'.Format::AlphaNumeric(strtolower($Controller->RequestMethod)));
+      $Smarty->assign('BodyIdentifier', $BodyIdentifier);
+      $Smarty->assign('Config', Gdn::Config());
+      
+      // Make sure that any datasets use arrays instead of objects.
+      foreach($Controller->Data as $Key => $Value) {
+         if($Value instanceof Gdn_DataSet) {
+            $Value->DefaultDatasetType = DATASET_TYPE_ARRAY;
+         }
+      }
+      
+      $Smarty->assign($Controller->Data);
+      $Smarty->assign('Controller', $Controller);
+      
+      $Smarty->display($Path);
+   }
+   
+   /**
+    * @return Smarty The smarty object used for rendering.
+    */
+   public function Smarty() {
+      if(is_null($this->_Smarty)) {   
+         $Smarty = Gdn::Factory('Smarty');
+         
+         $Smarty->cache_dir = PATH_CACHE . DS . 'Smarty' . DS . 'cache';
+         $Smarty->compile_dir = PATH_CACHE . DS . 'Smarty' . DS . 'compile';
+         $Smarty->plugins_dir[] = PATH_LIBRARY . DS . 'vendors' . DS . 'SmartyPlugins';
+         
+         $this->_Smarty = $Smarty;
+      }
+      return $this->_Smarty;
+   }
 }

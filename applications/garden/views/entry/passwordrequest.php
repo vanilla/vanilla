b4c4cb837e184e73409fd6c0e963f1a41d20ed37
mark <mark@vanillaforums.com>
2010-01-08 08:35:38 -0600

    Html & local def fixes.

--- applications/garden/views/entry/passwordrequest.php
+++ applications/garden/views/entry/passwordrequest.php
@@ -1,21 +1,23 @@
 <?php if (!defined('APPLICATION')) exit(); ?>
 <h1><?php echo Translate("Reset my password") ?></h1>
-<?php
-// Make sure to force this form to post to the correct place in case the view is
-// rendered within another view (ie. /garden/entry/index/):
-echo $this->Form->Open(array('Action' => Url('/entry/passwordrequest'), 'id' => 'Form_User_Password'));
-echo $this->Form->Errors(); ?>
-<ul>
-   <li>
-      <?php
-         echo $this->Form->Label('Enter your email address', 'Email');
-         echo $this->Form->TextBox('Email');
-      ?>
-   </li>
-   <li>
-      <?php
-         echo $this->Form->Button('Request a new password');
-      ?>
-   </li>
-</ul>
-<?php echo $this->Form->Close();
+<div class="Box">
+   <?php
+   // Make sure to force this form to post to the correct place in case the view is
+   // rendered within another view (ie. /garden/entry/index/):
+   echo $this->Form->Open(array('Action' => Url('/entry/passwordrequest'), 'id' => 'Form_User_Password'));
+   echo $this->Form->Errors(); ?>
+   <ul>
+      <li>
+         <?php
+            echo $this->Form->Label('Enter your email address', 'Email');
+            echo $this->Form->TextBox('Email');
+         ?>
+      </li>
+      <li>
+         <?php
+            echo $this->Form->Button('Request a new password');
+         ?>
+      </li>
+   </ul>
+   <?php echo $this->Form->Close(); ?>
+</div>
--- applications/garden/views/entry/registerthanks.php
+++ applications/garden/views/entry/registerthanks.php
@@ -1,3 +1,5 @@
 <?php if (!defined('APPLICATION')) exit(); ?>
 <h1><?php echo Gdn::Translate('Thank You!'); ?></h1>
-<p><?php echo Gdn::Translate('Your application will be reviewed by an administrator. You will be notified by email if your application is approved.'); ?></p>
+<div class="Box">
+   <p><?php echo Gdn::Translate('Your application will be reviewed by an administrator. You will be notified by email if your application is approved.'); ?></p>
+</div>

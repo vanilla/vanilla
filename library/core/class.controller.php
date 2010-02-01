c3559f773b28e635f2bb8af4aab32c90428b8c7f
mark <mark@vanillaforums.com>
2010-02-01 09:38:35 -0600

    Fixed a bug that caused the FavIcon to link incorrectly when rewrite urls is not enabled. Fixed a bug in Asset() that caused paths with domain to return incorrectly.

--- library/core/class.controller.php
+++ library/core/class.controller.php
@@ -932,7 +932,7 @@ class Gdn_Controller extends Gdn_Pluggable {
             }
          }
          // Add the favicon
-         $this->Head->SetFavIcon(Url('/themes/'.$this->Theme.'/design/favicon.png', TRUE));
+         $this->Head->SetFavIcon(Asset('themes/'.$this->Theme.'/design/favicon.png'));
          
          // Make sure the head module gets passed into the assets collection.
          $this->AddModule('Head');

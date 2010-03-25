fe671f91344fe661bef9190d5e4ce27200ec8244
mark <mark@vanillaforums.com>
2010-03-25 09:44:20 -0400

    Added missing translations.

--- applications/vanilla/views/discussions/helper_functions.php
+++ applications/vanilla/views/discussions/helper_functions.php
@@ -31,27 +31,27 @@ function WriteDiscussion($Discussion, &$Sender, &$Session, $Alt) {
                
                // Dismiss an announcement
                if ($Discussion->Announce == '1' && $Discussion->Dismissed != '1')
-                  $Sender->Options .= '<li>'.Anchor('Dismiss', 'vanilla/discussion/dismissannouncement/'.$Discussion->DiscussionID.'/'.$Session->TransientKey(), 'DismissAnnouncement') . '</li>';
+                  $Sender->Options .= '<li>'.Anchor(Gdn::Translate('Dismiss'), 'vanilla/discussion/dismissannouncement/'.$Discussion->DiscussionID.'/'.$Session->TransientKey(), 'DismissAnnouncement') . '</li>';
                
                // Edit discussion
                if ($Discussion->FirstUserID == $Session->UserID || $Session->CheckPermission('Vanilla.Discussions.Edit', $Discussion->CategoryID))
-                  $Sender->Options .= '<li>'.Anchor('Edit', 'vanilla/post/editdiscussion/'.$Discussion->DiscussionID, 'EditDiscussion') . '</li>';
+                  $Sender->Options .= '<li>'.Anchor(Gdn::Translate('Edit'), 'vanilla/post/editdiscussion/'.$Discussion->DiscussionID, 'EditDiscussion') . '</li>';
       
                // Announce discussion
                if ($Session->CheckPermission('Vanilla.Discussions.Announce', $Discussion->CategoryID))
-                  $Sender->Options .= '<li>'.Anchor($Discussion->Announce == '1' ? 'Unannounce' : 'Announce', 'vanilla/discussion/announce/'.$Discussion->DiscussionID.'/'.$Session->TransientKey(), 'AnnounceDiscussion') . '</li>';
+                  $Sender->Options .= '<li>'.Anchor(Gdn::Translate($Discussion->Announce == '1' ? 'Unannounce' : 'Announce'), 'vanilla/discussion/announce/'.$Discussion->DiscussionID.'/'.$Session->TransientKey(), 'AnnounceDiscussion') . '</li>';
       
                // Sink discussion
                if ($Session->CheckPermission('Vanilla.Discussions.Sink', $Discussion->CategoryID))
-                  $Sender->Options .= '<li>'.Anchor($Discussion->Sink == '1' ? 'Unsink' : 'Sink', 'vanilla/discussion/sink/'.$Discussion->DiscussionID.'/'.$Session->TransientKey().'?Target='.urlencode($Sender->SelfUrl), 'SinkDiscussion') . '</li>';
+                  $Sender->Options .= '<li>'.Anchor(Gdn::Translate($Discussion->Sink == '1' ? 'Unsink' : 'Sink'), 'vanilla/discussion/sink/'.$Discussion->DiscussionID.'/'.$Session->TransientKey().'?Target='.urlencode($Sender->SelfUrl), 'SinkDiscussion') . '</li>';
       
                // Close discussion
                if ($Session->CheckPermission('Vanilla.Discussions.Close', $Discussion->CategoryID))
-                  $Sender->Options .= '<li>'.Anchor($Discussion->Closed == '1' ? 'Reopen' : 'Close', 'vanilla/discussion/close/'.$Discussion->DiscussionID.'/'.$Session->TransientKey().'?Target='.urlencode($Sender->SelfUrl), 'CloseDiscussion') . '</li>';
+                  $Sender->Options .= '<li>'.Anchor(Gdn::Translate($Discussion->Closed == '1' ? 'Reopen' : 'Close'), 'vanilla/discussion/close/'.$Discussion->DiscussionID.'/'.$Session->TransientKey().'?Target='.urlencode($Sender->SelfUrl), 'CloseDiscussion') . '</li>';
                
                // Delete discussion
                if ($Session->CheckPermission('Vanilla.Discussions.Delete', $Discussion->CategoryID))
-                  $Sender->Options .= '<li>'.Anchor('Delete', 'vanilla/discussion/delete/'.$Discussion->DiscussionID.'/'.$Session->TransientKey().'?Target='.urlencode($Sender->SelfUrl), 'DeleteDiscussion') . '</li>';
+                  $Sender->Options .= '<li>'.Anchor(Gdn::Translate('Delete'), 'vanilla/discussion/delete/'.$Discussion->DiscussionID.'/'.$Session->TransientKey().'?Target='.urlencode($Sender->SelfUrl), 'DeleteDiscussion') . '</li>';
                
                // Allow plugins to add options
                $Sender->FireEvent('DiscussionOptions');

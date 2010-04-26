b3440fd95f575aa69ac5a02c7db690abae4c2f8d
Mark O'Sullivan <mark@vanillaforums.com>
2010-04-26 10:50:17 -0400

    Changed DiscussionController to not pass formatted html, and instead pass the values and let the js (or theme) decide how it is presented. Added Event. Removed obselete code.

--- applications/vanilla/js/discussion.js
+++ applications/vanilla/js/discussion.js
@@ -1,21 +1,5 @@
 jQuery(document).ready(function($) {
    
-/* Options */
-
-   // Show options (if present) when hovering over the comment.
-   $('.Comment').livequery(function() {
-      $(this).hover(function() {
-         $(this).find('ul.Options:first').show();
-      }, function() {
-         $(this).find('ul.Options:first').hide();
-         /*
-         var opts = $(this).find('ul.Options:first');
-         if (!$(opts).find('li.Parent').hasClass('Active'))
-            $(opts).hide();
-         */
-      });
-   });
-   
 /* Comment Form */
 
    if ($.autogrow)

5356baa1d03e4683a15c3ebfa083c0b6c156733f
Tim Gunter <icyliquid@Vanilla-IceBook.local>
2010-05-06 12:00:50 -0400

    Added a new method to home controller for 'maintenance mode', and added routing and dispatching to match

---
+++ applications/dashboard/views/home/updatemode.php
@@ -0,0 +1,11 @@
+<?php if (!defined('APPLICATION')) exit(); ?>
+
+<div class="SplashInfo">
+   <h1><?php echo T('Maintenance Mode'); ?></h1>
+   <p><?php echo T('The site is currently undergoing scheduled maintenance.'); ?></p>
+</div>
+
+<div class="SplashImage">
+   <img src="applications/dashboard/design/images/icecream.jpg" />
+</div>
+<!-- Domain: <?php echo Gdn::Config('Garden.Domain', ''); ?> -->

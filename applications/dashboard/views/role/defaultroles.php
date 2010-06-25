f39163beb45ca47ddbbe14a404dff4ed26666ca8
Todd Burry <todd@vanillaforums.com>
2010-06-25 18:23:42 -0400

    Moved default roll warning to the role controller.

--- applications/dashboard/views/role/defaultroles.php
+++ applications/dashboard/views/role/defaultroles.php
@@ -7,11 +7,11 @@ echo $this->Form->Errors();
 <ul>
    <li id="NewUserRoles">
       <div><?php echo T('Member roles', 'Check all roles that should be applied to new/approved users'), T(':'); ?></div>
-      <?php echo $this->Form->CheckBoxList('DefaultRoles', $this->Data('RoleData'), NULL, array('TextField' => 'Name', 'ValueField' => 'RoleID')); ?>
+      <?php echo $this->Form->CheckBoxList('DefaultRoles', $this->Data('RoleData'), NULL, array('TextField' => 'Name', 'ValueField' => 'RoleID', 'listclass' => 'ColumnCheckBoxList')); ?>
    </li>
    <li>
       <div><?php echo T('Guest roles', 'Check all roles that should be applied to guests'), T(':'); ?></div>
-      <?php echo $this->Form->CheckBoxList('GuestRoles', $this->Data('RoleData'), NULL, array('TextField' => 'Name', 'ValueField' => 'RoleID')); ?>
+      <?php echo $this->Form->CheckBoxList('GuestRoles', $this->Data('RoleData'), NULL, array('TextField' => 'Name', 'ValueField' => 'RoleID', 'listclass' => 'ColumnCheckBoxList')); ?>
    </li>
 </ul>
 <?php echo $this->Form->Close('Save');

1eaadf2032625a52608cc64cce358e5de504f043
Mark O'Sullivan <mark@vanillaforums.com>
2010-06-26 19:04:32 -0400

    Formatting fixes.

--- applications/vanilla/views/discussions/bookmarked.php
+++ applications/vanilla/views/discussions/bookmarked.php
@@ -1,17 +1,16 @@
 <?php if (!defined('APPLICATION')) exit();
 include($this->FetchViewLocation('helper_functions', 'discussions', 'vanilla'));
-$ViewLocation = $this->FetchViewLocation('discussions');
-echo $this->Pager->ToString('less');
+
 WriteFilterTabs($this);
 if ($this->DiscussionData->NumRows() > 0) {
 ?>
 <ul class="DataList Discussions Bookmarks">
-   <?php include($ViewLocation); ?>
+   <?php include($this->FetchViewLocation('discussions')); ?>
 </ul>
 <?php
+   echo $this->Pager->ToString('more');
 } else {
-?>
-<div class="Empty"><?php echo T('You do not have any bookmarks.'); ?></div>
-<?php
+   ?>
+   <div class="Empty"><?php echo T('No discussions were found.'); ?></div>
+   <?php
 }
-echo $this->Pager->ToString('more');

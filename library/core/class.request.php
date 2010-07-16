a0e154e91016cd6f036817f3bfb72bc1a1a949f7
Todd Burry <todd@vanillaforums.com>
2010-07-16 08:19:08 -0400

    Removed most references to the console in Gdn_Request. We haven't finished console report and the code was making assumptions that made the application fail on nginx and possibly other web servers.

--- library/core/class.request.php
+++ library/core/class.request.php
@@ -298,23 +298,15 @@ class Gdn_Request {
     * @return void
     */
    protected function _LoadEnvironment() {
-   
       $this->_EnvironmentElement('ConfigWebRoot', Gdn::Config('Garden.WebRoot'));
       $this->_EnvironmentElement('ConfigStrips', Gdn::Config('Garden.StripWebRoot', FALSE));
 
-      if (!isset($_SERVER['SHELL'])) {
-         $this->RequestHost(     isset($_SERVER['HTTP_HOST']) ? ArrayValue('HTTP_HOST',$_SERVER) : ArrayValue('SERVER_NAME',$_SERVER));
-         $this->RequestMethod(   isset($_SERVER['REQUEST_METHOD']) ? ArrayValue('REQUEST_METHOD',$_SERVER) : 'CONSOLE');
-         
-         $this->RequestScheme(   (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https' : 'http');
-      } else {
-         $this->RequestScheme('console');
-      }
+      $this->RequestHost(     isset($_SERVER['HTTP_HOST']) ? ArrayValue('HTTP_HOST',$_SERVER) : ArrayValue('SERVER_NAME',$_SERVER));
+      $this->RequestMethod(   isset($_SERVER['REQUEST_METHOD']) ? ArrayValue('REQUEST_METHOD',$_SERVER) : 'CONSOLE');
+      $this->RequestScheme(   (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https' : 'http');
       
-      if ($this->RequestScheme() != "console" && is_array($_GET)) {
+      if (is_array($_GET)) {
          $Get = FALSE;
-//         if (is_array($_SERVER) && isset($_SERVER['QUERY_STRING']))
-//            $Get =& $_SERVER['QUERY_STRING'];
          if ($Get === FALSE) $Get =& $_GET;
          if (!is_array($Get)) {
             $Original = array();

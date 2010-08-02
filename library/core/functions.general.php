34b88e242a1ab964c3776476de2649426afdb7c0
Tim Gunter <tim@vanillaforums.com>
2010-08-02 12:32:45 -0400

    Removed 2nd declaration of parse_ini_string

--- library/core/functions.general.php
+++ library/core/functions.general.php
@@ -1298,30 +1298,6 @@ if (!function_exists('Url')) {
    }
 }
 
-if( !function_exists('parse_ini_string') ){
-   function parse_ini_string( $string ) {
-      $array = Array();
-      $lines = explode("\n", $string );
-   
-      foreach( $lines as $line ) {
-         $statement = preg_match("/^(?!;)(?P<key>[\w+\.\-]+?)\s*=\s*(?P<value>.+?)\s*$/", $line, $match );
-   
-         if( $statement ) {
-            $key    = $match[ 'key' ];
-            $value    = $match[ 'value' ];
-   
-            # Remove quote
-            if( preg_match( "/^\".*\"$/", $value ) || preg_match( "/^'.*'$/", $value ) ) {
-               $value = mb_substr( $value, 1, mb_strlen( $value ) - 2 );
-            }
-   
-            $array[ $key ] = $value;
-         }
-      }
-      return $array;
-   }
-}
-
 if (!function_exists('check_utf8')){
    function check_utf8($str) {
        $len = strlen($str);

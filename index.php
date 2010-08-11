c4d14820c238c5dacc34ded51a720d91ac4e7781
Tim Gunter <tim@vanillaforums.com>
2010-08-11 15:11:24 -0400

    Vew version

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 define('APPLICATION', 'Vanilla');
-define('APPLICATION_VERSION', '2.0.2a');
+define('APPLICATION_VERSION', '2.0.2');
 /*
 Copyright 2008, 2009 Vanilla Forums Inc.
 This file is part of Garden.

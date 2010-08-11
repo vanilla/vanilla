4a5970dfab462586e0a03e04c39ec5e60c5adbf4
Tim Gunter <tim@vanillaforums.com>
2010-08-11 15:49:36 -0400

    Slices are always relative paths

--- applications/dashboard/views/authentication/configure.php
+++ applications/dashboard/views/authentication/configure.php
@@ -1,4 +1,4 @@
-<div class="AuthenticationConfigure Slice" rel="/dashboard/authentication/configure">
+<div class="AuthenticationConfigure Slice" rel="dashboard/authentication/configure">
    <div class="Info">
       <?php echo $this->Data('ConfigureMessage'); ?>
    </div>

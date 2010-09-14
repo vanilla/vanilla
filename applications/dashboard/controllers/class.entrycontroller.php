2a820ab5ff282d04801ff8d63ba78591229b5322
Tim Gunter <tim@vanillaforums.com>
2010-09-14 15:25:10 -0400

    Fix signout. Now allows event hooks.

--- applications/dashboard/controllers/class.entrycontroller.php
+++ applications/dashboard/controllers/class.entrycontroller.php
@@ -167,9 +167,18 @@ class EntryController extends Gdn_Controller {
       $this->Auth('default');
    }
 
-   public function SignOut() {
+   public function SignOut($TransientKey = "") {
+      $SessionAuthenticator = Gdn::Session()->GetPreference('Authenticator');
+      $AuthenticationScheme = ($SessionAuthenticator) ? $SessionAuthenticator : 'default';
+
+      try {
+         $Authenticator = Gdn::Authenticator()->GetAuthenticator($AuthenticationScheme);
+      } catch (Exception $e) {
+         $Authenticator = Gdn::Authenticator()->GetAuthenticator();
+      }
+   
       $this->FireEvent("SignOut");
-      $this->Leave('default');
+      $this->Leave($AuthenticationScheme, $TransientKey);
    }
   
    /** A version of signin that supports multiple authentication methods.
--- conf/config-defaults.php
+++ conf/config-defaults.php
@@ -46,7 +46,7 @@ $Configuration['Garden']['Cookie']['HashMethod']                = 'md5'; // md5 
 $Configuration['Garden']['Authenticator']['DefaultScheme']      = 'password'; // Types include 'Password', 'Handshake', 'Openid'
 $Configuration['Garden']['Authenticator']['RegisterUrl']        = '/entry/register?Target=%2$s';
 $Configuration['Garden']['Authenticator']['SignInUrl']          = '/entry/signin?Target=%2$s';
-$Configuration['Garden']['Authenticator']['SignOutUrl']         = '/entry/leave/%1$s/{Session_TransientKey}?Target=%2$s';
+$Configuration['Garden']['Authenticator']['SignOutUrl']         = '/entry/signout/{Session_TransientKey}?Target=%2$s';
 $Configuration['Garden']['Authenticator']['EnabledSchemes']     = array('password');
 $Configuration['Garden']['Authenticator']['SyncScreen']         = "smart";
 $Configuration['Garden']['Authenticators']['password']['Name']  = "Password";

b1fc9376f406dfa0ff07fb07016573a8147bd48c
Tim Gunter <tim@vanillaforums.com>
2010-09-24 16:59:43 -0400

    Added REST compatible HTTP response codes to Router interface

--- library/core/class.router.php
+++ library/core/class.router.php
@@ -8,10 +8,12 @@ class Gdn_Router {
 
    public function __construct() {
       $this->RouteTypes = array(
-         'Internal'     => 'Internal',
-         'Temporary'    => 'Temporary (302)',
-         'Permanent'    => 'Permanent (301)',
-         'NotFound'     => 'Not Found (404)'
+         'Internal'        => 'Internal',
+         'Temporary'       => 'Temporary (302)',
+         'Permanent'       => 'Permanent (301)',
+         'NotFound'        => 'Not Found (404)',
+         'NotAuthorized'   => 'Not Authorized (401)',
+         'PaymentRequired' => 'Payment Required (402)'
       );
       $this->ReservedRoutes = array('DefaultController', 'Default404', 'DefaultPermission', 'UpdateMode');
       $this->_LoadRoutes();

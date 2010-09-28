568f3f1383fe6b6dbbf1b371c2dbb5c4a013542a
Tim Gunter <tim@vanillaforums.com>
2010-09-28 17:02:01 -0400

    Prevent banned users from signing in

--- library/core/class.session.php
+++ library/core/class.session.php
@@ -234,6 +234,7 @@ class Gdn_Session {
          $this->User = $UserModel->GetSession($this->UserID);
 
          if ($this->User) {
+         
             if ($UserID && $SetIdentity)
                Gdn::Authenticator()->SetIdentity($UserID);
          
@@ -248,6 +249,10 @@ class Gdn_Session {
             $this->_Preferences = Gdn_Format::Unserialize($this->User->Preferences);
             $this->_Attributes = Gdn_Format::Unserialize($this->User->Attributes);
             $this->_TransientKey = is_array($this->_Attributes) ? ArrayValue('TransientKey', $this->_Attributes) : FALSE;
+            
+            if (!$this->CheckPermission('Garden.SignIn.Allow'))
+               return $this->End(Gdn::Authenticator());
+               
             if ($this->_TransientKey === FALSE)
                $this->_TransientKey = $UserModel->SetTransientKey($this->UserID);
                

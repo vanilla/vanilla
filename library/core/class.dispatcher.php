077322ed965b4427ca9fa65052f3fa8b841b5efc
Todd Burry <todd@vanillaforums.com>
2010-10-04 17:37:37 -0400

    Fixed bug in Gdn_Dispatcher where the .json/.xml file extensions were sometimes not recognized.

--- library/core/class.dispatcher.php
+++ library/core/class.dispatcher.php
@@ -605,10 +605,10 @@ class Gdn_Dispatcher extends Gdn_Pluggable {
          if (in_array(strtoupper($Parts[1]), array(DELIVERY_METHOD_JSON, DELIVERY_METHOD_XHTML, DELIVERY_METHOD_XML))) {
             return array($Parts[0], strtoupper($Parts[1]));
          } else {
-            return array($Name, FALSE);
+            return array($Name, $this->_DeliveryMethod);
          }
       } else {
-         return array($Name, FALSE);
+         return array($Name, $this->_DeliveryMethod);
       }
    }
 }

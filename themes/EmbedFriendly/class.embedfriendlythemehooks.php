9b0234397f8726d078583f952c9100a996b424d1
Mark O'Sullivan <mark@vanillaforums.com>
2010-10-13 15:56:28 -0400

    Added option to hide side panel for EmbedFriendly theme.

--- themes/EmbedFriendly/class.embedfriendlythemehooks.php
+++ themes/EmbedFriendly/class.embedfriendlythemehooks.php
@@ -13,11 +13,62 @@ class EmbedFriendlyThemeHooks implements Gdn_IPlugin {
    public function Setup() {
 		// Set the order for the modules (make sure new discussion module is before content).
 		SaveToConfig('Modules.Vanilla.Content', array('MessageModule', 'Notices', 'NewConversationModule', 'NewDiscussionModule', 'Content', 'Ads'));
+		SaveToConfig('Modules.Conversations.Content', array('MessageModule', 'Notices', 'NewConversationModule', 'NewDiscussionModule', 'Content', 'Ads'));
    }
 
    public function OnDisable() {
       return TRUE;
    }
+	
+	public function SettingsController_AfterCurrentTheme_Handler($Sender) {
+		$SingleColumn = C('Themes.EmbedFriendly.SingleColumn');
+		echo Wrap(
+			T('This theme allows you to hide the side panel next to your forum and conversations. This is super handy if the website you are embedding in does not have a lot of width to squeeze into.')
+			.Wrap(Anchor(
+				T($SingleColumn ? 'Show the side panel' : 'Hide the side panel'),
+				'settings/embedfriendlytogglepanel/'.Gdn::Session()->TransientKey(),
+				'SmallButton'
+			), 'div')
+		, 'div', array('class' => 'Description'));
+	}
+	
+	public function SettingsController_EmbedFriendlyTogglePanel_Create($Sender) {
+		$this->_TogglePanel($Sender);
+		Redirect('settings/themes');
+	}
+	
+	public function PluginController_BeforeEmbedRecommend_Handler($Sender) {
+		$SingleColumn = C('Themes.EmbedFriendly.SingleColumn');
+		echo '<div class="EmbedRecommend">
+			<strong>Theme Options</strong>'
+			.Wrap(
+				T('This theme allows you to hide the side panel next to your forum and conversations. This is super handy if the website you are embedding in does not have a lot of width to squeeze into.')
+				.Wrap(Anchor(
+					T($SingleColumn ? 'Show the side panel' : 'Hide the side panel'),
+					'plugin/embedfriendlytogglepanel/'.Gdn::Session()->TransientKey(),
+					'SmallButton'
+				), 'div', array('style' => 'margin-top: 10px;'))
+			, 'em')
+		.'</div>';
+	}
+	
+	public function PluginController_EmbedFriendlyTogglePanel_Create($Sender) {
+		$this->_TogglePanel($Sender);
+		Redirect('plugin/embed');
+	}
+
+	private function _TogglePanel($Sender) {
+		$Sender->Permission('Garden.Themes.Manage');
+		$TransientKey = GetValue(0, $Sender->RequestArgs);
+		if (Gdn::Session()->ValidateTransientKey($TransientKey))
+			SaveToConfig('Themes.EmbedFriendly.SingleColumn', C('Themes.EmbedFriendly.SingleColumn') ? FALSE : TRUE);
+	}
+
+
+	public function Base_Render_Before($Sender) {
+		if (($Sender->MasterView == 'default' || $Sender->MasterView == '') && C('Themes.EmbedFriendly.SingleColumn'))
+			$Sender->AddCSSFile('singlecolumn.css');
+	}
 	
    public function CategoriesController_Render_Before($Sender) {
 		$this->_AddButton($Sender, 'NewDiscussionModule');
@@ -40,7 +91,8 @@ class EmbedFriendlyThemeHooks implements Gdn_IPlugin {
 	}
 	
 	private function _AddButton($Sender, $ModuleName) {
-		$Sender->AddModule($ModuleName, 'Content');
+		if (C('Themes.EmbedFriendly.SingleColumn'))
+			$Sender->AddModule($ModuleName, 'Content');
 	}
    
 }

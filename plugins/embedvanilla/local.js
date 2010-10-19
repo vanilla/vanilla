171f140453c7aea9add3b53b467ee6d3e16bc060
Mark O'Sullivan <mark@vanillaforums.com>
2010-10-19 17:46:33 -0400

    Added EmbedVanilla plugin.

---
+++ plugins/embedvanilla/local.js
@@ -0,0 +1,122 @@
+$(function() {
+   var currentHeight = null,
+      remotePostMessage = null,
+      inIframe = top !== self,
+      inDashboard = gdn.definition('InDashboard', '') != '',
+      remoteUrl = gdn.definition('RemoteUrl', ''),
+      forceRemoteUrl = gdn.definition('ForceRemoteUrl', '') != '',
+      webroot = gdn.definition('WebRoot'),
+      pathroot = gdn.definition('UrlFormat').replace('/{Path}', '').replace('{Path}', '');
+      
+   if (inIframe) {
+      if ("postMessage" in parent) {
+         remotePostMessage = function(message, target) {
+            return parent.postMessage(message, target);
+         }
+         setLocation = function(newLocation) {
+            parent.window.frames[0].location.replace(newLocation);
+         }
+      } else {
+         var messages = [];
+   
+         messageUrl = function(message) {
+            var id = Math.floor(Math.random() * 100000);
+            if (remoteUrl.substr(-1))
+               remoteUrl += '/';
+               
+            return remoteUrl + "/poll.html#poll:" + id + ":" + message;
+         }
+        
+         remotePostMessage = function(message, target) {
+            messages.push(message);
+         }
+        
+         setLocation = function(newLocation) {
+            if (messages.length == 0)
+               parent.window.frames[0].location.replace(newLocation);
+            else {
+               setTimeout(function(){
+                  setLocation(newLocation);
+               },500);
+            }
+         }
+         
+         setMessage = function() {
+            if (messages.length == 0)
+               return;
+            
+            var message = messages.splice(0, 1)[0];
+            document.getElementById('messageFrame').src = messageUrl(message);
+         }
+           
+         $(function() {
+            var body = document.getElementsByTagName("body")[0],
+               messageIframe = document.createElement("iframe");
+       
+            messageIframe.id = "messageFrame";
+            messageIframe.name = "messageFrame";
+            messageIframe.src = messageUrl('');
+            messageIframe.style.display = "none";
+            body.appendChild(messageIframe);
+            setMessage();
+            setInterval(setMessage, 300);
+         });
+      }
+   }
+
+   // If not embedded and we should be, redirect to the embedded version.
+   if (!inIframe && forceRemoteUrl && remoteUrl != '') {
+      console.log('test');
+      var path = document.location.toString().substr(webroot.length);
+      var hashIndex = path.indexOf('#');
+      if (hashIndex > -1)
+         path = path.substr(0, hashIndex);
+      
+      document.location = remoteUrl + '#' + path;
+   } else if (inIframe && inDashboard) {
+      remotePostMessage('unembed', '*');
+   }
+
+   // hijack all anchors to see if they should go to "top" or be within the embed (ie. are they in Vanilla or not?)
+   if (inIframe) {
+      setHeight = function() {
+         var newHeight = document.body.offsetHeight;
+         if (newHeight != currentHeight) {
+            currentHeight = newHeight;
+            remotePostMessage('height:'+currentHeight, '*');
+         }
+      }
+   
+      setHeight();
+      setInterval(setHeight, 300);
+    
+      $('a').live('click', function() {
+         var href = $(this).attr('href'),
+            isHttp = href.substr(0, 7) == 'http://' || href.substr(0,8) == 'https://';
+                
+         if (isHttp && href.substr(0, webroot.length) != webroot) {
+            $(this).attr('target', '_blank');
+         } else {
+            // Strip the path from the root folder of the app
+            var path = isHttp ? href.substr(webroot.length) : href.substr(pathroot.length);
+            var hashIndex = path.indexOf('#');
+            var hash = '';
+            if (hashIndex > -1) {
+               hash = path.substr(hashIndex);
+               path = path.substr(0, hashIndex);
+            }
+            
+            remotePostMessage('location:' + path, '*');
+            // setLocation(pathroot + path + hash);
+            // return false;
+         }
+      });
+      
+      // Simulate a page unload when popups are opened (so they are scrolled into view).
+      $('body').bind('popupReveal', function() {
+         remotePostMessage('scrolltop', '*');
+      });
+      
+      $(window).unload(function() { remotePostMessage('unload', '*'); });
+   }
+});
---
+++ plugins/embedvanilla/remote.js
@@ -0,0 +1,146 @@
+if (window.vanilla == undefined)
+   window.vanilla = {};
+
+if (window.vanilla.embeds == undefined)
+   window.vanilla.embeds = {};
+
+window.vanilla.embed = function(host) {
+   var scripts = document.getElementsByTagName('script'),
+      id = Math.floor((Math.random()) * 100000).toString(),
+      embedUrl = window.location.href.split('#')[0],
+      jsPath = '/plugins/embedvanilla/remote.js',
+      currentPath = window.location.hash.substr(1),
+      disablePath = currentPath && currentPath[0] != "/";
+      disablePath |= (window != top);
+
+   if (!currentPath || disablePath)
+      currentPath = "/";
+
+   if (window.gadgets)
+      embedUrl = '';
+      
+   if (typeof(host) == 'undefined') {
+      host = '';
+      for (i = 0; i < scripts.length; i++) {
+         if (scripts[i].src.indexOf(jsPath) > 0) {
+            host = scripts[i].src;
+            host = host.replace('http://', '').replace('https://', '');
+            host = host.substr(0, host.indexOf(jsPath));
+         }
+      }
+   }
+      
+   window.vanilla.embeds[id] = this;
+   if (window.postMessage) {
+      onMessage = function(e) {
+         var message = e.data.split(':');
+         var frame = document.getElementById('vanilla'+id);
+         if (frame.contentWindow != e.source)
+            return;
+         processMessage(message);
+      }
+      if (window.addEventListener)
+         window.addEventListener("message", onMessage, false);
+      else
+         window.attachEvent("onmessage", onMessage);
+   } else {
+      var messageId = null;
+      setInterval(function() {
+         try {
+            var vid = 'vanilla' + id;
+            var hash = window.frames[vid].frames['messageFrame'].location.hash.substr(6);
+         } catch(e) {
+            return;
+         }
+
+         var message = hash.split(':');
+         var newMessageId = message[0];
+         if (newMessageId == messageId)
+            return;
+         
+         messageId = newMessageId;
+         message.splice(0, 1);
+         processMessage(message);
+      }, 300);
+   }
+
+
+   checkHash = function() {
+      var path = window.location.hash.substr(1);
+      if (path != currentPath) {
+         currentPath = path;
+         window.frames['vanilla'+id].location.replace(vanillaUrl(path));
+      }
+   }
+
+   if (!window.gadgets) {
+      if (!disablePath) {
+         if ("onhashchange" in window) {
+            if (window.addEventListener)
+               window.addEventListener("hashchange", checkHash, false);
+            else
+               window.attachEvent("onhashchange", checkHash);
+         } else {
+            setInterval(checkHash, 300);
+         }
+      }
+   }
+
+   processMessage = function(message) {
+      if (message[0] == 'height') {
+         setHeight(message[1]);
+      } else if (message[0] == 'location') {
+         if (disablePath) {
+            currentPath = cmd[1];
+         } else {
+            currentPath = window.location.hash.substr(1);
+            if (currentPath != message[1]) {
+               currentPath = message[1];
+               location.href = embedUrl + "#" + currentPath;
+            }
+         }
+      } else if (message[0] == 'unload') {
+         if (window.attachEvent || scrollPosition('vanilla'+id) < 0)
+            document.getElementById('vanilla'+id).scrollIntoView(true);
+
+      } else if (message[0] == 'scrolltop') {
+         document.getElementById('vanilla'+id).scrollIntoView(true);
+      } else if (message[0] == 'unembed') {
+         document.location = 'http://' + host + window.location.hash.substr(1);
+      }
+   }
+
+   scrollPosition = function(id) {
+      var node = document.getElementById(id),
+         top = 0,
+         topScroll = 0;
+      if (node.offsetParent) {
+         do {
+            top += node.offsetTop;
+            topScroll += node.offsetParent ? node.offsetParent.scrollTop : 0;
+         } while (node = node.offsetParent);
+         return top - topScroll;
+      }
+      return -1;
+   }
+
+   setHeight = function(height) {
+      document.getElementById('vanilla'+id).style['height'] = height + "px";
+      if (window.gadgets)
+         gadgets.window.adjustHeight();
+   }
+
+   vanillaUrl = function(path) {
+      var concat = path.indexOf('?') > -1 ? '&' : '?';
+      return 'http://' + host + path + concat + 'remote=' + encodeURIComponent(embedUrl);
+   }
+
+   document.write('<iframe id="vanilla'+id+'" name="vanilla'+id+'" src="'+vanillaUrl(currentPath)+'" scrolling="no" frameborder="0" border="0" width="100%" height="1000" style="width: 100%; height: 1000px; border: 0; display: block;"></iframe>');
+   return this;
+};
+try {
+   if (window.location.hash.substr(0, 6) != "#poll:")
+      window.vanilla.embed();
+} catch(e) {
+   document.write("<div style=\"padding: 10px; font-size: 12px; font-family: 'lucida grande'; background: #fff; color:#000\";>Failed to embed Vanilla: " + e + "</div>");
+}

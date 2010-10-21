abe569614162918f1869d9a838a3bebd73e243fa
Mark O'Sullivan <mark@vanillaforums.com>
2010-10-21 14:32:29 -0400

    Added DiscussionsModule.

---
+++ applications/vanilla/views/modules/discussions.php
@@ -0,0 +1,20 @@
+<?php if (!defined('APPLICATION')) exit();
+$Data = $this->_Sender->Data['DiscussionsModuleData'];
+if (is_object($Data) && $Data->NumRows() > 0) {
+   $DiscussionView = $this->FetchViewLocation('discussion');
+   ?>
+<div id="Bookmarks" class="Box">
+   <h4><?php echo T('Recent Discussions'); ?></h4>
+   <ul id="Bookmark_List" class="PanelInfo PanelDiscussions">
+      <?php
+      foreach ($Data->Result() as $Discussion) {
+         include($DiscussionView);
+      }
+      if ($Data->NumRows() >= 10) {
+      ?>
+      <li class="ShowAll"><?php echo Anchor(T('↳ Show All'), 'discussions'); ?></li>
+      <?php } ?>
+   </ul>
+</div>
+   <?php
+}

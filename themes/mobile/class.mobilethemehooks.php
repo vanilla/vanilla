97c1be26eb726da00e9583761662a756b111dbeb
Mark O'Sullivan <mark@vanillaforums.com>
2010-10-24 02:30:36 -0400

    Added disappearing address bar on iphone trick.

--- themes/mobile/class.mobilethemehooks.php
+++ themes/mobile/class.mobilethemehooks.php
@@ -21,6 +21,12 @@ class MobileThemeHooks implements Gdn_IPlugin {
 	public function Base_Render_Before($Sender) {
 		if (IsMobile() && is_object($Sender->Head)) {
 			$Sender->Head->AddTag('meta', array('name' => 'viewport', 'content' => "width=device-width,minimum-scale=1.0,maximum-scale=1.0"));
+			
+			$Sender->Head->AddString('<script type="text/javascript">
+	setTimeout(function () {
+  window.scrollTo(0, 1);
+}, 1000);
+</script>');
 		}
 	}
 	

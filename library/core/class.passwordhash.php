b602f7ba62c67a252147cf778e9a18e516dee5ca
Todd Burry <todd@vanillaforums.com>
2010-12-14 17:30:09 -0500

    Changed vBulletin password hash to match the solution in github issue 700.

--- library/core/class.passwordhash.php
+++ library/core/class.passwordhash.php
@@ -85,13 +85,11 @@ class Gdn_PasswordHash extends PasswordHash {
             throw new Gdn_UserException(sprintf(T('You need to reset your password.', 'You need to reset your password. This is most likely because an administrator recently changed your account information. Click <a href="%s">here</a> to reset your password.'), Url('entry/passwordrequest')));
             break;
 			case 'vbulletin':
-            if (strpos($StoredHash, '$') !== FALSE) {
-               // This is a newer form of vBulletin hash.
-               list($VbStoredHash, $Salt) = explode('$', $StoredHash);
-            } else {
-               $VbStoredHash = substr($StoredHash, 0, 32);
-               $Salt = trim(substr($StoredHash, 32));
-            }
+            // assume vbulletin's password hash has a fixed length of 32, the salt length will vary between version 3 and 4
+            $SaltLength = strlen($StoredHash) - 32;
+            $Salt = trim(substr($StoredHash, -$SaltLength, $SaltLength));
+            $VbStoredHash = substr($StoredHash, 0, strlen($StoredHash) - $SaltLength);
+            
 				$VbHash = md5(md5($Password).$Salt);
 				$Result = $VbHash == $VbStoredHash;
 				break;

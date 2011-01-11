d4b1961a7e711be2ef76a1ee4f0a2414d0674c3e
Todd Burry <todd@vanillaforums.com>
2011-01-11 14:32:54 -0500

    Different ErrorPermission for entry/invite #2 (from johnsimpson).

--- applications/dashboard/models/class.invitationmodel.php
+++ applications/dashboard/models/class.invitationmodel.php
@@ -113,7 +113,7 @@ class InvitationModel extends Gdn_Model {
       if ($Invitation === FALSE) {
          throw new Exception(T('ErrorRecordNotFound'));
       } else if ($Session->UserID != $Invitation->SenderUserID) {
-         throw new Exception(T('InviteErrorPermission'));
+         throw new Exception(T('InviteErrorPermission', T('ErrorPermission')));
       } else {
          // Some information for the email
          $RegistrationUrl = Gdn::Request()->Url("entry/register/{$Invitation->Code}", TRUE);
@@ -147,7 +147,7 @@ class InvitationModel extends Gdn_Model {
       
       // Does this user own the invitation?
       if ($UserID != $Invitation->SenderUserID)
-         throw new Exception(T('InviteErrorPermission'));
+         throw new Exception(T('InviteErrorPermission', T('ErrorPermission')));
       
       // Has the invitation been accepted?
       if ($Invitation->AcceptedUserID > 0)

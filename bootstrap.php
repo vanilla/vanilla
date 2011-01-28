f5172c9b139d2938c369d1fd552a763323cdfc1c
root <root@hardy2.(none)>
2011-01-28 18:14:42 +0000

    Added conditional loading of default constants incase they arent overridden by deployment specific files

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,11 @@ if(file_exists(PATH_ROOT.DS.'conf'.DS.'bootstrap.before.php'))
 /// Define core constants.
 if(!defined('PATH_CONF')) define('PATH_CONF', PATH_ROOT.DS.'conf');
 if(!defined('PATH_USER_CONF')) define('PATH_USER_CONF', PATH_CONF);
-include(PATH_USER_CONF . DS . 'constants.php');
+
+// Include default constants if none were defined elsewhere
+if (!defined('VANILLA_CONSTANTS'))
+   include(PATH_CONF . DS . 'constants.php');
+   
 if(!defined('PATH_APPLICATIONS')) define('PATH_APPLICATIONS', PATH_ROOT.DS.'applications');
 if(!defined('PATH_CACHE')) define('PATH_CACHE', PATH_ROOT.DS.'cache');
 if(!defined('PATH_LIBRARY')) define('PATH_LIBRARY', PATH_ROOT.DS.'library');
@@ -63,6 +67,9 @@ require_once(PATH_LIBRARY_CORE . DS . 'class.factory.php');
 Gdn::SetFactory(new Gdn_Factory(), FALSE);
 $FactoryOverwriteBak = Gdn::FactoryOverwrite(FALSE);
 
+// Cache Layer
+Gdn::FactoryInstall(Gdn::AliasCache, 'Gdn_Cache', CombinePaths(array(PATH_LIBRARY_CORE,'class.cache.php')), Gdn::FactoryRealSingleton, 'Initialize');
+
 /// Install the configuration.
 Gdn::FactoryInstall(Gdn::AliasConfig, 'Gdn_Configuration', PATH_LIBRARY_CORE.DS.'class.configuration.php', Gdn::FactorySingleton);
 $Gdn_Config = Gdn::Factory(Gdn::AliasConfig);
@@ -70,8 +77,13 @@ $Gdn_Config = Gdn::Factory(Gdn::AliasConfig);
 /// Configuration Defaults.
 $Gdn_Config->Load(PATH_CONF.DS.'config-defaults.php', 'Use');
 
-// Load the custom configurations so that we know what apps are enabled.
-$Gdn_Config->Load(PATH_USER_CONF.DS.'config.php', 'Use');
+// Load installation-specific static configuration so that we know what apps are enabled.
+$Gdn_Config->Load(PATH_CONF.DS.'config.php', 'Use');
+
+if (PATH_USER_CONF.DS.'config.php' != PATH_CONF.DS.'config.php') {
+   // Load the custom configurations 
+   $Gdn_Config->Load(PATH_USER_CONF.DS.'config.php', 'Use');
+}
 
 // This header is redundantly set in the controller.
 //header('X-Garden-Version: '.APPLICATION.' '.APPLICATION_VERSION);
@@ -118,8 +130,6 @@ Gdn::FactoryInstall('Smarty', 'Smarty', PATH_LIBRARY.DS.'vendors'.DS.'Smarty-2.6
 Gdn::FactoryInstall('ViewHandler.tpl', 'Gdn_Smarty', PATH_LIBRARY_CORE.DS.'class.smarty.php', Gdn::FactorySingleton);
 // Application manager.
 Gdn::FactoryInstall('ApplicationManager', 'Gdn_ApplicationManager', PATH_LIBRARY_CORE.DS.'class.applicationmanager.php', Gdn::FactorySingleton);
-// Cache Layer
-Gdn::FactoryInstall(Gdn::AliasCache, 'Gdn_Cache', CombinePaths(array(PATH_LIBRARY_CORE,'class.cache.php')), Gdn::FactoryRealSingleton, 'Initialize');
 // Theme manager
 Gdn::FactoryInstall('ThemeManager', 'Gdn_ThemeManager', PATH_LIBRARY_CORE.DS.'class.thememanager.php', Gdn::FactoryInstance);
 Gdn::FactoryInstall(Gdn::AliasSlice, 'Gdn_Slice', PATH_LIBRARY_CORE.DS.'class.slice.php', Gdn::FactorySingleton);

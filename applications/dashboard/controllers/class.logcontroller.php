08fe9dd448145676d12b9ae0ccb131bad15c030b
Todd Burry <todd@vanillaforums.com>
2011-02-03 20:41:53 -0500

    Added the base logging functionality to the core.

---
+++ applications/dashboard/controllers/class.logcontroller.php
@@ -0,0 +1,116 @@
+<?php if (!defined('APPLICATION')) exit();
+/**
+ * @copyright Copyright 2008, 2009 Vanilla Forums Inc.
+ * @license http://www.opensource.org/licenses/gpl-2.0.php GPLv2
+ */
+
+class LogController extends DashboardController {
+   /// PROPERTIES ///
+
+   /**
+    * @var Gdn_Form
+    */
+   public $Form;
+
+   /**
+    * @var LogModel
+    */
+   public $LogModel;
+
+
+   /// METHODS ///
+   public function  __construct() {
+      parent::__construct();
+      
+      $this->Form = new Gdn_Form();
+      $this->LogModel = new LogModel();
+   }
+
+   public function Confirm($Action, $LogIDs = '') {
+      $this->Permission('Garden.Moderation.Manage');
+
+      if (trim($LogIDs))
+         $LogIDs = explode(',', $LogIDs);
+      else
+         $LogIDs = array();
+
+      $this->SetData('Action', $Action);
+      $this->SetData('ItemCount', count($LogIDs));
+
+      $this->Render();
+   }
+
+   public function Delete($LogIDs) {
+      $this->Permission('Garden.Moderation.Manage');
+      // Grab the logs.
+      $this->LogModel->Delete($LogIDs);
+   }
+
+   public function Edits($Page = '') {
+      $this->Permission('Garden.Moderation.Manage');
+      list($Offset, $Limit) = OffsetLimit($Page, 10);
+      $this->SetData('Title', T('Edit/Delete Log'));
+
+      $Where = array('Operation' => array('Edit', 'Delete'));
+      
+      $RecordCount = $this->LogModel->GetCountWhere($Where);
+      $this->SetData('RecordCount', $RecordCount);
+      if ($Offset >= $RecordCount)
+         $Offset = $RecordCount - $Limit;
+
+      $Log = $this->LogModel->GetWhere($Where, 'LogID', 'Desc', $Offset, $Limit);
+      $this->SetData('Log', $Log);
+
+      if ($this->DeliveryType() == DELIVERY_TYPE_VIEW)
+         $this->View = 'Table';
+
+      $this->AddSideMenu('dashboard/log/edits');
+      $this->Render();
+   }
+
+   protected function FormatContent($Log) {
+      return $this->LogModel->FormatContent($Log);
+   }
+
+   public function Initialize() {
+      parent::Initialize();
+      $this->AddJsFile('log.js');
+      $this->AddJsFile('jquery.expander.js');
+      $this->AddJsFile('jquery.ui.packed.js');
+   }
+
+   public function Restore($LogIDs) {
+      $this->Permission('Garden.Moderation.Manage');
+
+      // Grab the logs.
+      $Logs = $this->LogModel->GetIDs($LogIDs);
+      foreach ($Logs as $Log) {
+         $this->LogModel->Restore($Log);
+      }
+      $this->LogModel->Recalculate();
+   }
+
+
+
+   public function Spam($Page = '') {
+      $this->Permission('Garden.Moderation.Manage');
+      list($Offset, $Limit) = OffsetLimit($Page, 10);
+      $this->SetData('Title', T('Manage Spam'));
+
+      $Where = array('Operation' => array('Spam'));
+
+      $RecordCount = $this->LogModel->GetCountWhere($Where);
+      $this->SetData('RecordCount', $RecordCount);
+      if ($Offset >= $RecordCount)
+         $Offset = $RecordCount - $Limit;
+
+      $Log = $this->LogModel->GetWhere($Where, 'LogID', 'Desc', $Offset, $Limit);
+      $this->SetData('Log', $Log);
+
+      if ($this->DeliveryType() == DELIVERY_TYPE_VIEW)
+         $this->View = 'Table';
+
+      $this->AddSideMenu('dashboard/log/spam');
+      $this->Render();
+   }
+}
---
+++ applications/dashboard/views/log/confirm.php
@@ -0,0 +1,41 @@
+<?php if (!defined('APPLICATION')) exit(); ?>
+<div class="Wrap">
+   <?php
+   $ItemCount = $this->Data('ItemCount');
+
+   if (!$ItemCount) {
+      echo '<h1>', T('No Items Selected'), '</h1>';
+      echo '<p>', T('Make sure you select at least one item before continuing.'), '</p>';
+   } else {
+      echo '<h1>', T('Please Confirm'), '</h1>';
+
+      // Give a description of what is done.'
+      switch (strtolower($this->Data('Action'))) {
+         case 'delete':
+            echo '<p>',
+               T('Deleting forever cannot be undone.', 'When you delete your selection forever then those operations are removed from this list and cannot be undone.'),
+               '</p>';
+
+            echo '<p>',
+               Plural($ItemCount, T('Are you sure you want to delete 1 item forever?'), T('Are you sure you want to delete %s items forever?')),
+               '</p>';
+            break;
+         case 'restore':
+            echo '<p>',
+               T('Restoring your selection removes the items from this list.', 'When you restore your selectection the items are removed from this list and put back into the site.'),
+               '</p>';
+
+            echo '<p>',
+               Plural($ItemCount, T('Are you sure you want to restore 1 item?'), T('Are you sure you want to restore %s items?')),
+               '</p>';
+            break;
+      }
+
+      echo '<div class="Center">',
+         Anchor(T('Yes'), '#', array('class' => 'Button ConfirmYes', 'style' => 'display: inline-block; width: 50px')),
+         ' ',
+         Anchor(T('No'), '#', array('class' => 'Button ConfirmNo', 'style' => 'display: inline-block; width: 50px')),
+         '</div>';
+   }
+   ?>
+</div>

5a6ee493a483330336f29dcdca98af1ac53c1c31
lincolnwebs <lincolnwebs@gmail.com>
2011-02-13 16:36:12 -0500

    Flagging: Duplicate flags now appear as comments in previously-started discussion, Adds email notifications, Adds documentation, Adds required hook to UserModel, Increments version number

--- plugins/Flagging/views/report.php
+++ plugins/Flagging/views/report.php
@@ -1,23 +1,21 @@
 <?php if (!defined('APPLICATION')) exit(); ?>
 <?php 
-$Flag = $this->Data['Plugin.Flagging.Data']; // shortcut to original post meta data
-$UserName = GetValue('UserName', $this->Report);
-$UserID = GetValue('UserID', $this->Report);
-$FlaggedContent = GetValue('FlaggedContent', $this->Report);
+$Flag = $this->Data['Plugin.Flagging.Data'];
+$Report = $this->Data['Plugin.Flagging.Report'];
 
-echo Anchor($UserName, '/profile/'.$UserID.'/'.$UserName) . ' '. T('reported'); 
+echo Anchor($Report['UserName'], '/profile/'.$Report['UserID'].'/'.$Report['UserName']) . ' '. T('reported'); 
 
 if ($Flag['Context'] == 'comment') 
    echo ' ' . T('a comment in');
-?> <strong><?php echo Anchor(GetValue('DiscussionName', $this->Report), $Flag['URL']); ?></strong>
+?> <strong><?php echo Anchor($Report['DiscussionName'], $Flag['URL']); ?></strong>
    
 <?php echo T('Reason'); ?>:
-   <blockquote rel="<?php echo $UserName; ?>"><?php echo GetValue('Reason', $this->Report); ?></blockquote>
+   <blockquote rel="<?php echo $Report['UserName']; ?>"><?php echo $Report['Reason']; ?></blockquote>
 <?php echo T('Flagged Content'); ?>:
    <blockquote rel="<?php echo $Flag['ElementAuthor']; ?>"><?php 
    
-   echo substr($FlaggedContent, 0 , 500); 
-   if(strlen($FlaggedContent) > 500)
+   echo substr($Report['FlaggedContent'], 0 , 500); 
+   if(strlen($Report['FlaggedContent']) > 500)
       echo '&hellip;';
    
    ?></blockquote>

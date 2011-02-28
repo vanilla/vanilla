36988fbcc407eda74c114cf12ef9f9e30e844a40
lincolnwebs <lincolnwebs@gmail.com>
2011-02-28 01:25:24 -0500

    Flagging: Fix reason getting blanked (issue I just introduced today)

--- plugins/Flagging/views/report.php
+++ plugins/Flagging/views/report.php
@@ -2,6 +2,7 @@
 <?php 
 $Flag = $this->Data['Plugin.Flagging.Data'];
 $Report = $this->Data['Plugin.Flagging.Report'];
+$Reason = $this->Data['Plugin.Flagging.Reason'];
 
 echo Anchor($Flag['UserName'], '/profile/'.$Flag['UserID'].'/'.$Flag['UserName']) . ' '. T('reported'); 
 
@@ -10,7 +11,7 @@ if ($Flag['Context'] == 'comment')
 ?> <strong><?php echo Anchor($Report['DiscussionName'], $Flag['URL']); ?></strong>
    
 <?php echo T('Reason'); ?>:
-   <blockquote rel="<?php echo $Flag['UserName']; ?>"><?php echo $Report['Reason']; ?></blockquote>
+   <blockquote rel="<?php echo $Flag['UserName']; ?>"><?php echo $Reason; ?></blockquote>
 <?php echo T('Flagged Content'); ?>:
    <blockquote rel="<?php echo $Flag['ElementAuthor']; ?>"><?php 
    

7aba4a64cd3cedfa24b03d37bcd3c1149be3f238
Todd Burry <todd@vanillaforums.com>
2011-03-01 17:58:34 -0500

    Fixed some permission checking in the post controller.

--- applications/vanilla/controllers/class.postcontroller.php
+++ applications/vanilla/controllers/class.postcontroller.php
@@ -128,19 +128,24 @@ class PostController extends VanillaController {
          $Draft = $this->Form->ButtonExists('Save Draft') ? TRUE : FALSE;
          $Preview = $this->Form->ButtonExists('Preview') ? TRUE : FALSE;
          if (!$Preview) {
-            // Check category permissions
-            if ($this->Form->GetFormValue('Announce', '') != '' && !$Session->CheckPermission('Vanilla.Discussions.Announce', TRUE, 'Category', $this->Category->PermissionCategoryID))
-               $this->Form->AddError('You do not have permission to announce in this category', 'Announce');
+            if (!is_object($this->Category) && isset($FormValues['CategoryID']))
+               $this->Category = $aCategoryData[$FormValues['CategoryID']];
 
-            if ($this->Form->GetFormValue('Close', '') != '' && !$Session->CheckPermission('Vanilla.Discussions.Close', TRUE, 'Category', $this->Category->PermissionCategoryID))
-               $this->Form->AddError('You do not have permission to close in this category', 'Close');
+            if (is_object($this->Category)) {
+               // Check category permissions.
+               if ($this->Form->GetFormValue('Announce', '') != '' && !$Session->CheckPermission('Vanilla.Discussions.Announce', TRUE, 'Category', $this->Category->PermissionCategoryID))
+                  $this->Form->AddError('You do not have permission to announce in this category', 'Announce');
 
-            if ($this->Form->GetFormValue('Sink', '') != '' && !$Session->CheckPermission('Vanilla.Discussions.Sink', TRUE, 'Category', $this->Category->PermissionCategoryID))
-               $this->Form->AddError('You do not have permission to sink in this category', 'Sink');
-               
-            if (!$Session->CheckPermission('Vanilla.Discussions.Add', TRUE, 'Category', $this->Category->PermissionCategoryID))
-               $this->Form->AddError('You do not have permission to start discussions in this category', 'CategoryID');
-               
+               if ($this->Form->GetFormValue('Close', '') != '' && !$Session->CheckPermission('Vanilla.Discussions.Close', TRUE, 'Category', $this->Category->PermissionCategoryID))
+                  $this->Form->AddError('You do not have permission to close in this category', 'Close');
+
+               if ($this->Form->GetFormValue('Sink', '') != '' && !$Session->CheckPermission('Vanilla.Discussions.Sink', TRUE, 'Category', $this->Category->PermissionCategoryID))
+                  $this->Form->AddError('You do not have permission to sink in this category', 'Sink');
+
+               if (!$Session->CheckPermission('Vanilla.Discussions.Add', TRUE, 'Category', $this->Category->PermissionCategoryID))
+                  $this->Form->AddError('You do not have permission to start discussions in this category', 'CategoryID');
+            }
+
             // Make sure that the title will not be invisible after rendering
             $Name = $this->Form->GetFormValue('Name', '');
             if ($Name != '' && Gdn_Format::Text($Name) == '')

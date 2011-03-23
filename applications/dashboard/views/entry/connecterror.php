970a5f527b3f484ef737e5e10dcb7eadaa4e5677
Todd Burry <todd@vanillaforums.com>
2011-03-22 20:35:38 -0400

    Fixed connecterror bug.

--- applications/dashboard/views/entry/connecterror.php
+++ applications/dashboard/views/entry/connecterror.php
@@ -1,4 +1,6 @@
-<?php if (!defined('APPLICATION')) exit(); ?>
+<?php if (!defined('APPLICATION')) exit(); 
+$ConnectSource = $this->Form->GetFormValue('ProviderName');
+?>
 <div class="Connect">
 	<h1><?php echo StringIsNullOrEmpty($ConnectSource) ? T("Sign in") : sprintf(T('%s Connect'), $ConnectSource); ?></h1>
 	<div class="Box">

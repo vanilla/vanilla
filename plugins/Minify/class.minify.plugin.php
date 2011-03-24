86d772207da438c00dda84f0335f6ca36dbe4a27
Todd Burry <todd@vanillaforums.com>
2011-03-24 11:44:32 -0400

    Fixed symlink problem in the minify plugin.

--- plugins/Minify/class.minify.plugin.php
+++ plugins/Minify/class.minify.plugin.php
@@ -12,7 +12,7 @@ Contact Vanilla Forums Inc. at support [at] vanillaforums [dot] com
 $PluginInfo['Minify'] = array(
    'Name' => 'Minify',
    'Description' => 'Analyzes each page request for Javascript and CSS files, merging and minifying them where applicable.',
-   'Version' => '1.0.2b',
+   'Version' => '1.0.3b',
    'Author' => "Mark O'Sullivan",
    'AuthorEmail' => 'mark@vanillaforums.com',
    'AuthorUrl' => 'http://markosullivan.ca'

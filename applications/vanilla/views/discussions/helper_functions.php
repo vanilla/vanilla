636aaa7f46e8abb6cddbf56f95eb641fe356a039
Todd Burry <todd@vanillaforums.com>
2011-04-12 14:05:02 -0400

    Fixed permission problem with edit discussions.

--- applications/vanilla/views/discussions/helper_functions.php
+++ applications/vanilla/views/discussions/helper_functions.php
@@ -35,10 +35,13 @@ function WriteDiscussion($Discussion, &$Sender, &$Session, $Alt2) {
    <?php
    $Sender->FireEvent('BeforeDiscussionContent');
    WriteOptions($Discussion, $Sender, $Session);
+
+   if (!property_exists($Sender, 'CanEditDiscussions'))
+      $Sender->CanEditDiscussions = GetValue('PermsDiscussionsEdit', CategoryModel::Categories($Discussion->CategoryID));
          
    if ($Sender->CanEditDiscussions) {
       if (!property_exists($Sender, 'CheckedDiscussions'))
-         $Sender->CheckedDiscussions = $Session->GetAttribute('CheckedDiscussions', array());
+         $Sender->CheckedDiscussions = (array)$Session->GetAttribute('CheckedDiscussions', array());
 
       $ItemSelected = in_array($Discussion->DiscussionID, $Sender->CheckedDiscussions);
       echo '<div class="Administration"><input type="checkbox" name="DiscussionID[]" value="'.$Discussion->DiscussionID.'"'.($ItemSelected?' checked="checked"':'').' /></div>';

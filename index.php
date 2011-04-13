f4ea4bb71035c1acec419a97326711293df362d5
yama <yamamoto@kyms.ne.jp>
2011-04-14 07:21:25 +0900

    2.0.17.8 → 2.0.17.9

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 define('APPLICATION', 'Vanilla');
-define('APPLICATION_VERSION', '2.0.17.8');
+define('APPLICATION_VERSION', '2.0.17.9');
 /*
 Copyright 2008, 2009 Vanilla Forums Inc.
 This file is part of Garden.

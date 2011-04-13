f1101fa5d4ebf31fa3c38e1e883d168345ce35ed
Mark O'Sullivan <mark@vanillaforums.com>
2011-04-13 07:23:15 -0400

    Added module for showing/hiding followed/unfollowed categories (so unfollowed ones can be refollowed).

---
+++ applications/vanilla/views/modules/categoryfollowtoggle.php
@@ -0,0 +1,18 @@
+<?php if (!defined('APPLICATION')) exit();
+$ShowAllCategories = Gdn::Session()->GetPreference('ShowAllCategories');
+$Url = Gdn::Request()->Path();
+?>
+<div class="Box CategoryFollowToggleBox">
+   <h4><?php echo T('Category Management'); ?></h4>
+   <?php if ($ShowAllCategories) { ?>
+      <p>You are currently viewing all categories.<p>
+   <?php
+      echo Wrap(Anchor('Only show followed categories', $Url.'?ShowAllCategories=false'), 'p');
+   } else {
+   ?>
+      <p>You are currently only viewing categories that you follow.</p>
+   <?php
+      echo Wrap(Anchor('Show unfollowed categories', $Url.'?ShowAllCategories=true'), 'p');
+   }
+   ?>
+</div>

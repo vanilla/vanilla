6a7702f103834eb28d2898141b2382c707045668
lincolnwebs <lincolnwebs@gmail.com>
2011-04-17 00:00:53 -0400

    Applies fix to ProxyConnect (thanks larrykluger) closes #658

--- library/core/functions.general.php
+++ library/core/functions.general.php
@@ -1384,7 +1384,7 @@ if (!function_exists('ProxyRequest')) {
          throw new Exception(T('Encountered an error while making a request to the remote server: Your PHP configuration does not allow curl or fsock requests.'));
       }
       
-      if (!$Success)
+      if ($Success)
          return $Response;
       
       $ResponseHeaderData = trim(substr($Response, 0, strpos($Response, "\r\n\r\n")));

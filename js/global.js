7991acfb0204b78a4b3954e7a5b3b5c9bf69a751
Todd Burry <todd@vanillaforums.com>
2011-05-05 13:55:55 -0400

    Changed global.js to fill in the search box based on .Search instead of #Search.

--- js/global.js
+++ js/global.js
@@ -316,14 +316,13 @@ jQuery(document).ready(function($) {
 
    // Fill the search input with "search" if empty and blurred
    var searchText = gdn.definition('Search', 'Search');
-   $('#Search input.InputBox').val(searchText);
-   $('#Search input.InputBox').blur(function() {
-      var searchText = gdn.definition('Search', 'Search');
+   if (!$('.Search input.InputBox').val())
+      $('.Search input.InputBox').val(searchText);
+   $('.Search input.InputBox').blur(function() {
       if (typeof $(this).val() == 'undefined' || $(this).val() == '')
          $(this).val(searchText);
    });
-   $('#Search input.InputBox').focus(function() {
-      var searchText = gdn.definition('Search', 'Search');
+   $('.Search input.InputBox').focus(function() {
       if ($(this).val() == searchText)
          $(this).val('');
    });

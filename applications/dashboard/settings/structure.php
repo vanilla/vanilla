8df09d53eff4ffb99971d38ae51feaad277a9710
Tim Gunter <tim@vanillaforums.com>
2011-05-16 20:23:01 -0400

    Update regarding structure for new method

--- applications/dashboard/settings/structure.php
+++ applications/dashboard/settings/structure.php
@@ -424,6 +424,7 @@ $Construct->Table('Regarding')
    ->Column('ParentID', 'int(11)', TRUE)
    ->Column('ForeignURL', 'varchar(255)', TRUE)
    ->Column('Comment', 'text', FALSE)
+   ->Column('Reports', 'int(11)', FALSE)
    ->Engine('InnoDB')
    ->Set($Explicit, $Drop);
 

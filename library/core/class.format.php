00536137488f0fb88b4cd5ddc8fee5a7f76e59bf
Todd Burry <todd@vanillaforums.com>
2011-05-24 22:24:20 -0400

    Cleaning up function layout.

--- library/core/class.format.php
+++ library/core/class.format.php
@@ -331,6 +331,71 @@ class Gdn_Format {
       return round($Bytes, $Precision) . $Units[$Pow]; 
    }
 
+   /**
+   * @var array Unicode to ascii conversion table.
+   */
+
+   protected static $_CleanChars = array(
+      '-' => ' ', '_' => ' ', '&lt;' => '', '&gt;' => '', '&#039;' => '', '&amp;' => '',
+      '&quot;' => '', 'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'Ae',
+      '&Auml;' => 'A', 'Å' => 'A', 'Ā' => 'A', 'Ą' => 'A', 'Ă' => 'A', 'Æ' => 'Ae',
+      'Ç' => 'C', 'Ć' => 'C', 'Č' => 'C', 'Ĉ' => 'C', 'Ċ' => 'C', 'Ď' => 'D', 'Đ' => 'D',
+      'Ð' => 'D', 'È' => 'E', 'É' => 'E', 'Ê' => 'E', 'Ë' => 'E', 'Ē' => 'E',
+      'Ę' => 'E', 'Ě' => 'E', 'Ĕ' => 'E', 'Ė' => 'E', 'Ĝ' => 'G', 'Ğ' => 'G',
+      'Ġ' => 'G', 'Ģ' => 'G', 'Ĥ' => 'H', 'Ħ' => 'H', 'Ì' => 'I', 'Í' => 'I',
+      'Î' => 'I', 'Ï' => 'I', 'Ī' => 'I', 'Ĩ' => 'I', 'Ĭ' => 'I', 'Į' => 'I',
+      'İ' => 'I', 'Ĳ' => 'IJ', 'Ĵ' => 'J', 'Ķ' => 'K','Ł' => 'K', 'Ľ' => 'K',
+      'Ĺ' => 'K', 'Ļ' => 'K', 'Ŀ' => 'K', 'Ñ' => 'N', 'Ń' => 'N', 'Ň' => 'N',
+      'Ņ' => 'N', 'Ŋ' => 'N', 'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O', 'Õ' => 'O',
+      'Ö' => 'Oe', '&Ouml;' => 'Oe', 'Ø' => 'O', 'Ō' => 'O', 'Ő' => 'O', 'Ŏ' => 'O',
+      'Œ' => 'OE', 'Ŕ' => 'R', 'Ř' => 'R', 'Ŗ' => 'R', 'Ś' => 'S', 'Š' => 'S',
+      'Ş' => 'S', 'Ŝ' => 'S', 'Ș' => 'S', 'Ť' => 'T', 'Ţ' => 'T', 'Ŧ' => 'T',
+      'Ț' => 'T', 'Ù' => 'U', 'Ú' => 'U', 'Û' => 'U', 'Ü' => 'Ue', 'Ū' => 'U',
+      '&Uuml;' => 'Ue', 'Ů' => 'U', 'Ű' => 'U', 'Ŭ' => 'U', 'Ũ' => 'U', 'Ų' => 'U',
+      'Ŵ' => 'W', 'Ý' => 'Y', 'Ŷ' => 'Y', 'Ÿ' => 'Y', 'Ź' => 'Z', 'Ž' => 'Z',
+      'Ż' => 'Z', 'Þ' => 'T', 'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a',
+      'ä' => 'ae', '&auml;' => 'ae', 'å' => 'a', 'ā' => 'a', 'ą' => 'a', 'ă' => 'a',
+      'æ' => 'ae', 'ç' => 'c', 'ć' => 'c', 'č' => 'c', 'ĉ' => 'c', 'ċ' => 'c',
+      'ď' => 'd', 'đ' => 'd', 'ð' => 'd', 'è' => 'e', 'é' => 'e', 'ê' => 'e',
+      'ë' => 'e', 'ē' => 'e', 'ę' => 'e', 'ě' => 'e', 'ĕ' => 'e', 'ė' => 'e',
+      'ƒ' => 'f', 'ĝ' => 'g', 'ğ' => 'g', 'ġ' => 'g', 'ģ' => 'g', 'ĥ' => 'h',
+      'ħ' => 'h', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i', 'ī' => 'i',
+      'ĩ' => 'i', 'ĭ' => 'i', 'į' => 'i', 'ı' => 'i', 'ĳ' => 'ij', 'ĵ' => 'j',
+      'ķ' => 'k', 'ĸ' => 'k', 'ł' => 'l', 'ľ' => 'l', 'ĺ' => 'l', 'ļ' => 'l',
+      'ŀ' => 'l', 'ñ' => 'n', 'ń' => 'n', 'ň' => 'n', 'ņ' => 'n', 'ŉ' => 'n',
+      'ŋ' => 'n', 'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'õ' => 'o', 'ö' => 'oe',
+      '&ouml;' => 'oe', 'ø' => 'o', 'ō' => 'o', 'ő' => 'o', 'ŏ' => 'o', 'œ' => 'oe',
+      'ŕ' => 'r', 'ř' => 'r', 'ŗ' => 'r', 'š' => 's', 'ù' => 'u', 'ú' => 'u',
+      'û' => 'u', 'ü' => 'ue', 'ū' => 'u', '&uuml;' => 'ue', 'ů' => 'u', 'ű' => 'u',
+      'ŭ' => 'u', 'ũ' => 'u', 'ų' => 'u', 'ŵ' => 'w', 'ý' => 'y', 'ÿ' => 'y',
+      'ŷ' => 'y', 'ž' => 'z', 'ż' => 'z', 'ź' => 'z', 'þ' => 't', 'ß' => 'ss',
+      'ſ' => 'ss', 'ый' => 'iy', 'А' => 'A', 'Б' => 'B', 'В' => 'V', 'Г' => 'G',
+      'Д' => 'D', 'Е' => 'E', 'Ё' => 'YO', 'Ж' => 'ZH', 'З' => 'Z', 'И' => 'I',
+      'Й' => 'Y', 'К' => 'K', 'Л' => 'L', 'М' => 'M', 'Н' => 'N', 'О' => 'O',
+      'П' => 'P', 'Р' => 'R', 'С' => 'S', 'Т' => 'T', 'У' => 'U', 'Ф' => 'F',
+      'Х' => 'H', 'Ц' => 'C', 'Ч' => 'CH', 'Ш' => 'SH', 'Щ' => 'SCH', 'Ъ' => '',
+      'Ы' => 'Y', 'Ь' => '', 'Э' => 'E', 'Ю' => 'YU', 'Я' => 'YA', 'а' => 'a',
+      'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'yo',
+      'ж' => 'zh', 'з' => 'z', 'и' => 'i', 'й' => 'y', 'к' => 'k', 'л' => 'l',
+      'м' => 'm', 'н' => 'n', 'о' => 'o', 'п' => 'p', 'р' => 'r', 'с' => 's',
+      'т' => 't', 'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'c', 'ч' => 'ch',
+      'ш' => 'sh', 'щ' => 'sch', 'ъ' => '', 'ы' => 'y', 'ь' => '', 'э' => 'e',
+      'ю' => 'yu', 'я' => 'ya'
+   );
+
+   /**
+    * Convert certain unicode characters into their ascii equivalents.
+    *
+    * @param mixed $Mixed The text to clean.
+    * @return string
+    */
+   public static function Clean($Mixed) {
+       if (!is_string($Mixed)) return self::To($Mixed, 'Clean');
+       $Mixed = strtr($Mixed, self::$_CleanChars);
+       $Mixed = preg_replace('/[^A-Za-z0-9 ]/', '', urldecode($Mixed));
+       $Mixed = preg_replace('/ +/', '-', trim($Mixed));
+       return strtolower($Mixed);
+   }
 
 
    /**
@@ -844,6 +909,33 @@ EOT;
    }
 
    /**
+    * Formats seconds in a human-readable way (ie. 45 seconds, 15 minutes, 2 hours, 4 days, 2 months, etc).
+    */
+   public static function Seconds($Seconds) {
+      $Minutes = round($Seconds/60);
+      $Hours = round($Seconds/3600);
+      $Days = round($Seconds/86400);
+      $Weeks = round($Seconds/604800);
+      $Months = round($Seconds/2629743.83);
+      $Years = round($Seconds/31556926);
+
+      if ($Seconds < 60)
+         return sprintf(Plural($Seconds, '%s second', '%s seconds'), $Seconds);
+      elseif ($Minutes < 60)
+         return sprintf(Plural($Minutes, '%s minute', '%s minutes'), $Minutes);
+      elseif ($Hours < 24)
+         return sprintf(Plural($Hours, '%s hour', '%s hours'), $Hours);
+      elseif ($Days < 7)
+         return sprintf(Plural($Days, '%s day', '%s days'), $Days);
+      elseif ($Weeks < 4)
+         return sprintf(Plural($Weeks, '%s week', '%s weeks'), $Weeks);
+      elseif ($Months < 12)
+         return sprintf(Plural($Months, '%s month', '%s months'), $Months);
+      else
+         return sprintf(Plural($Years, '%s year', '%s years'), $Years);
+   }
+
+   /**
     * Takes any variable and serializes it.
     *
     * @param mixed $Mixed An object, array, or string to be serialized.
@@ -1061,318 +1153,6 @@ EOT;
 
          return $Match[1];
       }
-   }
-
-   /**
-    * Formats seconds in a human-readable way (ie. 45 seconds, 15 minutes, 2 hours, 4 days, 2 months, etc).
-    */
-   public static function Seconds($Seconds) {
-      $Minutes = round($Seconds/60);
-      $Hours = round($Seconds/3600);
-      $Days = round($Seconds/86400);
-      $Weeks = round($Seconds/604800);
-      $Months = round($Seconds/2629743.83);
-      $Years = round($Seconds/31556926);
-
-      if ($Seconds < 60)
-         return sprintf(Plural($Seconds, '%s second', '%s seconds'), $Seconds);
-      elseif ($Minutes < 60)
-         return sprintf(Plural($Minutes, '%s minute', '%s minutes'), $Minutes);
-      elseif ($Hours < 24)
-         return sprintf(Plural($Hours, '%s hour', '%s hours'), $Hours);
-      elseif ($Days < 7)
-         return sprintf(Plural($Days, '%s day', '%s days'), $Days);
-      elseif ($Weeks < 4)
-         return sprintf(Plural($Weeks, '%s week', '%s weeks'), $Weeks);
-      elseif ($Months < 12)
-         return sprintf(Plural($Months, '%s month', '%s months'), $Months);
-      else
-         return sprintf(Plural($Years, '%s year', '%s years'), $Years);
-   }
-   
-   
-   
-   /**
-   * 
-   */
-   
-   protected static $ReplacePairs = array(
-        '-' => ' ',
-        '_' => ' ',
-        '&lt;' => '',
-        '&gt;' => '',
-        '&#039;' => '',
-        '&amp;' => '',
-        '&quot;' => '',
-        'À' => 'A',
-        'Á' => 'A',
-        'Â' => 'A',
-        'Ã' => 'A',
-        'Ä' => 'Ae',
-        '&Auml;' => 'A',
-        'Å' => 'A',
-        'Ā' => 'A',
-        'Ą' => 'A',
-        'Ă' => 'A',
-        'Æ' => 'Ae',
-        'Ç' => 'C',
-        'Ć' => 'C',
-        'Č' => 'C',
-        'Ĉ' => 'C',
-        'Ċ' => 'C',
-        'Ď' => 'D',
-        'Đ' => 'D',
-        'Ð' => 'D',
-        'È' => 'E',
-        'É' => 'E',
-        'Ê' => 'E',
-        'Ë' => 'E',
-        'Ē' => 'E',
-        'Ę' => 'E',
-        'Ě' => 'E',
-        'Ĕ' => 'E',
-        'Ė' => 'E',
-        'Ĝ' => 'G',
-        'Ğ' => 'G',
-        'Ġ' => 'G',
-        'Ģ' => 'G',
-        'Ĥ' => 'H',
-        'Ħ' => 'H',
-        'Ì' => 'I',
-        'Í' => 'I',
-        'Î' => 'I',
-        'Ï' => 'I',
-        'Ī' => 'I',
-        'Ĩ' => 'I',
-        'Ĭ' => 'I',
-        'Į' => 'I',
-        'İ' => 'I',
-        'Ĳ' => 'IJ',
-        'Ĵ' => 'J',
-        'Ķ' => 'K',
-        'Ł' => 'K',
-        'Ľ' => 'K',
-        'Ĺ' => 'K',
-        'Ļ' => 'K',
-        'Ŀ' => 'K',
-        'Ñ' => 'N',
-        'Ń' => 'N',
-        'Ň' => 'N',
-        'Ņ' => 'N',
-        'Ŋ' => 'N',
-        'Ò' => 'O',
-        'Ó' => 'O',
-        'Ô' => 'O',
-        'Õ' => 'O',
-        'Ö' => 'Oe',
-        '&Ouml;' => 'Oe',
-        'Ø' => 'O',
-        'Ō' => 'O',
-        'Ő' => 'O',
-        'Ŏ' => 'O',
-        'Œ' => 'OE',
-        'Ŕ' => 'R',
-        'Ř' => 'R',
-        'Ŗ' => 'R',
-        'Ś' => 'S',
-        'Š' => 'S',
-        'Ş' => 'S',
-        'Ŝ' => 'S',
-        'Ș' => 'S',
-        'Ť' => 'T',
-        'Ţ' => 'T',
-        'Ŧ' => 'T',
-        'Ț' => 'T',
-        'Ù' => 'U',
-        'Ú' => 'U',
-        'Û' => 'U',
-        'Ü' => 'Ue',
-        'Ū' => 'U',
-        '&Uuml;' => 'Ue',
-        'Ů' => 'U',
-        'Ű' => 'U',
-        'Ŭ' => 'U',
-        'Ũ' => 'U',
-        'Ų' => 'U',
-        'Ŵ' => 'W',
-        'Ý' => 'Y',
-        'Ŷ' => 'Y',
-        'Ÿ' => 'Y',
-        'Ź' => 'Z',
-        'Ž' => 'Z',
-        'Ż' => 'Z',
-        'Þ' => 'T',
-        'à' => 'a',
-        'á' => 'a',
-        'â' => 'a',
-        'ã' => 'a',
-        'ä' => 'ae',
-        '&auml;' => 'ae',
-        'å' => 'a',
-        'ā' => 'a',
-        'ą' => 'a',
-        'ă' => 'a',
-        'æ' => 'ae',
-        'ç' => 'c',
-        'ć' => 'c',
-        'č' => 'c',
-        'ĉ' => 'c',
-        'ċ' => 'c',
-        'ď' => 'd',
-        'đ' => 'd',
-        'ð' => 'd',
-        'è' => 'e',
-        'é' => 'e',
-        'ê' => 'e',
-        'ë' => 'e',
-        'ē' => 'e',
-        'ę' => 'e',
-        'ě' => 'e',
-        'ĕ' => 'e',
-        'ė' => 'e',
-        'ƒ' => 'f',
-        'ĝ' => 'g',
-        'ğ' => 'g',
-        'ġ' => 'g',
-        'ģ' => 'g',
-        'ĥ' => 'h',
-        'ħ' => 'h',
-        'ì' => 'i',
-        'í' => 'i',
-        'î' => 'i',
-        'ï' => 'i',
-        'ī' => 'i',
-        'ĩ' => 'i',
-        'ĭ' => 'i',
-        'į' => 'i',
-        'ı' => 'i',
-        'ĳ' => 'ij',
-        'ĵ' => 'j',
-        'ķ' => 'k',
-        'ĸ' => 'k',
-        'ł' => 'l',
-        'ľ' => 'l',
-        'ĺ' => 'l',
-        'ļ' => 'l',
-        'ŀ' => 'l',
-        'ñ' => 'n',
-        'ń' => 'n',
-        'ň' => 'n',
-        'ņ' => 'n',
-        'ŉ' => 'n',
-        'ŋ' => 'n',
-        'ò' => 'o',
-        'ó' => 'o',
-        'ô' => 'o',
-        'õ' => 'o',
-        'ö' => 'oe',
-        '&ouml;' => 'oe',
-        'ø' => 'o',
-        'ō' => 'o',
-        'ő' => 'o',
-        'ŏ' => 'o',
-        'œ' => 'oe',
-        'ŕ' => 'r',
-        'ř' => 'r',
-        'ŗ' => 'r',
-        'š' => 's',
-        'ù' => 'u',
-        'ú' => 'u',
-        'û' => 'u',
-        'ü' => 'ue',
-        'ū' => 'u',
-        '&uuml;' => 'ue',
-        'ů' => 'u',
-        'ű' => 'u',
-        'ŭ' => 'u',
-        'ũ' => 'u',
-        'ų' => 'u',
-        'ŵ' => 'w',
-        'ý' => 'y',
-        'ÿ' => 'y',
-        'ŷ' => 'y',
-        'ž' => 'z',
-        'ż' => 'z',
-        'ź' => 'z',
-        'þ' => 't',
-        'ß' => 'ss',
-        'ſ' => 'ss',
-        'ый' => 'iy',
-        'А' => 'A',
-        'Б' => 'B',
-        'В' => 'V',
-        'Г' => 'G',
-        'Д' => 'D',
-        'Е' => 'E',
-        'Ё' => 'YO',
-        'Ж' => 'ZH',
-        'З' => 'Z',
-        'И' => 'I',
-        'Й' => 'Y',
-        'К' => 'K',
-        'Л' => 'L',
-        'М' => 'M',
-        'Н' => 'N',
-        'О' => 'O',
-        'П' => 'P',
-        'Р' => 'R',
-        'С' => 'S',
-        'Т' => 'T',
-        'У' => 'U',
-        'Ф' => 'F',
-        'Х' => 'H',
-        'Ц' => 'C',
-        'Ч' => 'CH',
-        'Ш' => 'SH',
-        'Щ' => 'SCH',
-        'Ъ' => '',
-        'Ы' => 'Y',
-        'Ь' => '',
-        'Э' => 'E',
-        'Ю' => 'YU',
-        'Я' => 'YA',
-        'а' => 'a',
-        'б' => 'b',
-        'в' => 'v',
-        'г' => 'g',
-        'д' => 'd',
-        'е' => 'e',
-        'ё' => 'yo',
-        'ж' => 'zh',
-        'з' => 'z',
-        'и' => 'i',
-        'й' => 'y',
-        'к' => 'k',
-        'л' => 'l',
-        'м' => 'm',
-        'н' => 'n',
-        'о' => 'o',
-        'п' => 'p',
-        'р' => 'r',
-        'с' => 's',
-        'т' => 't',
-        'у' => 'u',
-        'ф' => 'f',
-        'х' => 'h',
-        'ц' => 'c',
-        'ч' => 'ch',
-        'ш' => 'sh',
-        'щ' => 'sch',
-        'ъ' => '',
-        'ы' => 'y',
-        'ь' => '',
-        'э' => 'e',
-        'ю' => 'yu',
-        'я' => 'ya'
-   );
-   
-   
-   public static function Clean($Mixed) {
-       if (!is_string($Mixed)) return self::To($Mixed, 'Clean');
-       $Mixed = strtr($Mixed, self::$ReplacePairs);
-       $Mixed = preg_replace('/[^A-Za-z0-9 ]/', '', urldecode($Mixed));
-       $Mixed = preg_replace('/ +/', '-', trim($Mixed));
-       return strtolower($Mixed);
    }
    
 }

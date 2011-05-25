a19d735408396be0bee34a9dc9dd58adcbbcb77c
Tim Gunter <tim@vanillaforums.com>
2011-05-25 14:09:37 -0400

    Fix layout of stats config page

--- applications/dashboard/views/statistics/verify.php
+++ applications/dashboard/views/statistics/verify.php
@@ -3,7 +3,7 @@
       <div class="StatisticsVerification StatisticsOk"><?php echo T("Verified!"); ?></div>
    <?php } else { ?>
       <div class="StatisticsVerification StatisticsProblem">
-         <?php echo T("Problem with credentials"); ?>
+         <?php echo T("Problem with credentials."); ?>
          <?php echo $this->Form->Hidden('ClearCredentials',array('value'=>1)); ?>
          <p><?php echo $this->Form->Button('Re-Register API Key', array('class' => 'SmallButton')); ?></p>
       </div>

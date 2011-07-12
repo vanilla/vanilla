444099fdaa4ebdcd2fe8e060a3c38ea574fda278
Tim 'Thunderpuncher' Gunter <tim@vanillaforums.com>
2011-07-12 01:39:21 -0400

    Flip role data arrays in UI due to weird assoc array style in checkboxlist

--- applications/dashboard/views/user/add.php
+++ applications/dashboard/views/user/add.php
@@ -41,7 +41,7 @@ echo $this->Form->Errors();
    </li>
    <li>
       <strong><?php echo T('Check all roles that apply to this user:'); ?></strong>
-      <?php echo $this->Form->CheckBoxList("RoleID", $this->RoleData, $this->UserRoleData); ?>
+      <?php echo $this->Form->CheckBoxList("RoleID", array_flip($this->RoleData), array_flip($this->UserRoleData)); ?>
    </li>
 </ul>
 <?php echo $this->Form->Close('Save');

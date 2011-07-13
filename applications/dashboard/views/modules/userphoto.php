545f287e666e0f55f29be826b09abe1306f37977
Todd Burry <todd@vanillaforums.com>
2011-07-13 16:08:30 -0400

    Allow absolute urls in user profiles.

--- applications/dashboard/views/modules/userphoto.php
+++ applications/dashboard/views/modules/userphoto.php
@@ -3,7 +3,7 @@ if ($this->User->Photo != '') {
 ?>
    <div class="Photo">
       <?php
-      if (strpos($this->User->Photo, 'http') === 0)
+      if (StringBeginsWith($this->User->Photo, 'http'))
          echo Img($this->User->Photo);
       else
          echo Img(Gdn_Upload::Url(ChangeBasename($this->User->Photo, 'p%s')));

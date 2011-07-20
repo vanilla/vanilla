bbb433dd0e5fb298b017226cf1221f573cfec1ce
lincolnwebs <lincolnwebs@gmail.com>
2011-07-19 23:18:59 -0400

    GettingStarted: fix step order and abolish hideous ellipses.

--- applications/dashboard/views/settings/gettingstarted.php
+++ applications/dashboard/views/settings/gettingstarted.php
@@ -13,6 +13,33 @@ echo $this->Form->Errors();
    </div>
    <div class="Step">
       <div class="NumberPoint">1</div>
+      <h2><?php echo T('Customize'); ?></h2>
+      <p><?php echo T('Define your forum homepage, upload your logo, and grab a theme.'); ?></p>
+      <div class="Videos">
+         <a href="http://www.screenr.com/Qb0" target="_blank"><img src="<?php echo Asset('applications/dashboard/design/images/welcome-vid-banner.png'); ?>" title="<?php echo T('Change your banner'); ?>" /></a>
+         <a href="http://www.screenr.com/iCE" target="_blank"><img src="<?php echo Asset('applications/dashboard/design/images/welcome-vid-homepage.png'); ?>" title="<?php echo T('Define your forum homepage'); ?>" /></a>
+         <a href="http://www.screenr.com/Vb0" target="_blank"><img src="<?php echo Asset('applications/dashboard/design/images/welcome-vid-themes.png'); ?>" title="<?php echo T('How to use themes'); ?>" /></a>
+      </div>
+   </div>
+   <div class="Step">
+      <div class="NumberPoint">2</div>
+      <h2><?php echo T('Organize'); ?></h2>
+      <p><?php echo T('Create & organize discussion categories and manage your users.'); ?></p>
+      <div class="Videos">
+         <a href="http://www.screenr.com/CfE" target="_blank"><img src="<?php echo Asset('applications/dashboard/design/images/welcome-vid-categories.png'); ?>" title="<?php echo T('Organize discussion categories'); ?>" /></a>
+         <a href="http://www.screenr.com/kwG" target="_blank"><img src="<?php echo Asset('applications/dashboard/design/images/welcome-vid-users.png'); ?>" title="<?php echo T('Manage users'); ?>" /></a>
+      </div>
+   </div>
+   <div class="Step">
+      <div class="NumberPoint">3</div>
+      <h2><?php echo T('Advanced Stuff'); ?></h2>
+      <p><?php echo T('Embed your community forum into your website to increase engagement!'); ?></p>
+      <div class="Videos">
+         <a href="http://www.screenr.com/y4D" target="_blank"><img src="<?php echo Asset('applications/dashboard/design/images/welcome-vid-embed.png'); ?>" title="<?php echo T('Embed your forum in your web site'); ?>" /></a>
+      </div>
+   </div>
+   <div class="Step">
+      <div class="NumberPoint">4</div>
       <h2><?php echo T('Encourage your friends to join your new community!'); ?></h2>
       <div class="TextBoxWrapper">
          <?php
@@ -43,33 +70,6 @@ Follow the link below to log in.");
       });
       </script>
       <?php echo $this->Form->Button(T('Send Invitations!')); ?>
-   </div>
-   <div class="Step">
-      <div class="NumberPoint">2</div>
-      <h2><?php echo T('Customize'); ?></h2>
-      <p><?php echo T('Define your forum homepage, upload your logo, and more...'); ?></p>
-      <div class="Videos">
-         <a href="http://www.screenr.com/Qb0" target="_blank"><img src="<?php echo Asset('applications/dashboard/design/images/welcome-vid-banner.png'); ?>" title="<?php echo T('Change your banner'); ?>" /></a>
-         <a href="http://www.screenr.com/iCE" target="_blank"><img src="<?php echo Asset('applications/dashboard/design/images/welcome-vid-homepage.png'); ?>" title="<?php echo T('Define your forum homepage'); ?>" /></a>
-         <a href="http://www.screenr.com/Vb0" target="_blank"><img src="<?php echo Asset('applications/dashboard/design/images/welcome-vid-themes.png'); ?>" title="<?php echo T('How to use themes'); ?>" /></a>
-      </div>
-   </div>
-   <div class="Step">
-      <div class="NumberPoint">3</div>
-      <h2><?php echo T('Organize'); ?></h2>
-      <p><?php echo T('Create & organize discussion categories, manage your users, and more...'); ?></p>
-      <div class="Videos">
-         <a href="http://www.screenr.com/CfE" target="_blank"><img src="<?php echo Asset('applications/dashboard/design/images/welcome-vid-categories.png'); ?>" title="<?php echo T('Organize discussion categories'); ?>" /></a>
-         <a href="http://www.screenr.com/kwG" target="_blank"><img src="<?php echo Asset('applications/dashboard/design/images/welcome-vid-users.png'); ?>" title="<?php echo T('Manage users'); ?>" /></a>
-      </div>
-   </div>
-   <div class="Step">
-      <div class="NumberPoint">4</div>
-      <h2><?php echo T('Advanced Stuff'); ?></h2>
-      <p><?php echo T('Embed your community forum into your website to increase engagement...'); ?></p>
-      <div class="Videos">
-         <a href="http://www.screenr.com/y4D" target="_blank"><img src="<?php echo Asset('applications/dashboard/design/images/welcome-vid-embed.png'); ?>" title="<?php echo T('Embed your forum in your web site'); ?>" /></a>
-      </div>
    </div>
 </div>
 <?php echo $this->Form->Close(); ?>

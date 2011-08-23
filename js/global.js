a9aab939348b046e27bdcbacdb6511a6013dd7c0
Todd Burry <todd@vanillaforums.com>
2011-08-23 15:21:14 -0400

    MSIE overlay fix.

--- js/global.js
+++ js/global.js
@@ -1,6 +1,10 @@
 
 // This file contains javascript that is global to the entire Garden application
 jQuery(document).ready(function($) {
+   if ($.browser.msie) {
+      $('body').addClass('MSIE');
+   }
+   
    var d = new Date();
    var clientDate = d.getFullYear()+'-'+(d.getMonth() + 1)+'-'+d.getDate()+' '+d.getHours()+':'+d.getMinutes();
 

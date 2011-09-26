a4b593dbbbb2ecbc5633e61f9e9342808c444973
Todd Burry <todd@vanillaforums.com>
2011-09-26 14:03:33 -0400

    Fixed bug in Gdn_Form->CategoryDropDown() where the first item is selected even when there is a null row.

--- library/core/class.form.php
+++ library/core/class.form.php
@@ -267,7 +267,7 @@ class Gdn_Form extends Gdn_Pluggable {
       // If making headings disabled and there was no default value for
       // selection, make sure to select the first non-disabled value, or the
       // browser will auto-select the first disabled option.
-      $ForceCleanSelection = ($DoHeadings && !$HasValue);
+      $ForceCleanSelection = ($DoHeadings && !$HasValue && !$IncludeNull);
       
       // Write out the category options
       if (is_array($SafeCategoryData)) {

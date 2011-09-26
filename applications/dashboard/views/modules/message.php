872223155398499d4891e7e9429c81e67f2868d1
Todd Burry <todd@vanillaforums.com>
2011-09-26 17:07:29 -0400

    Make messages use the cache.

--- applications/dashboard/views/modules/message.php
+++ applications/dashboard/views/modules/message.php
@@ -1,13 +1,15 @@
 <?php if (!defined('APPLICATION')) exit();
+$Message = $this->_Message;
 
-if (is_object($this->_Message)) {
-   echo '<div class="DismissMessage'.($this->_Message->CssClass == '' ? '' : ' '.$this->_Message->CssClass).'">';
+
+if (is_array($Message)) {
+   echo '<div class="DismissMessage'.($Message['CssClass'] == '' ? '' : ' '.$Message['CssClass']).'">';
    $Session = Gdn::Session();
-   if ($this->_Message->AllowDismiss == '1' && $Session->IsValid()) {
-      echo Anchor('×', '/dashboard/message/dismiss/'.$this->_Message->MessageID.'/'.$Session->TransientKey().'?Target='.$this->_Sender->SelfUrl, 'Dismiss');
+   if ($Message['AllowDismiss'] == '1' && $Session->IsValid()) {
+      echo Anchor('×', "/dashboard/message/dismiss/{$Message['MessageID']}/".$Session->TransientKey().'?Target='.$this->_Sender->SelfUrl, 'Dismiss');
    }
    
    // echo Gdn_Format::To($this->_Message->Content, 'Html');
-   echo $this->_Message->Content;
+   echo $Message['Content'];
    echo '</div>';
 }

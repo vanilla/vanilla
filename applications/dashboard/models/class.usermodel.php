b970d1fc78502e6461e8e0fcf016fc392ab05a6b
sbenes <sbenes@vipasolutions.com>
2011-09-27 17:00:08 -0500

    Updated RemovePicture() to set the Photo as NULL instead of a string that says "NULL"

--- applications/dashboard/models/class.usermodel.php
+++ applications/dashboard/models/class.usermodel.php
@@ -721,7 +721,7 @@ class UserModel extends Gdn_Model {
    }
    
    public function RemovePicture($UserID) {
-      $this->SetField($UserID, 'Photo', 'NULL');
+      $this->SetField($UserID, 'Photo', NULL);
    }
 
    public function ProfileCount($User, $Column) {

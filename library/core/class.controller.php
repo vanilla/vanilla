800cf8df19d8035b986a45fc8ffb8083adf7faa4
Todd Burry <todd@vanillaforums.com>
2011-10-04 12:00:50 -0400

    Clean data out of api requests for security.

--- library/core/class.controller.php
+++ library/core/class.controller.php
@@ -1222,7 +1222,13 @@ class Gdn_Controller extends Gdn_Pluggable {
       }
       
       // Remove values that should not be transmitted via api
-      $Data = RemoveKeysFromNestedArray($Data, array('Email', 'Password', 'HashMethod', 'DateOfBirth', 'TransientKey', 'Permissions'));
+      $Remove = array('Email', 'Password', 'HashMethod', 'DateOfBirth', 'TransientKey', 'Permissions', 'Attributes');
+      if (!Gdn::Session()->CheckPermission('Garden.Moderation.Manage')) {
+         $Remove[] = 'InsertIPAddress';
+         $Remove[] = 'UpdateIPAddress';
+         $Remove[] = 'LastIPAddress';
+      }
+      $Data = RemoveKeysFromNestedArray($Data, $Remove);
       
       // Make sure the database connection is closed before exiting.
       $this->Finalize();

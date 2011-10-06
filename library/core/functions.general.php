1b67f793a7a1837d428ef5eccce780b06dba79ae
Tim 'Thunderpuncher' Gunter <tim@vanillaforums.com>
2011-10-06 12:17:23 -0400

    Fixed missing fnmatch on Windows with PHP<5.3

--- library/core/functions.general.php
+++ library/core/functions.general.php
@@ -805,6 +805,60 @@ if (!function_exists('FetchPageInfo')) {
 }
 
 /**
+ * Replace missing 'fnmatch' on Windows + PHP <5.3
+ */
+if (!function_exists('fnmatch')) { 
+   define('FNM_PATHNAME', 1); 
+   define('FNM_NOESCAPE', 2); 
+   define('FNM_PERIOD', 4); 
+   define('FNM_CASEFOLD', 16); 
+
+   function fnmatch($pattern, $string, $flags = 0) { 
+      return pcre_fnmatch($pattern, $string, $flags); 
+   } 
+
+   function pcre_fnmatch($pattern, $string, $flags = 0) { 
+      $modifiers = null; 
+      $transforms = array( 
+         '\*'    => '.*', 
+         '\?'    => '.', 
+         '\[\!'    => '[^', 
+         '\['    => '[', 
+         '\]'    => ']', 
+         '\.'    => '\.', 
+         '\\'    => '\\\\' 
+      );
+
+      // Forward slash in string must be in pattern: 
+      if ($flags & FNM_PATHNAME) { 
+         $transforms['\*'] = '[^/]*'; 
+      } 
+
+      // Back slash should not be escaped: 
+      if ($flags & FNM_NOESCAPE) { 
+         unset($transforms['\\']); 
+      } 
+
+      // Perform case insensitive match: 
+      if ($flags & FNM_CASEFOLD) { 
+         $modifiers .= 'i'; 
+      } 
+
+      // Period at start must be the same as pattern: 
+      if ($flags & FNM_PERIOD) { 
+         if (strpos($string, '.') === 0 && strpos($pattern, '.') !== 0) return false; 
+      } 
+
+      $pattern = '#^' 
+         . strtr(preg_quote($pattern, '#'), $transforms) 
+         . '$#' 
+         . $modifiers; 
+
+      return (boolean)preg_match($pattern, $string); 
+   }
+}
+
+/**
  * Formats a string by inserting data from its arguments, similar to sprintf, but with a richer syntax.
  *
  * @param string $String The string to format with fields from its args enclosed in curly braces. The format of fields is in the form {Field,Format,Arg1,Arg2}. The following formats are the following:

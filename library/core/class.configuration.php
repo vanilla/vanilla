945cdebdd97e9a8dda93eb55cefd8aa4b1b80204
Tim 'Thunderpuncher' Gunter <tim@vanillaforums.com>
2011-10-17 20:30:58 -0400

    Update docs

--- library/core/class.configuration.php
+++ library/core/class.configuration.php
@@ -1,33 +1,15 @@
 <?php if (!defined('APPLICATION')) exit();
-/*
-Copyright 2008, 2009 Vanilla Forums Inc.
-This file is part of Garden.
-Garden is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-Garden is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with Garden.  If not, see <http://www.gnu.org/licenses/>.
-Contact Vanilla Forums Inc. at support [at] vanillaforums [dot] com
-*/
 
 /**
  * The Configuration class can be used to load configuration arrays from files,
  * retrieve settings from the arrays, assign new values to the arrays, and save
  * the arrays back to the files.
  *
- * Usage:
- * <code>
- *  $Configuration->LoadFromFile($Name, $FileName); // Loads the configuration array $GroupName['Setting'] = $Value;
- *  $Setting = $Configuration->Get('Setting');
- *  $Configuration->Set('Setting2', 'Value');
- *  $Configuration->Set('Setting3', $Object);
- *  $Configuration->Set('Setting4', $Array);
- * </code>
- *
- * @author Mark O'Sullivan
- * @copyright 2003 Mark O'Sullivan
+ * @author Tim Gunter <tim@vanillaforums.com>
+ * @copyright 2003 Vanilla Forums, Inc
  * @license http://www.opensource.org/licenses/gpl-2.0.php GPL
  * @package Garden
- * @version @@GARDEN-VERSION@@
- * @namespace Garden.Core
+ * @since 2.0
  */
 
 class Gdn_Configuration extends Gdn_Pluggable {

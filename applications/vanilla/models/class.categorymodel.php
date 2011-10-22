4cabbf9f0d39a92b4b7c4ff93b30b314d41b8a69
Todd Burry <todd@vanillaforums.com>
2011-10-21 21:52:08 -0400

    Fixed category write-through caching.

--- applications/vanilla/models/class.categorymodel.php
+++ applications/vanilla/models/class.categorymodel.php
@@ -1104,6 +1104,7 @@ class CategoryModel extends Gdn_Model {
       $Category = $Categories[$ID];
       $Category = array_merge($Category, $Data);
       $Categories[$ID] = $Category;
+      self::CalculateData($Categories);
       Gdn::Cache()->Store(self::CACHE_KEY, $Categories, array(Gdn_Cache::FEATURE_EXPIRY => 600));
    }
    

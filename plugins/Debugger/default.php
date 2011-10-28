0e851eef540d4dd858288b4c60a8746bb08d6c6f
Todd Burry <todd@vanillaforums.com>
2011-10-28 14:42:34 -0400

    But the debugger plugin's output into a view. Added the cache revisions to the debugger plugin's output.

--- plugins/Debugger/default.php
+++ plugins/Debugger/default.php
@@ -48,63 +48,7 @@ class DebuggerPlugin extends Gdn_Plugin {
          return;
       }
       
-      if (!$Sender->Head)
-         $Sender->Head = new HeadModule($Sender);
-         
-      $Sender->Head->AddCss('/plugins/Debugger/style.css');
-
-
-      //$Session = Gdn::Session();
-      //if ($Session->CheckPermission('Plugins.Debugger.View')) {
-      $String = '<div id="Sql" class="DebugInfo">';
-
-      $String .= '<h2>'.T('Debug Information').'</h2>';
-
-      // Add the canonical Url.
-      if (method_exists($Sender, 'CanonicalUrl')) {
-         $CanonicalUrl = htmlspecialchars($Sender->CanonicalUrl(), ENT_COMPAT, 'UTF-8');
-
-         $String .= '<div class="CanonicalUrl"><b>'.T('Canonical Url')."</b>: <a href=\"$CanonicalUrl\" accesskey=\"r\">$CanonicalUrl</a></div>";
-      }
-
-      $Database = Gdn::Database();
-      $SQL = $Database->SQL();
-      if(!is_null($Database)) {
-         $Queries = $Database->Queries();
-         $String .= '<h3>'.count($Queries).' queries in '.$Database->ExecutionTime().'s</h3>';
-         foreach ($Queries as $Key => $QueryInfo) {
-            $Query = $QueryInfo['Sql'];
-            // this is a bit of a kludge. I found that the regex below would mess up when there were incremented named parameters. Ie. it would replace :Param before :Param0, which ended up with some values like "'4'0".
-            if(isset($QueryInfo['Parameters']) && is_array($QueryInfo['Parameters'])) {
-               $tmp = $QueryInfo['Parameters'];
-
-               $Query = $SQL->ApplyParameters($Query, $tmp);
-            }
-            $String .= $QueryInfo['Method']
-               .'<small>'.@number_format($QueryInfo['Time'], 6).'s</small>'
-               .(isset($QueryInfo['Cache']) ? '<div><b>Cache:</b> '.var_export($QueryInfo['Cache'], TRUE).'</div>' : '')
-               .'<pre>'.htmlspecialchars($Query).';</pre>';
-         }
-      }
-      $String .= '<h3>Controller Data</h3><pre>';
-      
-      $String .= self::FormatData($Sender->Data);
-
-      $String .= '</pre>';
-
-      global $Start;
-      $String .= '<h3>Page completed in '.round(Now() - $_SERVER['REQUEST_TIME'], 4).'s</h3>';
-         /*
-         <div>
-            <strong>Application:</strong> ' . $Sender->ApplicationFolder . ';
-            <strong>Controller:</strong> ' . $Sender->ClassName . ';
-            <strong>Method:</strong> ' . $Sender->RequestMethod . ';
-         </div>
-      </div>';
-           */
-      $String .= '</div>';
-      echo $String;
-   //}
+      require $Sender->FetchViewLocation('Debug', '', 'plugins/Debugger');
    }
 
    public static function FormatData($Data, $Indent = '') {

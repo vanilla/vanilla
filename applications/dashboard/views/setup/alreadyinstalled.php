f1a10557bd2e8e0af39538a0a50309675ed3b0cc
Todd Burry <todd@vanillaforums.com>
2011-10-28 09:50:28 -0400

    Display an already installed page instead of an error page if Vanilla is already installed.

---
+++ applications/dashboard/views/setup/alreadyinstalled.php
@@ -0,0 +1,10 @@
+<?php if (!defined('APPLICATION')) exit();
+?>
+<div class="Title">
+   <h1><?php echo T("Vanilla is installed!"); ?></h1>
+</div>
+<div class="Form">
+   <ul>
+      <li><?php echo Anchor(T('Click here to carry on.'), '/'); ?>.</li>
+   </ul>
+</div>

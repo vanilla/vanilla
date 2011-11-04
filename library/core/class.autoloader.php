f331b4313cb51e5d1d7a7018de8282af50e7e1a1
Tim 'Thunderpuncher' Gunter <tim@vanillaforums.com>
2011-11-04 16:25:58 -0400

    Fix docblock comment for autoloader class.

--- library/core/class.autoloader.php
+++ library/core/class.autoloader.php
@@ -1,12 +1,4 @@
 <?php if (!defined('APPLICATION')) exit();
-/*
-Copyright 2008, 2009 Vanilla Forums Inc.
-This file is part of Garden.
-Garden is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-Garden is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with Garden.  If not, see <http://www.gnu.org/licenses/>.
-Contact Vanilla Forums Inc. at support [at] vanillaforums [dot] com
-*/
 
 /**
  * Vanilla framework autoloader.
@@ -16,12 +8,11 @@ Contact Vanilla Forums Inc. at support [at] vanillaforums [dot] com
  *
  * This is a static class that hooks into the SPL autoloader.
  *
- * @author Tim Gunter
- * @copyright 2003 Mark O'Sullivan
+ * @author Tim Gunter <tim@vanillaforums.com>
+ * @copyright 2003 Vanilla Forums, Inc
  * @license http://www.opensource.org/licenses/gpl-2.0.php GPL
  * @package Garden
- * @version @@GARDEN-VERSION@@
- * @namespace Garden.Core
+ * @since 2.0.16
  */
 
 class Gdn_Autoloader {

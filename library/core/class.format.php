c7250d8cbe186c1fb16fedb45551de1c13691bb8
Todd Burry <todd@vanillaforums.com>
2011-11-22 15:15:04 -0500

    Trim '.' and '-' off the ends of Gdn_Format::Url() strings.

--- library/core/class.format.php
+++ library/core/class.format.php
@@ -1225,6 +1225,7 @@ EOT;
          $Mixed = str_replace(' ', '-', trim($Mixed)); // get rid of spaces
          $Mixed = preg_replace('/-+/', '-', $Mixed); // limit to 1 hyphen at a time
          $Mixed = urlencode(strtolower($Mixed));
+         $Mixed = trim($Mixed, '.-');
          return $Mixed;
       } else {
          // Better Unicode support.
@@ -1235,6 +1236,7 @@ EOT;
          $Mixed = preg_replace('`([^\PS+])`u', '', $Mixed); // get rid of symbols
          $Mixed = preg_replace('`[\s\-/+]+`u', '-', $Mixed); // replace certain characters with dashes
          $Mixed = rawurlencode(strtolower($Mixed));
+         $Mixed = trim($Mixed, '.-');
 			return $Mixed;
       }
    }

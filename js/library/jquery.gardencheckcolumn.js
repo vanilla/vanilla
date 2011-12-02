b7f1df2bbf858a8bcc6979ed6cbbabda7d1b8d25
Tim 'Thunderpuncher' Gunter <tim@vanillaforums.com>
2011-12-02 12:30:32 -0500

    Fix checkcolumn for jQuery 1.6.

--- js/library/jquery.gardencheckcolumn.js
+++ js/library/jquery.gardencheckcolumn.js
@@ -1,47 +1,55 @@
-/**************************************************************
-jQuery / Garden CheckColumn Plugin v1
-**************************************************************/
+/* 
+ * Garden CheckColumn Plugin (1.1)
+ * by Mark O'Sullivan (mark@vanillaforums.com)
+ * by Tim Gunter (tim@vanillaforums.com)
+ *
+ * Copyright (c) 2008 Vanilla Forums, Inc
+ * Licensed under the GPL (GPL-LICENSE.txt) license. 
+ *
+ * NOTE: This script requires jQuery to work.
+ * Download jQuery at www.jquery.com
+ */
 
-(function($) {
-  $.fn.checkColumn = function(opt) {
-   opt = $.extend({
-     noOptionsYet: 0
-   }, opt);
-   
-  // Remove the cellpadding on anchor cells
-  $(this).find('thead td').css('padding', '0px');
-   
-  // Handle column heading clicks
-  $(this).find('thead td').each(function() {
-      var columnIndex = $(this).attr('cellIndex');
-      var text = $(this).html();
-      var anchor = document.createElement('a');
-      anchor.onclick = function(sender) {
-        var rows = $(this).parents('table').find('tbody tr');
-        var checkbox = false;
-        for (i = 0; i < rows.length; i++) {
-          checkbox = $(rows[i]).find('td:eq(' + (columnIndex) + ')').find(":checkbox");
-          if (checkbox) {
-            if ($(checkbox).attr('checked')) {
-              checkbox.removeAttr('checked');
-            } else {
-              checkbox.attr('checked', 'checked');
-            }
-          }
-        }
-        return false;
-      }
-      anchor.innerHTML = text;
-      anchor.href = '#';
-      $(this).html(anchor);
-  });
-  
-   
-   // Return the object for chaining
-     return $(this);
-  }
-})(jQuery);
+jQuery(document).ready(function($){
+   $.fn.checkColumn = function(opt) {
+      opt = $.extend({
+        noOptionsYet: 0
+      }, opt);
 
-$(function() {
-  $('table.CheckColumn').checkColumn();
+      // Remove the cellpadding on anchor cells
+      $(this).find('thead td').css('padding', '0px');
+
+      // Handle column heading clicks
+      $(this).find('thead td').each(function(i,el) {
+         el = $(el);
+         var columnIndex = el.prop('cellIndex');
+         var text = el.html();
+         el.html('');
+         
+         var anchor = $('<a></a>');
+         anchor.click(function(event) {
+            var rows = $(el).parents('table').find('tbody tr');
+            var checkbox = false;
+            rows.each(function(j,row){
+               checkbox = $(row).find('td:eq(' + (columnIndex) + ')').find(":checkbox");
+               if (checkbox) {
+                  if (checkbox.prop('checked')) {
+                     checkbox.removeAttr('checked');
+                  } else {
+                     checkbox.prop('checked', 'checked');
+                  }
+               }
+            })
+            return false;
+         });
+         anchor.html(text);
+         anchor.prop('href', '#');
+         el.append(anchor);
+      });
+   
+      // Return the object for chaining
+      return $(this);
+   }
+   
+   $('table.CheckColumn').checkColumn();
 });

73404d4a7c22be32d84c98ba77af46bea9f497c8
Todd Burry <todd@vanillaforums.com>
2011-12-21 22:57:52 -0500

    Added js to sign users out when there is no transient key.

--- applications/dashboard/views/entry/signout.php
+++ applications/dashboard/views/entry/signout.php
@@ -4,12 +4,28 @@ $Session = Gdn::Session();
 <div id="Leave">
    <h1><?php echo T('Sign Out'); ?></h1>
    <div class="Box">
-   <?php if ($this->Leaving) { ?>
-      <p class="Leaving"><?php echo T('SigningOut', 'Hang on a sec while we sign you out.'); ?></p>
-   <?php } else if ($Session->IsValid()) { ?>
-      <p><?php printf(T('AttemptingSignOut', 'You are attempting to sign out. Are you sure you want to %s?'), Anchor(T('sign out'), SignOutUrl())); ?></p>
-   <?php } else { ?>
-      <p><?php echo T('SignedOut', 'You are signed out.'); ?></p>
-   <?php } ?>
+      <p id="LeavingWrap" class="Leaving" <?php if (!$this->Leaving) echo 'style="display: none;"'; ?>><?php echo T('SigningOut', 'Hang on a sec while we sign you out.'); ?> <span class="TinyProgress"></span></p>
+   
+      <?php if (!$this->Leaving): ?>
+      
+      <?php if ($Session->IsValid()) { ?>
+      <p id="SignoutWrap">
+         <script language="javascript">
+            jQuery(document).ready(function($) {
+               var url = $('#SignoutLink').attr('href');
+               if (url) {
+                  $('#SignoutWrap').hide();
+                  $('#LeavingWrap').show();
+                  window.location.replace(url);
+               }
+            });
+         </script>
+         <?php printf(T('AttemptingSignOut', 'You are attempting to sign out. Are you sure you want to %s?'), Anchor(T('sign out'), SignOutUrl($this->Data('Target')), '', array('id' => 'SignoutLink'))); ?>
+      </p>
+      <?php } else { ?>
+         <p><?php echo T('SignedOut', 'You are signed out.'); ?></p>
+      <?php } ?>
+      
+      <?php endif; ?>   
    </div>
 </div>

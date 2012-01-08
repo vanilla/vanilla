362422dbd1c7ce51620c0fbccb1609e06698115b
Todd Burry <todd@vanillaforums.com>
2012-01-08 17:48:41 -0500

    Added clear fixes to the categories.

--- applications/vanilla/views/modules/categories.php
+++ applications/vanilla/views/modules/categories.php
@@ -10,11 +10,9 @@ if ($this->Data !== FALSE) {
 <div class="Box BoxCategories">
    <h4><?php echo Anchor(T('Categories'), 'categories/all'); ?></h4>
    <ul class="PanelInfo PanelCategories">
-      <li<?php
+      <li class="ClearFix<?php
       if (!is_numeric($CategoryID))
-         echo ' class="Active"';
-         
-      ?>><span><strong><?php echo Anchor(Gdn_Format::Text(T('All Discussions')), '/discussions'); ?></strong> <span class="Aside"><span class="Count"><?php echo Gdn_Format::BigNumber($CountDiscussions); ?></span></span></span></li>
+         echo ' Active';?>"><span><strong><?php echo Anchor(Gdn_Format::Text(T('All Discussions')), '/discussions'); ?></strong> <span class="Aside"><span class="Count"><?php echo Gdn_Format::BigNumber($CountDiscussions); ?></span></span></span></li>
 <?php
    $MaxDepth = C('Vanilla.Categories.MaxDisplayDepth');
    $DoHeadings = C('Vanilla.Categories.DoHeadings');
@@ -28,7 +26,7 @@ if ($this->Data !== FALSE) {
       else
          $CssClass = 'Depth'.$Category->Depth.($CategoryID == $Category->CategoryID ? ' Active' : '');
       
-      echo '<li class="'.$CssClass.'">';
+      echo '<li class="ClearFix '.$CssClass.'">';
 
       if ($DoHeadings && $Category->Depth == 1) {
          echo Gdn_Format::Text($Category->Name);

ef8816c5679e77b842545f27f26f3ca4a770de10
Mark O'Sullivan <mark@vanillaforums.com>
2012-03-22 16:58:46 -0400

    Made some simple layout & text changes to error & maintenance screens.

--- applications/dashboard/views/error.master.php
+++ applications/dashboard/views/error.master.php
@@ -4,7 +4,7 @@ echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-ca">
 <head>
-   <title>Bonk</title>
+   <title>Something has gone wrong.</title>
    <meta name="robots" content="noindex" />
    <?php
    if ($CssPath !== FALSE)
@@ -14,8 +14,11 @@ echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
 <body>
    <div id="Content">
       <div class="SplashInfo">
-         <h1>Bonk</h1>
-         <p>Something funky happened. Please bear with us while we iron out the kinks.</p>
+         <h1>Something has gone wrong.</h1>
+         <p>
+            We've run into a problem and are unable to handle this request right now.
+            <br />Please check back in a little while.
+         </p>
       </div>
    </div>
 </body>

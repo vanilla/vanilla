23f642563b04414f7a0a153c7653bdd28a4f3a8e
Todd Burry <todd@vanillaforums.com>
2012-03-26 12:04:15 -0400

    Version 2.0.18.4.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 define('APPLICATION', 'Vanilla');
-define('APPLICATION_VERSION', '2.0.18.3');
+define('APPLICATION_VERSION', '2.0.18.4');
 /*
 Copyright 2008, 2009 Vanilla Forums Inc.
 This file is part of Garden.

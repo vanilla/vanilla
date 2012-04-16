bd8920fe5b8418fe254de7f375feab6b01eda942
Mark O'Sullivan <mark@vanillaforums.com>
2012-04-16 11:16:44 -0400

    Typo fix

--- library/vendors/SmartyPlugins/function.link.php
+++ library/vendors/SmartyPlugins/function.link.php
@@ -18,7 +18,7 @@ Contact Vanilla Forums Inc. at support [at] vanillaforums [dot] com
  *    - <b>signinout</b>: This will return a signin/signout url that will toggle depending on whether or not the user is already signed in. When this path is given the text is automaticall set.
  * - <b>withdomain</b>: Whether or not to add the domain to the url.
  * - <b>text</b>: Html text to be put inside an anchor. If this value is set then an html <a></a> is returned rather than just a url.
- * - <b>id, class, etc.></b>: When an anchor is generated then any other attributes are passed through and will be written in the resulting tag.
+ * - <b>id, class, etc.</b>: When an anchor is generated then any other attributes are passed through and will be written in the resulting tag.
  * @param Smarty The smarty object rendering the template.
  * @return The url.
  */

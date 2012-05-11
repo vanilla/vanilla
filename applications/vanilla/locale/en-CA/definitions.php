31e8c090109053cb3322281a7380bfca475fff86
Mark O'Sullivan <mark@vanillaforums.com>
2012-05-11 16:09:47 -0400

    Added definition.

--- applications/vanilla/locale/en-CA/definitions.php
+++ applications/vanilla/locale/en-CA/definitions.php
@@ -3,3 +3,4 @@ $Definition['EmbeddedDiscussionFormat'] = '<div class="EmbeddedContent">{Image}<
 <p>{Excerpt}</p>
 <p><a href="{Url}">Read the full story here</a></p><div class="ClearFix"></div></div>';
 $Definition['EmbeddedNoBodyFormat'] = '{Url}';
+$Definition['CategoryID'] = 'Category';

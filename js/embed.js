33b2879c1b47b6e36a7770f39101ff346bc0c01f
Mark O'Sullivan <mark@vanillaforums.com>
2012-05-14 16:35:29 -0400

    Added "do not show add iframe to page until container is in the viewable region" functionality to embedded comments. This feature is jQuery-dependent. If the container page does not have jQuery, this code will fallback and just load the iframe on every page load.

--- js/embed.js
+++ js/embed.js
@@ -246,8 +246,22 @@ window.vanilla.embed = function(host) {
    if (!container)
       document.write('<div id="vanilla-comments"></div>');
    container = document.getElementById('vanilla-comments');
-   if (container)
-      container.appendChild(vanillaIframe);
+   if (container) {
+      // If jQuery is present in the page, include our defer-until-visible script
+      if (typeof jQuery != 'undefined') {
+         var vanillaLazyLoad = document.createElement('script');
+         vanillaLazyLoad.type = 'text/javascript';
+         vanillaLazyLoad.src = vanilla_forum_url + '/js/library/jquery.appear.js';
+         (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(vanillaLazyLoad);
+         jQuery(document).ready(function($) {
+            $('#vanilla-comments').appear(function() {
+               container.appendChild(vanillaIframe);
+            });
+         });
+      } else {
+         container.appendChild(vanillaIframe); // fallback: just load it
+      }
+   }
 
    // Include our embed css into the page
    var vanilla_embed_css = document.createElement('link');
@@ -255,7 +269,6 @@ window.vanilla.embed = function(host) {
    vanilla_embed_css.type = 'text/css';
    vanilla_embed_css.href = host_base_url + (host_base_url.substring(host_base_url.length-1) == '/' ? '' : '/') +'applications/dashboard/design/embed.css';
    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(vanilla_embed_css);
-   
    return this;
 };
 try {

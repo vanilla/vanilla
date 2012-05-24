42a02c2c3c13edb76b2c7049ae126406cdab85d5
Todd Burry <todd@vanillaforums.com>
2012-05-24 00:43:36 -0400

    Increment application version...

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
  */
 
 define('APPLICATION', 'Vanilla');
-define('APPLICATION_VERSION', '2.1a17');
+define('APPLICATION_VERSION', '2.1a18');
 
 // Report and track all errors.
 error_reporting(E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR);

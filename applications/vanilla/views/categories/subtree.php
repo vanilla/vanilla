b952c2cbdfc63721328589efaf30bd8ccf9cb20d
Todd Burry <todd@vanillaforums.com>
2012-06-11 21:30:33 -0400

    A few more category subtree tweaks.

--- applications/vanilla/views/categories/subtree.php
+++ applications/vanilla/views/categories/subtree.php
@@ -10,7 +10,7 @@ require_once $this->FetchViewLocation('helper_functions', 'categories', 'vanilla
 
 ?>
 <h2 class="ChildCategories-Title Hidden"><?php echo T('Child Categories'); ?></h2>
-<ul class="ChildCategoryList">
+<ul class="DataList ChildCategoryList">
    <?php
    foreach ($SubCategories['Children'] as $Row):
       if (!$Row['PermsDiscussionsView'])
@@ -21,9 +21,13 @@ require_once $this->FetchViewLocation('helper_functions', 'categories', 'vanilla
       <li id="Category_<?php echo $Row['CategoryID']; ?>" class="Item Category">
          <div class="ItemContent Category">
             <?php echo Wrap(Anchor($Row['Name'], $Row['Url'], 'Title'), 'h3', array('class' => 'CategoryName TitleWrap')); ?>
+            
+            <?php if ($Row['Description']): ?>
             <div class="CategoryDescription">
                <?php echo $Row['Description']; ?>
             </div>
+            <?php endif; ?>
+            
             <div class="Meta Hidden">
                <span class="MItem MItem-Count DiscussionCount"><?php
                   echo Plural(

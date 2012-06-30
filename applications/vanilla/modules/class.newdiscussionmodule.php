473d4e01643bed034a7e24d62d163c68659a90f3
Mark O'Sullivan <mark@vanillaforums.com>
2012-06-30 18:39:53 -0400

    Changed new discussion module to use button group dropdown when there is more than one type of new discussion type available. Upgraded buttons plugin with appropriate styles.

--- applications/vanilla/modules/class.newdiscussionmodule.php
+++ applications/vanilla/modules/class.newdiscussionmodule.php
@@ -14,10 +14,17 @@ class NewDiscussionModule extends Gdn_Module {
    }
    
    public function ToString() {
+      Gdn::Controller()->EventArguments['NewDiscussionModule'] = &$this;
+      Gdn::Controller()->FireEvent('BeforeNewDiscussionButton');
       $HasPermission = Gdn::Session()->CheckPermission('Vanilla.Discussions.Add', TRUE, 'Category', 'any');
       if ($HasPermission)
          return parent::ToString();
 
       return '';
+   }
+   
+   public $Buttons = array();
+   public function AddButton($Text, $Url) {
+      $this->Buttons[] = array('Text' => $Text, 'Url' => $Url);
    }
 }
--- applications/vanilla/views/modules/newdiscussion.php
+++ applications/vanilla/views/modules/newdiscussion.php
@@ -1,12 +1,23 @@
 <?php if (!defined('APPLICATION')) exit();
 echo '<div class="BoxButtons BoxNewDiscussion">';
 
+$Text = T('Start a New Discussion');
+$Url = '/post/discussion'.(array_key_exists('CategoryID', $Data) ? '/'.$Data['CategoryID'] : '');
+$Css = 'Button Action BigButton NewDiscussion';
 
-echo Anchor(
-   T('Start a New Discussion'), 
-   '/post/discussion'.(array_key_exists('CategoryID', $Data) ? '/'.$Data['CategoryID'] : ''), 
-   'Button Action BigButton NewDiscussion'
-);
-Gdn::Controller()->FireEvent('AfterNewDiscussionButton');
+if (count($this->Buttons) == 0) {
+   echo Anchor($Text, $Url, $Css);
+} else {
+   echo '<div class="ButtonGroup Action Big">';
+      echo Anchor($Text, $Url, 'Button');
+      echo Anchor(Sprite('SpDropDownHandle'), '#', 'Button Handle');
+      echo '<ul class="Dropdown MenuItems">';
+         echo Wrap(Anchor($Text, $Url), 'li');
+         foreach ($this->Buttons as $Button) {
+            echo Wrap(Anchor($Button['Text'], $Button['Url']), 'li');
+         }
+      echo '</ul>';
+   echo '</div>';
+}
 
 echo '</div>';

491bfa58b260c7dc0d4dfc1f61b7359d7b530e68
Todd Burry <todd@vanillaforums.com>
2012-07-05 16:33:45 -0400

    Make the tagged with page use and h1 and generic, translatable text. Remove the crazy margin from the token input.

--- plugins/Tagging/views/taggeddiscussions.php
+++ plugins/Tagging/views/taggeddiscussions.php
@@ -1,7 +1,7 @@
 <?php if (!defined('APPLICATION')) exit();
 include($this->FetchViewLocation('helper_functions', 'discussions', 'vanilla'));
 ?>
-<div class="TaggedHeading"><?php printf("Questions tagged with '%s'", htmlspecialchars($this->Tag)); ?></div>
+<h1 class="H TaggedHeading"><?php printf(T('Tagged with "%s"'), htmlspecialchars($this->Tag)); ?></h1>
 <?php if ($this->DiscussionData->NumRows() > 0) { ?>
 <ul class="DataList Discussions">
    <?php include($this->FetchViewLocation('discussions')); ?>

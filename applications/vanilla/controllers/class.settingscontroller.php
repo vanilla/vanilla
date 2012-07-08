501a052c3062214ba7e49af1b2bea06f332ed8d9
Todd Burry <todd@vanillaforums.com>
2012-07-08 13:35:07 -0400

    Revert "Trying to figure out what's going on with inf category sorting."
    This reverts commit d9a4523cd24a5e7d4294a6d4409f3614e49f8ca3.

--- applications/vanilla/controllers/class.settingscontroller.php
+++ applications/vanilla/controllers/class.settingscontroller.php
@@ -490,10 +490,6 @@ class SettingsController extends Gdn_Controller {
       // Check permission
       $this->Permission('Vanilla.Categories.Manage');
       
-      decho($this->DeliveryMethod(), 'DM');
-      decho($this->DeliveryType(), 'DT');
-      die();
-      
       // Set delivery type to true/false
 		$TransientKey = GetIncomingValue('TransientKey');
       if (Gdn::Request()->IsPostBack()) {

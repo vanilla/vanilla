301a54618134f66738e773f4c113c5765df24dc9
Todd Burry <todd@vanillaforums.com>
2012-07-13 16:34:32 -0400

    Added a proper count to the site totals module.

--- applications/dashboard/modules/class.sitetotalsmodule.php
+++ applications/dashboard/modules/class.sitetotalsmodule.php
@@ -15,13 +15,29 @@ class SiteTotalsModule extends Gdn_Module {
       $Px = Gdn::Database()->DatabasePrefix;
       $Sql = "show table status where Name in ('{$Px}User', '{$Px}Discussion', '{$Px}Comment')";
       
-      $Data = Gdn::Database()->Query($Sql, NULL, array())->ResultArray();
       $Result = array('User' => 0, 'Discussion' => 0, 'Comment' => 0);
-      foreach ($Data as $Row) {
-         $Name = StringBeginsWith($Row['Name'], $Px, TRUE, TRUE);
-         $Result[$Name] = $Row['Rows'];
+      foreach ($Result as $Name => $Value) {
+         $Result[$Name] = $this->GetCount($Name);
       }
       $this->SetData('Totals', $Result);
+   }
+   
+   protected function GetCount($Table) {
+      // Try and get the count from the cache.
+      $Key = "$Table.CountRows";
+      $Count = Gdn::Cache()->Get($Key);
+      if ($Count !== Gdn_Cache::CACHEOP_FAILURE)
+         return $Count;
+      
+      // The count wasn't in the cache so grab it from the table.
+      $Count = Gdn::SQL()
+         ->Select($Table.'ID', 'count', 'CountRows')
+         ->From($Table)
+         ->Get()->Value('CountRows');
+      
+      // Save the value to the cache.
+      Gdn::Cache()->Store($Key, $Count, array(Gdn_Cache::FEATURE_EXPIRY => 5 * 60 + mt_rand(0, 30)));
+      return $Count;
    }
    
    public function ToString() {

2822ed970ef253a7eb0fb804f3a07a82b2a0cd40
Todd Burry <todd@vanillaforums.com>
2012-07-17 14:37:19 -0400

    Made the New Discussion button work with the Vanilla.DefaultNewButton config setting.

--- applications/vanilla/views/modules/newdiscussion.php
+++ applications/vanilla/views/modules/newdiscussion.php
@@ -1,7 +1,7 @@
 <?php if (!defined('APPLICATION')) exit();
 echo '<div class="BoxButtons BoxNewDiscussion">';
 
-$Text = T('Start a New Discussion');
+$Text = T('Start a New Discussion', 'New Discussion');
 $Url = '/post/discussion'.(array_key_exists('CategoryID', $Data) ? '/'.$Data['CategoryID'] : '');
 $Css = 'Button Primary Action NewDiscussion';
 $Css .= strpos($this->CssClass, 'Big') !== FALSE ? ' BigButton' : '';
@@ -10,7 +10,7 @@ if (count($this->Buttons) == 0) {
 } else {
    // Make the core button action be the first item in the button group.
    array_unshift($this->Buttons, array('Text' => $Text, 'Url' => $Url));
-   echo ButtonGroup($this->Buttons, $this->CssClass);
+   echo ButtonGroup($this->Buttons, $this->CssClass, $this->DefaultButton);
 }
 Gdn::Controller()->FireEvent('AfterNewDiscussionButton');
 

008c29c5d1e8a26e6e7d4caa37c7a6a3aba7c626
Tim 'Thunderpuncher' Gunter <tim@vanillaforums.com>
2012-07-23 14:28:35 -0400

    Refactor the app entry point slightly.

--- index.php
+++ index.php
@@ -16,28 +16,31 @@ define('APPLICATION', 'Vanilla');
 define('APPLICATION_VERSION', '2.1a22');
 
 // Report and track all errors.
+
 error_reporting(E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR);
 ini_set('display_errors', 'on');
 ini_set('track_errors', 1);
 
 ob_start();
 
-// 1. Define the constants we need to get going.
+// Define the constants we need to get going.
+
 define('DS', '/');
 define('PATH_ROOT', getcwd());
 
-// 2. Include the bootstrap to configure the framework.
+// Include the bootstrap to configure the framework.
+
 require_once(PATH_ROOT.'/bootstrap.php');
 
-// 3. Create and configure the dispatcher.
-// TIM: Removed this change temporarily for .com hosting
-// Gdn::Authenticator()->StartAuthenticator();
+// Create and configure the dispatcher.
+
 $Dispatcher = Gdn::Dispatcher();
 
 $EnabledApplications = Gdn::ApplicationManager()->EnabledApplicationFolders();
 $Dispatcher->EnabledApplicationFolders($EnabledApplications);
 $Dispatcher->PassProperty('EnabledApplications', $EnabledApplications);
 
-// 4. Process the request.
+// Process the request.
+
 $Dispatcher->Start();
 $Dispatcher->Dispatch();

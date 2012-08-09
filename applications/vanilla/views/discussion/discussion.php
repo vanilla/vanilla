8947c58c8b7ff181bfdea436ce7b1a26986b4105
Todd Burry <todd@vanillaforums.com>
2012-08-09 12:46:01 -0400

    Add via %s in the discussion view.

--- applications/vanilla/views/discussion/discussion.php
+++ applications/vanilla/views/discussion/discussion.php
@@ -42,6 +42,10 @@ $this->EventArguments['Type'] = 'Discussion';
                ?>
             </span>
             <?php
+            // Include source if one was set
+            if ($Source = GetValue('Source', $Discussion))
+               echo ' '.Wrap(sprintf(T('via %s'), T($Source.' Source', $Source)), 'span', array('class' => 'MItem MItem-Source')).' ';
+            
             // Category
             if (C('Vanilla.Categories.Use')) {
                echo ' <span class="MItem Category">';

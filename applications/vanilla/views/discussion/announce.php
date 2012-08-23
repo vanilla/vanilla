20988d5c0000831f2d064a59761cd93fb03f4eec
Todd Burry <todd@vanillaforums.com>
2012-08-23 10:31:33 -0400

    Don't double translate /discussion/announce.

--- applications/vanilla/views/discussion/announce.php
+++ applications/vanilla/views/discussion/announce.php
@@ -8,9 +8,9 @@ echo $this->Form->Errors();
 
 echo '<div class="P">'.T('Where do you want to announce this discussion?').'</div>';
 
-echo '<div class="P">', $this->Form->Radio('Announce', sprintf(T('In <b>%s.</b>'), $this->Data('Category.Name')), array('Value' => '2')), '</div>';
-echo '<div class="P">', $this->Form->Radio('Announce', sprintf(T('In <b>%s</b> and recent discussions.'), $this->Data('Category.Name')), array('Value' => '1')), '</div>';
-echo '<div class="P">', $this->Form->Radio('Announce', T("Don't announce."), array('Value' => '0')), '</div>';
+echo '<div class="P">', $this->Form->Radio('Announce', '@'.sprintf(T('In <b>%s.</b>'), $this->Data('Category.Name')), array('Value' => '2')), '</div>';
+echo '<div class="P">', $this->Form->Radio('Announce', '@'.sprintf(T('In <b>%s</b> and recent discussions.'), $this->Data('Category.Name')), array('Value' => '1')), '</div>';
+echo '<div class="P">', $this->Form->Radio('Announce', '@'.T("Don't announce."), array('Value' => '0')), '</div>';
 
 echo '<div class="Buttons Buttons-Confirm">';
 echo $this->Form->Button('OK');

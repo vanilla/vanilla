a9b74b332cb83948bebdf969521db09428bbc3d3
lincolnwebs <lincolnwebs@gmail.com>
2012-09-06 14:02:18 -0400

    Change Log datatype error fix

--- applications/dashboard/views/log/helper_functions.php
+++ applications/dashboard/views/log/helper_functions.php
@@ -7,13 +7,12 @@ function PluralCount($Count, $Type) {
 }
 
 function OtherRecordsMeta($Data) {
-   if (!isset($Data['_Data'])) {
+   if (!GetValue('_Data', $Data)) {
       return '';
    }
    
-   
    $Result = '<div><b>'.T('Other Records').':</b></div><div>';
-   $_Data = $Data['_Data'];
+   $_Data = GetValue('_Data', $Data);
    foreach ($_Data as $Type => $Rows) {
       $Result .= '<span class="Meta"><span class="Meta-Value">'.
       PluralCount(count($Rows), $Type).

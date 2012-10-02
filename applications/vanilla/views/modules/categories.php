df2b16f0c68918f0b49dbe4d3f2ce6771a77273b
Todd Burry <todd@vanillaforums.com>
2012-10-02 07:42:21 -0400

    Change the way the category module is wrapped.

--- applications/vanilla/views/modules/categories.php
+++ applications/vanilla/views/modules/categories.php
@@ -11,8 +11,10 @@ if ($this->Data !== FALSE) {
    <h4><?php echo T('Categories'); ?></h4>
    <ul class="PanelInfo PanelCategories">
    <?php
-   echo '<li'.($OnCategories ? ' class="Active"' : '').'>'.Wrap(Anchor(Gdn_Format::Text(T('All Categories')), '/categories'), 'strong')
-      .' <span class="Aside"><span class="Count">'.BigPlural($CountDiscussions, '%s discussion').'</span></span></li>';
+   echo '<li'.($OnCategories ? ' class="Active"' : '').'>'.
+      Anchor(T('All Categories')
+      .' <span class="Aside"><span class="Count">'.BigPlural($CountDiscussions, '%s discussion').'</span></span>', '/categories', 'ItemLink')
+      .'</li>';
 
    $MaxDepth = C('Vanilla.Categories.MaxDisplayDepth');
    $DoHeadings = C('Vanilla.Categories.DoHeadings');
@@ -31,8 +33,9 @@ if ($this->Data !== FALSE) {
       if ($DoHeadings && $Category->Depth == 1) {
          echo Gdn_Format::Text($Category->Name);
       } else {
-         echo Wrap(Anchor(Gdn_Format::Text($Category->Name), CategoryUrl($Category)), 'strong')
-            .' <span class="Aside"><span class="Count">'.BigPlural($Category->CountAllDiscussions, '%s discussion').'</span></span>';
+         $CountText = ' <span class="Aside"><span class="Count">'.BigPlural($Category->CountAllDiscussions, '%s discussion').'</span></span>';
+         
+         echo Anchor(Gdn_Format::Text($Category->Name).$CountText, CategoryUrl($Category), 'ItemLink');
       }
       echo "</li>\n";
    }

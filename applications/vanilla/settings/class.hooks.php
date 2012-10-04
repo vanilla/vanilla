73ea95d3ded0d7f8e053d067927fe678a64df1d4
Tim 'Thunderpuncher' Gunter <tim@vanillaforums.com>
2012-10-04 15:47:19 -0400

    Trim comments.

--- applications/vanilla/settings/class.hooks.php
+++ applications/vanilla/settings/class.hooks.php
@@ -1,29 +1,10 @@
 <?php if (!defined('APPLICATION')) exit();
-/*
-Copyright 2008, 2009 Vanilla Forums Inc.
-This file is part of Garden.
-Garden is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-Garden is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with Garden.  If not, see <http://www.gnu.org/licenses/>.
-Contact Vanilla Forums Inc. at support [at] vanillaforums [dot] com
-*/
+ 
 /**
  * VanillaHooks Plugin
  *
  * The class.hooks.php file is essentially a giant plugin container for an app
  * that is automatically enabled when the app is.
- *
- * @package Vanilla
- */
- 
-/**
- * Defines all plugins Vanilla uses. 
- * 
- * These fire automatically when their respective events are called with 
- * $this->FireEvent('EventName') and are passed the object that calls the event by reference.
- * Plugin methods are usually defined by names in the pattern: {Class}_{Event}_Handler.
- * The Render method may be hooked into using {Class}_Render_Before. 
- * You may use "Base" as the class when hooking into Dashboard events.
  *
  * @link http://vanillaforums.org/docs/PluginQuickStart
  * @since 2.0.0

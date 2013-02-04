ba42779ad4b0cac6a9cd60457cd5c7371c372825
Todd Burry <todd@vanillaforums.com>
2013-02-03 21:37:40 -0500

    Added the NewDiscussionModule->QueryString parameter.

--- applications/vanilla/modules/class.newdiscussionmodule.php
+++ applications/vanilla/modules/class.newdiscussionmodule.php
@@ -16,6 +16,7 @@ class NewDiscussionModule extends Gdn_Module {
    public $DefaultButton;
    
    public $CssClass = 'Button Action Big Primary';
+   public $QueryString = '';
    
    public function __construct($Sender = '', $ApplicationFolder = FALSE) {
       parent::__construct($Sender, $ApplicationFolder);
@@ -29,6 +30,12 @@ class NewDiscussionModule extends Gdn_Module {
       $HasPermission = Gdn::Session()->CheckPermission('Vanilla.Discussions.Add', TRUE, 'Category', 'any');
       if (!$HasPermission)
          return '';
+      
+      if ($this->QueryString) {
+         foreach ($this->Buttons as &$Row) {
+            $Row['Url'] .= (strpos($Row['Url'], '?') !== FALSE ? '&' : '?').$this->QueryString;
+         }
+      }
       
       return parent::ToString();
    }

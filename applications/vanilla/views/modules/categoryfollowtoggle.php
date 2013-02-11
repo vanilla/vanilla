be8cc3228d4d5ba1511691f955497f51131778b5
Tim 'Thunderpuncher' Gunter <tim@vanillaforums.com>
2013-02-11 14:07:50 -0500

    Update category follow toggle module and remove it from the panel by default.

--- applications/vanilla/views/modules/categoryfollowtoggle.php
+++ applications/vanilla/views/modules/categoryfollowtoggle.php
@@ -2,17 +2,21 @@
 $ShowAllCategoriesPref = Gdn::Session()->GetPreference('ShowAllCategories');
 $Url = Gdn::Request()->Path();
 ?>
-<div class="Box CategoryFollowToggleBox">
-   <h4><?php echo T('Category Management'); ?></h4>
-   <?php if ($ShowAllCategoriesPref) { ?>
-      <p><?php echo T('CategoriesViewingAll', 'You are currently viewing all categories.'); ?></p>
-   <?php
-      echo Wrap(Anchor(T('CategoriesShowFollowed', 'Only show followed categories'), $Url.'?ShowAllCategories=false'), 'p');
-   } else {
-   ?>
-      <p><?php echo T('CategoriesViewingFollowed', 'You are currently only viewing categories that you follow.'); ?></p>
-   <?php
-      echo Wrap(Anchor(T('CategoriesShowUnfollowed', 'Show hidden categories'), $Url.'?ShowAllCategories=true'), 'p');
-   }
-   ?>
+
+<div class="CategoryFilter">
+   <div class="CategoryFilterTitle"><?php echo T('Category Filter'); ?></div>
+   <div class="CategoryFilterOptions">
+      <?php echo Wrap(T('Viewing'), 'span').': '; ?>
+      <?php 
+      if ($ShowAllCategoriesPref):
+         echo Wrap(T('all categories'), 'span', array('class' => 'CurrentFilter'));
+         echo ' | ';
+         echo Wrap(Anchor(T('followed categories'), $Url.'?ShowAllCategories=false'), 'span');
+      else:
+         echo Wrap(Anchor(T('all categories'), $Url.'?ShowAllCategories=true'), 'span');
+         echo ' | ';
+         echo Wrap(T('followed categories'), 'span', array('class' => 'CurrentFilter'));
+      endif;
+      ?>
+   </div>
 </div>

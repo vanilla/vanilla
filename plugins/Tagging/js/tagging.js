019b1de5cfaaea7f37813ecbce3528db845594ab
Tim 'Thunderpuncher' Gunter <tim@vanillaforums.com>
2013-02-18 18:34:04 -0500

    Add tagging permissions.

--- plugins/Tagging/js/tagging.js
+++ plugins/Tagging/js/tagging.js
@@ -7,4 +7,27 @@ jQuery(document).ready(function($) {
       var taglist = $(frm).find('input#Form_Tags');
       taglist.triggerHandler('BeforeSubmit',[frm]);
    });
+   
+   var tags = $("#Form_Tags").val();
+   if (tags && tags.length)
+      tags = tags.split(",");
+   
+   var TagSearch = gdn.definition('PluginsTaggingSearchUrl', false);
+   var TagAdd = gdn.definition('PluginsTaggingAdd', false);
+   $("#Form_Tags").tokenInput(TagSearch, {
+      hintText: "Start to type...",
+      searchingText: "Searching...",
+      searchDelay: 300,
+      minChars: 1,
+      maxLength: 25,
+      prePopulate: tags,
+      dataFields: ["#Form_CategoryID"],
+      onFocus: function() { $(".Help").hide(); $(".HelpTags").show(); },
+      onBlankAdd: function(hidden_input, item) {
+         if (!TagAdd) {
+            var Tag = $('.token-input-list').find('li:contains("'+item+'")');
+            Tag.addClass('not-allowed');
+         }
+      }
+  });
 });

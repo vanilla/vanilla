de0cb6a94808f45bf4c3a9690b4cce3d505399b0
Todd Burry <todd@vanillaforums.com>
2013-03-13 11:38:39 -0400

    Set the DiscussionList section on some of the ancillary discussion lists.

--- applications/vanilla/controllers/class.discussionscontroller.php
+++ applications/vanilla/controllers/class.discussionscontroller.php
@@ -315,6 +315,7 @@ class DiscussionsController extends VanillaController {
     */
    public function Bookmarked($Page = '0') {
       $this->Permission('Garden.SignIn.Allow');
+      Gdn_Theme::Section('DiscussionList');
 
       // Figure out which discussions layout to choose (Defined on "Homepage" settings page).
       $Layout = C('Vanilla.Discussions.Layout');
@@ -412,6 +413,7 @@ class DiscussionsController extends VanillaController {
     */
    public function Mine($Page = 'p1') {
       $this->Permission('Garden.SignIn.Allow');
+      Gdn_Theme::Section('DiscussionList');
       
       // Set criteria & get discussions data
       list($Offset, $Limit) = OffsetLimit($Page, C('Vanilla.Discussions.PerPage', 30));

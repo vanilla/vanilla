e38e6a90e686e48b491185dd2e48f5a99667d698
Todd Burry <todd@vanillaforums.com>
2013-03-13 16:30:16 -0400

    Tagging plugin: - Version bump for css refresh.

--- plugins/Tagging/class.tagging.plugin.php
+++ plugins/Tagging/class.tagging.plugin.php
@@ -20,7 +20,7 @@
 $PluginInfo['Tagging'] = array(
    'Name' => 'Tagging',
    'Description' => 'Users may add tags to each discussion they create. Existing tags are shown in the sidebar for navigation by tag.',
-   'Version' => '1.6.1',
+   'Version' => '1.6.2',
    'SettingsUrl' => '/dashboard/settings/tagging',
    'SettingsPermission' => 'Garden.Settings.Manage',
    'Author' => "Mark O'Sullivan",

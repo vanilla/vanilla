4dc941da49dc86d7e7b85ed3860e57b357b42c97
Tim Gunter <tim@vanillaforums.com>
2013-03-17 14:15:54 -0400

    Fix security hole allowing Format to be changed during edit.

--- applications/vanilla/controllers/class.postcontroller.php
+++ applications/vanilla/controllers/class.postcontroller.php
@@ -728,6 +728,8 @@ class PostController extends VanillaController {
          $this->Form->SetModel($this->DraftModel);
          $this->Comment = $this->DraftModel->GetID($DraftID);
       }
+      
+      $this->Form->SetFormValue('Format', GetValue('Format', $this->Comment));
       $this->View = 'editcomment';
       $this->Comment($this->Comment->DiscussionID);
    }

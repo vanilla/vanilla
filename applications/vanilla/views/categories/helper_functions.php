59a54c07aa994dd07dadb2f55466dc43ae26ea36
Todd Burry <todd@vanillaforums.com>
2013-03-18 10:44:33 -0400

    Give missing category photos a hidden class.

--- applications/vanilla/views/categories/helper_functions.php
+++ applications/vanilla/views/categories/helper_functions.php
@@ -33,7 +33,7 @@ function CategoryPhoto($Row) {
       $Result = Anchor(
          ' ',
          CategoryUrl($Row, '', '//'),
-         'Item-Icon PhotoWrap PhotoWrap-Category NoPhoto');
+         'Item-Icon PhotoWrap PhotoWrap-Category Hidden NoPhoto');
    }
    
    return $Result;

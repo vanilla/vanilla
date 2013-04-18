c8a2bd9d3d70896a45de00bed4fd96ab50b280ab
Todd Burry <todd@vanillaforums.com>
2013-04-17 20:17:11 -0400

    Fix port typo.

--- library/core/class.request.php
+++ library/core/class.request.php
@@ -818,7 +818,7 @@ class Gdn_Request {
       
       $Port = $this->Port();
       $Host = $this->Host();
-      if (!in_array($Host, array(80, 443)))
+      if (!in_array($Port, array(80, 443)))
          $Host .= ':'.$Port;
       
       if ($WithDomain === '//') {

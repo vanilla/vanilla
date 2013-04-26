7c0730ac4eee1bf96a0f7ff23c04ac0b4ea5af7c
Tim Gunter <tim@vanillaforums.com>
2013-04-26 10:12:37 -0400

    Make ProxyRequest compatible with latest cURL.

--- library/core/class.proxyrequest.php
+++ library/core/class.proxyrequest.php
@@ -394,7 +394,7 @@ class ProxyRequest {
       if ($this->UseSSL) {
          $this->Action(" Using SSL");
          curl_setopt($Handler, CURLOPT_SSL_VERIFYPEER, !$SSLNoVerify);
-         curl_setopt($Handler, CURLOPT_SSL_VERIFYHOST, !$SSLNoVerify);
+         curl_setopt($Handler, CURLOPT_SSL_VERIFYHOST, $SSLNoVerify ? 2 : 0);
       }
 
       if ($Timeout > 0)

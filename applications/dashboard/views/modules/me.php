8753802369da5c2ee1427178821d4c3a5c4d2402
lincolnwebs <lincolnwebs@gmail.com>
2013-05-17 10:21:08 -0400

    Spam Queue: Remove alert counters in MeModule and Dash Sidemenu

--- applications/dashboard/views/modules/me.php
+++ applications/dashboard/views/modules/me.php
@@ -9,9 +9,9 @@ $DashboardCount = 0;
 // Spam & Moderation Queue
 if ($Session->CheckPermission('Garden.Settings.Manage') || $Session->CheckPermission('Garden.Moderation.Manage')) {
    $LogModel = new LogModel();
-   $SpamCount = $LogModel->GetOperationCount('spam');
+   //$SpamCount = $LogModel->GetOperationCount('spam');
    $ModerationCount = $LogModel->GetOperationCount('moderate');
-   $DashboardCount += $SpamCount + $ModerationCount;
+   $DashboardCount += $ModerationCount;
 }
 // Applicant Count
 if ($Session->CheckPermission('Garden.Users.Approve')) {
@@ -71,7 +71,7 @@ if ($Session->IsValid()):
                if ($Session->CheckPermission('Garden.Settings.Manage') || $Session->CheckPermission('Garden.Moderation.Manage')) {
                   echo Wrap('<hr />', 'li');
                   $CApplicant = $ApplicantCount > 0 ? ' '.Wrap($ApplicantCount, 'span class="Alert"') : '';
-                  $CSpam = $SpamCount > 0 ? ' '.Wrap($SpamCount, 'span class="Alert"') : '';
+                  $CSpam = ''; //$SpamCount > 0 ? ' '.Wrap($SpamCount, 'span class="Alert"') : '';
                   $CModeration = $ModerationCount > 0 ? ' '.Wrap($ModerationCount, 'span class="Alert"') : '';
                   echo Wrap(Anchor(Sprite('SpApplicants').' '.T('Applicants').$CApplicant, '/dashboard/user/applicants'), 'li');
                   echo Wrap(Anchor(Sprite('SpSpam').' '.T('Spam Queue').$CSpam, '/dashboard/log/spam'), 'li');

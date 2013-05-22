1bc3bbeebbb08e0358e9e761b2929e17b3cffd8e
Todd Burry <todd@vanillaforums.com>
2013-05-22 10:21:25 -0400

    Make {activity_link} consistent with the other links.

--- library/vendors/SmartyPlugins/function.activity_link.php
+++ library/vendors/SmartyPlugins/function.activity_link.php
@@ -12,7 +12,8 @@ Contact Vanilla Forums Inc. at support [at] vanillaforums [dot] com
 /**
  */
 function smarty_function_activity_link($Params, &$Smarty) {
+   $Wrap = GetValue('wrap', $Params, 'li');
    return Gdn_Theme::Link('activity',
       GetValue('text', $Params, T('Activity')),
-      GetValue('format', $Params, '<li><a href="%url" class="%class">%text</a></li>'));
+      GetValue('format', $Params, Wrap('<a href="%url" class="%class">%text</a>', $Wrap)));
 }

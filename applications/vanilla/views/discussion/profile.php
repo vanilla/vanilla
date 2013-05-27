12413365d0e252c126fd2bdc92f7b2b3c62999dc
Todd Burry <todd@vanillaforums.com>
2013-05-26 22:26:20 -0400

    - Add some DataListWrap and FormTitleWrappers. - Change the profile option page headings into h1. - Use some DismissMessage divs instead of the generic ones.

--- applications/vanilla/views/discussion/profile.php
+++ applications/vanilla/views/discussion/profile.php
@@ -1,10 +1,14 @@
 <?php if (!defined('APPLICATION')) exit();
+
+echo '<div class="DataListWrap">';
 echo '<h2 class="H">'.T('Comments').'</h2>';
+echo '<ul class="DataList SearchResults">';
+
 if (sizeof($this->Data('Comments'))) {
-   echo '<ul class="DataList SearchResults">';
    echo $this->FetchView('profilecomments', 'Discussion', 'Vanilla');
-   echo '</ul>';
    echo $this->Pager->ToString('more');
 } else {
-   echo '<div class="Empty">'.T('This user has not commented yet.').'</div>';
+   echo '<li class="Item Empty">'.T('This user has not commented yet.').'</li>';
 }
+echo '</ul>';
+echo '</div>';

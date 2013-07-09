6f0f0a8f722590f0e7f439712f9834937c03b23c
Tim Gunter <tim@vanillaforums.com>
2013-07-09 11:48:43 -0400

    ButtonBar: Fix sprites - transparent

--- plugins/ButtonBar/class.buttonbar.plugin.php
+++ plugins/ButtonBar/class.buttonbar.plugin.php
@@ -12,7 +12,7 @@
 $PluginInfo['ButtonBar'] = array(
    'Name' => 'Button Bar',
    'Description' => 'Adds several simple buttons above comment boxes, allowing additional formatting.',
-   'Version' => '1.5',
+   'Version' => '1.6',
    'MobileFriendly' => TRUE,
    'RequiredApplications' => array('Vanilla' => '2.1'),
    'RequiredTheme' => FALSE, 

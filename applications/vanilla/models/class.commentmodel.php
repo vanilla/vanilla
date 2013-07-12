9ec5bc9251b7b6062d88da15b2d4da8bd6eb443d
Todd Burry <todd@vanillaforums.com>
2013-07-11 23:02:06 -0400

    Revert "Disable user comment counts..."
    This reverts commit 6dcdf6b7914885f7f3741ef9ea2dcb90ca3250b7.

--- applications/vanilla/models/class.commentmodel.php
+++ applications/vanilla/models/class.commentmodel.php
@@ -1137,8 +1137,6 @@ class CommentModel extends VanillaModel {
     * @param int $DiscussionID Unique ID of the discussion we are updating.
     */
    public function UpdateUserCommentCounts($DiscussionID) {
-      return false;
-      
       $Sql = "update ".$this->Database->DatabasePrefix."UserDiscussion ud
          set CountComments = (
             select count(c.CommentID)+1 

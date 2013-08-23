43a17f79652fe161e83a2b1a4cd9fe7725f089cc
Tim Gunter <tim@vanillaforums.com>
2013-08-23 17:17:07 -0400

    Twitter: Attempt to use passed twttr reference to avoid race condition

--- plugins/Twitter/js/twitter.js
+++ plugins/Twitter/js/twitter.js
@@ -5,35 +5,31 @@
  * @author Tim Gunter <tim@vanillaforums.com>
  */
 
-jQuery.ready(function($){
+window.twttr = (function (d,s,id) {
+  var t, js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return; js=d.createElement(s); js.id=id;
+  js.src="https://platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs);
+  return window.twttr || (t = { _e: [], ready: function(f){ t._e.push(f) } });
+}(document, "script", "twitter-wjs"));
 
-   window.twttr = (function (d,s,id) {
-      var t, js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return; js=d.createElement(s); js.id=id;
-      js.src="https://platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs);
-      return window.twttr || (t = { _e: [], ready: function(f){ t._e.push(f) } });
-   }(document, "script", "twitter-wjs"));
+twttr.ready(function(twttr){
 
-   twttr.ready(function(){
+   $('div.twitter-card').each(function(i, el){
+      var card = $(el);
+      var tweetUrl = card.attr('data-tweeturl');
+      var tweetID = card.attr('data-tweetid');
+      var cardref = card.get(0);
 
-      $('div.twitter-card').each(function(i, el){
-         var card = $(el);
-         var tweetUrl = card.attr('data-tweeturl');
-         var tweetID = card.attr('data-tweetid');
-         var cardref = card.get(0);
-
-         twttr.widgets.createTweet(
-            tweetID,
-            cardref,
-            function(el){ card.children('a').first().css('display','none'); },
-            {
-               conversation: "none",
-               align: "center"
-            }
-         );
-
-      });
+      twttr.widgets.createTweet(
+         tweetID,
+         cardref,
+         function(el){ card.children('a').first().css('display','none'); },
+         {
+            conversation: "none",
+            align: "center"
+         }
+      );
 
    });
-   
+
 });

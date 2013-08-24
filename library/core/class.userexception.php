3e8a5e62c6ce185597a99e1b22de43bbed37f411
Todd Burry <todd@vanillaforums.com>
2013-08-24 19:48:05 -0400

    Remove previous from user exception. Fixes issue #1255.

--- library/core/class.userexception.php
+++ library/core/class.userexception.php
@@ -19,7 +19,7 @@ class Gdn_UserException extends Exception {
 	 * @param string $Message A user readable message for the exception.
 	 * @param Exception $Previous The previous exception used for exception chaining.
 	 */
-   public function __construct($Message, $Code = 400, $Previous = NULL) {
-		parent::__construct($Message, $Code, $Previous);
+   public function __construct($Message, $Code = 400) {
+		parent::__construct($Message, $Code);
 	}
 }

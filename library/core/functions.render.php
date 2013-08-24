884c9d0ce225773f5f3f6f10f4403825b24e5dfe
lincolnwebs <lincolnwebs@gmail.com>
2013-08-24 17:13:00 -0400

    Extra  check to match 2.2

--- library/core/functions.render.php
+++ library/core/functions.render.php
@@ -684,7 +684,7 @@ if (!function_exists('UserPhoto')) {
       $Name = GetValue('Name', $User);
       $Title = htmlspecialchars(GetValue('Title', $Options, $Name));
       
-      if ($FullUser['Banned']) {
+      if ($FullUser && $FullUser['Banned']) {
          $Photo = C('Garden.BannedPhoto', 'http://cdn.vanillaforums.com/images/banned_large.png');;
          $Title .= ' ('.T('Banned').')';
       }

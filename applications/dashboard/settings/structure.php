9acd97fbd34d5f46a45746a714aa9de7fac0eb1b
Todd Burry <todd@vanillaforums.com>
2013-08-29 10:39:58 -0400

    Save Garden.InputFormatter to the config so we can change the default later. Fixes issue #1679.

--- applications/dashboard/settings/structure.php
+++ applications/dashboard/settings/structure.php
@@ -722,6 +722,10 @@ $Construct
    ->Column('NewUserID', 'int')
    ->Set();
 
+// Save the current input formatter to the user's config.
+// This will allow us to change the default later and grandfather existing forums in.
+SaveToConfig('Garden.InputFormatter', C('Garden.InputFormatter'));
+
 // Make sure the smarty folders exist.
 if (!file_exists(PATH_CACHE.'/Smarty')) @mkdir(PATH_CACHE.'/Smarty');
 if (!file_exists(PATH_CACHE.'/Smarty/cache')) @mkdir(PATH_CACHE.'/Smarty/cache');

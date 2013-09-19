205532aa5221eb958c4d925111f3c33164b0006c
Todd Burry <todd@vanillaforums.com>
2013-09-19 10:57:08 -0400

    Place the activity & wall forms into a .FormWrapper-Condensed.

--- applications/dashboard/views/activity/all.php
+++ applications/dashboard/views/activity/all.php
@@ -7,11 +7,17 @@ include_once $this->FetchViewLocation('helper_functions');
 $this->FireEvent('BeforeStatusForm');
 $Session = Gdn::Session();
 if ($Session->CheckPermission('Garden.Profiles.Edit')) {
+   echo '<div class="FormWrapper FormWrapper-Condensed">';
    echo $this->Form->Open(array('action' => Url('/activity/post/'.$this->Data('Filter')), 'class' => 'Activity'));
    echo $this->Form->Errors();
    echo Wrap($this->Form->TextBox('Comment', array('MultiLine' => TRUE)), 'div', array('class' => 'TextBoxWrapper'));
+   
+   echo '<div class="Buttons">';
    echo $this->Form->Button('Share', array('class' => 'Button Primary'));
+   echo '</div>';
+   
    echo $this->Form->Close();
+   echo '</div>';
 }
 echo '</div>';
 echo '<ul class="DataList Activities">';
--- applications/dashboard/views/profile/activity.php
+++ applications/dashboard/views/profile/activity.php
@@ -6,11 +6,17 @@ $Session = Gdn::Session();
 if ($Session->IsValid() && CheckPermission('Garden.Profiles.Edit')) {
    $this->FireEvent('BeforeStatusForm');      
    $ButtonText = $Session->UserID == $this->User->UserID ? 'Share' : 'Add Comment';
+   
+   
+   echo '<div class="FormWrapper FormWrapper-Condensed">';
    echo $this->Form->Open(array('action' => Url("/activity/post/{$this->User->UserID}?Target=".urlencode(UserUrl($this->User))), 'class' => 'Activity'));
    echo $this->Form->Errors();
    echo Wrap($this->Form->BodyBox('Comment'), 'div', array('class' => 'TextBoxWrapper'));
+   echo '<div class="Buttons">';
    echo $this->Form->Button($ButtonText, array('class' => 'Button Primary'));
+   echo '</div>';
    echo $this->Form->Close();
+   echo '</div>';
 }
 
 // Include the activities

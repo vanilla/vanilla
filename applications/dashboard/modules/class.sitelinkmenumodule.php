3db6ffaad49827c3566cca2b5635d5d89010174f
Todd Burry <todd@vanillaforums.com>
2013-10-27 15:56:18 -0400

    Added the SiteLinkMenuModule. This is a global menu for the entire site.

---
+++ applications/dashboard/modules/class.sitelinkmenumodule.php
@@ -0,0 +1,32 @@
+<?php if (!defined('APPLICATION')) exit();
+
+/**
+ * A module for a list of links.
+ * 
+ * @author Todd Burry <todd@vanillaforums.com> 
+ * @copyright 2003 Vanilla Forums, Inc
+ * @license http://www.opensource.org/licenses/gpl-2.0.php GPL
+ * @package Garden
+ * @since 2.3
+ */
+
+/**
+ * 
+ */
+class SiteLinkMenuModule extends LinkMenuModule {
+   /// Properties ///
+   
+   
+   /// Methods ///
+   
+   public function render() {
+      // The module contains different links depending on its section.
+      if (InSection('Profile')) {
+         $this->FireEvent('Profile');
+      } else {
+         $this->FireEvent('Site');
+      }
+      
+      parent::render();
+   }
+}

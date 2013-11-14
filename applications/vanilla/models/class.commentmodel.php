8f756f331c68eb9f65004c4ed7ae767f812d77c5
R-J <robin.jurinka@gmx.net>
2013-11-14 17:43:39 +0100

    Update class.commentmodel.php
    Check permissions when using advanced notifications
    Partly fixing https://github.com/vanillaforums/Garden/pull/1740

--- applications/vanilla/models/class.commentmodel.php
+++ applications/vanilla/models/class.commentmodel.php
@@ -1018,6 +1018,10 @@ class CommentModel extends VanillaModel {
             continue;
          
          $UserID = $Row['UserID'];
+         // Check user can still see the discussion.
+         if (!Gdn::UserModel()->GetCategoryViewPermission($UserID, $Category['CategoryID']))
+            continue;
+
          $Name = $Row['Name'];
          if (strpos($Name, '.Email.') !== FALSE) {
             $NotifyUsers[$UserID]['Emailed'] = ActivityModel::SENT_PENDING;

fba5e076262991c22a9cc1eb8a8065b758d8df1c
R-J <robin.jurinka@gmx.net>
2013-11-14 17:34:16 +0100

    Update class.discussionmodel.php
    Check for permissions when notifying of new discussions
    Partly fixing https://github.com/vanillaforums/Garden/issues/1518

--- applications/vanilla/models/class.discussionmodel.php
+++ applications/vanilla/models/class.discussionmodel.php
@@ -1735,6 +1735,10 @@ class DiscussionModel extends VanillaModel {
             continue;
 
          $UserID = $Row['UserID'];
+         // Check user can still see the discussion.
+         if (!Gdn::UserModel()->GetCategoryViewPermission($UserID, $Category['CategoryID']))
+            continue;
+            
          $Name = $Row['Name'];
          if (strpos($Name, '.Email.') !== FALSE) {
             $NotifyUsers[$UserID]['Emailed'] = ActivityModel::SENT_PENDING;

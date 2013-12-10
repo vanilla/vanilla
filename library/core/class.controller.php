e99402c552c927be7a9d5880a726db46e8c4f813
Tim Gunter <tim@vanillaforums.com>
2013-12-10 18:54:01 -0500

    Fix controller

--- library/core/class.controller.php
+++ library/core/class.controller.php
@@ -855,61 +855,6 @@ class Gdn_Controller extends Gdn_Pluggable {
       else if ($this->SyndicationMethod == SYNDICATION_RSS)
          $View .= '_rss';
 
-      $ViewPath2 = ViewLocation($View, $ControllerName, $ApplicationFolder);
-
-      $LocationName = ConcatSep('/', strtolower($ApplicationFolder), $ControllerName, $View);
-      $ViewPath = ArrayValue($LocationName, $this->_ViewLocations, FALSE);
-      if ($ViewPath === FALSE) {
-         // Define the search paths differently depending on whether or not we are in a plugin or application.
-         $ApplicationFolder = trim($ApplicationFolder, '/');
-         if (StringBeginsWith($ApplicationFolder, 'plugins/')) {
-            $KeyExplode = explode('/',$ApplicationFolder);
-            $PluginName = array_pop($KeyExplode);
-            $PluginInfo = Gdn::PluginManager()->GetPluginInfo($PluginName);
-
-            $BasePath = GetValue('SearchPath', $PluginInfo);
-            $ApplicationFolder = GetValue('Folder', $PluginInfo);
-         } else {
-            $BasePath = PATH_APPLICATIONS;
-            $ApplicationFolder = strtolower($ApplicationFolder);
-         }
-
-         $SubPaths = array();
-         // Define the subpath for the view.
-         // The $ControllerName used to default to '' instead of FALSE.
-         // This extra search is added for backwards-compatibility.
-         if (strlen($ControllerName) > 0)
-            $SubPaths[] = "views/$ControllerName/$View";
-         else {
-            $SubPaths[] = "views/$View";
-
-            $SubPaths[] = 'views/'.StringEndsWith($this->ControllerName, 'Controller', TRUE, TRUE)."/$View";
-         }
-
-         // Views come from one of four places:
-         $ViewPaths = array();
-
-         // 1. An explicitly defined path to a view
-         if (strpos($View, DS) !== FALSE)
-            $ViewPaths[] = $View;
-
-         if ($this->Theme) {
-            // 2. Application-specific theme view. eg. /path/to/application/themes/theme_name/app_name/views/controller_name/
-            foreach ($SubPaths as $SubPath) {
-               $ViewPaths[] = PATH_THEMES."/{$this->Theme}/$ApplicationFolder/$SubPath.*";
-               // $ViewPaths[] = CombinePaths(array(PATH_THEMES, $this->Theme, $ApplicationFolder, 'views', $ControllerName, $View . '.*'));
-            }
-
-            // 3. Garden-wide theme view. eg. /path/to/application/themes/theme_name/views/controller_name/
-            foreach ($SubPaths as $SubPath) {
-               $ViewPaths[] = PATH_THEMES."/{$this->Theme}/$SubPath.*";
-               //$ViewPaths[] = CombinePaths(array(PATH_THEMES, $this->Theme, 'views', $ControllerName, $View . '.*'));
-            }
-         }
-
-         // 4. Application/plugin default. eg. /path/to/application/app_name/views/controller_name/
-         foreach ($SubPaths as $SubPath) {
-            $ViewPaths[] = "$BasePath/$ApplicationFolder/$SubPath.*";
       $ViewPath = ViewLocation($View, $ControllerName, $ApplicationFolder);
 
       // echo '<div>['.$LocationName.'] RETURNS ['.$ViewPath.']</div>';

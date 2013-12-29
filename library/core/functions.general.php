63849a99db8ebde3acc6143aaf39b8908e48922a
lincolnwebs <lincolnwebs@gmail.com>
2013-12-29 15:10:19 -0500

    Remove E_DEPRECATED from Debug mode

--- library/core/functions.general.php
+++ library/core/functions.general.php
@@ -666,7 +666,7 @@ if (!function_exists('Debug')) {
 
       $Debug = $Value;
       if ($Debug) {
-         error_reporting(E_ALL & ~E_STRICT);
+         error_reporting(E_ALL & ~E_STRICT & ~E_DEPRECATED);
          ini_set('display_errors', 1);
       } else
          error_reporting(E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR);

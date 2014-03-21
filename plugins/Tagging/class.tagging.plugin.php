a2f7fd7f273df80f8c687d27593bf8189ee74fa5
Kasper Isager <kasperisager@gmail.com>
2014-03-21 19:56:20 -0400

    Bump Tagging to 1.8.7

--- plugins/Tagging/class.tagging.plugin.php
+++ plugins/Tagging/class.tagging.plugin.php
@@ -23,7 +23,7 @@
 $PluginInfo['Tagging'] = array(
    'Name' => 'Tagging',
    'Description' => 'Users may add tags to each discussion they create. Existing tags are shown in the sidebar for navigation by tag.',
-   'Version' => '1.8.6',
+   'Version' => '1.8.7',
    'SettingsUrl' => '/dashboard/settings/tagging',
    'SettingsPermission' => 'Garden.Settings.Manage',
    'Author' => "Mark O'Sullivan",

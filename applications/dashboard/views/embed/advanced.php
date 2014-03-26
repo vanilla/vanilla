7fd488311e6a467ea40e1b4dd1715a5d1f3edc2d
Angelo D'Agostino <angelo.dagostino@kemat1an.it>
2014-03-26 12:42:48 +0100

    Fixed T() string
    Replaced T() string in one line with sprintf.

--- applications/dashboard/views/embed/advanced.php
+++ applications/dashboard/views/embed/advanced.php
@@ -23,31 +23,24 @@ p.WarningMessage {
 </style>
 <h1><?php echo T('Advanced Embed Settings'); ?></h1>
 <div class="Info">
-   <?php
-   echo sprintf(T('The following settings apply to all types of embedded forum content: '));
-   echo Anchor(T('blog comments'), 'embed/comments');
-   echo sprintf(T(', and '));
-   echo Anchor(T('forum embedding'), 'embed/forum');
-   echo '.';
-   if (!$AllowEmbed) {
-      echo Wrap('<span style="background: #ff0;">'.T('Embedding is currently DISABLED.').'</span>', 'p');
-      echo Anchor(T('Enable Embedding'), 'embed/advanced/enable/'.Gdn::Session()->TransientKey(), 'SmallButton');
-   } else {
-      echo Wrap('<span style="background: #ff0;">'.T('Embedding is currently ENABLED.').'</span>', 'p');
-      echo Anchor(T('Disable Embedding'), 'embed/advanced/disable/'.Gdn::Session()->TransientKey(), 'SmallButton');
-   ?>
+	<?php echo sprintf(T('The following settings apply to all types of embedded forum content: %s and %s.'), Anchor(T('blog comments'), 'embed/comments'), Anchor(T('forum embedding'), 'embed/forum'));
+	if (!$AllowEmbed) {
+	echo Wrap('<span style="background: #ff0;">'.T('Embedding is currently DISABLED.').'</span>', 'p');
+	echo Anchor(T('Enable Embedding'), 'embed/advanced/enable/'.Gdn::Session()->TransientKey(), 'SmallButton');
+	}
+	else {
+	echo Wrap('<span style="background: #ff0;">'.T('Embedding is currently ENABLED.').'</span>', 'p');
+	echo Anchor(T('Disable Embedding'), 'embed/advanced/disable/'.Gdn::Session()->TransientKey(), 'SmallButton');
+	?>
 </div>
 <h1><?php echo T('Settings'); ?></h1>
 <div class="Info">
    <h2><?php echo T('Trusted Domains'); ?></h2>
-   <p><?php echo T('WhiteListDescription', "You can optionally specify a white-list of trusted domains (ie.
-   yourdomain.com) that are allowed to embed elements of your community
-   (forum, comments, or modules)."); ?></p>
+   <p><?php echo T('You can optionally specify a white-list of trusted domains (ie. yourdomain.com) that are allowed to embed elements of your community (forum, comments, or modules).'); ?></p>
    <p><small>
       <strong><?php echo T('Notes:'); ?></strong>
       <?php echo T('Specify one domain per line, without protocol (ie. yourdomain.com).'); ?>
-      <br /><?php echo T('TrustedDomainDescription', "The domain will include all subdomains (ie. yourdomain.com will
-      also allow blog.yourdomain.com, news.yourdomain.com, etc)"); ?>
+      <br /><?php echo T('The domain will include all subdomains (ie. yourdomain.com will also allow blog.yourdomain.com, news.yourdomain.com, etc.).'); ?>
       <br /><?php echo T('Leaving this input blank will mean that you allow embedding on any site, anywhere.'); ?>
    </small></p>
    <?php

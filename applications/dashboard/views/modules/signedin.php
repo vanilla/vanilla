9af87d50eaae01cba21fbc7f794782ff0e08f26a
Angelo D'Agostino <angelo.dagostino@kemat1an.it>
2014-03-28 15:25:44 +0100

    Translation, Received and Notifications Count
    Fixed Received and Notification Count, added T() for My Profile string.

--- applications/dashboard/views/modules/signedin.php
+++ applications/dashboard/views/modules/signedin.php
@@ -12,13 +12,13 @@ if ($Session->IsValid() && C('Garden.Modules.ShowSignedInModule')) {
 
 ?>
 <div class="Box ProfileBox">
-   <h4>My Profile</h4>
+   <h4><?php echo T('My Profile'); ?></h4>
    <ul class="PanelInfo">
       <li><?php echo Anchor($Name, 'profile/'.$Session->User->UserID.'/'.Gdn_Format::Url($Name)); ?></li>
       <?php if (C('EnabledApplications.Conversations')) { ?>
-      <li><?php echo Anchor(T('Inbox'), '/messages/all'); ?><span class="Aside"><span class="Count"><?php echo $CountInbox; ?></span></span></li>
+      <li><span class="Aside"><span class="Count"><?php echo $CountInbox; ?></span></span><?php echo Anchor(T('Inbox'), '/messages/all'); ?></li>
       <?php } ?>
-      <li><?php echo Anchor(T('Notifications'), '/profile/notifications'); ?><span class="Aside"><span class="Count"><?php echo $CountNotifications; ?></span></span></li>
+      <li><span class="Aside"><span class="Count"><?php echo $CountNotifications; ?></span></span><?php echo Anchor(T('Notifications'), '/profile/notifications'); ?></li>
       <?php if ($Session->CheckPermission('Garden.Settings.Manage')) { ?>
       <li><?php echo Anchor(T('Dashboard'), '/dashboard/settings'); ?></li>
       <?php } ?>

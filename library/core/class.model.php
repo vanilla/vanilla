5ba68e29c1c9057e9eae0e2cb6da8fff4b3f47f5
lincolnwebs <lincolnwebs@gmail.com>
2014-04-18 11:24:59 -0400

    Filter empty integers for MySQL strict mode

--- library/core/class.model.php
+++ library/core/class.model.php
@@ -251,9 +251,15 @@ class Gdn_Model extends Gdn_Pluggable {
          // Quote all of the fields.
          $QuotedFields = array();
          foreach ($Fields as $Name => $Value) {
-            if (is_array($Value) && in_array($Name, array('Attributes', 'Data')))
+            if (is_array($Value) && in_array($Name, array('Attributes', 'Data'))) {
                $Value = empty($Value) ? NULL : serialize($Value);
-            
+            }
+
+            // Make sure integers are not empty for MySQL strict mode.
+            if (empty($Value) && stristr($SchemaFields[$Name]->Type, 'int') !== FALSE) {
+               $Value = 0;
+            }
+
             $QuotedFields[$this->SQL->QuoteIdentifier(trim($Name, '`'))] = $Value;
          }
 
@@ -288,8 +294,14 @@ class Gdn_Model extends Gdn_Pluggable {
          // Quote all of the fields.
          $QuotedFields = array();
          foreach ($Fields as $Name => $Value) {
-            if (is_array($Value) && in_array($Name, array('Attributes', 'Data')))
+            if (is_array($Value) && in_array($Name, array('Attributes', 'Data'))) {
                $Value = empty($Value) ? NULL : serialize($Value);
+            }
+
+            // Make sure integers are not empty for MySQL strict mode.
+            if (empty($Value) && stristr($SchemaFields[$Name]->Type, 'int') !== FALSE) {
+               $Value = 0;
+            }
             
             $QuotedFields[$this->SQL->QuoteIdentifier(trim($Name, '`'))] = $Value;
          }

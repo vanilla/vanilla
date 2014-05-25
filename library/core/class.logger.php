3dad0d7e4f26de2bf582b216e8a3269a35bdd700
Todd Burry <todd@vanillaforums.com>
2014-05-25 15:10:05 -0400

    Set a default Logger::$logLevel.

--- library/core/class.logger.php
+++ library/core/class.logger.php
@@ -95,6 +95,8 @@ class Logger {
             throw new Exception("Invalid log level $value.", 422);
          }
          self::$logLevel = $value;
+      } elseif ($value === null) {
+         $value = LogLevel::NOTICE;
       }
       return self::$logLevel;
    }

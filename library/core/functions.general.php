5c6b8256120bafe4df65407fb0f7dae76f09329b
Todd Burry <todd@vanillaforums.com>
2014-05-25 21:04:53 -0400

    Set the log level when calling Debug().

--- library/core/functions.general.php
+++ library/core/functions.general.php
@@ -778,12 +778,19 @@ if (!function_exists('Debug')) {
       if ($Value === NULL)
          return $Debug;
 
+      $Changed = $Debug != $Value;
       $Debug = $Value;
       if ($Debug) {
          error_reporting(E_ALL & ~E_STRICT & ~E_DEPRECATED);
          ini_set('display_errors', 1);
-      } else
+         Logger::logLevel(LogLevel::DEBUG);
+      } else {
          error_reporting(E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR);
+
+         if ($Changed) {
+            Logger::logLevel(C('Garden.LogLevel', LogLevel::INFO));
+         }
+      }
    }
 }
 

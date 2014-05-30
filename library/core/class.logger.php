11156fdc2965f25b1efcd568d985852cdbf65c04
Todd Burry <todd@vanillaforums.com>
2014-05-29 22:23:34 -0400

    Lowercase Logger fields.

--- library/core/class.logger.php
+++ library/core/class.logger.php
@@ -62,7 +62,7 @@ class Logger {
     * @param array $context
     */
    public static function event($event, $level, $message, $context = array()) {
-      $context['Event'] = $event;
+      $context['event'] = $event;
       static::log($level, $message, $context);
    }
 
@@ -174,13 +174,13 @@ class Logger {
 
       // Add default fields to the context if they don't exist.
       $defaults = array(
-         'InsertUserID' => Gdn::Session()->UserID,
-         'InsertName' => val("Name", Gdn::Session()->User, 'anonymous'),
-         'InsertIPAddress' => Gdn::Request()->IpAddress(),
-         'TimeInserted' => time(),
-         'Method' => Gdn::Request()->RequestMethod(),
-         'Domain' => rtrim(Url('/', true), '/'),
-         'Path' => Url('', '/')
+         'userid' => Gdn::Session()->UserID,
+         'username' => val("Name", Gdn::Session()->User, 'anonymous'),
+         'ip' => Gdn::Request()->IpAddress(),
+         'timestamp' => time(),
+         'method' => Gdn::Request()->RequestMethod(),
+         'domain' => rtrim(Url('/', true), '/'),
+         'path' => Url('', '/')
       );
       $context = $context + $defaults;
       static::getLogger()->log($level, $message, $context);

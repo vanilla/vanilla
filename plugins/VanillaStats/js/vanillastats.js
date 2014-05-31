86ca31396e7d50ff99bfc00362ce5a861d3d5519
Todd Burry <todd@vanillaforums.com>
2014-05-30 22:45:24 -0400

    Remove the dashboard summaries.
    The dashboard summaries use unoptimized queries and take down larger sites.

--- plugins/VanillaStats/js/vanillastats.js
+++ plugins/VanillaStats/js/vanillastats.js
@@ -11,9 +11,6 @@ jQuery(document).ready(function($){
       if ($('#Content h1 span').length == 0)
          $('<span class="TinyProgress"></span>').appendTo('#Content h1:last');
 
-      if ($('div.DashboardSummaries div.Loading').length == 0)
-         $('div.DashboardSummaries').html('<div class="Loading"></div>');
-
       // Grab the installation id, version, and token
       var vanillaId = $('input.VanillaID').val();
       var vanillaVersion = $('input.VanillaVersion').val();
@@ -37,18 +34,6 @@ jQuery(document).ready(function($){
       // LOCAL QUERY
       var range = $('input.Range').val();
       var dateRange = $('input.DateRange').val();
-
-      $.ajax({
-         url: gdn.url('/index.php?p=/dashboard/settings/dashboardsummaries&DeliveryType=VIEW&Range='+range+'&DateRange='+dateRange),
-         success: function(data) {
-            $('div.DashboardSummaries').html(data);
-         },
-         error: function(xhr, status, error) {
-            $('div.DashboardSummaries').html('<div class="NoStats">Remote Analytics Server request failed.</div>');
-            $('div.DashboardSummaries div.NoStats').css('padding','10px');
-         },
-         timeout: 15000 // 15 seconds in ms
-      });
 
       // Remove Spinners
       $('#Content h1 span.TinyProgress').remove();

764b8b400e546dfa92e55f2a3f2ecde79636a91a
lincolnwebs <lincolnwebs@gmail.com>
2014-06-28 18:20:48 -0400

    Fix pager inconsistencies between discussions and categories

--- applications/vanilla/controllers/class.discussionscontroller.php
+++ applications/vanilla/controllers/class.discussionscontroller.php
@@ -158,6 +158,7 @@ class DiscussionsController extends VanillaController {
          $CountDiscussions,
          'discussions/%1$s'
       );
+      PagerModule::Current($this->Pager);
       if (!$this->Data('_PagerUrl'))
          $this->SetData('_PagerUrl', 'discussions/{Page}');
       $this->SetData('_Page', $Page);

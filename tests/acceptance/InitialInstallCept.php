ecb346e3d417c9fa686d52f988ba72c626b06ab1
John Ashton <john.robert.ashton@gmail.com>
2014-07-22 12:03:27 -0400

    Codeception: WIP Initial Commit

---
+++ tests/acceptance/InitialInstallCept.php
@@ -0,0 +1,12 @@
+<?php
+
+exec('git clone vanilla/vanilla ./frontend');
+
+//$I = new AcceptanceTester($scenario);
+//$I->wantTo('log in as regular user');
+//$I->amOnPage('/entry/signin');
+//$I->fillField('Email','admin');
+//$I->fillField('Password','admin');
+//$I->click('Sign_In');
+//$I->see('admin');
+

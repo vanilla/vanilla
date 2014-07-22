5ed35755e40b30e95e530e01f3ac3d63639faeb8
John Ashton <john.robert.ashton@gmail.com>
2014-07-22 12:14:15 -0400

    Codeception: WIP

---
+++ tests/acceptance/000_InstallCest.php
@@ -0,0 +1,44 @@
+<?php
+use \AcceptanceTester;
+
+class InstallCest {
+
+    public function _before() {
+
+        exec('mysql -u' . MYSQL_USER . ' -p' . MYSQL_PASSWORD . ' -e "drop database if exists ' . MYSQL_DATABASE . ' "');
+        exec('mysql -u' . MYSQL_USER . ' -p' . MYSQL_PASSWORD . ' -e "create database if not exists ' . MYSQL_DATABASE . ' "');
+        @exec('rm ' .  __DIR__ . '/../../conf/config.php');
+
+    }
+
+    public function _after() {
+    }
+
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function setupTest(AcceptanceTester $I) {
+
+
+        $I->wantTo('setup');
+        $I->amOnPage('/');
+        $I->see('Installer');
+
+        $I->fillField('Database-dot-Host', MYSQL_HOST);
+        $I->fillField('Database-dot-Name', MYSQL_DATABASE);
+        $I->fillField('Database-dot-User', MYSQL_USER);
+        $I->fillField('Database-dot-Password', MYSQL_PASSWORD);
+        $I->fillField('Garden-dot-Title', VANILLA_APP_TITLE);
+        $I->fillField('Email', VANILLA_ADMIN_EMAIL);
+        $I->fillField('Name', VANILLA_ADMIN_USER);
+        $I->fillField('Password', VANILLA_ADMIN_PASSWORD);
+        $I->fillField('PasswordMatch', VANILLA_ADMIN_PASSWORD);
+
+        $I->click('Continue');
+        $I->dontSee('Access denied');
+
+    }
+
+
+}
---
+++ tests/acceptance/SigninCest.php
@@ -0,0 +1,21 @@
+<?php
+use \AcceptanceTester;
+
+class SigninCest {
+
+    public function _before() {
+    }
+
+    public function _after() {
+    }
+
+    public function loginTest(AcceptanceTester $I) {
+        $I->wantTo('log in as admin user');
+        $I->amOnPage('/entry/signin');
+        $I->fillField('Email', VANILLA_ADMIN_USER);
+        $I->fillField('Password', VANILLA_ADMIN_PASSWORD);
+        $I->click('Sign_In');
+        $I->see('admin');
+
+    }
+}

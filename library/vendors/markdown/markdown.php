8c7261278c05f31af23326b4fdb1effa11289302
lincolnwebs <lincolnwebs@gmail.com>
2014-07-23 11:37:21 -0400

    Revert "Markdown: change default to preserve newlines"
    This reverts commit 04bc21140cb83a3121e3b15174073b0961ab3381.

--- library/vendors/markdown/markdown.php
+++ library/vendors/markdown/markdown.php
@@ -67,14 +67,8 @@ function Markdown($text) {
 		$parser = new $parser_class;
 	}
 
-   # Transform text using parser.
-   $text = $parser->transform($text);
-
-   // VANILLA HACK FOR NEWLINES
-   if (C('Markdown.PreserveNewlines', TRUE)) {
-      $text = FixNl2Br(nl2br($text));
-   }
-   return $text;
+	# Transform text using parser.
+	return $parser->transform($text);
 }
 
 

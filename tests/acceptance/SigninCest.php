20c400e50417c8452c6abbd6e4e408467513108d
John Ashton <john.robert.ashton@gmail.com>
2014-07-28 21:41:11 -0400

    Codeception: WIP.

--- tests/acceptance/SigninCest.php
+++ tests/acceptance/SigninCest.php
@@ -11,7 +11,8 @@ class SigninCest {
 
     public function loginTest(AcceptanceTester $I) {
         $I->wantTo('log in as admin user');
-        $I->amOnPage('/entry/signin');
+//        $I->amOnPage('/entry/signin');
+        $I->amOnPage('/index.php?p=/entry/signin');
         $I->fillField('Email', VANILLA_ADMIN_USER);
         $I->fillField('Password', VANILLA_ADMIN_PASSWORD);
         $I->click('Sign_In');

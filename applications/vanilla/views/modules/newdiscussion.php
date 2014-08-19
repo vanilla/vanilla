26b0fe727dff48dfc42e1641091a2794e2c40afe
lincolnwebs <lincolnwebs@gmail.com>
2014-08-19 11:42:03 -0400

    Removes unused variables in NewDiscussionModule view

--- applications/vanilla/views/modules/newdiscussion.php
+++ applications/vanilla/views/modules/newdiscussion.php
@@ -1,11 +1,6 @@
 <?php if (!defined('APPLICATION')) exit();
 echo '<div class="BoxButtons BoxNewDiscussion">';
-$Text = T('Start a New Discussion', 'New Discussion');
-$UrlCode = GetValue('UrlCode', GetValue('Category', $Data), '');
-$Url = '/post/discussion/'.$UrlCode;
-if ($this->QueryString) {
-   $Url .= (strpos($Url, '?') !== FALSE ? '&' : '?').$this->QueryString;
-}
+
 $Css = 'Button Primary Action NewDiscussion';
 $Css .= strpos($this->CssClass, 'Big') !== FALSE ? ' BigButton' : '';
 

35dbe6d045dd694961718ddcbef1a8961ebababa
Todd Burry <todd@vanillaforums.com>
2014-08-25 11:24:54 -0400

    Fix sign in and register button bug.

--- library/core/functions.render.php
+++ library/core/functions.render.php
@@ -950,7 +950,7 @@ if (!function_exists('DiscussionLink')) {
 }
 
 if (!function_exists('RegisterUrl')) {
-   function RegisterUrl($Target = '') {
+   function RegisterUrl($Target = '', $force = false) {
       $registrationMethod = strtolower(C('Garden.Registration.Method'));
 
       if ($registrationMethod === 'closed') {
@@ -960,7 +960,7 @@ if (!function_exists('RegisterUrl')) {
       // Check to see if there is even a sign in button.
       if (!$force && $registrationMethod === 'connect') {
          $defaultProvider = Gdn_AuthenticationProviderModel::GetDefault();
-         if (!val('RegistrationUrl', $defaultProvider)) {
+         if ($defaultProvider && !val('RegistrationUrl', $defaultProvider)) {
             return '';
          }
       }
@@ -972,9 +972,9 @@ if (!function_exists('RegisterUrl')) {
 if (!function_exists('SignInUrl')) {
    function SignInUrl($target = '', $force = false) {
       // Check to see if there is even a sign in button.
-      if (!$force && strcasecmp(C('Garden.Registration.Method'), 'Connect') != 0) {
+      if (!$force && strcasecmp(C('Garden.Registration.Method'), 'Connect') !== 0) {
          $defaultProvider = Gdn_AuthenticationProviderModel::GetDefault();
-         if (!val('SignInUrl', $defaultProvider)) {
+         if ($defaultProvider && !val('SignInUrl', $defaultProvider)) {
             return '';
          }
       }

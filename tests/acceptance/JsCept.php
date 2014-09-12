0c5cea57b8a9f2ac95df9f67c0548b4db5500d39
John Ashton <john.robert.ashton@gmail.com>
2014-09-12 12:30:10 -0400

    Codeception:  More tests.
    Add Asserts acceptance and functional.
    Add jQuery version test.

--- tests/acceptance/JsCept.php
+++ tests/acceptance/JsCept.php
@@ -3,4 +3,5 @@ $I = new AcceptanceTester($scenario);
 $I->wantTo('check jQuery is loaded');
 $I->waitForJS("return $.active == 0;", 60);
 $jQueryVersion = $I->executeJS('return jQuery.fn.jquery;');
+$I->assertEquals("1.10.2", $jQueryVersion);
 $I->comment('Jquery Version: ' . $jQueryVersion);

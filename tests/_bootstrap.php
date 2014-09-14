8d4978810a0bf2cd24e8db111fb2303211a358ba
John Ashton <john.robert.ashton@gmail.com>
2014-09-14 17:10:26 -0400

    Codeception: Change mysql info.

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -2,7 +2,7 @@
 // This is global bootstrap for autoloading 
 
 $localBootstrapFile = __DIR__ . '/_bootstrap.'.gethostname() . '.php';
-echo 'Checking for local bootstrap file: ' . $localBootstrapFile . PHP_EOL;
+// echo 'Checking for local bootstrap file: ' . $localBootstrapFile . PHP_EOL;
 if (file_exists($localBootstrapFile)) {
 
     echo 'Loading local bootstrap file' . PHP_EOL;
@@ -10,7 +10,7 @@ if (file_exists($localBootstrapFile)) {
 
 } else {
 
-    echo 'File not found.  Using global bootstrap.' . PHP_EOL;
+//    echo 'File not found.  Using global bootstrap.' . PHP_EOL;
 
     define('VANILLA_APP_TITLE', 'Codeception');
     define('VANILLA_ADMIN_EMAIL', 'codeception@vanillaforums.com');
@@ -18,8 +18,8 @@ if (file_exists($localBootstrapFile)) {
     define('VANILLA_ADMIN_PASSWORD', 'admin');
 
     define('MYSQL_HOST', 'localhost');
-    define('MYSQL_USER', 'root');
-    define('MYSQL_PASSWORD', '');
+    define('MYSQL_USER', 'codeception');
+    define('MYSQL_PASSWORD', 'codeception');
     define('MYSQL_DATABASE', 'codeception_vanilla');
 
 }

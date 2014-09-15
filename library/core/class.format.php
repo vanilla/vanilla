22107ff138a28181236687e7a21a2dab2df5cfe7
John Ashton <john.robert.ashton@gmail.com>
2014-09-15 12:13:33 -0400

    Format: Update Vimeo embed code.

--- library/core/class.format.php
+++ library/core/class.format.php
@@ -1109,7 +1109,7 @@ class Gdn_Format {
         && !C('Garden.Format.DisableUrlEmbeds')) {
          $ID = $Matches[2];
          $Result = <<<EOT
-<div class="VideoWrap"><div class="Video Vimeo"><object width="$Width" height="$Height"><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="//vimeo.com/moogaloop.swf?clip_id=$ID&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1" /><embed src="http://vimeo.com/moogaloop.swf?clip_id=$ID&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="$Width" height="$Height"></embed></object></div></div>
+      <iframe src="//player.vimeo.com/video/{$ID}" width="{$Width}" height="{$Height}" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 EOT;
 
       // Twitter

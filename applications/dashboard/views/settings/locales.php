b793fa265a880abb0a8a9ccb7d0dbad2ed9318e3
Todd Burry <todd@vanillaforums.com>
2014-10-10 17:29:20 -0400

    Revert "Fix the translation of “Locale”."
    This reverts commit 521e5b5445014368f9ff1721899280d5a670b10f.

--- applications/dashboard/views/settings/locales.php
+++ applications/dashboard/views/settings/locales.php
@@ -53,7 +53,7 @@ foreach ($this->Data('AvailableLocales') as $Key => $Info) {
    // Hide skeleton locale pack
    if ($Key == 'skeleton')
       continue;
-
+      
    $ToggleText = $this->Data("EnabledLocales.$Key") ? 'Disable' : 'Enable';
    $RowClass = $this->Data("EnabledLocales.$Key") ? 'Enabled' : 'Disabled';
    if ($Alt) $RowClass .= ' Alt';
@@ -74,7 +74,7 @@ foreach ($this->Data('AvailableLocales') as $Key => $Info) {
          $RequiredApplications = GetValue('RequiredApplications', $Info, FALSE);
          $RequiredPlugins = GetValue('RequiredPlugins', $Info, FALSE);
 
-         $InfoItems = ArrayTranslate($Info, array('Locale' => T('Locale'), 'Version' => T('Version')));
+         $InfoItems = ArrayTranslate($Info, array('Locale' => T('_Locale'), 'Version' => T('Version')));
          $InfoString = ImplodeAssoc(': ', '<span>|</span>', $InfoItems);
 
 //            if (is_array($RequiredApplications) || is_array($RequiredPlugins)) {

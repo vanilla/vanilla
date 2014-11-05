e7d6ebd41790b165c62830d7a8d9d91a3018de75
Todd Burry <todd@vanillaforums.com>
2014-11-05 09:35:45 -0500

    Fix the doc block for CategoryModel::Categories().

--- applications/vanilla/models/class.categorymodel.php
+++ applications/vanilla/models/class.categorymodel.php
@@ -98,12 +98,12 @@ class CategoryModel extends Gdn_Model {
    }
 
    /**
+    * Gets either all of the categories or a single category.
     *
-    *
+    * @param int|string|bool $ID Either the category ID or the category url code.
+    * If nothing is passed then all categories are returned.
+    * @return array Returns either one or all categories.
     * @since 2.0.18
-    * @access public
-    * @param int $ID
-    * @return object DataObject
     */
    public static function Categories($ID = FALSE) {
 

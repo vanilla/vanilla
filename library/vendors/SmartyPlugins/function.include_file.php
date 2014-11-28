0a19045bf48210d2e913fdf07409dacacdc70e1f
beckyvb <beckyvanbussel@gmail.com>
2014-11-28 12:05:08 -0500

    Smarty: IncludeFile: Change header and check if file exists before including.

--- library/vendors/SmartyPlugins/function.include_file.php
+++ library/vendors/SmartyPlugins/function.include_file.php
@@ -1,12 +1,8 @@
 <?php if (!defined('APPLICATION')) exit();
-/*
-Copyright 2008, 2009 Vanilla Forums Inc.
-This file is part of Garden.
-Garden is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-Garden is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with Garden.  If not, see <http://www.gnu.org/licenses/>.
-Contact Vanilla Forums Inc. at support [at] vanillaforums [dot] com
-*/
+/**
+ * @copyright Copyright 2008-2014 Vanilla Forums Inc.
+ * @license http://www.opensource.org/licenses/gpl-2.0.php GPLv2
+ */
 
 
 /**
@@ -23,5 +19,9 @@ function smarty_function_include_file($Params, &$Smarty) {
    if (strpos($Name, '..') !== false) {
       return '<!-- Error, moving up directory path not allowed -->';
    }
-   return file_get_contents(rtrim($Smarty->template_dir, '/').'/'.$Name);
+   $filename = rtrim($Smarty->template_dir, '/').'/'.$Name;
+   if (!file_exists($filename)) {
+      return '<!-- Error, file does not exist -->';
+   }
+   return file_get_contents($filename);
 }

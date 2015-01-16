9633237ee590ef9053057ea06821f8c5e36a824c
Benjam Welker <bwelker@daz3d.com>
2015-01-16 15:58:45 -0700

    remove crumb div

--- library/vendors/SmartyPlugins/function.dazwrapper_posthead.php
+++ library/vendors/SmartyPlugins/function.dazwrapper_posthead.php
@@ -13,9 +13,22 @@ function smarty_function_dazwrapper_posthead($Params, &$Smarty) {
 		$wrapperHtml = $Smarty->get_template_vars('DAZ_Wrapper');
 	}
 
+	// remove the magento #crumbs div
+	$dom = new DOMDocument;
+	libxml_use_internal_errors(true); // PHP DOM doesn't like HTML5
+	$dom->loadHTML($wrapperHtml);
+	libxml_clear_errors( );
+	$xPath = new DOMXPath($dom);
+	$nodes = $xPath->query('//*[@id="crumbs"]');
+	if ($nodes->item(0)) {
+		$nodes->item(0)->parentNode->removeChild($nodes->item(0));
+	}
+	$wrapperHtml = $dom->saveHTML( );
+
 	// from the close </title> tag to the end of the header
 	$start = strpos($wrapperHtml, '</title>') + strlen('</title>');
 	$length = strpos($wrapperHtml, '<!--ENDHEADER-->') - $start;
 	$header = substr($wrapperHtml, $start, $length);
+
 	return $header;
 }

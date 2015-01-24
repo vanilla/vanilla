4fd03e94f3b47b53f83b813bbaaffea5fb8da79e
lincolnwebs <lincolnwebs@gmail.com>
2015-01-24 10:18:39 -0500

    derp

--- applications/dashboard/views/home/updatemode.php
+++ applications/dashboard/views/home/updatemode.php
@@ -1,7 +1,7 @@
 <?php if (!defined('APPLICATION')) exit(); ?>
 
 <div class="SplashInfo">
-   <h1><?php echo T('wMaintenance Mode'); ?></h1>
+   <h1><?php echo T('Maintenance Mode'); ?></h1>
    <p><?php echo T('The site is currently undergoing maintenance.'); ?></p>
 </div>
 <!-- Domain: <?php echo Gdn::Config('Garden.Domain', ''); ?> -->

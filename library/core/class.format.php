890f50468c04b38afa1dcda194f30db5eb4718bd
9adefaf01e5bf6426d838cd20eae582d2b6ba647 <tophf@mail.com>
2015-02-28 02:04:53 +0300

    Don't linkify @mentions and #hashtags in CODE blocks

--- library/core/class.format.php
+++ library/core/class.format.php
@@ -1339,6 +1339,12 @@ EOT;
             return $Formatter->FormatMentions($Mixed);
          }
 
+         $protectCodeBlocks = C('Garden.Format.Mentions') || C('Garden.Format.Hashtags');
+         if($protectCodeBlocks)
+            $Mixed = preg_replace_callback('/<code>.*?[@#].*?<\/code>/si',
+               function ($match) { return preg_replace('/[@#]/', '\0\0', $match[0]);
+            }, $Mixed);
+
          // Handle @mentions.
          if(C('Garden.Format.Mentions')) {
             $urlFormat = str_replace('{name}', '$2', self::$MentionsUrlFormat);
@@ -1358,6 +1364,11 @@ EOT;
 					$Mixed
 				);
 			}
+
+         if($protectCodeBlocks)
+            $Mixed = preg_replace_callback('/<code>.*?[@#]{2}.*?<\/code>/si',
+               function ($match) { return preg_replace('/([@#]){2}/', '\1', $match[0]);
+            }, $Mixed);
 
 			// Handle "/me does x" action statements
          if(C('Garden.Format.MeActions')) {

de84f106801f2cd88d4a4abb44137321e1396ae1
Todd Burry <todd@vanillaforums.com>
2015-05-08 16:14:12 -0400

    Don’t trace missing *custom.css files.
    We look for custom.css files a lot, but they aren’t usually required.

--- applications/dashboard/models/class.assetmodel.php
+++ applications/dashboard/models/class.assetmodel.php
@@ -298,7 +298,9 @@ class AssetModel extends Gdn_Model {
             return $Info;
          }
       }
-      Trace("Could not find file '$Filename' in folder '$Folder'.");
+      if (!StringEndsWith($Filename, 'custom.css')) {
+         Trace("Could not find file '$Filename' in folder '$Folder'.");
+      }
 
       return false;
    }

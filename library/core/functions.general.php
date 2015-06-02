62374628da806f91b611b7ab205c2eb83bb4bf1e
Todd Burry <todd@vanillaforums.com>
2015-06-02 18:44:12 -0400

    Remove the fnmatch() function
    This function is available on all platforms as of PHP 5.3.

--- library/core/functions.general.php
+++ library/core/functions.general.php
@@ -1046,60 +1046,6 @@ if (!function_exists('DomGetImages')) {
     }
 }
 
-/**
- * Replace missing 'fnmatch' on Windows + PHP <5.3
- */
-if (!function_exists('fnmatch')) {
-    define('FNM_PATHNAME', 1);
-    define('FNM_NOESCAPE', 2);
-    define('FNM_PERIOD', 4);
-    define('FNM_CASEFOLD', 16);
-
-    function fnmatch($pattern, $string, $flags = 0) {
-        return pcre_fnmatch($pattern, $string, $flags);
-    }
-
-    function pcre_fnmatch($pattern, $string, $flags = 0) {
-        $modifiers = null;
-        $transforms = array(
-            '\*' => '.*',
-            '\?' => '.',
-            '\[\!' => '[^',
-            '\[' => '[',
-            '\]' => ']',
-            '\.' => '\.',
-            '\\' => '\\\\'
-        );
-
-        // Forward slash in string must be in pattern:
-        if ($flags & FNM_PATHNAME) {
-            $transforms['\*'] = '[^/]*';
-        }
-
-        // Back slash should not be escaped:
-        if ($flags & FNM_NOESCAPE) {
-            unset($transforms['\\']);
-        }
-
-        // Perform case insensitive match:
-        if ($flags & FNM_CASEFOLD) {
-            $modifiers .= 'i';
-        }
-
-        // Period at start must be the same as pattern:
-        if ($flags & FNM_PERIOD) {
-            if (strpos($string, '.') === 0 && strpos($pattern, '.') !== 0) return false;
-        }
-
-        $pattern = '#^'
-            .strtr(preg_quote($pattern, '#'), $transforms)
-            .'$#'
-            .$modifiers;
-
-        return (boolean)preg_match($pattern, $string);
-    }
-}
-
 if (!function_exists('ForceIPv4')) {
     /**
      * Force a string into ipv4 notation.

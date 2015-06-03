920fc23ebb69ff52936cfe781c884cbea5b02593
Todd Burry <todd@vanillaforums.com>
2015-06-03 13:45:30 -0400

    Update code standards for functions.compatibility

--- library/core/functions.compatibility.php
+++ library/core/functions.compatibility.php
@@ -1,6 +1,6 @@
-<?php if (!defined('APPLICATION')) exit();
+<?php
 /**
- * General functions Interim Compatibility Map
+ * General functions Interim Compatibility Map.
  *
  * These functions are copies of existing functions but with new and improved
  * names. Parent functions will be deprecated in a future release.
@@ -23,34 +23,32 @@ if (!function_exists('gzopen') && function_exists('gzopen64')) {
     }
 }
 
-/**
- * Takes an associative array in the layout of parse_url, and constructs a URL from it
- *
- * @see http://www.php.net/manual/en/function.http-build-url.php#96335
- *
- * @param   mixed   (Part(s) of ) an URL in form of a string or associative array like parse_url() returns
- * @param   mixed   Same as the first argument
- * @param   int     A bitmask of binary or'ed HTTP_URL constants (Optional)HTTP_URL_REPLACE is the default
- * @param   array   If set, it will be filled with the parts of the composed url like parse_url() would return
- *
- * @return  string  constructed URL
- */
-// Based on https://github.com/fuel/core/blob/974281dde67345ca8d7cfa27bcf4aa55c984d48e/base.php#L248
-// Bug http://stackoverflow.com/questions/7751679/php-http-build-url-and-pecl-install/7753154#comment11239561_7753154
 if (!function_exists('http_build_url')) {
 
-    define('HTTP_URL_REPLACE', 1);                // Replace every part of the first URL when there's one of the second URL
-    define('HTTP_URL_JOIN_PATH', 2);            // Join relative paths
-    define('HTTP_URL_JOIN_QUERY', 4);            // Join query strings
-    define('HTTP_URL_STRIP_USER', 8);            // Strip any user authentication information
-    define('HTTP_URL_STRIP_PASS', 16);            // Strip any password authentication information
-    define('HTTP_URL_STRIP_AUTH', 32);            // Strip any authentication information
-    define('HTTP_URL_STRIP_PORT', 64);            // Strip explicit port numbers
-    define('HTTP_URL_STRIP_PATH', 128);            // Strip complete path
-    define('HTTP_URL_STRIP_QUERY', 256);        // Strip query string
-    define('HTTP_URL_STRIP_FRAGMENT', 512);        // Strip any fragments (#identifier)
-    define('HTTP_URL_STRIP_ALL', 1024);            // Strip anything but scheme and host
+    define('HTTP_URL_REPLACE', 1);  // Replace every part of the first URL when there's one of the second URL
+    define('HTTP_URL_JOIN_PATH', 2); // Join relative paths
+    define('HTTP_URL_JOIN_QUERY', 4); // Join query strings
+    define('HTTP_URL_STRIP_USER', 8); // Strip any user authentication information
+    define('HTTP_URL_STRIP_PASS', 16); // Strip any password authentication information
+    define('HTTP_URL_STRIP_AUTH', 32); // Strip any authentication information
+    define('HTTP_URL_STRIP_PORT', 64); // Strip explicit port numbers
+    define('HTTP_URL_STRIP_PATH', 128); // Strip complete path
+    define('HTTP_URL_STRIP_QUERY', 256); // Strip query string
+    define('HTTP_URL_STRIP_FRAGMENT', 512); // Strip any fragments (#identifier)
+    define('HTTP_URL_STRIP_ALL', 1024); // Strip anything but scheme and host
 
+    /**
+     * Takes an associative array in the layout of parse_url, and constructs a URL from it.
+     *
+     * @param mixed $url Part(s) of a URL in form of a string or associative array like parse_url() returns.
+     * @param mixed $parts Same as the first argument.
+     * @param int $flags A bit mask of binary or'ed HTTP_URL constants (Optional)HTTP_URL_REPLACE is the default.
+     * @param array &$new_url If set, it will be filled with the parts of the composed url like parse_url() would return.
+     * @return  string  Returns the constructed URL.
+     * @see http://www.php.net/manual/en/function.http-build-url.php#96335
+     * @see https://github.com/fuel/core/blob/974281dde67345ca8d7cfa27bcf4aa55c984d48e/base.php#L248
+     * @see http://stackoverflow.com/questions/7751679/php-http-build-url-and-pecl-install/7753154#comment11239561_7753154
+     */
     function http_build_url($url, $parts = array(), $flags = HTTP_URL_REPLACE, &$new_url = false) {
         $keys = array('user', 'pass', 'port', 'path', 'query', 'fragment');
 
@@ -62,8 +60,8 @@ if (!function_exists('http_build_url')) {
             $flags |= HTTP_URL_STRIP_PATH;
             $flags |= HTTP_URL_STRIP_QUERY;
             $flags |= HTTP_URL_STRIP_FRAGMENT;
-        } // HTTP_URL_STRIP_AUTH becomes HTTP_URL_STRIP_USER and HTTP_URL_STRIP_PASS
-        else if ($flags & HTTP_URL_STRIP_AUTH) {
+        } elseif ($flags & HTTP_URL_STRIP_AUTH) {
+            // HTTP_URL_STRIP_AUTH becomes HTTP_URL_STRIP_USER and HTTP_URL_STRIP_PASS
             $flags |= HTTP_URL_STRIP_USER;
             $flags |= HTTP_URL_STRIP_PASS;
         }
@@ -91,32 +89,36 @@ if (!function_exists('http_build_url')) {
         // replace the original URL with it's new parts (if applicable)
         if ($flags & HTTP_URL_REPLACE) {
             foreach ($keys as $key) {
-                if (isset($parts[$key]))
+                if (isset($parts[$key])) {
                     $parse_url[$key] = $parts[$key];
+                }
             }
         } else {
             // join the original URL path with the new path
             if (isset($parts['path']) && ($flags & HTTP_URL_JOIN_PATH)) {
-                if (isset($parse_url['path']))
+                if (isset($parse_url['path'])) {
                     $parse_url['path'] = rtrim(str_replace(basename($parse_url['path']), '', $parse_url['path']), '/').'/'.ltrim($parts['path'], '/');
-                else
+                } else {
                     $parse_url['path'] = $parts['path'];
+                }
             }
 
             // join the original query string with the new query string
             if (isset($parts['query']) && ($flags & HTTP_URL_JOIN_QUERY)) {
-                if (isset($parse_url['query']))
+                if (isset($parse_url['query'])) {
                     $parse_url['query'] .= '&'.$parts['query'];
-                else
+                } else {
                     $parse_url['query'] = $parts['query'];
+                }
             }
         }
 
         // strips all the applicable sections of the URL
         // note: scheme and host are never stripped
         foreach ($keys as $key) {
-            if ($flags & (int)constant('HTTP_URL_STRIP_'.strtoupper($key)))
+            if ($flags & (int)constant('HTTP_URL_STRIP_'.strtoupper($key))) {
                 unset($parse_url[$key]);
+            }
         }
 
 
@@ -150,6 +152,7 @@ if (!function_exists('http_build_url')) {
 if (!function_exists('is_id')) {
     /**
      * Finds whether the type given variable is a database id.
+     *
      * @param mixed $val The variable being evaluated.
      * @return bool Returns true if the variable is a database id or false if it isn't.
      */
@@ -160,7 +163,7 @@ if (!function_exists('is_id')) {
 
 if (!function_exists('paths')) {
     /**
-     * Concatenate path elements into single string
+     * Concatenate path elements into single string.
      *
      * Takes a variable number of arguments and concatenates them. Delimiters will
      * not be duplicated. Example: all of the following invocations will generate
@@ -171,15 +174,18 @@ if (!function_exists('paths')) {
      * '/path', 'to', 'vanilla', 'applications', 'dashboard'
      * '/path/', '/to/', '/vanilla/', '/applications/', '/dashboard'
      *
-     * @param function arguments
-     * @return the concatentated path.
+     * @return string Returns the concatenated path.
      */
     function paths() {
         $paths = func_get_args();
         $delimiter = '/';
         if (is_array($paths)) {
             $mungedPath = implode($delimiter, $paths);
-            $mungedPath = str_replace(array($delimiter.$delimiter.$delimiter, $delimiter.$delimiter), array($delimiter, $delimiter), $mungedPath);
+            $mungedPath = str_replace(
+                array($delimiter.$delimiter.$delimiter, $delimiter.$delimiter),
+                array($delimiter, $delimiter),
+                $mungedPath
+            );
             return str_replace(array('http:/', 'https:/'), array('http://', 'https://'), $mungedPath);
         } else {
             return $paths;
@@ -242,15 +248,15 @@ if (!function_exists('valr')) {
 }
 
 /**
- * Set a key to a value in a collection
+ * Set a key to a value in a collection.
  *
  * Works with single keys or "dot" notation. If $key is an array, a simple
  * shallow array_merge is performed.
  *
  * @param string $key The key or property name of the value.
- * @param array $collection The array or object to search.
- * @param type $value The value to set
- * @return mixed Newly set value or if array merge
+ * @param array &$collection The array or object to search.
+ * @param mixed $value The value to set.
+ * @return mixed Newly set value or if array merge.
  */
 function setvalr($key, &$collection, $value = null) {
     if (is_array($key)) {
@@ -271,7 +277,7 @@ function setvalr($key, &$collection, $value = null) {
                     $selection[$subSelector] = array();
                 }
                 $selection = &$selection[$subSelector];
-            } else if (is_object($selection)) {
+            } elseif (is_object($selection)) {
                 if (!isset($selection->$subSelector)) {
                     $selection->$subSelector = new stdClass();
                 }
@@ -292,14 +298,14 @@ function setvalr($key, &$collection, $value = null) {
 
 if (!function_exists('svalr')) {
     /**
-     * Set a key to a value in a collection
+     * Set a key to a value in a collection.
      *
      * Works with single keys or "dot" notation. If $key is an array, a simple
      * shallow array_merge is performed.
      *
      * @param string $key The key or property name of the value.
-     * @param array $collection The array or object to search.
-     * @param type $value The value to set
+     * @param array &$collection The array or object to search.
+     * @param mixed $value The value to set.
      * @return mixed Newly set value or if array merge
      * @deprecated Use {@link setvalr()}.
      */
@@ -310,7 +316,7 @@ if (!function_exists('svalr')) {
 
 if (!function_exists('requestContext')) {
     /**
-     * Get request context
+     * Get request context.
      *
      * This method determines if current request is operating within HTTP, or
      * elsewhere such as the command line.
@@ -340,12 +346,11 @@ if (!function_exists('requestContext')) {
 
 if (!function_exists('safeHeader')) {
     /**
-     * Context-aware call to header()
+     * Context-aware call to header().
      *
      * This method is context-aware and will avoid sending headers if the request
      * context is not HTTP.
      *
-     * @staticvar string $context
      * @param string $header
      * @param bool $replace
      * @param int|null $http_response_code
@@ -367,12 +372,11 @@ if (!function_exists('safeHeader')) {
 
 if (!function_exists('safeCookie')) {
     /**
-     * Context-aware call to setcookie()
+     * Context-aware call to setcookie().
      *
      * This method is context-aware and will avoid setting cookies if the request
      * context is not HTTP.
      *
-     * @staticvar string $context
      * @param string $name
      * @param string $value
      * @param integer $expire

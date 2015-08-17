f0b93ef797919a57b926f061403096bb23aaa0f6
beckyvb <beckyvanbussel@gmail.com>
2015-08-17 14:30:15 -0400

    Application manager: Ensure application name index is capitalized before retrieving info.

--- library/core/class.applicationmanager.php
+++ library/core/class.applicationmanager.php
@@ -118,6 +118,7 @@ class Gdn_ApplicationManager {
      * @return bool|mixed Returns the application's info, a specific value, or false if the application cannot be found.
      */
     public function getApplicationInfo($applicationName, $key = null) {
+        $applicationName = ucfirst($applicationName);
         $ApplicationInfo = val($applicationName, $this->availableApplications(), null);
         if (is_null($ApplicationInfo)) {
             return false;
@@ -311,7 +312,7 @@ class Gdn_ApplicationManager {
                 )
             );
         }
-        
+
         // 2. Disable it
         removeFromConfig("EnabledApplications.{$applicationName}");
 

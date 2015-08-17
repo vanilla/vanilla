5e90f7183d7068bcdde521537db3bf1d1749dce1
beckyvb <beckyvanbussel@gmail.com>
2015-08-17 15:34:31 -0400

    Fix form postbacks checks.

--- applications/vanilla/settings/class.hooks.php
+++ applications/vanilla/settings/class.hooks.php
@@ -418,7 +418,7 @@ class VanillaHooks implements Gdn_IPlugin {
 //      }
 
         if (Gdn::session()->checkPermission('Garden.AdvancedNotifications.Allow')) {
-            $PostBack = $Sender->Form->isAuthenticatedPostBack();
+            $PostBack = $Sender->Form->authenticatedPostBack();
             $Set = array();
 
             // Add the category definitions to for the view to pick up.

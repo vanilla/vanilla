e10363efd1f68b0f8a952d24be200983e9cc660c
lincolnwebs <lincolnwebs@gmail.com>
2015-08-26 15:40:46 -0400

    Add X_PATH_INFO fallback to deal with naughty hosts

--- library/core/class.request.php
+++ library/core/class.request.php
@@ -516,6 +516,12 @@ class Gdn_Request {
 
             if (!empty($_SERVER['X_REWRITE'])) {
                 $path = $_SERVER['PATH_INFO'];
+                // Some hosts block PATH_INFO from being passed (or even manually set).
+                // We set X_PATH_INFO in the htaccess as a fallback for those situations.
+                // If you work for one of those hosts, know that many beautiful kittens lost their lives for your sins.
+                if (!$Path && !empty($_SERVER['REDIRECT_X_PATH_INFO'])) {
+                    $Path = $_SERVER['REDIRECT_X_PATH_INFO'];
+                }
             } elseif (isset($get['_p'])) {
                 $path = $get['_p'];
                 unset($_GET['_p']);

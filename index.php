2e2c7a0e0eb68493e33d0a540e1f47258eaf9c68
lincolnwebs <lincolnwebs@gmail.com>
2015-09-07 18:51:22 -0400

    Cache break

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
  */
 
 define('APPLICATION', 'Vanilla');
-define('APPLICATION_VERSION', '2.2.100.5');
+define('APPLICATION_VERSION', '2.2.100.6');
 
 // Report and track all errors.
 error_reporting(E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR);

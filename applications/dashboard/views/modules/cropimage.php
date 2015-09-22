5ce89b208fbe554a3bb3fe99ea64c0be46b85255
beckyvb <beckyvanbussel@gmail.com>
2015-09-22 13:22:14 -0400

    Crop image module: Specify app folder for assets.
    Also make headings less specific and add css class to button.

--- applications/dashboard/views/modules/cropimage.php
+++ applications/dashboard/views/modules/cropimage.php
@@ -1,7 +1,7 @@
 <div class="box-crop">
     <div class="box-image box-source">
         <?php
-        echo '<h2>'.t('Avatar').'</h2>';
+        echo '<h2>'.t('Source').'</h2>';
         echo img($this->getSourceImageUrl(), array('id' => 'cropbox'));
         echo '<label>'.t('Define Thumbnail', 'Click and drag across the picture to define the thumbnail.').'</label>'; ?>
     </div>
@@ -14,7 +14,7 @@
         </div>
         <?php
         if ($this->saveButton) {
-            echo $this->form->button('Save Thumbnail', array('id' => 'save-crop', 'class' => 'SmallButton Primary Hidden', 'style' => 'margin-top: 20px;'));
+            echo $this->form->button('Save Thumbnail', array('id' => 'save-crop', 'class' => 'Button SmallButton Primary Hidden', 'style' => 'margin-top: 20px;'));
         }
         ?>
     </div>

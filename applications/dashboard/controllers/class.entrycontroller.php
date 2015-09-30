32e11c4774d541d0da2d9c42ee3cfd573e9864c1
Todd Burry <todd@vanillaforums.com>
2015-09-30 17:13:27 -0400

    Filter out roles

--- applications/dashboard/controllers/class.entrycontroller.php
+++ applications/dashboard/controllers/class.entrycontroller.php
@@ -398,6 +398,7 @@ EOT;
         // Filter the form data for users here. SSO plugins must reset validated data each postback.
         $filteredData = Gdn::userModel()->filterForm($currentData, true);
         $filteredData = array_replace($filteredData, arrayTranslate($currentData, ['TransientKey', 'hpt']));
+        unset($filteredData['Roles'], $filteredData['RoleID']);
         $this->Form->formValues($filteredData);
 
         try {

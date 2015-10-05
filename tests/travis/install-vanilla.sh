d138f97602677697bbcda5997ee079faedea9b0f
Todd Burry <todd@vanillaforums.com>
2015-10-05 14:54:51 -0400

    More installer debugging

--- tests/travis/install-vanilla.sh
+++ tests/travis/install-vanilla.sh
@@ -4,14 +4,14 @@
 mysql -e 'create database travis_tests;'
 
 # Install Vanilla. The -f option will make curl return 22 on any HTTP error.
-curl -vf \
-       -d "Database-dot-Host=localhost" \
-       -d "Database-dot-Name=travis_tests" \
-       -d "Database-dot-User=travis" \
-       -d "Database-dot-Password=" \
-       -d "Garden-dot-Title=Travis" \
-       -d "Email=travis@example.com" \
-       -d "Name=travis" \
-       -d "Password=travis" \
-       -d "PasswordMatch=travis" \
-       http://vanilla.test:8080/dashboard/setup.json &> /dev/stdout
+curl -v \
+    -d "Database-dot-Host=localhost" \
+    -d "Database-dot-Name=travis_tests" \
+    -d "Database-dot-User=travis" \
+    -d "Database-dot-Password=" \
+    -d "Garden-dot-Title=Travis" \
+    -d "Email=travis@example.com" \
+    -d "Name=travis" \
+    -d "Password=travis" \
+    -d "PasswordMatch=travis" \
+    http://vanilla.test:8080/dashboard/setup.json &> /dev/stdout

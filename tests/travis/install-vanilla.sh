3c7b2a7d526c5f4c9a5138cf626f327b3d863aea
Todd Burry <todd@vanillaforums.com>
2015-10-05 14:46:43 -0400

    Add the Vanilla install script

---
+++ tests/travis/install-vanilla.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Create the database for Vanilla.
+mysql -e 'create database travis_tests;'
+
+# Install Vanilla. The -f option will make curl return 22 on any HTTP error.
+curl -vf \
+       -d "Database-dot-Host=localhost" \
+       -d "Database-dot-Name=travis_tests" \
+       -d "Database-dot-User=travis" \
+       -d "Database-dot-Password=" \
+       -d "Garden-dot-Title=Travis" \
+       -d "Email=travis@example.com" \
+       -d "Name=travis" \
+       -d "Password=travis" \
+       -d "PasswordMatch=travis" \
+       http://vanilla.test/dashboard/setup.json

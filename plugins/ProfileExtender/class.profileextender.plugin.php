2c6d6c21f6a42c02020cc040dcd527581a00f81d
Samuel Hilson <samrhilson@gmail.com>
2015-10-09 09:23:05 -0500

    Get UserID from available source depending on where profile is edited.
    UserID is available in different location when you are editing and extended profile field from profile vs the dashboard. This adds a quick inline if to detect which location that UserID will be set and uses the correct location for UserID from the $Sender.

--- plugins/ProfileExtender/class.profileextender.plugin.php
+++ plugins/ProfileExtender/class.profileextender.plugin.php
@@ -241,7 +241,8 @@ class ProfileExtenderPlugin extends Gdn_Plugin {
         $this->ProfileFields = $this->getProfileFields();
 
         // Get user-specific data
-        $this->UserFields = Gdn::userModel()->GetMeta($Sender->User->UserID, 'Profile.%', 'Profile.');
+        $UserID = (isset($Sender->User)) ? $Sender->User->UserID : $Sender->Data['User']['UserID'];
+        $this->UserFields = Gdn::userModel()->GetMeta($UserID, 'Profile.%', 'Profile.');
         // Fill in user data on form
         foreach ($this->UserFields as $Field => $Value) {
             $Sender->Form->setValue($Field, $Value);

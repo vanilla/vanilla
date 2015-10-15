7efeb28b42a922bfa688b01cfac73091e97751d5
Zachary Doll <hgtonight@gmail.com>
2015-10-15 10:54:54 -0500

    Array column is PHP 5.5+
    This is the recommended userland implementation of array_column per: http://php.net/manual/en/function.array-column.php

--- applications/dashboard/controllers/class.utilitycontroller.php
+++ applications/dashboard/controllers/class.utilitycontroller.php
@@ -337,8 +337,9 @@ class UtilityController extends DashboardController {
       $Files = array();
         $appName = $appName == '' ? 'all' : $appName;
         if ($appName == 'all') {
-			// Load all application structure files.
-			$ApplicationManager = new Gdn_ApplicationManager();
+            require_once PATH_LIBRARY.'/vendors/misc/functions.arraycolumn.php';
+         // Load all application structure files.
+         $ApplicationManager = new Gdn_ApplicationManager();
             $Apps = $ApplicationManager->enabledApplications();
             $AppNames = array_column($Apps, 'Folder');
             foreach ($AppNames as $appName) {

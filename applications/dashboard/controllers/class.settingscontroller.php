d64cbd24877fd97a3af31c2a12eda59d2318cf67
initvector <ryan.p@vanillaforums.com>
2015-10-21 14:36:08 -0400

    Update permission check in settings

--- applications/dashboard/controllers/class.settingscontroller.php
+++ applications/dashboard/controllers/class.settingscontroller.php
@@ -1048,6 +1048,8 @@ class SettingsController extends DashboardController {
      * @param string $TransientKey Security token.
      */
     public function testAddon($AddonType = '', $AddonName = '', $TransientKey = '') {
+        $this->permission('Garden.Settings.Manage');
+
         if (!in_array($AddonType, array('Plugin', 'Application', 'Theme', 'Locale'))) {
             $AddonType = 'Plugin';
         }

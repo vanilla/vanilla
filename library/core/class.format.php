d4e45ae34cc19bbe7d61bed239bd08f4344ac08d
Adrian <adrianspeyer@users.noreply.github.com>
2015-12-22 06:58:30 -0500

    Fixing Wisita embed quotes issue

--- library/core/class.format.php
+++ library/core/class.format.php
@@ -1399,7 +1399,7 @@ EOT;
             && !c('Garden.Format.DisableUrlEmbeds')
         ) {
             $Result = <<<EOT
-<script charset="ISO-8859-1" src="//fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_{$Matches[3]}" videoFoam=true" style="height:100%;width:100%">&nbsp;</div></div></div>
+<script charset="ISO-8859-1" src="//fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_{$Matches[3]}" videoFoam="true" style="height:100%;width:100%">&nbsp;</div></div></div>
 EOT;
 
 

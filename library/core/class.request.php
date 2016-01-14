bbec0a8fb0512aed63563a70269fc8749ac706fd
initvector <ryan.p@vanillaforums.com>
2016-01-14 10:51:11 -0500

    Revert "Update retrieval of PATH_INFO"
    This reverts commit 3c9c0e99af3704df2a4ea460aff4c25d06a9e003.

--- library/core/class.request.php
+++ library/core/class.request.php
@@ -515,8 +515,7 @@ class Gdn_Request {
             }
 
             if (!empty($_SERVER['X_REWRITE']) || !empty($_SERVER['REDIRECT_X_REWRITE'])) {
-                $path = $this->getValueFrom(self::INPUT_SERVER, 'PATH_INFO');
-
+                $path = $_SERVER['PATH_INFO'];
                 // Some hosts block PATH_INFO from being passed (or even manually set).
                 // We set X_PATH_INFO in the .htaccess as a fallback for those situations.
                 // If you work for one of those hosts, know that many beautiful kittens lost their lives for your sins.

ed51f878aa3996a5b4a8f2d11c62f4dca7a76e69
initvector <ryan.p@vanillaforums.com>
2016-01-19 14:33:13 -0500

    Adding no-store to category controllers

--- applications/vanilla/controllers/class.categorycontroller.php
+++ applications/vanilla/controllers/class.categorycontroller.php
@@ -19,6 +19,15 @@ class CategoryController extends VanillaController {
     public function __construct() {
         parent::__construct();
         $this->CategoryModel = new CategoryModel();
+
+        /**
+         * The default Cache-Control header does not include no-store, which can cause issues with outdated category
+         * information (e.g. counts).  The same check is performed here as in Gdn_Controller before the Cache-Control
+         * header is added, but this value includes the no-store specifier.
+         */
+        if (Gdn::session()->isValid()) {
+            $this->setHeader('Cache-Control', 'private, no-cache, no-store, max-age=0, must-revalidate');
+        }
     }
 
     public function follow($CategoryID, $Value, $TKey) {

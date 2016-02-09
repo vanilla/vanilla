0a3a1a77acaa51daa62b36b4d37262784a6d1815
beckyvb <beckyvanbussel@gmail.com>
2016-02-09 17:30:55 -0500

    Add messaging to inform user if username exists and check on keyup.

--- applications/dashboard/controllers/class.entrycontroller.php
+++ applications/dashboard/controllers/class.entrycontroller.php
@@ -394,6 +394,9 @@ EOT;
         // Fire ConnectData event & error handling.
         $currentData = $this->Form->formValues();
 
+        $this->addDefinition('Username already exists.', t('Username already exists.'));
+        $this->addDefinition('Choose a name to identify yourself on the site.', t('Choose a name to identify yourself on the site.'));
+
         // Filter the form data for users here. SSO plugins must reset validated data each postback.
         $filteredData = Gdn::userModel()->filterForm($currentData, true);
         $filteredData = array_replace($filteredData, arrayTranslate($currentData, ['TransientKey', 'hpt']));

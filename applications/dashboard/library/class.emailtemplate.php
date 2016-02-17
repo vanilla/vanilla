c25fa885a00cfd40717a97c6a2b60bd554b47aff
beckyvb <beckyvanbussel@gmail.com>
2016-02-17 13:05:42 -0500

    Fix fetchViewLocation invocation in email template.

--- applications/dashboard/library/class.emailtemplate.php
+++ applications/dashboard/library/class.emailtemplate.php
@@ -125,7 +125,7 @@ class EmailTemplate extends Gdn_Pluggable {
      * @throws Exception
      */
     public function setView($view, $controllerName = 'email', $applicationFolder = 'dashboard') {
-        $this->view = Gdn::controller()->fetchViewLocation($view, $controllerName, $applicationFolder);
+        $this->view = AssetModel::viewLocation($view, $controllerName, $applicationFolder);
         return $this;
     }
 

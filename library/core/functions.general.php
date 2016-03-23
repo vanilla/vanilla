359fbe8ea71cf6a6f7b71483226454ac80338a29
lincolnwebs <lincolnwebs@gmail.com>
2016-03-23 08:41:55 -0400

    Replace tilde regex delims

--- library/core/functions.general.php
+++ library/core/functions.general.php
@@ -1728,8 +1728,8 @@ if (!function_exists('htmlEntityDecode')) {
     function htmlEntityDecode($string, $quote_style = ENT_QUOTES, $charset = "utf-8") {
         $string = html_entity_decode($string, $quote_style, $charset);
         $string = str_ireplace('&apos;', "'", $string);
-        $string = preg_replace_callback('~&#x([0-9a-fA-F]+);~i', "chr_utf8_callback", $string);
-        $string = preg_replace('~&#([0-9]+);~e', 'chr_utf8("\\1")', $string);
+        $string = preg_replace_callback('/&#x([0-9a-fA-F]+);/i', "chr_utf8_callback", $string);
+        $string = preg_replace('/&#([0-9]+);/e', 'chr_utf8("\\1")', $string);
         return $string;
     }
 
@@ -3801,19 +3801,19 @@ if (!function_exists('slugify')) {
      */
     function slugify($text) {
         // replace non letter or digits by -
-        $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+        $text = preg_replace('/[^\pL\d]+/u', '-', $text);
 
         // transliterate
         $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
 
         // remove unwanted characters
-        $text = preg_replace('~[^-\w]+~', '', $text);
+        $text = preg_replace('/[^-\w]+/', '', $text);
 
         // trim
         $text = trim($text, '-');
 
         // remove duplicate -
-        $text = preg_replace('~-+~', '-', $text);
+        $text = preg_replace('/-+/', '-', $text);
 
         // lowercase
         $text = strtolower($text);

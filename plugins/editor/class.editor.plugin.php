78342296edfdb6f106f199cfe9defb03def99dfd
Patrick <patrick.k@vanillaforums.com>
2016-04-08 14:38:07 -0400

    Pass more args to BeforeSaveUploads event in editor.

--- plugins/editor/class.editor.plugin.php
+++ plugins/editor/class.editor.plugin.php
@@ -729,7 +729,9 @@ class EditorPlugin extends Gdn_Plugin {
             $this->EventArguments['TmpFilePath'] =& $tmpFilePath;
             $this->EventArguments['FileExtension'] = $fileExtension;
             $this->EventArguments['ValidImage'] = $validImage;
-            $this->fireEvent("BeforeSaveUploads");
+            $this->EventArguments['AbsoluteFileDestination'] =& $absoluteFileDestination;
+            $this->EventArguments['DiscussionID'] =& $discussionID;
+            $this->fireEvent('BeforeSaveUploads');
 
             if (!$validImage) {
                 $filePathParsed = $Upload->saveAs(

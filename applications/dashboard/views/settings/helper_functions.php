28e668f3bd0d0e1823eb3797d7b8821201dc1266
Todd Burry <todd@vanillaforums.com>
2016-04-15 21:39:09 -0400

    Tweak some of the help & tutorial code (#3729)
    Fix some Scrutinizer issues.

--- applications/dashboard/views/settings/helper_functions.php
+++ applications/dashboard/views/settings/helper_functions.php
@@ -2,7 +2,7 @@
 /**
  * Get all tutorials, or a specific one.
  */
-function getTutorials($TutorialCode = '') {
+function getTutorials($tutorialCode = '') {
     // Define all Tutorials
     $Tutorials = array(
         array(
@@ -62,22 +62,22 @@ function getTutorials($TutorialCode = '') {
     );
 
     // Default Thumbnails
-    $Thumbnail = Asset('applications/dashboard/design/images/help-tn-200.jpg');
-    $LargeThumbnail = Asset('applications/dashboard/design/images/help-tn-640.jpg');
-    for ($i = 0; $i < count($Tutorials); $i++) {
-        $Tutorials[$i]['Thumbnail'] = $Thumbnail;
-        $Tutorials[$i]['LargeThumbnail'] = $LargeThumbnail;
+    $thumbnail = asset('applications/dashboard/design/images/help-tn-200.jpg');
+    $largeThumbnail = asset('applications/dashboard/design/images/help-tn-640.jpg');
+    foreach ($Tutorials as &$tutorial) {
+        $tutorial['Thumbnail'] = $thumbnail;
+        $tutorial['LargeThumbnail'] = $largeThumbnail;
     }
 
-    if ($TutorialCode != '') {
-        $Keys = consolidateArrayValuesByKey($Tutorials, 'Code');
-        $Index = array_search($TutorialCode, $Keys);
-        if ($Index === FALSE)
-            return FALSE; // Not found!
+    if ($tutorialCode != '') {
+        $Keys = array_column($Tutorials, 'Code');
+        $Index = array_search($tutorialCode, $Keys);
+        if ($Index === false) {
+            return false; // Not found!
+        }
 
         // Found it, so define it's thumbnail location
         $Tutorial = val($Index, $Tutorials);
-        $VideoID = val('VideoID', $Tutorial);
         try {
             $videoInfo = json_decode(file_get_contents("http://vimeo.com/api/v2/video/{$Tutorial['VideoID']}.json"));
             if ($videoInfo && $vimeo = array_shift($videoInfo)) {

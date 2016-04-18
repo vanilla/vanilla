389f7ea69669ec3829648addb83a6b0051a41a88
Todd Burry <todd@vanillaforums.com>
2016-04-18 15:40:59 -0400

    Rollback overzealous language attributes
    The Gdn::locale() might not be available when errors occur so it should not be referenced in those scenarios.

--- applications/dashboard/views/deverror.master.php
+++ applications/dashboard/views/deverror.master.php
@@ -1,6 +1,6 @@
 <?php echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo Gdn::locale()->language(true); ?>">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
     <title>Fatal Error</title>
     <meta name="robots" content="noindex"/>

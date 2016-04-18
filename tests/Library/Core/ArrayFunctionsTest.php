db05e3f824ecd26f40f9d7a787f49a472b641916
initvector <ryan.p@vanillaforums.com>
2016-04-18 11:21:18 -0400

    Add basic unit tests for dbencode and dbdecode

--- tests/Library/Core/ArrayFunctionsTest.php
+++ tests/Library/Core/ArrayFunctionsTest.php
@@ -13,6 +13,27 @@ namespace VanillaTests\Library\Core;
 class ArrayFunctionsTest extends \PHPUnit_Framework_TestCase {
 
     /**
+     *
+     */
+    public function testDbEncodeArray() {
+        $data = ['Forum' => 'Vanilla'];
+        $encoded = dbencode($data);
+        $this->assertSame($data, dbdecode($encoded));
+    }
+
+    /**
+     *
+     */
+    public function testDbEncodeObject() {
+        $data = new \stdClass();
+        $data->Forum = 'Vanilla';
+        $encoded = dbencode($data);
+        $decoded = dbdecode($encoded);
+
+        $this->assertTrue(is_object($decoded) && $decoded->Forum === 'Vanilla');
+    }
+
+    /**
      * Encoding a value of null should be null.
      */
     public function testDbEncodeNull() {

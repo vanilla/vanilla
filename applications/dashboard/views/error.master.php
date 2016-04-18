b904b3e1655bd2ca9d3172fff29e2177dac79dd1
Ryan <initvector@users.noreply.github.com>
2016-04-18 07:50:16 -0400

    Add and update lang attributes on HTML tags (#3704)
    * Add and update lang attributes on HTML tags
    
    * Update Garden.Locale config checks to Gdn::locale
    
    * Update Locale::language to generate ISO 639-1 language codes

--- applications/dashboard/views/error.master.php
+++ applications/dashboard/views/error.master.php
@@ -4,7 +4,7 @@ if (ob_get_level()) {
 }
 echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo Gdn::locale()->language(true); ?>">
 <head>
     <title>Something has gone wrong.</title>
     <meta name="robots" content="noindex"/>

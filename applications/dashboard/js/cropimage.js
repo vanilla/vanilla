61b2b5858e1974ec9fd780c83d8bd7bb88e0a711
beckyvb <beckyvanbussel@gmail.com>
2016-04-22 10:02:40 -0400

    Start crop image on contentLoad.

--- applications/dashboard/js/cropimage.js
+++ applications/dashboard/js/cropimage.js
@@ -1,42 +1,51 @@
-jQuery(document).ready(function($) {
-    $('#save-crop').hide();
+var crop = {
 
-    if ($.Jcrop) {
-        // Set jcrop settings on source image.
-        $('#cropbox').Jcrop({
-            onChange: setPreviewAndCoords,
-            onSelect: setPreviewAndCoords,
-            onRelease: removePreviewAndCoords,
-            aspectRatio: 1
-        });
-    }
+    element: null,
+
+    start: function(element) {
+        //console.log(element);
+        if ($('#cropbox', element).length === 0) {
+            return;
+        }
+        crop.element = element;
+        $('#save-crop', crop.element).hide();
+        if ($.Jcrop) {
+            // Set jcrop settings on source image.
+            $('#cropbox', crop.element).Jcrop({
+                onChange: crop.setPreviewAndCoords,
+                onSelect: crop.setPreviewAndCoords,
+                onRelease: crop.removePreviewAndCoords,
+                aspectRatio: 1
+            });
+        }
+    },
 
     /**
      * Changes the preview cropped image based on the crop parameters and updates the form values.
      *
-     * @param c The source image element to crop.
+     * @param c The source image crop.element to crop.
      */
-    function setPreviewAndCoords(c) {
+    setPreviewAndCoords: function (c) {
         // Show the preview image and the 'save' button and hide the current cropped image.
-        $('#preview').show();
-        $('#current-crop').hide();
-        $('#save-crop').show();
+        $('#preview', crop.element).show();
+        $('#current-crop', crop.element).hide();
+        $('#save-crop', crop.element).show();
 
         // Get constants from the form's hidden inputs.
-        var cropSizeWidth = $('#Form_CropSizeWidth').val();
-        var cropSizeHeight = $('#Form_CropSizeHeight').val();
-        var sourceHeight = $('#Form_HeightSource').val();
-        var sourceWidth = $('#Form_WidthSource').val();
+        var cropSizeWidth = $('#Form_CropSizeWidth', crop.element).val();
+        var cropSizeHeight = $('#Form_CropSizeHeight', crop.element).val();
+        var sourceHeight = $('#Form_HeightSource', crop.element).val();
+        var sourceWidth = $('#Form_WidthSource', crop.element).val();
 
         // Set the ratios for scaling.
         var rx = cropSizeWidth / c.w;
         var ry = cropSizeHeight / c.h;
 
         // Set the form's hidden inputs based on the crop's position and size.
-        $('#Form_x').val(c.x);
-        $('#Form_y').val(c.y);
-        $('#Form_w').val(c.w);
-        $('#Form_h').val(c.h);
+        $('#Form_x', crop.element).val(c.x);
+        $('#Form_y', crop.element).val(c.y);
+        $('#Form_w', crop.element).val(c.w);
+        $('#Form_h', crop.element).val(c.h);
 
         // Calculate the preview image's css rules.
         $('#preview').css({
@@ -45,16 +54,22 @@ jQuery(document).ready(function($) {
             marginLeft: '-' + Math.round(rx * c.x) + 'px',
             marginTop: '-' + Math.round(ry * c.y) + 'px'
         });
-    }
+    },
 
     /**
      * Hides the cropping preview and save button and shows the current cropped image.
      */
-    function removePreviewAndCoords() {
-        if ($('#current-crop')) {
-            $('#current-crop').show();
-            $('#preview').hide();
-            $('#save-crop').hide();
+    removePreviewAndCoords: function() {
+        if ($('#current-crop', crop.element)) {
+            $('#current-crop', crop.element).show();
+            $('#preview', crop.element).hide();
+            $('#save-crop', crop.element).hide();
         }
     }
+
+}
+
+
+$(document).on('contentLoad', function(e) {
+    crop.start(e.target);
 });

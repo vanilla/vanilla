c3f12b82d989d301928755ea64b40e3e535ff948
beckyvb <beckyvanbussel@gmail.com>
2016-04-22 14:28:53 -0400

    Refactor funcTrigger to accept simple non-html strings.

--- js/global.js
+++ js/global.js
@@ -119,6 +119,7 @@
             var $elem = $($.parseHTML(html + '')); // Typecast html to a string and create a DOM node
             $(this)[func]($elem);
             $elem.trigger('contentLoad');
+            }
         });
         return this;
     };

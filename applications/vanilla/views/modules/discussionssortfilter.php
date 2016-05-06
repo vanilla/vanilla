582f949f5a323bc3af573626b4d85da7be4d762b
Becky Van Bussel <beckyvb@users.noreply.github.com>
2016-05-06 13:19:58 -0400

    Opt out of sort/filter rendering (#3865)
    * Opt out of sort/filter rendering.
    
    Allows the discussion module renderer to opt out of showing sorts or filters.
    
    * Make setters chainable in Discussion Sort Filter Module.

--- applications/vanilla/views/modules/discussionssortfilter.php
+++ applications/vanilla/views/modules/discussionssortfilter.php
@@ -1,18 +1,21 @@
 <?php if (!defined('APPLICATION')) exit(); ?>
 <div class="ButtonGroup discussion-sort-filter-module pull-left">
-    <span class="discussion-sorts">
-    <?php
-    foreach ($this->getSortData() as $sort) {
-        echo anchor(val('name', $sort), val('url', $sort), array(
+    <?php if ($this->showSorts()) { ?>
+        <span class="discussion-sorts">
+        <?php foreach ($this->getSortData() as $sort) {
+            echo anchor(val('name', $sort), val('url', $sort), array(
                     'rel' => val('rel', $sort),
-                    'class' => 'btn-default Button NavButton SortButton '.val('cssClass', $sort, '')
-                )).' ';
-    }
-    ?>
-    </span>
-    <span class="discussion-filters">
-    <?php foreach ($this->getFilterDropdowns() as $dropdown) {
-        echo $dropdown;
-    } ?>
-    </span>
+                    'class' => 'btn-default Button NavButton SortButton ' . val('cssClass', $sort, '')
+                )) . ' ';
+        }
+        ?>
+        </span>
+    <?php }
+    if ($this->showFilters()) { ?>
+        <span class="discussion-filters">
+        <?php foreach ($this->getFilterDropdowns() as $dropdown) {
+            echo $dropdown;
+        } ?>
+        </span>
+    <?php } ?>
 </div>

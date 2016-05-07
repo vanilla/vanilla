18eaf27feb8b325aafd9419aa325ee85ebd3253e
lincolnwebs <lincolnwebs@gmail.com>
2016-05-07 11:31:35 -0400

    2.2.1 badda boom

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if (PHP_VERSION_ID < 50300) {
 }
 
 define('APPLICATION', 'Vanilla');
-define('APPLICATION_VERSION', '2.2');
+define('APPLICATION_VERSION', '2.2.1');
 
 // Report and track all errors.
 error_reporting(E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR);

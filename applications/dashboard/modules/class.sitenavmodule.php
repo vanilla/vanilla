1aef6069f3882ebc1fec752827ed031a774f7da7
beckyvb <beckyvanbussel@gmail.com>
2016-05-18 13:05:56 -0400

    New Site Nav Module.

--- applications/dashboard/modules/class.sitenavmodule.php
+++ applications/dashboard/modules/class.sitenavmodule.php
@@ -13,32 +13,94 @@
  */
 class SiteNavModule extends NavModule {
 
+    const SECTION_GLOBAL = 'globals';
+    const SECTION_DEFAULT = 'defaults';
+
+    const LINKS_INDEX = 'links';
+    const GROUPS_INDEX = 'groups';
+
     /** @var array  */
-    protected static $customSections = array('EditProfile', 'Profile');
+    protected static $sectionItems = [];
+    protected static $initStaticFired = false;
 
-    /** @var string The section to */
-    protected $section;
-
-    protected $defaultEvent = 'default';
-
-    public function __construct() {
-        parent::__construct();
-    }
-
-    /**
-     * @return string
-     */
-    public function getSection() {
-        return $this->section;
-    }
-
-    /**
-     * @param string $section
-     * @return $this
-     */
-    public function setSection($section) {
-        $this->section = $section;
+    public function addLinkToSection($section, $text, $url, $key = '', $cssClass = '', $sort = [], $modifiers = [], $disabled = false) {
+        $args = func_get_args();
+        self::$sectionItems[strtolower($section)][self::LINKS_INDEX][] = $args;
         return $this;
+    }
+
+    public function addLinkToSectionIf($isAllowed, $section, $text, $url, $key = '', $cssClass = '', $sort = [], $modifiers = [], $disabled = false) {
+        if (!$this->isAllowed($isAllowed)) {
+            return $this;
+        } else {
+            return $this->addLinkToSection($section, $text, $url, $key, $cssClass, $sort, $modifiers, $disabled);
+        }
+    }
+
+    public function addGroupToSection($section, $text = '', $key = '', $cssClass = '', $sort = [], $modifiers = []) {
+        $args = func_get_args();
+        self::$sectionItems[strtolower($section)][self::GROUPS_INDEX][] = $args;
+        return $this;
+    }
+
+    public function addGroupToSectionIf($isAllowed, $section, $text = '', $key = '', $cssClass = '', $sort = [], $modifiers = []) {
+        if (!$this->isAllowed($isAllowed)) {
+            return $this;
+        } else {
+            return $this->addGroupToSection($section, $text, $key, $cssClass, $sort, $modifiers);
+        }
+    }
+
+    public function addLink($text, $url, $key = '', $cssClass = '', $sort = [], $modifiers = [], $disabled = false) {
+        $this->addLinkToSection(self::SECTION_DEFAULT, $text, $url, $key, $cssClass, $sort, $modifiers, $disabled);
+        return $this;
+    }
+
+    public function addLinkIf($isAllowed, $text, $url, $key = '', $cssClass = '', $sort = [], $modifiers = [], $disabled = false) {
+        if (!$this->isAllowed($isAllowed)) {
+            return $this;
+        } else {
+            return $this->addLink($text, $url, $key, $cssClass, $sort, $modifiers, $disabled);
+        }
+    }
+
+    public function addGroup($text = '', $key = '', $cssClass = '', $sort = [], $modifiers = []) {
+        $this->addGroupToSection(self::SECTION_DEFAULT, $text, $key, $cssClass, $sort, $modifiers);
+        return $this;
+    }
+
+    public function addGroupIf($isAllowed, $text = '', $key = '', $cssClass = '', $sort = [], $modifiers = []) {
+        if (!$this->isAllowed($isAllowed)) {
+            return $this;
+        } else {
+            return $this->addGroup($text, $key, $cssClass, $sort, $modifiers);
+        }
+    }
+
+    public function addLinkToGlobals($text, $url, $key = '', $cssClass = '', $sort = [], $modifiers = [], $disabled = false) {
+        $this->addLinkToSection(self::SECTION_GLOBAL, $text, $url, $key, $cssClass, $sort, $modifiers, $disabled);
+        return $this;
+    }
+
+    public function addLinkToGlobalsIf($isAllowed = true, $text, $url, $key = '', $cssClass = '', $sort = [], $modifiers = [], $disabled = false) {
+        if (!$this->isAllowed($isAllowed)) {
+            return $this;
+        } else {
+            return $this->addLinkToGlobals($text, $url, $key, $cssClass, $sort, $modifiers, $disabled);
+        }
+    }
+
+    public function addGroupToGlobals($text = '', $key = '', $cssClass = '', $sort = [], $modifiers = []) {
+        $this->addGroupToSection(self::SECTION_GLOBAL, $text, $key, $cssClass, $sort, $modifiers);
+        return $this;
+    }
+
+    public function addGroupToGlobalsIf($isAllowed, $text = '', $key = '', $cssClass = '', $sort = [], $modifiers = []) {
+        if (!$this->isAllowed($isAllowed)) {
+            return $this;
+        } else {
+            return $this->addGroupToGlobals($text, $key, $cssClass, $sort, $modifiers);
+        }
     }
 
     /**
@@ -47,25 +109,46 @@ class SiteNavModule extends NavModule {
      * @throws Exception
      */
     public function prepare() {
-        $section_found = false;
-        
-        // The module contains different links depending on its section.
-        foreach (self::$customSections as $section) {
-            if (InSection($section)) {
-                $this->fireEvent($section);
-                $section_found = true;
-                break;
+
+        if (!self::$initStaticFired) {
+            self::$initStaticFired = true;
+            $this->fireEvent('init');
+        }
+
+        $currentSections = Gdn_Theme::section('', 'get');
+        $currentSections = array_map('strtolower', $currentSections);
+
+
+        if (!empty(array_intersect(array_keys(self::$sectionItems), $currentSections))) {
+            $customMenu = true;
+        }
+
+        if (!$customMenu) {
+            $currentSections = [self::SECTION_DEFAULT];
+        }
+
+        // Add global items
+        $currentSections[] = self::SECTION_GLOBAL;
+
+        foreach ($currentSections as &$currentSection) {
+            if ($section = val(strtolower($currentSection), self::$sectionItems)) {
+                $this->addSectionItems($section);
+            }
+        }
+        return parent::prepare();
+    }
+
+    public function addSectionItems($section) {
+        if ($groups = val(self::GROUPS_INDEX, $section)) {
+            foreach ($groups as $group) {
+                parent::addGroup($group[1], $group[2]);
             }
         }
 
-        // If a section wasn't found then add the default nav.
-        if (!$section_found) {
-            $this->fireEvent($this->defaultEvent);
+        if ($links = val(self::LINKS_INDEX, $section)) {
+            foreach ($links as $link) {
+                parent::addLink($link[1], $link[2], $link[3], $link[4], $link[5], $link[6], $link[7]);
+            }
         }
-
-        // Fire an event for everything.
-        $this->fireEvent('all');
-
-        return parent::prepare();
     }
 }

fb0dd6072afa8f11fa2a4ec94db7220c070e1fdc
initvector <ryan.p@vanillaforums.com>
2016-05-18 20:32:25 -0400

    Remove timed redirect from /home/leaving

--- applications/dashboard/controllers/class.homecontroller.php
+++ applications/dashboard/controllers/class.homecontroller.php
@@ -100,16 +100,11 @@ class HomeController extends Gdn_Controller {
      * @param string $target
      */
     public function leaving($target = '') {
-        $delayInSeconds = 3;
         $this->setData('Target', $target);
 
         $this->removeCssFile('admin.css');
         $this->addCssFile('style.css');
         $this->addCssFile('vanillicon.css', 'static');
-        $this->Head->addTag('meta', [
-            'http-equiv' => 'refresh',
-            'content' => "{$delayInSeconds};url=".htmlspecialchars($target)
-        ]);
         $this->MasterView = 'default';
         $this->render();
     }

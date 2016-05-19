6afbb2e3e9ad6efb664e84251051726086685a6b
Todd Burry <todd@vanillaforums.com>
2016-05-18 20:59:17 -0400

    Chang the home/leave wording and remove the antiquated double space

--- applications/dashboard/views/home/leaving.php
+++ applications/dashboard/views/home/leaving.php
@@ -1,7 +1,7 @@
 <?php if (!defined('APPLICATION')) exit(); ?>
 <p>
     <?php echo sprintf(
-        t('You are now leaving %1$s.  Click the link to continue: %2$s.'),
+        t('You are now leaving %1$s. Click the link to continue to %2$s.'),
         c('Garden.Title', ''),
         anchor($this->data('Target'), $this->data('Target'))
     ); ?>

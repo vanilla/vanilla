20861df2c41e9f7a5e69da6fa85d197f11990b6a
beckyvb <beckyvanbussel@gmail.com>
2016-05-20 12:13:29 -0400

    Tweaks to form class to add wrapping to inputs. Also adds toggle.

--- applications/vanilla/views/vanillasettings/editcategory.php
+++ applications/vanilla/views/vanillasettings/editcategory.php
@@ -8,10 +8,11 @@ echo $this->Form->errors();
     <li>
         <?php
         echo $this->Form->label('Category', 'Name');
-        echo $this->Form->textBox('Name');
+        echo $this->Form->textBox('Name', ['Wrap' => true]);
         ?>
     </li>
-    <li id="UrlCode">
+    <li>
+        <div id="UrlCode">
         <?php
         echo wrap(t('Category Url:'), 'strong');
         echo ' ';
@@ -23,17 +24,18 @@ echo $this->Form->errors();
         echo anchor(t('edit'), '#', 'Edit');
         echo anchor(t('OK'), '#', 'Save SmallButton');
         ?>
+        </div>
     </li>
     <li>
         <?php
         echo $this->Form->label('Description', 'Description');
-        echo $this->Form->textBox('Description', array('MultiLine' => TRUE));
+        echo $this->Form->textBox('Description', array('MultiLine' => TRUE, 'Wrap' => true));
         ?>
     </li>
     <li>
         <?php
         echo $this->Form->label('Css Class', 'CssClass');
-        echo $this->Form->textBox('CssClass', array('MultiLine' => FALSE));
+        echo $this->Form->textBox('CssClass', array('MultiLine' => FALSE, 'Wrap' => true));
         ?>
     </li>
     <li>
@@ -61,47 +63,50 @@ echo $this->Form->errors();
     </li>
     <li>
         <?php
-        echo $this->Form->CheckBox('HideAllDiscussions', 'Hide from the recent discussions page.');
+        echo $this->Form->toggle('HideAllDiscussions', 'Hide from the recent discussions page.');
         ?>
     </li>
     <?php if ($this->ShowCustomPoints): ?>
         <li>
             <?php
-            echo $this->Form->CheckBox('CustomPoints', 'Track points for this category separately.');
+            echo $this->Form->toggle('CustomPoints', 'Track points for this category separately.');
             ?>
         </li>
     <?php endif; ?>
     <li>
         <?php
-        echo $this->Form->CheckBox('Archived', 'This category is archived.');
+        echo $this->Form->toggle('Archived', 'This category is archived.');
         ?>
     </li>
     <?php $this->fireEvent('AfterCategorySettings'); ?>
     <li>
         <?php
         if (count($this->PermissionData) > 0) {
-            echo $this->Form->CheckBox('CustomPermissions', 'This category has custom permissions.');
-
-            echo '<div class="CategoryPermissions">';
-
-            if (count($this->data('DiscussionTypes')) > 1) {
-                echo '<div class="P DiscussionTypes">';
-                echo $this->Form->label('Discussion Types');
-                foreach ($this->data('DiscussionTypes') as $Type => $Row) {
-                    echo $this->Form->CheckBox("AllowedDiscussionTypes[]", val('Plural', $Row, $Type), array('value' => $Type));
-                }
-                echo '</div>';
-            }
-
-            echo $this->Form->Simple(
-                $this->data('_PermissionFields', array()),
-                array('Wrap' => array('', ''), 'ItemWrap' => array('<div class="P">', '</div>')));
-
-            echo t('Check all permissions that apply for each role');
-            echo $this->Form->CheckBoxGridGroups($this->PermissionData, 'Permission');
-            echo '</div>';
+            echo $this->Form->toggle('CustomPermissions', 'This category has custom permissions.');
         }
         ?>
     </li>
 </ul>
+<?php
+echo '<div class="CategoryPermissions">';
+if (count($this->data('DiscussionTypes')) > 1) {
+echo '<div class="P DiscussionTypes">';
+    echo $this->Form->label('Discussion Types');
+    echo '<div class="checkbox-list">';
+    foreach ($this->data('DiscussionTypes') as $Type => $Row) {
+    echo $this->Form->CheckBox("AllowedDiscussionTypes[]", val('Plural', $Row, $Type), array('value' => $Type));
+    }
+    echo '</div>';
+    echo '</div>';
+}
+
+echo $this->Form->Simple(
+$this->data('_PermissionFields', array()),
+array('Wrap' => array('', ''), 'ItemWrap' => array('<div class="P">', '</div>')));
+
+echo t('Check all permissions that apply for each role');
+echo $this->Form->CheckBoxGridGroups($this->PermissionData, 'Permission');
+echo '</div>';
+?>
+
 <?php echo $this->Form->close('Save'); ?>

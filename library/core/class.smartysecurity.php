f0767d143415cba624d2daf458164be86d48d316
initvector <ryan.p@vanillaforums.com>
2016-05-23 16:07:51 -0400

    Extend custom Smarty_Security and override isTrustedPhpFunction

---
+++ library/core/class.smartysecurity.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Vanilla implementation of Smarty security policy object.
+ */
+class Gdn_SmartySecurity extends Smarty_Security {
+
+    /**
+     * Check if PHP function is trusted.
+     *
+     * @param  string $function_name
+     * @param  object $compiler compiler object
+     *
+     * @return boolean                 true if function is trusted
+     * @throws SmartyCompilerException if php function is not trusted
+     */
+    public function isTrustedPhpFunction($function_name, $compiler) {
+        if (isset($this->php_functions)) {
+            if (empty($this->php_functions)) {
+                return true;
+            } else {
+                return in_array(strtolower($function_name), $this->php_functions);
+            }
+        }
+
+        $compiler->trigger_template_error("PHP function '{$function_name}' not allowed by security setting");
+
+        return false; // should not, but who knows what happens to the compiler in the future?
+    }
+
+    /**
+     * Set allowed PHP functions.  Normalize casing for comparison.
+     *
+     * @param array $php_functions PHP functions to allow.
+     * @throws Gdn_UserException if $php_functions is not an array.
+     * @return array
+     */
+    public function setPhpFunctions($php_functions) {
+        if (!is_array($php_functions)) {
+            throw new Gdn_UserException('$php_functions must be an array.');
+        }
+
+        $this->php_functions = array_map('strtolower', $php_functions);
+    }
+}

5a265ade15c910d8686880b368caeb9e34c00341
Todd Burry <todd@vanillaforums.com>
2016-06-01 15:55:48 -0400

    Add the add category button

--- applications/vanilla/views/vanillasettings/categories.php
+++ applications/vanilla/views/vanillasettings/categories.php
@@ -1,4 +1,11 @@
+<div class="heading-bar">
+    <div class="actions">
+        <?php
+        echo anchor(t('Add Category'), 'vanilla/settings/addcategory', 'Button');
+        ?>
+    </div>
 <h1><?php echo t('Manage Categories'); ?></h1>
+</div>
 
 <?php
 writeCategoryBreadcrumbs($this->data('Ancestors', []));

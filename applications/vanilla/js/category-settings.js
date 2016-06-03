719b5580e6793340a1422717ac1c42e5d316b35f
Todd Burry <todd@vanillaforums.com>
2016-06-03 11:32:14 -0400

    Implement category tree sorting

--- applications/vanilla/js/category-settings.js
+++ applications/vanilla/js/category-settings.js
@@ -1,12 +1,43 @@
 (function(window, $, document) {
+    var massageTree = function (subtree) {
+        var result = [];
+
+        subtree.forEach(function (row) {
+            var resultRow = { CategoryID: row.id };
+
+            if (row.children) {
+                resultRow.Children = massageTree(row.children);
+            }
+            result.push(resultRow);
+        });
+        return result;
+    };
+
     $(document)
         .on('contentLoad', function(e) {
             $('.dd', e.target).nestable({
                 expandBtnHTML: '<button data-action="expand"><svg class="icon icon-16 icon-chevron-closed" viewBox="0 0 16 16"><use xlink:href="#chevron-closed" /></svg></button>',
                 collapseBtnHTML: '<button data-action="collapse"><svg class="icon icon-16 icon-chevron-open" viewBox="0 0 16 16"><use xlink:href="#chevron-open" /></svg></button>'
+            })
+            .on('dragEnd', function(event, item, source, destination, position) {
+                var tree = $(source).nestable('serialize');
+                var postTree = massageTree(tree);
+
+                $.ajax({
+                    type: "POST",
+                    url: gdn.url('/vanilla/settings/categoriestree.json'),
+                    data: {
+                        TransientKey: gdn.getMeta('TransientKey'),
+                        Subtree: JSON.stringify(postTree)
+                    },
+                    dataType: 'json',
+                    error: function (xhr) {
+                        gdn.informError(xhr);
+                    }
+                });
             });
 
-            console.log($('.dd', e.target).nestable('serialize'));
+            // console.log($('.dd', e.target).nestable('serialize'));
         })
         .on('click', '.js-displayas', function(e) {
             e.preventDefault();
@@ -42,7 +73,7 @@
                 },
                 dataType: 'json',
                 error: function (xhr) {
-                    gdn.informError(xhr, draft);
+                    gdn.informError(xhr);
                 },
                 success: function (data) {
                     setUI($content, data.DisplayAs);

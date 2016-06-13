9ab343cb0843f35e7c3713434b40c0c7f22fa169
beckyvb <beckyvanbussel@gmail.com>
2016-06-13 10:11:45 -0400

    Changes to HTML to comply with new dashboard design

--- views/index.php
+++ views/index.php
@@ -1,74 +1,77 @@
-<?php if (!defined('APPLICATION')) exit();
+<?php if (!defined('APPLICATION')) exit(); ?>
 
-echo wrap($this->data('Title'), 'h1');
-?>
+<div class="header-block">
+<?php echo wrap($this->data('Title'), 'h1');
+echo anchor(sprintf(t('Add %s'), t('Pocket')), 'settings/pockets/add', 'btn btn-primary'); ?>
+</div>
 <div class="Info"><?php echo t('Pockets allow you to add free-form HTML to various places around the application.'); ?>
+    <div class="table-wrap">
+        <table>
+            <tr>
+                <td width="200"><?php
+                if (C('Plugins.Pockets.ShowLocations')) {
+                    echo anchor(t('Hide Pocket Locations'), '/settings/pockets/hidelocations', 'SmallButton');
+                } else {
+                    echo anchor(t('Show Pocket Locations'), '/settings/pockets/showlocations', 'SmallButton');
+                }
+                ?></td>
+                <td><?php echo t('This option shows/hides the locations where pockets can go.', 'This option shows/hides the locations where pockets can go, but only for users that have permission to add/edit pockets. Try showing the locations and then visit your site.'); ?></td>
+            </tr>
+        </table>
+    </div>
+</div>
+<div class="table-wrap">
+    <table id="Pockets" class="AltColumns">
+        <thead>
+            <tr>
+                <th><?php echo t('Pocket'); ?></th>
+                <th><?php echo t('Body'); ?></th>
+                <th><?php echo t('Options'); ?></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($this->data('PocketData') as $PocketRow) {
+                 $MobileOnly = $PocketRow['MobileOnly'];
+                 $MobileNever = $PocketRow['MobileNever'];
+                 $NoAds = $PocketRow['Type'] == Pocket::TYPE_AD;
 
-<table>
-    <tr>
-        <td width="200"><?php
-        if (C('Plugins.Pockets.ShowLocations')) {
-            echo anchor(t('Hide Pocket Locations'), '/settings/pockets/hidelocations', 'SmallButton');
-        } else {
-            echo anchor(t('Show Pocket Locations'), '/settings/pockets/showlocations', 'SmallButton');
-        }
-        ?></td>
-        <td><?php echo t('This option shows/hides the locations where pockets can go.', 'This option shows/hides the locations where pockets can go, but only for users that have permission to add/edit pockets. Try showing the locations and then visit your site.'); ?></td>
-    </tr>
-    <tr>
-        <td><?php echo anchor(sprintf(t('Add %s'), t('Pocket')), 'settings/pockets/add', 'SmallButton'); ?></td>
-        <td><?php echo t('Add a new Pocket to your site.'); ?></td>
-    </tr>
-</table></div>
-<table id="Pockets" class="AltColumns">
-    <thead>
-        <tr>
-            <th><?php echo t('Pocket'); ?></th>
-            <th><?php echo t('Page'); ?></th>
-            <th class="Alt"><?php echo t('Location'); ?></th>
-            <th><?php echo t('Body'); ?></th>
-            <th class="Alt"><?php echo t('Notes'); ?></th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-        foreach ($this->data('PocketData') as $PocketRow) {
-        	 $MobileOnly = $PocketRow['MobileOnly'];
-        	 $MobileNever = $PocketRow['MobileNever'];
-             $NoAds = $PocketRow['Type'] == Pocket::TYPE_AD;
+                echo '<tr'.($PocketRow['Disabled'] != Pocket::DISABLED ? '' : ' class="Disabled"').'>';
 
-            echo '<tr'.($PocketRow['Disabled'] != Pocket::DISABLED ? '' : ' class="Disabled"').'>';
-
-            echo '<td>',
-                '<strong>', htmlspecialchars($PocketRow['Name']), '</strong>',
-                '<div>',
-                anchor('Edit', "/settings/pockets/edit/{$PocketRow['PocketID']}"),
-                ' <span>|</span> ',
-                anchor('Delete', "/settings/pockets/delete/{$PocketRow['PocketID']}", 'Popup'),
-                '</div>',
-                '</td>';
-
-            echo '<td>',htmlspecialchars($PocketRow['Page']), '</td>';
-            echo '<td  class="Alt">', htmlspecialchars($PocketRow['Location']);
-            if ($MobileOnly) {
-            	echo '<br>(', t('Shown only on mobile'), ')';
+                echo '<td>',
+                    '<strong>', htmlspecialchars($PocketRow['Name']), '</strong>';
+                    if ($notes = $PocketRow['Notes']) {
+                        echo '<div class="info pocket-notes">'.sprintf(t('%s: %s'), t('Notes'), $notes).'</div>';
+                    }
+                    if ($page = htmlspecialchars($PocketRow['Page'])) {
+                        echo '<div class="info pocket-page">'.sprintf(t('%s: %s'), t('Page'), $page).'</div>';
+                    }
+                    if ($location = htmlspecialchars($PocketRow['Location'])) {
+                        echo '<div class="info pocket-location">'.sprintf(t('%s: %s'), t('Location'), $location).'</div>';
+                    }
+                    if ($MobileOnly) {
+                        echo '<div class="info">(', t('Shown only on mobile'), ')</div>';
+                    }
+                    if ($MobileNever) {
+                        echo '<div class="info">(', t('Hidden for mobile'), ')</div>';
+                    }
+                    if ($MobileNever && $MobileOnly) {
+                        echo '<div class="info">(', t('Hidden for everything!'), ')</div>';
+                    }
+                    if ($NoAds) {
+                        echo '<div class="info">(', t('Users with the no ads permission will not see this pocket.'), ')</div>';
+                    }
+                    echo'</div>';
+                    '</td>';
+                echo '<td><pre style="white-space: pre-wrap;">', nl2br(htmlspecialchars(substr($PocketRow['Body'], 0, 200))), '</pre></td>';
+                echo '<td><div class="btn-group">',
+                    anchor('Edit', "/settings/pockets/edit/{$PocketRow['PocketID']}", 'btn btn-edit'),
+                    anchor('Delete', "/settings/pockets/delete/{$PocketRow['PocketID']}", 'Popup btn btn-delete'),
+                    '</div></td>';
+                echo "</tr>\n";
             }
-            if ($MobileNever) {
-            	echo '<br>(', t('Hidden for mobile'), ')';
-            }
-            if ($MobileNever && $MobileOnly) {
-            	echo '<br><b>(', t('Hidden for everything!'), ')</b>';
-            }
-            if ($NoAds) {
-                echo '<br>(', t('Users with the no ads permission will not see this pocket.'), ')';
-            }
-            echo'</td>';
-            echo '<td>', nl2br(htmlspecialchars(substr($PocketRow['Body'], 0, 200))), '</td>';
-            echo '<td  class="Alt">', $PocketRow['Notes'], '</td>';
-
-            echo "</tr>\n";
-        }
-        ?>
-    </tbody>
-</table>
+            ?>
+        </tbody>
+    </table>
+</div>
 <?php echo $this->Form->close(''); ?>

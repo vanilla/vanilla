b820d712c62eee9f52187a5b0e76097ca218d0fc
Allan Jardine <allan.jardine@sprymedia.co.uk>
2016-06-16 11:39:16 +0100

    Fix: Advanced Editor upload failed if MySQL was in strict mode
    If MySQL is in strict mode it will reject uploads from the advanced
    editor with the following error
    
    > PHP Fatal error: Incorrect integer value: '' for column 'ThumbWidth'
      at row 1|Gdn_Database|Query|insert GDN_Media
    
    This is because the advanced editor is trying to insert a string value
    into an integer. Setting a default value of 0 for the thumb and image
    height and width values corrects this.
    
    * Forum thread on this topic:
      https://vanillaforums.org/discussion/32255/advanced-editor-upload-failing-incorrect-integer-value#latest
     master

--- plugins/editor/class.editor.plugin.php
+++ plugins/editor/class.editor.plugin.php
@@ -750,10 +750,10 @@ class EditorPlugin extends Gdn_Plugin {
 
             // Determine if image, and thus requires thumbnail generation, or simply saving the file.
             // Not all files will be images.
-            $thumbHeight = '';
-            $thumbWidth = '';
-            $imageHeight = '';
-            $imageWidth = '';
+            $thumbHeight = 0;
+            $thumbWidth = 0;
+            $imageHeight = 0;
+            $imageWidth = 0;
             $thumbPathParsed = array('SaveName' => '');
             $thumbUrl = '';
 

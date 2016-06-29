a7971e148a2f5b6b06638dd9ffbf7632ce33616e
initvector <ryan.p@vanillaforums.com>
2016-06-29 12:30:25 -0400

    Add jQuery pop-in functionality

---
+++ js/library/jquery.popin.js
@@ -0,0 +1,30 @@
+/**
+ * Popin elements
+ * 
+ * Allow elements with the .js-popin class to asynchronously load their contents from the server.
+ */
+"use strict";
+
+(function() {
+    $(document).on("contentLoad", function(event) {
+        var $target = $(event.target);
+
+        $target.find(".js-popin").each(function(index, element){
+            var $element = $(element);
+            var rel = $element.attr("rel");
+
+            if (typeof rel === "string" && rel.length !== 0) {
+                $element.addClass("Loading");
+                $.get(
+                    gdn.url(rel),
+                    { DeliveryType: "VIEW" },
+                    function(data, textStatus, jqXHR) {
+                        $element.removeClass("Loading");
+                        $element.html(data);
+                    },
+                    "html"
+                );
+            }
+        });
+    })
+})(jQuery);

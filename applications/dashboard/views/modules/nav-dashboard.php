b3e5b57dd0e60b62e9e7bbebeb942c35c10a68e5
beckyvb <beckyvanbussel@gmail.com>
2016-07-04 17:22:57 -0400

    Nav caret transitions.

--- applications/dashboard/views/modules/nav-dashboard.php
+++ applications/dashboard/views/modules/nav-dashboard.php
@@ -5,7 +5,7 @@ if (!function_exists('renderDashboardNav')) {
     {
         foreach ($items as $item) {
             if (val('type', $item) == 'group') { ?>
-                <h4 class="nav-heading"><a data-toggle="collapse" class="collapsed" href="#<?php echo trim(val('headerCssClass', $item))?>"><?php echo val('text', $item); ?></a></h4>
+                <h4 class="nav-heading"><a data-toggle="collapse" class="" href="#<?php echo trim(val('headerCssClass', $item))?>"><?php echo val('text', $item); ?></a></h4>
                 <ul class="nav nav-pills nav-stacked collapse in" id="<?php echo trim(val('headerCssClass', $item)); ?>">
                 <?php
                 if (val('items', $item)) {

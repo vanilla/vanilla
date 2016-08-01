965a1c0b6818d28d2cded46688527deea02d9295
initvector <ryan.p@vanillaforums.com>
2016-08-01 16:03:23 -0400

    Only run IP address migration when not in capture-only mode

--- applications/dashboard/settings/structure.php
+++ applications/dashboard/settings/structure.php
@@ -18,6 +18,8 @@ if (!isset($Explicit)) {
     $Explicit = false;
 }
 
+$captureOnly = isset($Structure) && val('CaptureOnly', $Structure);
+
 $Database = Gdn::database();
 $SQL = $Database->sql();
 $Construct = $Database->structure();
@@ -767,7 +769,7 @@ $Construct
     ->set($Explicit, $Drop);
 
 // If the AllIPAddresses column exists, attempt to migrate legacy IP data to the UserIP table.
-if ($AllIPAddressesExists) {
+if (!$captureOnly && $AllIPAddressesExists) {
     $limit = 10000;
     $resetBatch = 100;
 

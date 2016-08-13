633d57a4e0cb767c1ac5adba6b43aa076a47e7fc
beckyvb <beckyvanbussel@gmail.com>
2016-08-13 12:02:49 -0400

    Add styling for captcha settings on registration page.

--- plugins/recaptcha/views/settings/registration.php
+++ plugins/recaptcha/views/settings/registration.php
@@ -1,22 +1,23 @@
-<li id="CaptchaSettings">
-    <div
-        class="Info"><?php echo t('The basic registration form requires new users to copy text from a CAPTCHA image.', '<strong>The basic registration form requires</strong> new users to use reCAPTCHA to keep spammers out of the site. You need an account at <a href="https://www.google.com/recaptcha/">https://www.google.com/recaptcha/</a>. Signing up is FREE and easy. Once you have signed up, come back here and enter the following settings:'); ?></div>
-    <table class="Label AltColumns">
-        <thead>
-        <tr>
-            <th><?php echo t('Key Type'); ?></th>
-            <th class="Alt"><?php echo t('Key Value'); ?></th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr class="Alt">
-            <th><?php echo t('Public Key'); ?></th>
-            <td class="Alt"><?php echo $this->Form->textBox('Recaptcha.PublicKey'); ?></td>
-        </tr>
-        <tr>
-            <th><?php echo t('Private Key'); ?></th>
-            <td class="Alt"><?php echo $this->Form->textBox('Recaptcha.PrivateKey'); ?></td>
-        </tr>
-        </tbody>
-    </table>
-</li>
+<div id="CaptchaSettings">
+    <div class="alert alert-warning padded"><?php echo t('The basic registration form requires new users to copy text from a CAPTCHA image.', '<strong>The basic registration form requires</strong> new users to use reCAPTCHA to keep spammers out of the site. You need an account at <a href="https://www.google.com/recaptcha/">https://www.google.com/recaptcha/</a>. Signing up is FREE and easy. Once you have signed up, come back here and enter the following settings:'); ?></div>
+    <div class="table-wrap">
+        <table>
+            <thead>
+            <tr>
+                <th><?php echo t('Key Type'); ?></th>
+                <th><?php echo t('Key Value'); ?></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th><?php echo t('Public Key'); ?></th>
+                <td class="Alt"><?php echo $this->Form->textBox('Recaptcha.PublicKey'); ?></td>
+            </tr>
+            <tr>
+                <th><?php echo t('Private Key'); ?></th>
+                <td class="Alt"><?php echo $this->Form->textBox('Recaptcha.PrivateKey'); ?></td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>

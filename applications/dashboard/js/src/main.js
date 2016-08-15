e41e772fb9193b92a25de1a7d8233ad8346bc4e1
beckyvb <beckyvanbussel@gmail.com>
2016-08-15 16:44:56 -0400

    Maintain an aspect ratio on the theme image wraps while maintaining responsive scaling of images.

--- applications/dashboard/js/src/main.js
+++ applications/dashboard/js/src/main.js
@@ -559,6 +559,20 @@
         }
     }
 
+    function themeInit(element) {
+        var aspectRatio = [2, 3];
+        $('.js-themes .image-wrap', element).each(function() {
+            var width = $(this).width();
+            $(this).height(aspectRatio[0] * width / aspectRatio[1])
+        });
+        $(window).resize(function() {
+            $('.js-themes .image-wrap', element).each(function() {
+                var width = $(this).width();
+                $(this).height(aspectRatio[0] * width / aspectRatio[1])
+            });
+        });
+    }
+
     $(document).on('contentLoad', function(e) {
         prettyPrintInit(e.target);
         aceInit(e.target);
@@ -570,6 +584,7 @@
         drawerInit(e.target);
         icheckInit(e.target);
         expanderInit(e.target);
+        themeInit(e.target);
 
     });
 

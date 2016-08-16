9bb6762e7dc0e2e1fdab63c6139cc5ba3ac54db7
beckyvb <beckyvanbussel@gmail.com>
2016-08-16 08:42:56 -0400

    Tweak ace editor settings to remove console warning and optimize spacer image.

--- applications/dashboard/js/src/main.js
+++ applications/dashboard/js/src/main.js
@@ -60,6 +60,7 @@
             textarea.hide();
 
             var editor = ace.edit('editor-' + formID);
+            editor.$blockScrolling = Infinity;
             editor.getSession().setMode('ace/mode/' + mode);
             editor.setTheme('ace/theme/clouds');
 

75e0ac9c735d131ed3e23443da7ad195e06436ce
beckyvb <beckyvanbussel@gmail.com>
2016-08-18 14:40:28 -0400

    Dashboard v3: Avatar settings.

--- applications/dashboard/views/settings/avatars.php
+++ applications/dashboard/views/settings/avatars.php
@@ -1,50 +1,74 @@
 <?php if (!defined('APPLICATION')) exit(); ?>
 <h1><?php echo t('Avatars'); ?></h1>
-    <div class="avatar-default">
-    <h2><?php echo t('Default Avatar'); ?></h2>
-    <div class="avatars">
-        <div class="padded current-avatar">
-            <?php echo img($this->data('avatar'), array('style' => 'width: '.c('Garden.Thumbnail.Size').'px; height: '.c('Garden.Thumbnail.Size').'px;')); ?>
+<div class="avatar-default">
+    <div class="avatars padded flex">
+        <div class="current-avatar flex">
+            <div class="image-wrap">
+                <?php echo img($this->data('avatar'), array('style' => 'width: '.c('Garden.Thumbnail.Size').'px; height: '.c('Garden.Thumbnail.Size').'px;')); ?>
+            </div>
+            <div class="default-avatar-label-wrap padded-left">
+                <div class="label"><?php echo t('Default Avatar'); ?></div>
+                <?php echo t('This is the avatar that will be used if the user doesn’t upload their own avatar image.'); ?>
+            </div>
         </div>
-        <div class="change-avatar">
-            <?php echo anchor('Change', '/dashboard/settings/defaultavatar', 'Button Primary'); ?>
+        <?php
+        $disabled = '';
+        if (!c('Garden.DefaultAvatar')) {
+            $disabled = 'disabled ';
+        }
+        ?>
+        <div class="options padded-left flex">
+            <?php echo anchor(t('Remove'), '/dashboard/settings/removedefaultavatar/', $disabled.'btn btn-primary js-hijack js-modal-confirm remove-avatar'); ?>
+            <?php echo anchor(t('Change'), '/dashboard/settings/defaultavatar', 'btn btn-primary change-avatar'); ?>
         </div>
     </div>
 </div>
-<div class="avatar-advanced padded">
+<div class="avatar-advanced">
+    <div class="subheading-block">
+        <div class="subheading"><?php echo t('Avatar Settings'); ?></div>
+        <div><?php echo t('Change the sizes that avatar images are saved at.').' '.t('Changes will apply to newly uploaded avatars only.'); ?></div>
+    </div>
     <?php echo $this->Form->open(array('enctype' => 'multipart/form-data')); ?>
-    <h2><?php echo t('Avatar sizes'); ?></h2>
     <?php echo $this->Form->errors(); ?>
     <ul>
         <li class="form-group row">
-            <div class="label-wrap">
-            <?php
-            echo $this->Form->label('Thumbnail Size', 'Garden.Thumbnail.Size');
-            echo wrap(t('Avatars will have their thumbnails saved at this size.').t('Changes will apply to newly uploaded avatars only.'), 'div', array('class' => 'info')); ?>
+            <div class="label-wrap-wide">
+                <?php
+                echo $this->Form->label('Thumbnail Size', 'Garden.Thumbnail.Size');
+                echo wrap(t('Avatars will have their thumbnails saved at this size.'), 'div', array('class' => 'info')); ?>
             </div>
-            <div class="input-wrap">
-            <?php echo $this->Form->textBox('Garden.Thumbnail.Size'); ?>
+            <div class="input-wrap-right">
+                <div class="textbox-suffix" data-suffix="<?php echo t('px'); ?>">
+                    <?php echo $this->Form->textBox('Garden.Thumbnail.Size'); ?>
+                </div>
+            </div>
         </li>
         <li class="form-group row">
-            <div class="label-wrap">
-            <?php
-            echo $this->Form->label('Max Avatar Width', 'Garden.Profile.MaxWidth');
-            echo wrap(t('Avatars will be scaled down if they exceed this width.').t('Changes will apply to newly uploaded avatars only.'), 'div', array('class' => 'info')); ?>
+            <div class="label-wrap-wide">
+                <?php
+                echo $this->Form->label('Max Avatar Width', 'Garden.Profile.MaxWidth');
+                echo wrap(t('Avatars will be scaled down if they exceed this width.'), 'div', array('class' => 'info')); ?>
             </div>
-            <div class="input-wrap">
-            <?php echo $this->Form->textBox('Garden.Profile.MaxWidth'); ?>
+            <div class="input-wrap-right">
+                <div class="textbox-suffix" data-suffix="<?php echo t('px'); ?>">
+                    <?php echo $this->Form->textBox('Garden.Profile.MaxWidth'); ?>
+                </div>
+            </div>
         </li>
         <li class="form-group row">
-            <div class="label-wrap">
-            <?php
-            echo $this->Form->label('Max Avatar Height', 'Garden.Profile.MaxHeight');
-            echo wrap(t('Avatars will be scaled down if they exceed this height.').t('Changes will apply to newly uploaded avatars only.'), 'div', array('class' => 'info')); ?>
+            <div class="label-wrap-wide">
+                <?php
+                echo $this->Form->label('Max Avatar Height', 'Garden.Profile.MaxHeight');
+                echo wrap(t('Avatars will be scaled down if they exceed this height.'), 'div', array('class' => 'info')); ?>
             </div>
-            <div class="input-wrap">
-            <?php echo $this->Form->textBox('Garden.Profile.MaxHeight'); ?>
+            <div class="input-wrap-right">
+                <div class="textbox-suffix" data-suffix="<?php echo t('px'); ?>">
+                    <?php echo $this->Form->textBox('Garden.Profile.MaxHeight'); ?>
+                </div>
+            </div>
         </li>
     </ul>
-    <div class="buttons form-footer">
+    <div class="form-footer">
         <?php echo $this->Form->close('Save', '', array('class' => 'Button Primary')); ?>
     </div>
 </div>
--- applications/dashboard/views/settings/defaultavatar.php
+++ applications/dashboard/views/settings/defaultavatar.php
@@ -1,27 +1,30 @@
 <?php if (!defined('APPLICATION')) exit(); ?>
-<h1><?php echo t('Default Avatar'); ?></h1>
 <?php
 echo $this->Form->open(array('enctype' => 'multipart/form-data'));
 echo $this->Form->errors();
-$defaultAvatar = c('Garden.DefaultAvatar');
 ?>
+<div class="header-block">
+    <div class="title-block">
+        <?php echo anchor(dashboardSymbol('chevron-left'), "/dashboard/settings/avatars", 'btn btn-icon btn-return', ['aria-label' => t('Return')]); ?>
+        <h1><?php echo t('Default Avatar'); ?></h1>
+    </div>
+    <?php echo $this->Form->input('DefaultAvatar', 'file', array('class' => 'js-new-avatar-upload Hidden')); ?>
+    <div class="js-new-avatar btn btn-primary"><?php echo t('Upload New Avatar'); ?></div>
+</div>
 <?php
 if ($this->data('crop')) {
     echo $this->data('crop');
 } else { ?>
     <div class="avatars">
-        <div class="Padded current-avatar">
+        <div class="padded current-avatar">
             <?php echo img($this->data('avatar'), array('style' => 'width: '.c('Garden.Thumbnail.Size').'px; height: '.c('Garden.Thumbnail.Size').'px;')); ?>
         </div>
     </div>
 <?php } ?>
-<div class="btn-group">
-<?php echo $this->Form->input('DefaultAvatar', 'file', array('class' => 'js-new-avatar-upload Hidden')); ?>
-    <div class="js-new-avatar Button">Upload New Avatar</div>
-<?php
-    if ($defaultAvatar) {
-        echo anchor(t('Remove Default Avatar'), '/dashboard/settings/removedefaultavatar/', 'btn btn-primary js-hijack js-modal-confirm');
-    } ?>
+<?php if ($this->data('crop')) { ?>
+<div class="form-footer js-modal-footer">
+    <?php echo $this->Form->button('Save', ['class' => 'js-save-avatar-crop btn btn-primary']); ?>
 </div>
+<?php } ?>
 <?php echo $this->Form->close(); ?>
 

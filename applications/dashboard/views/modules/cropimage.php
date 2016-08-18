9fe0233e2f0214d9d2e6b0568edcd18e18e9d1a7
beckyvb <beckyvanbussel@gmail.com>
2016-08-18 14:40:28 -0400

    Dashboard v3: Avatar settings.

--- applications/dashboard/views/modules/cropimage.php
+++ applications/dashboard/views/modules/cropimage.php
@@ -1,15 +1,15 @@
-<div class="Info">
+<div class="padded">
     <?php echo t('Define Thumbnail', 'Click and drag across the picture to define your thumbnail.');?>
 </div>
 <div class="box-crop">
     <div class="box-image box-source">
         <?php
-        echo '<div>'.t('Picture').'</div>';
+        echo '<div class="crop-label">'.t('Picture').'</div>';
         echo img($this->getSourceImageUrl(), array('id' => 'cropbox')); ?>
     </div>
     <div class="box-image box-cropped-image">
-        <?php echo '<div>'.t('Thumbnail').'</div>'; ?>
-        <div class="thumbnail-preview" style="<?php echo 'width:'.$this->getWidth().'px;height:'.$this->getHeight().'px;'; ?>overflow:hidden;">
+        <?php echo '<div class="crop-label">'.t('Thumbnail').'</div>'; ?>
+        <div class="thumbnail-preview padded-left" style="<?php echo 'width:'.$this->getWidth().'px;height:'.$this->getHeight().'px;'; ?>overflow:hidden;">
             <?php
             echo img($this->getExistingCropUrl(), array('id' => 'current-crop', 'style' => 'min-width: '.$this->getWidth().'px; min-height: '.$this->getHeight().'px;'));
             echo img($this->getSourceImageUrl(), array('id' => 'preview')); ?>

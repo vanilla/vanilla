1ff7cdb96914e4195ede7de6ce917030b1220f38
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2016-08-23 13:43:00 -0400

    Added new smarty function to fetch HomePage title

---
+++ library/vendors/SmartyPlugins/function.homepage_title.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @copyright 2009-2016 Vanilla Forums Inc.
+ * @license http://www.opensource.org/licenses/gpl-2.0.php GNU GPL v2
+ * @package vanilla-smarty
+ * @since 2.0
+ */
+
+/**
+ * Returns the  custom text from a theme.
+ *
+ * @param array $Params The parameters passed into the function. This currently takes no parameters.
+ *  - <b>code</b>: The text code set in the theme's information.
+ *  - <b>default</b>: The default text if the user hasn't overridden.
+ * @param Smarty $Smarty The smarty object rendering the template.
+ * @return The text.
+ */
+function smarty_function_home_page_title($Params, &$Smarty) {
+    return c('Garden.HomepageTitle', '');
+}

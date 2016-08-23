16dc48564e8869960324aa017c67a9c9af52f81b
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2016-08-23 13:48:13 -0400

    Renamed function, removed unused params and improved comments

--- library/vendors/SmartyPlugins/function.homepage_title.php
+++ library/vendors/SmartyPlugins/function.homepage_title.php
@@ -4,17 +4,11 @@
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU GPL v2
  * @package vanilla-smarty
  * @since 2.0
- */
-
-/**
- * Returns the  custom text from a theme.
  *
- * @param array $Params The parameters passed into the function. This currently takes no parameters.
- *  - <b>code</b>: The text code set in the theme's information.
- *  - <b>default</b>: The default text if the user hasn't overridden.
- * @param Smarty $Smarty The smarty object rendering the template.
- * @return The text.
+ * Returns Homepage Title from config
+ *
+ * @return string
  */
-function smarty_function_home_page_title($Params, &$Smarty) {
+function smarty_function_homepage_title() {
     return c('Garden.HomepageTitle', '');
 }

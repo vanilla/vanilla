43a52f09b7823a5281b3ef12bf5f535c3a0547c4
initvector <ryan.p@vanillaforums.com>
2016-08-25 17:36:07 -0400

    Tweak markup in flat category module view

--- applications/vanilla/views/modules/flatcategory.php
+++ applications/vanilla/views/modules/flatcategory.php
@@ -1,19 +1,23 @@
 <?php if (!defined('APPLICATION')) exit(); ?>
 
-<?php if ($this->data('Layout') === 'table'): ?>
-    <?php writeCategoryTable($this->data('Categories', []), 1); ?>
-<?php else: ?>
-<ul class="DataList CategoryList">
-    <?php
-    foreach ($this->data('Categories') as $category) {
-        writeListItem($category);
-    }
-    ?>
-</ul>
-<?php endif; ?>
+<div class="Box BoxFlatCategory">
+    <h2><?php echo htmlspecialchars($this->data('ParentCategory.Name')); ?></h2>
 
-<div><?php echo wrap(
-    anchor(htmlspecialchars(t('View All')), $this->data('ParentCategory.Url')),
-    'span',
-    ['class' => 'MItem Category']
-); ?></div>
+    <?php if ($this->data('Layout') === 'table'): ?>
+        <?php writeCategoryTable($this->data('Categories', []), 1); ?>
+    <?php else: ?>
+    <ul class="DataList CategoryList">
+        <?php
+        foreach ($this->data('Categories') as $category) {
+            writeListItem($category);
+        }
+        ?>
+    </ul>
+    <?php endif; ?>
+
+    <div class="MoreWrap"><?php echo wrap(
+        anchor(htmlspecialchars(t('View All')), $this->data('ParentCategory.Url')),
+        'span',
+        ['class' => 'MItem Category']
+    ); ?></div>
+</div>

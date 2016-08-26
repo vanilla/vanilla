effc1a768f55e6994dc27d5b24f7b514b10d605d
beckyvb <beckyvanbussel@gmail.com>
2016-08-26 16:44:00 -0400

    Fix doctype.

--- applications/dashboard/views/admin.master.php
+++ applications/dashboard/views/admin.master.php
@@ -3,7 +3,7 @@
 $this->fireAs('dashboard')->fireEvent('render');
 ?>
 <?php echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo htmlspecialchars(Gdn::locale()->Locale); ?>">
 <head>
     <?php $this->renderAsset('Head'); ?>

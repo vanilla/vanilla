c3ed09882f4f812dbe0829ffefed3633409f0f46
lincolnwebs <lincolnwebs@gmail.com>
2016-08-29 11:46:17 -0400

    Coding standards in dbacontroller

--- applications/dashboard/controllers/class.dbacontroller.php
+++ applications/dashboard/controllers/class.dbacontroller.php
@@ -166,7 +166,7 @@ class DbaController extends DashboardController {
         $this->permission('Garden.Settings.Manage');
 
         if ($this->Request->isAuthenticatedPostBack()) {
-            if (ValidateRequired($this->Form->getFormValue('DefaultUserRole'))) {
+            if (validateRequired($this->Form->getFormValue('DefaultUserRole'))) {
                 $this->Model->fixUserRole($this->Form->getFormValue('DefaultUserRole'));
                 $this->setData('CompletedFix', true);
             }
@@ -193,6 +193,11 @@ class DbaController extends DashboardController {
         $this->render('Job');
     }
 
+    /**
+     * Set a job.
+     *
+     * @param string $Name
+     */
     protected function _setJob($Name) {
         $Args = array_change_key_case($this->ReflectArgs);
         $Url = "/dba/{$this->RequestMethod}.json?".http_build_query($Args);

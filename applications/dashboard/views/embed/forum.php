adcce7c28c450b646dbee0e37e2971f86a59bf59
beckyvb <beckyvanbussel@gmail.com>
2016-09-01 08:46:45 -0400

    Fix double url-ing in embed section.

--- applications/dashboard/views/embed/forum.php
+++ applications/dashboard/views/embed/forum.php
@@ -31,10 +31,10 @@
 </div>
 <?php
 $nav = new NavModule();
-$nav->addLink(t('Vanilla Plugin for WordPress'), url('embed/wordpress'), 'embed.wordpress', '', [], ['icon' => dashboardSymbol('plugin'), 'description' => t('Use Vanilla\'s Wordpress plugin if you want to embed in WordPress site.')]);
-$nav->addLink(t('Universal Forum Embed Code'), url('embed/universal'), 'embed.universal', '', [], ['icon' => dashboardSymbol('code'), 'description' => t('Use the forum embed code to embed the entire forum in a non-WordPress site.')]);
-$nav->addLink(t('Universal Comment Embed Code'), url('embed/comments'), 'embed.comments', '', [], ['icon' => dashboardSymbol('code-bubble'), 'description' => t('Use the comment embed code to embed Vanilla comments into a non-WordPress site.')]);
-$nav->addLink(t('Embed Settings'), url('embed/settings'), 'embed.settings', '', [], ['icon' => dashboardSymbol('settings'), 'description' => t('Use the comment embed code to embed Vanilla comments into a non-WordPress site.')]);
+$nav->addLink(t('Vanilla Plugin for WordPress'), 'embed/wordpress', 'embed.wordpress', '', [], ['icon' => dashboardSymbol('plugin'), 'description' => t('Use Vanilla\'s Wordpress plugin if you want to embed in WordPress site.')]);
+$nav->addLink(t('Universal Forum Embed Code'), 'embed/universal', 'embed.universal', '', [], ['icon' => dashboardSymbol('code'), 'description' => t('Use the forum embed code to embed the entire forum in a non-WordPress site.')]);
+$nav->addLink(t('Universal Comment Embed Code'), 'embed/comments', 'embed.comments', '', [], ['icon' => dashboardSymbol('code-bubble'), 'description' => t('Use the comment embed code to embed Vanilla comments into a non-WordPress site.')]);
+$nav->addLink(t('Embed Settings'), 'embed/settings', 'embed.settings', '', [], ['icon' => dashboardSymbol('settings'), 'description' => t('Use the comment embed code to embed Vanilla comments into a non-WordPress site.')]);
 $nav->setView('nav-adventure');
 
 echo $nav;

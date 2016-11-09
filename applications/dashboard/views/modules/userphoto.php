c330a99d5808d3ad8e4205f3ba2745cb97210a88
lincolnwebs <lincolnwebs@gmail.com>
2016-11-09 17:25:05 -0500

    Fix standards in userphoto view

--- applications/dashboard/views/modules/userphoto.php
+++ applications/dashboard/views/modules/userphoto.php
@@ -1,41 +1,43 @@
 <?php if (!defined('APPLICATION')) exit();
+
 $User = val('User', Gdn::controller());
 if (!$User && Gdn::session()->isValid()) {
     $User = Gdn::session()->User;
 }
 
-if (!$User)
+if (!$User) {
     return;
+}
 
 $Photo = $User->Photo;
 if ($Photo) {
-    if (!IsUrl($Photo)) {
-        $Photo = Gdn_Upload::url(changeBasename($Photo, 'p%s'));
-    }
+    $Photo = (isUrl($Photo)) ?: Gdn_Upload::url(changeBasename($Photo, 'p%s'));
 } else {
     $Photo = UserModel::getDefaultAvatarUrl($User, 'profile');
 }
 
 if ($User->Banned) {
     $BannedPhoto = c('Garden.BannedPhoto', 'https://c3409409.ssl.cf0.rackcdn.com/images/banned_large.png');
-    if ($BannedPhoto)
+    if ($BannedPhoto) {
         $Photo = Gdn_Upload::url($BannedPhoto);
+    }
 }
 
-if ($Photo) {
-    ?>
+if ($Photo) : ?>
     <div class="Photo PhotoWrap PhotoWrapLarge <?php echo val('_CssClass', $User); ?>">
         <?php
-        $Img = img($Photo, array('class' => 'ProfilePhotoLarge'));
-        $canEditPhotos = Gdn::session()->checkRankedPermission(c('Garden.Profile.EditPhotos', true)) || Gdn::session()->checkPermission('Garden.Users.Edit');
-        if (!$User->Banned && $canEditPhotos && (Gdn::session()->UserID == $User->UserID || Gdn::session()->checkPermission('Garden.Users.Edit')))
-            echo anchor(Wrap(t('Change Picture')), '/profile/picture?userid='.$User->UserID, 'ChangePicture Popup');
+        $canEditPhotos = Gdn::session()->checkRankedPermission(c('Garden.Profile.EditPhotos', true)) || checkPermission('Garden.Users.Edit');
 
-        echo $Img;
+        if (!$User->Banned && $canEditPhotos && (Gdn::session()->UserID == $User->UserID || checkPermission('Garden.Users.Edit'))) {
+            echo anchor(wrap(t('Change Picture')), '/profile/picture?userid='.$User->UserID, 'ChangePicture Popup');
+        }
+
+        echo img($Photo, ['class' => 'ProfilePhotoLarge']);
         ?>
     </div>
-<?php } else if ($User->UserID == Gdn::session()->UserID || Gdn::session()->checkPermission('Garden.Users.Edit')) { ?>
-    <div
-        class="Photo"><?php echo anchor(t('Add a Profile Picture'), '/profile/picture?userid='.$User->UserID, 'AddPicture BigButton'); ?></div>
+<?php elseif ($User->UserID == Gdn::session()->UserID || Gdn::session()->checkPermission('Garden.Users.Edit')) : ?>
+    <div class="Photo">
+        <?php echo anchor(t('Add a Profile Picture'), '/profile/picture?userid='.$User->UserID, 'AddPicture BigButton'); ?>
+    </div>
 <?php
-}
+endif;

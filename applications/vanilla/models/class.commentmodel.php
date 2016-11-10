a9ff257276952031d4940738e7577f0b2ea07cc9
Patrick Kelly <nervoustwit@users.noreply.github.com>
2016-11-10 16:24:32 -0500

    Remove unnecessary hook in comment model

--- applications/vanilla/models/class.commentmodel.php
+++ applications/vanilla/models/class.commentmodel.php
@@ -1157,8 +1157,6 @@ class CommentModel extends VanillaModel {
             $Activity['NotifyUserID'] = $UserID;
             $Activity['Emailed'] = val('Emailed', $Prefs, false);
             $Activity['Notified'] = val('Notified', $Prefs, false);
-            $this->EventArguments['Activity'] = &$Activity;
-            $this->fireEvent('BeforeCommentQueue');
             $ActivityModel->Queue($Activity);
         }
     }

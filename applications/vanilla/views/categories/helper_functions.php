2f9199781cb452ac76de53f6a4e8483878760310
beckyvb <beckyvanbussel@gmail.com>
2016-11-16 17:14:31 -0500

    Support headings when writing a category table
    The only way to get a table-view category list to display headings is to use the `Vanilla.Categories.DoHeadings` config setting. This rewrites the writeCategoryTable function to respect a category's `DisplayAs: Headings` property and render in the same way as they did with `Vanilla.Categories.DoHeadings` on.

--- applications/vanilla/views/categories/helper_functions.php
+++ applications/vanilla/views/categories/helper_functions.php
@@ -346,25 +346,42 @@ if (!function_exists('WriteCategoryList')):
 endif;
 
 if (!function_exists('WriteCategoryTable')):
+    function writeCategoryTable($Categories, $Depth = 1, $inTable = false, $inSection = false) {
+        foreach ($Categories as $Category) {
+            $displayAs = val('DisplayAs', $Category);
+            $urlCode = $Category['UrlCode'];
+            $class = val('CssClass', $Category);
+            $name = htmlspecialchars($Category['Name']);
 
-    function writeCategoryTable($Categories, $Depth = 1) {
-        ?>
-        <div class="DataTableWrap">
-            <table class="DataTable CategoryTable">
-                <thead>
-                <?php
-                WriteTableHead();
-                ?>
-                </thead>
-                <tbody>
-                <?php
-                foreach ($Categories as $Category) {
-                    WriteTableRow($Category, $Depth);
+            if ($displayAs === 'Heading') :
+                if ($inTable) {
+                    echo '</tbody></table></div>';
+                    $inTable = false;
                 }
+                $inSection = true;
                 ?>
-                </tbody>
-            </table>
-        </div>
-    <?php
+                <div id="CategoryGroup-<?php echo $urlCode; ?>" class="CategoryGroup <?php echo $class; ?>">
+                <h2 class="H"><?php echo $name; ?></h2>
+                <?php
+                writeCategoryTable($Category['Children'], $Depth + 1, $inTable, $inSection);
+            else :
+                if (!$inTable) { ?>
+                    <div class="DataTableWrap">
+                        <table class="DataTable CategoryTable">
+                            <thead>
+                            <?php WriteTableHead(); ?>
+                            </thead>
+                            <tbody>
+                    <?php $inTable = true;
+                }
+                WriteTableRow($Category, $Depth);
+            endif;
+        }
+        if ($inTable) {
+            echo '</tbody></table></div>';
+        }
+        if ($inSection) {
+            echo '</div>';
+        }
     }
 endif;

c72516f100ffb353b7cf953972bacda104aa6856
Todd Burry <todd@vanillaforums.com>
2016-12-19 13:08:27 -0500

    Add some basic Gdn_Factory tests
    These tests intentionally run through the Gdn::factory* methods since that is how Vanilla interacts with the factory.

---
+++ tests/Library/Core/FactoryTest.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * @author Todd Burry <todd@vanillaforums.com>
+ * @copyright 2009-2016 Vanilla Forums Inc.
+ * @license Proprietary
+ */
+
+namespace Library\Core;
+use Gdn;
+use VanillaTests\fixtures\Tuple;
+
+/**
+ * Tests for the {@link Gdn_Factory}.
+ */
+class FactoryTest extends \PHPUnit_Framework_TestCase {
+    const TUPLE = 'VanillaTests\fixtures\Tuple';
+
+    /**
+     * The factory should create new instances with arguments.
+     */
+    public function testFactoryInstances() {
+        Gdn::factoryInstall('t', self::TUPLE, '', Gdn::FactoryInstance);
+
+        /* @var Tuple $i1 */
+        $i1 = Gdn::factory('t', 1, 2);
+        /* @var Tuple $i2 */
+        $i2 = Gdn::factory('t', 3, 4);
+
+        $this->assertSame(1, $i1->a);
+        $this->assertSame(2, $i1->b);
+        $this->assertSame(3, $i2->a);
+        $this->assertSame(4, $i2->b);
+    }
+
+    /**
+     * The factory should create a single shared instance.
+     */
+    public function testFactorySingleton() {
+        Gdn::factoryInstall('sing', self::TUPLE, '', Gdn::FactorySingleton, [1, 2]);
+
+        /* @var Tuple $i1 */
+        $i1 = Gdn::factory('sing');
+        /* @var Tuple $i2 */
+        $i2 = Gdn::factory('sing');
+
+        $this->assertSame(1, $i1->a);
+        $this->assertSame(2, $i1->b);
+        $this->assertSame($i1, $i2);
+    }
+
+    /**
+     * Quote-unquote real singletons should call the appropriate static method to create.
+     */
+    public function testFactoryRealSingleton() {
+        Gdn::factoryInstall('rsing', self::TUPLE, '', Gdn::FactoryRealSingleton, 'Create');
+
+        /* @var Tuple $i1 */
+        $i1 = Gdn::factory('rsing');
+        /* @var Tuple $i2 */
+        $i2 = Gdn::factory('rsing');
+
+        $this->assertSame('a', $i1->a);
+        $this->assertSame('b', $i1->b);
+        $this->assertSame($i1, $i2);
+    }
+
+    /**
+     * I should be able to install an object instance.
+     */
+    public function testFactoryInstallInstance() {
+        $instance = new Tuple(1, 2);
+
+        Gdn::factoryInstall('MentionsFormatter', self::TUPLE, '', Gdn::FactorySingleton, $instance);
+
+        $r = Gdn::factory('MentionsFormatter');
+        $this->assertSame($instance, $r);
+    }
+
+    /**
+     * Factory prototypes should clone an original object.
+     */
+    public function testFactoryPrototypes() {
+        Gdn::factoryInstall('proto', self::TUPLE, '', Gdn::FactoryPrototype, new Tuple(1, 2));
+
+        /* @var Tuple $i1 */
+        $i1 = Gdn::factory('proto');
+        /* @var Tuple $i2 */
+        $i2 = Gdn::factory('proto');
+
+        $this->assertSame(1, $i1->a);
+        $this->assertSame(2, $i1->b);
+        $this->assertSame(1, $i2->a);
+        $this->assertSame(2, $i2->b);
+        $this->assertNotSame($i1, $i2);
+    }
+}

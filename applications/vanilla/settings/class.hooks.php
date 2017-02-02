456b4804136127a6afca17940c2ba769935e3fa7
beckyvb <beckyvanbussel@gmail.com>
2017-02-02 15:15:06 -0500

    Move spoilers css back.

--- applications/vanilla/settings/class.hooks.php
+++ applications/vanilla/settings/class.hooks.php
@@ -598,7 +598,6 @@ class VanillaHooks implements Gdn_IPlugin {
      */
     public function assetModel_afterGetCssFiles_handler($sender) {
         if (!inSection('Dashboard')) {
-            $sender->addCssFile('spoilers.css', 'vanilla', ['Sort' => 800]);
             $sender->addCssFile('tag.css', 'vanilla', ['Sort' => 800]);
         }
     }
@@ -622,6 +621,7 @@ class VanillaHooks implements Gdn_IPlugin {
         if (!inSection('Dashboard')) {
             // Spoilers assets
             $sender->addJsFile('spoilers.js', 'vanilla');
+            $sender->addCssFile('spoilers.css', 'vanilla');
             $sender->addDefinition('Spoiler', t('Spoiler'));
             $sender->addDefinition('show', t('show'));
             $sender->addDefinition('hide', t('hide'));

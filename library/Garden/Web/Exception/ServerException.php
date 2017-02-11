457f0545247615ae147bcc05082e3dc7beede248
Todd Burry <todd@vanillaforums.com>
2017-02-11 11:18:23 -0500

    Add new web exceptions
    The web exceptions map to HTTP errors and routing exceptions.

---
+++ library/Garden/Web/Exception/ServerException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Todd Burry <todd@vanillaforums.com>
+ * @copyright 2009-2017 Vanilla Forums Inc.
+ * @license GPLv2
+ */
+
+namespace Garden\Web\Exception;
+
+/**
+ * Represents a 5xx server exception.
+ */
+class ServerException extends \Exception {
+}

1dc425012818316eb892e501520c78b13e0b42f8
Todd Burry <todd@vanillaforums.com>
2017-02-23 11:08:24 -0500

    Add the Dispatcher and Data classes

---
+++ library/Garden/Web/Data.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ * @author Todd Burry <todd@vanillaforums.com>
+ * @copyright 2009-2017 Vanilla Forums Inc.
+ * @license GPLv2
+ */
+
+namespace Garden\Web;
+
+
+/**
+ * Represents the data in a web response.
+ */
+class Data implements \JsonSerializable {
+    private $data;
+
+    private $meta;
+
+    /**
+     * Create a {@link Data} instance representing the data in a web response.
+     *
+     * @param mixed $data The main response data.
+     * @param array|int $meta Either an array of meta information or an integer HTTP response status.
+     */
+    public function __construct($data, $meta = 200) {
+        $this->data = $data;
+
+        if (is_int($meta)) {
+            $this->meta = ['status' => $meta];
+        } else {
+            $this->meta = $meta;
+        }
+    }
+
+    /**
+     * Get a single item from the data array.
+     *
+     * @param string $name The key to get from.
+     * @param mixed $default The default value if no item at the key exists.
+     * @return mixed Returns the data value.
+     */
+    public function getDataItem($name, $default = null) {
+        if (!is_array($this->data) && !($this->data instanceof \ArrayAccess)) {
+            throw new \Exception("Data is not an array.", 500);
+        }
+        return isset($this->data[$name]) ? $this->data[$name] : $default;
+    }
+
+    /**
+     * Set a single item to the data array.
+     *
+     * @param string $name The key to set.
+     * @param mixed $value The new value.
+     * @return $this
+     */
+    public function setDataItem($name, $value) {
+        if (!is_array($this->data) && !($this->data instanceof \ArrayAccess)) {
+            throw new \Exception("Data is not an array.", 500);
+        }
+        $this->data[$name] = $value;
+        return $this;
+    }
+
+    /**
+     * Get the entire data payload.
+     *
+     * @return mixed Returns the data.
+     */
+    public function getData() {
+        return $this->data;
+    }
+
+    /**
+     * Set the entire data payload.
+     *
+     * @param mixed $data The new data array.
+     * @return $this
+     */
+    public function setData($data) {
+        $this->data = $data;
+        return $this;
+    }
+
+    /**
+     * Get the HTTP status.
+     *
+     * @return int Returns the status.
+     */
+    public function getStatus() {
+        return $this->getMetaItem('status', 200);
+    }
+
+    /**
+     * Set the HTTP status.
+     *
+     * @param int $status The new status.
+     * @return $this
+     */
+    public function setStatus($status) {
+        return $this->setMetaItem('status', $status);
+    }
+
+    /**
+     * Get a single item from the meta array.
+     *
+     * @param string $name The key to get from.
+     * @param mixed $default The default value if no item at the key exists.
+     * @return mixed Returns the meta value.
+     */
+    public function getMetaItem($name, $default = null) {
+        return isset($this->meta[$name]) ? $this->meta[$name] : $default;
+    }
+
+    /**
+     * Set a single item to the meta array.
+     *
+     * @param string $name The key to set.
+     * @param mixed $value The new value.
+     * @return $this
+     */
+    public function setMetaItem($name, $value) {
+        $this->meta[$name] = $value;
+        return $this;
+    }
+
+    /**
+     * Get the entire meta array.
+     *
+     * @return array Returns the meta.
+     */
+    public function getMeta() {
+        return $this->meta;
+    }
+
+    /**
+     * Set the entire meta array.
+     *
+     * @param array $meta The new meta array.
+     * @return $this
+     */
+    public function setMeta(array $meta) {
+        $this->meta = $meta;
+        return $this;
+    }
+
+    /**
+     * Specify data which should be serialized to JSON.
+     *
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
+     */
+    public function jsonSerialize() {
+        return $this->getData();
+    }
+
+    /**
+     * Render the response to the output.
+     */
+    public function render() {
+        header('', true, $this->getStatus());
+        header('Content-Type: application/json; charset=utf-8', true);
+
+        echo json_encode($this, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_PARTIAL_OUTPUT_ON_ERROR);
+    }
+}
---
+++ library/Garden/Web/Dispatcher.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * @author Todd Burry <todd@vanillaforums.com>
+ * @copyright 2009-2017 Vanilla Forums Inc.
+ * @license Proprietary
+ */
+
+namespace Garden\Web;
+
+
+use Garden\Web\Exception\NotFoundException;
+use Garden\Web\Exception\Pass;
+
+class Dispatcher {
+
+    /**
+     * @var Route[]
+     */
+    private $routes;
+
+    /**
+     * Add a route to the routes list.
+     *
+     * @param Route $route The route to add.
+     * @param string $key An optional key of the route. If set you can modify the route later.
+     * @return $this
+     */
+    public function addRoute(Route $route, $key = '') {
+        if ($key !== '') {
+            $this->routes[$key] = $route;
+        } else {
+            $this->routes[] = $route;
+        }
+        return $this;
+    }
+
+    /**
+     * Remove the route with a specified key.
+     *
+     * @param string|int $key The key of the route to remove.
+     * @return $this
+     */
+    public function removeRoute($key) {
+        unset($this->routes[$key]);
+        return $this;
+    }
+
+    /**
+     * Get the route with a specified key.
+     *
+     * @param string|int $key The route to get.
+     * @return Route|null Returns a {@link Route} or **null** if no route exists with the given key.
+     */
+    public function getRoute($key) {
+        return isset($this->routes[$key]) ? $this->routes[$key] : null;
+    }
+
+    /**
+     * Dispatch a request and return a response.
+     *
+     * This method currently returns a {@link Data} object that will be directly rendered. This really only for API calls
+     * and will be changed in the future. When that is done this method will be made public.
+     *
+     * @param RequestInterface $request The request to handle.
+     * @return Data Returns the response as a data object.
+     */
+    private function dispatch(RequestInterface $request) {
+        $ex = null;
+
+        foreach ($this->routes as $route) {
+            try {
+                $action = $route->match($request);
+                if ($action instanceof \Exception) {
+                    // Hold the action in case another route succeeds.
+                    $ex = $action;
+                } elseif ($action !== null) {
+                    ob_start();
+                    $actionResponse = $action();
+                    $ob = ob_get_clean();
+                    $response = $this->makeResponse($actionResponse, $ob);
+                    break;
+                }
+            } catch (Pass $pass) {
+                // Pass to the next route.
+                continue;
+            } catch (\Exception $dispatchEx) {
+                $response = $this->makeResponse($dispatchEx);
+                break;
+            }
+        }
+
+        if (!isset($response)) {
+            $ex = $ex ?: new NotFoundException($request->getPath());
+            $response = $this->makeResponse($ex);
+        }
+
+        return $response;
+    }
+
+    /**
+     * Handle a request.
+     *
+     * This method dispatches the request to an appropriate callback depending on the available routes and renders the contents.
+     *
+     * @param RequestInterface $request The request to handle.
+     */
+    public function handle(RequestInterface $request) {
+        $response = $this->dispatch($request);
+        $response->render();
+    }
+
+    /**
+     * Make a structured response object from a raw response.
+     *
+     * @param mixed $raw The raw response.
+     * @param string $ob The contents of the output buffer, if any.
+     * @return Data Returns the data response.
+     */
+    private function makeResponse($raw, $ob = '') {
+        if (is_array($raw)) {
+            // This is an array of response data.
+            $result = new Data($raw);
+        } elseif ($raw instanceof \Exception) {
+            $data = $raw instanceof \JsonSerializable ? $raw->jsonSerialize() : ['message' => $raw->getMessage(), 'status' => $raw->getCode()];
+            $result = new Data($data, $raw->getCode());
+        } elseif ($raw instanceof \JsonSerializable) {
+            $result = new Data((array)$raw->jsonSerialize());
+        } elseif (!empty($ob)) {
+            $result = new Data($ob);
+        } else {
+            $result = new Data(['message' => 'Could not encode the response.', 'status' => 500], 500);
+        }
+
+        return $result;
+    }
+}

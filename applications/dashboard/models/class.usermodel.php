2878ced51d6b75b3a35d787b41fce29c1f675aa7
Patrick Kelly <nervoustwit@users.noreply.github.com>
2017-03-07 11:15:31 -0500

    Add events to UserModel to allow plugins to refine the user search

--- applications/dashboard/models/class.usermodel.php
+++ applications/dashboard/models/class.usermodel.php
@@ -2376,7 +2376,13 @@ class UserModel extends Gdn_Model {
             $RoleID = $this->SQL->getWhere('Role', ['Name' => $Keywords])->value('RoleID');
         }
 
+        $this->EventArguments['Keywords'] = $Keywords;
+        $this->EventArguments['SearchParams'] =& $searchParams;
+        $this->fireEvent('BeforeUserQuery');
+
         $this->userQuery();
+
+        $this->fireEvent('AfterUserQuery');
 
         if (isset($Where)) {
             $this->SQL->where($Where);

a489853dc8f0da5de0e9c8456575271fe71c3040
initvector <ryan.p@vanillaforums.com>
2017-03-21 16:18:55 -0400

    Improve documentation in DiscussionsApiController

--- applications/vanilla/controllers/Api/DiscussionsApiController.php
+++ applications/vanilla/controllers/Api/DiscussionsApiController.php
@@ -11,25 +11,36 @@ use Garden\Schema\Schema;
  * API Controller for the `/discussions` resource.
  */
 class DiscussionsApiController extends AbstractApiController {
+
+    /** @var DiscussionModel */
     private $discussionModel;
 
-    /**
-     * @var Schema
-     */
+    /** @var Schema */
     private $discussionSchema;
 
-    /**
-     * @var
-     */
+    /** @var Schema */
     private $discussionPostSchema;
 
+    /** @var Schema */
+    private $idParamSchema;
+
+    /**
+     * DiscussionsApiController constructor.
+     *
+     * @param DiscussionModel $discussionModel
+     */
     public function __construct(DiscussionModel $discussionModel) {
         $this->discussionModel = $discussionModel;
     }
 
+    /**
+     * List discussions.
+     *
+     * @param array $query The query string.
+     */
     public function index(array $query) {
         $this->permission();
-        $sch = $this->schema([
+        $in = $this->schema([
             'categoryID:i?' => 'Filter by a category.',
             'inertUserID:i?' => 'Filter by author.',
             'page:i?',
@@ -37,9 +48,14 @@ class DiscussionsApiController extends AbstractApiController {
         ], 'in')->setDescription('List discussions.');
         $out = $this->schema([':a' => $this->discussionSchema()], 'out');
 
-        $query = $sch->validate($query);
+        $query = $in->validate($query);
     }
 
+    /**
+     * Get a discussion.
+     *
+     * @param int $id The ID of the discussion.
+     */
     public function get($id) {
         $this->permission();
 
@@ -47,6 +63,11 @@ class DiscussionsApiController extends AbstractApiController {
         $out = $this->schema($this->discussionSchema(), 'out');
     }
 
+    /**
+     * Get a discussion for editing.
+     *
+     * @param int $id The ID of the discussion.
+     */
     public function get_edit($id) {
         $this->permission();
 
@@ -54,57 +75,98 @@ class DiscussionsApiController extends AbstractApiController {
         $out = $this->schema(Schema::parse(['discussionID', 'name', 'body', 'format'])->add($this->fullSchema()), 'out');
     }
 
+    /**
+     * Add a discussion.
+     *
+     * @param array $body The request body.
+     */
     public function post(array $body) {
-        $this->permission('!update');
+        $this->permission();
 
-        $sch = $this->schema($this->discussionPostSchema(), 'in')->setDescription('Add a discussion.');
+        $in = $this->schema($this->discussionPostSchema(), 'in')->setDescription('Add a discussion.');
         $out = $this->schema($this->discussionSchema(), 'out');
-
     }
 
+    /**
+     * Update a discussion.
+     *
+     * @param int $id The ID of the discussion.
+     * @param array $body The request body.
+     */
     public function patch($id, array $body) {
         $this->permission();
 
-        $sch = $this->discussionPostSchema('in')->setDescription('Update a discussion.');
+        $in = $this->discussionPostSchema('in')->setDescription('Update a discussion.');
         $out = $this->schema($this->discussionSchema(), 'out');
     }
 
+    /**
+     * Announce a discussion.
+     *
+     * @param int $id The ID of the discussion.
+     */
     public function put_announce($id) {
         $this->permission();
 
-        $sch = $this->idParamSchema()->setDescription('Announce a discussion.');
+        $in = $this->idParamSchema()->setDescription('Announce a discussion.');
     }
 
+    /**
+     * Close a discussion.
+     *
+     * @param int $id The ID of the discussion.
+     */
     public function put_close($id) {
         $this->permission();
 
-        $sch = $this->idParamSchema()->setDescription('Close a discussion.');
+        $in = $this->idParamSchema()->setDescription('Close a discussion.');
     }
 
+    /**
+     * Sink a discussion.
+     *
+     * @param int $id The ID of the discussion.
+     */
     public function put_sink($id) {
         $this->permission();
 
-        $sch = $this->idParamSchema()->setDescription('Sink a discussion.');
+        $in = $this->idParamSchema()->setDescription('Sink a discussion.');
     }
 
+    /**
+     * Bookmark a discussion.
+     *
+     * @param int $id The ID of the discussion.
+     * @param array $body The request body.
+     */
     public function put_bookmark($id, array $body) {
         $this->permission();
 
-        $sch = $this
+        $in = $this
             ->schema(['bookmarked:b' => 'Pass true to bookmark or false to remove bookmark.'], 'in')
             ->setDescription('Bookmark a discussion.');
         $out = $this->schema(['bookmarked:b' => 'The current bookmark value.'], 'out');
 
-        $body = $sch->validate($body);
+        $body = $in->validate($body);
     }
 
+    /**
+     * Delete a discussion.
+     *
+     * @param int $id The ID of the discussion.
+     */
     public function delete($id) {
         $this->permission();
 
-        $in = $sch = $this->idParamSchema()->setDescription('Delete a discussion.');
+        $in = $this->idParamSchema()->setDescription('Delete a discussion.');
         $out = $this->schema([], 'out');
     }
 
+    /**
+     * Get a schema instance comprised of all available discussion fields.
+     *
+     * @return Schema Returns a schema object.
+     */
     protected function fullSchema() {
         return Schema::parse([
             'discussionID:i' => 'The ID of the discussion.',
@@ -118,6 +180,12 @@ class DiscussionsApiController extends AbstractApiController {
         ]);
     }
 
+    /**
+     * Get the full discussion schema.
+     *
+     * @param string $type The type of schema.
+     * @return Schema Returns a schema object.
+     */
     public function discussionSchema($type = '') {
         if ($this->discussionSchema === null) {
             $this->discussionSchema = $this->schema($this->fullSchema(), 'Discussion');
@@ -125,6 +193,12 @@ class DiscussionsApiController extends AbstractApiController {
         return $this->schema($this->discussionSchema, $type);
     }
 
+    /**
+     * Get a discussion schema with minimal add/edit fields.
+     *
+     * @param string $type The type of schema.
+     * @return Schema Returns a schema object.
+     */
     public function discussionPostSchema($type = '') {
         if ($this->discussionPostSchema === null) {
             $this->discussionPostSchema = $this->schema(
@@ -135,7 +209,19 @@ class DiscussionsApiController extends AbstractApiController {
         return $this->schema($this->discussionPostSchema, $type);
     }
 
+    /**
+     * Get an ID-only discussion record schema.
+     *
+     * @param string $type The type of schema.
+     * @return Schema Returns a schema object.
+     */
     public function idParamSchema($type = 'in') {
-        return $this->schema(['id:i' => 'The discussion ID.'], $type);
+        if ($this->idParamSchema === null) {
+            $this->idParamSchema = $this->schema(
+                Schema::parse(['id:i' => 'The discussion ID.']),
+                $type
+            );
+        }
+        return $this->schema($this->idParamSchema, $type);
     }
 }

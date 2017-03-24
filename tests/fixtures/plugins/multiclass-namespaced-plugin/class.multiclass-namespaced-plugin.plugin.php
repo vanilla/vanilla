30799f4279b6cbb6e8307f573936829b8cdd3027
DaazKu <daazku@gmail.com>
2017-03-24 12:51:30 -0400

    Update the test suite

---
+++ tests/fixtures/plugins/multiclass-namespaced-plugin/class.multiclass-namespaced-plugin.plugin.php
@@ -0,0 +1,33 @@
+<?php
+namespace {
+    $PluginInfo['multiclass-namespaced-plugin'] = [
+        'Name'        => 'Multi Class Namespaced Plugin',
+        'Description' => 'Multi Class Namespaced Fixture Plugin',
+        'Version'     => '1.0.0',
+        'Author'      => 'Alexandre (DaazKu) Chouinard',
+        'AuthorEmail' => 'alexandre.c@vanillaforums.com',
+        'License'     => 'GPLv2'
+    ];
+}
+
+namespace Deeply\Nested\Namespaced\Fixture {
+    class MultiClassNamespacedPlugin extends \Gdn_Plugin {
+        public function base_render_before() {
+            echo __CLASS__.' is loaded!';
+        }
+    }
+
+    class MultiClassPluginHelper {}
+}
+
+namespace Deeply\Nested\Namespaced {
+    class MultiClassPluginHelper {}
+}
+
+namespace Deeply\Nested {
+    class MultiClassPluginHelper {}
+}
+
+namespace Deeply {
+    class MultiClassPluginHelper {}
+}

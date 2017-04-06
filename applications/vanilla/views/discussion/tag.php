81dd2fc63632f9557efbfc84bd060cd3c6d0b885
DaazKu <daazku@gmail.com>
2017-04-06 12:00:49 -0400

    Update popup layout

--- applications/vanilla/views/discussion/tag.php
+++ applications/vanilla/views/discussion/tag.php
@@ -6,11 +6,18 @@ $form = $this->Form;
 echo $form->open(['id' => 'DiscussionAddTagForm']);
 echo $form->errors();
 
+echo '<p>'.t('Which tags would you like to add to the discussion?').'</p>';
+
 echo '<div class="Form-Tags P">';
     // Tag text box
-    echo $this->Form->label('Tags', 'Tags');
-    echo $this->Form->textBox('Tags', array('data-tags' => ''));
+    echo $form->label('Tags', 'Tags');
+    echo $form->textBox('Tags', array('data-tags' => ''));
 echo '</div>';
 
 ?>
-<?php echo $this->Form->close('Save');
+<?php
+echo '<div class="Buttons Buttons-Confirm">';
+echo    $this->Form->button('Save', array('class' => 'btn btn-primary'));
+echo    $this->Form->button('Cancel', array('type' => 'button', 'class' => 'btn Close'));
+echo '</div>';
+echo $form->close();

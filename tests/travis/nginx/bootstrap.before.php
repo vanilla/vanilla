e41a892ee7048947d88e11dfbf82a9d43ab0297b
DaazKu <daazku@gmail.com>
2017-04-19 11:52:19 -0400

    Clean bootstrap.before.php of unused code

--- tests/travis/nginx/bootstrap.before.php
+++ tests/travis/nginx/bootstrap.before.php
@@ -1,25 +1,10 @@
 <?php
-/**
- * This file will make Vanilla use a different config depending on which site you're on.
- */
-
-$host = $_SERVER['HTTP_HOST'];
-list($host, $_) = explode(':', $host, 2);
-
-// Get the config.
-if (isset($_SERVER['NODE_SLUG'])) {
-   // This is a site per folder setup.
-   $slug = "$host-{$_SERVER['NODE_SLUG']}";
-} else {
-   // This is a site per host setup.
-   if (in_array($host, ['vanilla.local', 'vanilla.lc'])) {
-      $slug = 'config';
-   } else {
-      $slug = $host;
-   }
-}
-
-// Use a config specific to the site.
-$configPath = PATH_ROOT."/conf/$slug.php";
-
-define('PATH_CONF_DEFAULT', $configPath);
+// Setup Unit tests config and cache paths
+call_user_func(function() {
+    $host = explode(':', $_SERVER['HTTP_HOST'], 2)[0];
+    // This is a site per host setup.
+    if ($host === 'vanilla.test') {
+        define('PATH_CONF_DEFAULT', PATH_ROOT."/conf/$host.php");
+        define('PATH_CACHE', PATH_ROOT."/cache/$host/");
+    }
+});

e89a4b24306c97b741a170ae2a5b16df77196bdb
DaazKu <daazku@gmail.com>
2017-04-19 12:18:09 -0400

    Update travis setup proces

--- tests/travis/setup.sh
+++ tests/travis/setup.sh
@@ -4,9 +4,10 @@ set -e
 set -x
 
 DIR=$(realpath $(dirname "$0"))
+TEMPLATES="$DIR/templates"
 USER=$(whoami)
 PHP_VERSION=$(phpenv version-name)
-ROOT=$(realpath "$DIR/../../..")
+ROOT=$(realpath "$DIR/../..")
 PORT=9000
 SERVER="/tmp/php.sock"
 
@@ -24,6 +25,7 @@ function tpl {
 # Make some working directories.
 mkdir "$DIR/nginx"
 mkdir "$DIR/nginx/sites-enabled"
+mkdir "$DIR/php"
 mkdir "$DIR/var"
 
 # Configure the PHP handler.
@@ -31,7 +33,7 @@ if [ "$PHP_VERSION" = 'hhvm' ] || [ "$PHP_VERSION" = 'hhvm-nightly' ]
 then
     HHVM_CONF="$DIR/nginx/hhvm.ini"
 
-    tpl "$DIR/hhvm.tpl.ini" "$HHVM_CONF"
+    tpl "$TEMPLATES/nginx/hhvm.ini.tpl" "$HHVM_CONF"
 
     cat "$HHVM_CONF"
 
@@ -42,22 +44,22 @@ then
     ps aux | grep "hhvm"
 else
     PHP_FPM_BIN="$HOME/.phpenv/versions/$PHP_VERSION/sbin/php-fpm"
-    PHP_FPM_CONF="$DIR/nginx/php-fpm.conf"
+    PHP_FPM_CONF="$DIR/php/php-fpm.conf"
 
     # Build the php-fpm.conf.
-    tpl "$DIR/php-fpm.tpl.conf" "$PHP_FPM_CONF"
+    tpl "$TEMPLATES/php/php-fpm.conf.tpl" "$PHP_FPM_CONF"
 
     # Start php-fpm
     "$PHP_FPM_BIN" --fpm-config "$PHP_FPM_CONF"
 fi
 
 # Build the default nginx config files.
-tpl "$DIR/nginx.tpl.conf" "$DIR/nginx/nginx.conf"
-tpl "$DIR/fastcgi.tpl.conf" "$DIR/nginx/fastcgi.conf"
-tpl "$DIR/default-site.tpl.conf" "$DIR/nginx/sites-enabled/default-site.conf"
+tpl "$TEMPLATES/nginx/nginx.conf.tpl" "$DIR/nginx/nginx.conf"
+tpl "$TEMPLATES/nginx/fastcgi.conf.tpl" "$DIR/nginx/fastcgi.conf"
+tpl "$TEMPLATES/nginx/sites-enabled/default-site.conf.tpl" "$DIR/nginx/sites-enabled/default-site.conf"
 
 # Copy the config changer that will allow Vanilla to use a different config file per host.
-cp "$DIR/bootstrap.before.php" "$ROOT/conf"
+cp "$TEMPLATES/vanilla/conf/bootstrap.before.php" "$ROOT/conf"
 
 # Start nginx.
 nginx -c "$DIR/nginx/nginx.conf"

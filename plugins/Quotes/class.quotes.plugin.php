029eba869d75cec9514ad82d08ddd26448b86c85
Patrick Kelly <nervoustwit@users.noreply.github.com>
2017-06-22 14:16:35 -0400

    Fix small typo in quotes plugin

--- plugins/Quotes/class.quotes.plugin.php
+++ plugins/Quotes/class.quotes.plugin.php
@@ -138,8 +138,8 @@ class QuotesPlugin extends Gdn_Plugin {
 
         $QuoteFolding = val('Quotes.Folding', $UserPrefs, '1');
         $Sender->addDefinition('QuotesFolding', $QuoteFolding);
-        $Sender->addDefinition('hide previous quotes', t('hide previous quotes', '&laquo; hide previous quote'));
-        $Sender->addDefinition('show previous quotes', t('show previous quotes', '&raquo; show previous quote'));
+        $Sender->addDefinition('hide previous quotes', t('hide previous quotes', '&laquo; hide previous quotes'));
+        $Sender->addDefinition('show previous quotes', t('show previous quotes', '&raquo; show previous quotes'));
     }
 
     /**

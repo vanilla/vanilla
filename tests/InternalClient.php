098e27f10a2d2aae98c6fe204cb2b4ea9da17914
DaazKu <daazku@gmail.com>
2017-09-05 17:39:52 -0400

    Move session initiation in SiteTestTrait to allow APIController calls in setupBeforeClass methods.

--- tests/InternalClient.php
+++ tests/InternalClient.php
@@ -84,10 +84,6 @@ class InternalClient extends HttpClient {
     public function setUserID($userID) {
         $this->userID = $userID;
 
-        /* @var \Gdn_Session $session */
-        $session = $this->container->get(\Gdn_Session::class);
-        $session->start($userID, false, false);
-
         return $this;
     }
 }

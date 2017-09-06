4a439c00c57dfbd469696c3f55652bce0cdd6030
Alexandre Chouinard <Daazku@gmail.com>
2017-09-06 10:56:32 -0400

    Restore session start in InternalClient::setUserID()

--- tests/InternalClient.php
+++ tests/InternalClient.php
@@ -84,6 +84,10 @@ class InternalClient extends HttpClient {
     public function setUserID($userID) {
         $this->userID = $userID;
 
+        /* @var \Gdn_Session $session */
+        $session = $this->container->get(\Gdn_Session::class);
+        $session->start($userID, false, false);
+        
         return $this;
     }
 }

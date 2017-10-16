867b1d32fcbb7f86c09e9dbc1e8973a2587c383d
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2017-10-16 16:30:32 -0400

    Removed spaces around "." operator

--- plugins/editor/views/editor.php
+++ plugins/editor/views/editor.php
@@ -60,7 +60,7 @@ foreach ($this->data('_EditorToolbar') as $button) {
 
          case 'link':
             $html_toolbar .= wrap(
-               wrap($html_arrow_down . $screenReaderMarkup, 'span', $button['attr']).''.
+               wrap($html_arrow_down.$screenReaderMarkup, 'span', $button['attr']).''.
                '<div class="editor-insert-dialog Flyout MenuItems" data-wysihtml5-dialog="createLink">
                      <input class="InputBox editor-input-url" data-wysihtml5-dialog-field="href" value="http://" />
                       <div class="MenuButtons">
@@ -73,7 +73,7 @@ foreach ($this->data('_EditorToolbar') as $button) {
 
          case 'image':
             $html_toolbar .= wrap(
-               wrap($html_arrow_down . $screenReaderMarkup, 'span', $button['attr']).''.
+               wrap($html_arrow_down.$screenReaderMarkup, 'span', $button['attr']).''.
                '<div class="editor-insert-dialog Flyout MenuItems editor-file-image editor-insert-image" data-wysihtml5-dialog="insertImage">
                       <div class="drop-section image-input" title="'.t('Paste the URL of an image to quickly embed it.').'">
                         <input class="InputBox editor-input-image" placeholder="'.t('Image URL').'" />
@@ -85,7 +85,7 @@ foreach ($this->data('_EditorToolbar') as $button) {
          case 'fileupload':
             $accept = $this->data('Accept', '');
             $html_toolbar .= wrap(
-                wrap($html_arrow_down . $screenReaderMarkup, 'span', $button['attr']).''.
+                wrap($html_arrow_down.$screenReaderMarkup, 'span', $button['attr']).''.
                 '<div class="editor-insert-dialog Flyout MenuItems editor-file-image">
                      <div class="file-title">'.t('Attach a file').' 
                         <span class="js-can-drop info">'.t('you can also drag-and-drop').'</span>
@@ -101,7 +101,7 @@ foreach ($this->data('_EditorToolbar') as $button) {
          case 'imageupload':
             $accept = $this->data('AcceptImage', '');
             $html_toolbar .= wrap(
-                wrap($html_arrow_down . $screenReaderMarkup, 'span', $button['attr']).''.
+                wrap($html_arrow_down.$screenReaderMarkup, 'span', $button['attr']).''.
                 '<div class="editor-insert-dialog Flyout MenuItems editor-file-image">
                      <div class="file-title">'.t('Insert an image').' 
                         <span class="js-can-drop info">'.t('you can also drag-and-drop').'</span>
@@ -151,7 +151,7 @@ foreach ($this->data('_EditorToolbar') as $button) {
             $colorOptions = $textColorOptions.$highlightColorOptions;
 
             $html_toolbar .= wrap(
-               wrap($html_arrow_down . $screenReaderMarkup, 'span', $button['attr']).''.
+               wrap($html_arrow_down.$screenReaderMarkup, 'span', $button['attr']).''.
                wrap($colorOptions, 'div', ['class' => 'editor-insert-dialog Flyout MenuItems', 'data-wysihtml5-dialog' => ''])
                , 'div', ['class' => "editor-dropdown editor-dropdown-color $cssHasHighlight"]
             );
@@ -160,7 +160,7 @@ foreach ($this->data('_EditorToolbar') as $button) {
          // All other dropdowns (color, format, emoji)
          default:
             $html_toolbar .= wrap(
-               wrap($html_arrow_down . $screenReaderMarkup, 'span', $button['attr']).''.
+               wrap($html_arrow_down.$screenReaderMarkup, 'span', $button['attr']).''.
                wrap($html_button_dropdown_options, 'div', ['class' => 'editor-insert-dialog Flyout MenuItems', 'data-wysihtml5-dialog' => ''])
                , 'div', ['class' => 'editor-dropdown editor-dropdown-default editor-action-'.$button['action']]);
             break;

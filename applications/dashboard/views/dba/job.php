4a84094f64979547965e141a0620cf69a6041068
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2017-11-15 16:34:15 -0500

    Updated styling of DBA count page to the new dashboard styles

--- applications/dashboard/views/dba/job.php
+++ applications/dashboard/views/dba/job.php
@@ -8,45 +8,68 @@
         color: red;
         text-decoration: line-through;
     }
+
+    .DBA-Job.isWorking .TinyProgress
+    .DBA-Job.isWorking .DBA-percentage,
+    {
+        display: block;
+    }
+
+    .DBA-percentage {
+        min-width: 39px;
+        display: none;
+        text-align: center;
+        padding-top: 0;
+        padding-bottom: 0;
+    }
+
 </style>
 
-<h1><?php echo $this->data('Title'); ?></h1>
 
+<?php
 
-<div class="Info">
-    <?php
-    echo $this->Form->open();
-    echo $this->Form->errors();
-    ?>
+echo heading($this->data('Title'));
+echo $this->Form->open();
+echo $this->Form->errors();
+?>
+<ul>
 
-    <ol class="DBA-Jobs">
-        <?php
+<?php
         $i = 0;
         foreach ($this->data('Jobs') as $Name => $Job):
             ?>
-            <li id="<?php echo "Job_$i"; ?>" class="DBA-Job" rel="<?php echo htmlspecialchars($Job); ?>">
+            <li id="<?php echo "Job_$i"; ?>" class="form-group DBA-Job" rel="<?php echo htmlspecialchars($Job); ?>">
                 <?php
                 if (!$this->Form->isPostBack()) {
                     $this->Form->setValue("Job_$i", true);
                 }
-
-                echo $this->Form->checkBox("Job_$i", htmlspecialchars($Name));
-
-                echo ' <span class="Count" style="display: none">0</span> ';
                 ?>
+
+                <div class="label-wrap-wide label-flex">
+                    <div class="flex-grow">
+                        <?php
+                        echo $this->Form->checkBox("Job_$i");
+                        ?>
+                        <div class="js-onComplete"">
+                            <?php echo htmlspecialchars($Name); ?>
+                        </div>
+                    </div>
+                    <span class="TinyProgress" style="display: none;">&nbsp;</span>
+                    <span class="DBA-percentage Count">0%</span>
+                </div>
             </li>
             <?php
             $i++;
         endforeach;
-        ?>
-    </ol>
 
-    <?php
-    if ($this->Form->isPostBack()) {
-        Gdn::controller()->addDefinition('Started', 1);
-    }
+        if ($this->Form->isPostBack()) {
+            Gdn::controller()->addDefinition('Started', 1);
+        }
 
-    echo $this->Form->button('Start');
-    echo $this->Form->close();
+        echo '<div class="form-footer">';
+        echo $this->Form->button('Start');
+        echo '</div>';
+
+        echo $this->Form->close();
     ?>
-</div>
+</ul>

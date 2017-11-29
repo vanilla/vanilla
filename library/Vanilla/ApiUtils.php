925a6d02f91e15fff87d20a3d331c19c4b877b85
initvector <ryan.p@vanillaforums.com>
2017-11-29 11:50:59 -0500

    Add casing helpers to ApiUtils

--- library/Vanilla/ApiUtils.php
+++ library/Vanilla/ApiUtils.php
@@ -7,10 +7,68 @@
 
 namespace Vanilla;
 
-
 use Garden\Schema\Schema;
+use Vanilla\Utility\CamelCaseScheme;
+use Vanilla\Utility\CapitalCaseScheme;
 
 class ApiUtils {
+
+    /**
+     * Convert array keys for functions that aren't compatible with camelCase.
+     *
+     * @param array $input API input (query, request body, etc.)
+     * @return array An array with CapitalCase keys.
+     */
+    public static function convertInputKeys(array $input) {
+        static $scheme;
+
+        if ($scheme === null) {
+            $scheme = new CapitalCaseScheme();
+        }
+
+        $result = $scheme->convertArrayKeys($input);
+        return $result;
+    }
+
+    /**
+     * Convert array keys for standard API output.
+     *
+     * @param array $output A single row as part of an API response.
+     * @return array An array with camelCase keys.
+     */
+    public static function convertOutputKeys(array $output) {
+        static $scheme;
+
+        if ($scheme === null) {
+            $scheme = new CamelCaseScheme();
+        }
+
+        $result = $scheme->convertArrayKeys($output);
+        return $result;
+    }
+
+    /**
+     * Generate pager info when the total number of records is not known.
+     *
+     * @param int|array|bool $rows The count of rows from the current query or an array of rows from a result.
+     * You can also pass **true** to signify there are more pages.
+     * @param string $url The basic URL format without querystring.
+     * @param array $query The current query string.
+     * @param Schema $schema The query string schema.
+     * @return array Returns an array suitable to generate a pager.
+     */
+    public static function morePagerInfo($rows, $url, array $query, Schema $schema) {
+        $count = is_array($rows) ? count($rows) : $rows;
+
+        $r = [
+            'page' => $query['page'] ?: 1,
+            'more' => $count === true || $count >= $query['limit'],
+            'urlFormat' => static::pagerUrlFormat($url, $query, $schema)
+        ];
+
+        return $r;
+    }
+
     /**
      * Get pager information when the total number of records is known.
      *
@@ -69,27 +127,5 @@ class ApiUtils {
             $url = (strpos($url, '?') === false ? '?' : '&').$argsStr;
         }
         return $url;
-    }
-
-    /**
-     * Generate pager info when the total number of records is not known.
-     *
-     * @param int|array|bool $rows The count of rows from the current query or an array of rows from a result.
-     * You can also pass **true** to signify there are more pages.
-     * @param string $url The basic URL format without querystring.
-     * @param array $query The current query string.
-     * @param Schema $schema The query string schema.
-     * @return array Returns an array suitable to generate a pager.
-     */
-    public static function morePagerInfo($rows, $url, array $query, Schema $schema) {
-        $count = is_array($rows) ? count($rows) : $rows;
-
-        $r = [
-            'page' => $query['page'] ?: 1,
-            'more' => $count === true || $count >= $query['limit'],
-            'urlFormat' => static::pagerUrlFormat($url, $query, $schema)
-        ];
-
-        return $r;
     }
 }

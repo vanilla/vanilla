e0d527615d5702d8821e33c1211d6fb8b32b3118
Lincoln Russell <lincoln@icrontic.com>
2017-12-14 15:03:48 -0500

    Greetings, my name is 2.5 Gold

--- environment.php
+++ environment.php
@@ -16,7 +16,7 @@ if (!defined('APPLICATION')) {
     define('APPLICATION', 'Vanilla');
 }
 if (!defined('APPLICATION_VERSION')) {
-    define('APPLICATION_VERSION', '2.5rc1');
+    define('APPLICATION_VERSION', '2.5');
 }
 if (!defined('DS')) {
     define('DS', DIRECTORY_SEPARATOR);

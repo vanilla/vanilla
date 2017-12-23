4f4d068d1da71bf5e9a69b2e045c119e2719bc28
Lincoln Russell <lincoln@icrontic.com>
2017-12-23 08:43:41 -0500

    Fix potential deprecated HtmLawed addon issue by including dummy file

---
+++ plugins/HtmLawed/class.htmlawed.plugin.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * HtmLawed Plugin.
+ *
+ * @copyright 2009-2017 Vanilla Forums Inc.
+ * @license http://www.opensource.org/licenses/gpl-2.0.php GNU GPL v2
+ * @package HtmLawed
+ */
+
+$PluginInfo['HtmLawed'] = [
+    'Description' => 'This addon is deprecated and can be removed. Its functionality is now in core.',
+    'Version' => '1.5',
+    'Author' => 'Todd Burry',
+    'AuthorEmail' => 'todd@vanillaforums.com',
+    'AuthorUrl' => 'http://vanillaforums.com/profile/todd',
+    'Hidden' => true
+];
+
+/**
+ * Class HTMLawedPlugin
+ */
+class HtmLawedPlugin extends Gdn_Plugin {
+
+    /**
+     * She's dead, Jim.
+     *
+     * @param $sender
+     */
+    public function base_render_before($sender) {
+        Gdn::pluginManager()->disablePlugin('HtmLawed');
+    }
+
+}

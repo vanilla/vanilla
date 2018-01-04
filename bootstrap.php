33dd7842581d2d1b3ec7e99a42db8ac1d9d68a1f
Lincoln Russell <lincoln@icrontic.com>
2018-01-04 16:37:39 -0500

    Happy 2018, may it not punch you in the face repeatedly

--- bootstrap.php
+++ bootstrap.php
@@ -9,7 +9,7 @@ if (!defined('APPLICATION')) exit();
 /**
  * Bootstrap.
  *
- * @copyright 2009-2017 Vanilla Forums Inc.
+ * @copyright 2009-2018 Vanilla Forums Inc.
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU GPL v2
  * @package Core
  * @since 2.0
--- environment.php
+++ environment.php
@@ -3,7 +3,7 @@
  * Bare minimum setup of the environment to use Vanilla's classes.
  *
  * @author Alexandre (DaazKu) Chouinard <alexandre.c@vanillaforums.com>
- * @copyright 2009-2017 Vanilla Forums Inc.
+ * @copyright 2009-2018 Vanilla Forums Inc.
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU GPL v2
  */
 

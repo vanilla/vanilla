9afb3d2fadc2f9d326c8461cabca33f4319fbb1f
Adam Charron <adam@charrondev.com>
2018-01-11 13:36:44 -0500

    Remove example code from dashboard addon

--- applications/dashboard/src/scripts/dashboard-admin.js
+++
@@ -1,3 +0,0 @@
-import * as utility from "@core/utility";
-
-utility.logWarning("Warning form the Dashboard application! This should display in the admin section.");
--- applications/dashboard/src/scripts/dashboard-app.js
+++
@@ -1,3 +0,0 @@
-import * as utility from "@core/utility";
-
-utility.logWarning("Warning form the Dashboard app section! This should display in an app section page.");

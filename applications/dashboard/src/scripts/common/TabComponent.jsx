c20560559ac4c42f92e5f2bc1f7de50a897db6d9
Adam Charron <adam@charrondev.com>
2018-01-11 13:31:21 -0500

    Remove unused files

--- applications/dashboard/src/scripts/common/TabComponent.jsx
+++
@@ -1,3 +0,0 @@
-import React from "react";
-
-export const TabComponent = () => <div className="TabComponent"></div>;
--- applications/dashboard/src/scripts/dashboard-app.js
+++ applications/dashboard/src/scripts/dashboard-app.js
@@ -1,4 +1,3 @@
 import * as utility from "@core/utility";
 
-utility.logWarning("Warning form the Dashboard app section! This should display on the profile page.");
-
+utility.logWarning("Warning form the Dashboard app section! This should display in an app section page.");

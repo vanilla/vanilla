2d822792cc7e6c30af90a015af77da3c910914db
DaazKu <daazku@gmail.com>
2018-01-15 11:55:20 -0500

    Update message on profile when all discusisons/commments have been filtering out due to permission check

--- applications/vanilla/views/discussion/profile.php
+++ applications/vanilla/views/discussion/profile.php
@@ -7,8 +7,12 @@ echo '<ul class="DataList SearchResults">';
 if (sizeof($this->data('Comments'))) {
     echo $this->fetchView('profilecomments', 'Discussion', 'Vanilla');
     echo $this->Pager->toString('more');
+} elseif ($this->data('UnfilteredCommentsCount', 0) > 0) {
+    echo '<li class="Item Empty">'.t('Every comments on this page have been filtered out because you do not have the permission to see them.').'</li>';
+    echo $this->Pager->toString('more');
 } else {
     echo '<li class="Item Empty">'.t('This user has not commented yet.').'</li>';
 }
+
 echo '</ul>';
 echo '</div>';

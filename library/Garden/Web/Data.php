38284f2f8f3dbb9ee5c9d851986c682b7a9a82ee
Ryan <initvector@users.noreply.github.com>
2018-01-18 17:33:43 -0500

    Fix copy-paste error

--- library/Garden/Web/Data.php
+++ library/Garden/Web/Data.php
@@ -301,13 +301,8 @@ class Data implements \JsonSerializable, \ArrayAccess, \Countable, \IteratorAggr
     /**
      * Count elements of an object.
      *
-     * @return int The custom count as an integer.
      * @link http://php.net/manual/en/countable.count.php
      * @return int The custom count as an integer.
-     * </p>
-     * <p>
-     * The return value is cast to an integer.
-     * @since 5.1.0
      */
     public function count() {
         return count($this->data);

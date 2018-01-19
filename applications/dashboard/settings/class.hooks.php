9a00811d26af9bb41c88a9f017561030e3a109c3
DaazKu <daazku@gmail.com>
2018-01-19 10:27:06 -0500

    Update afterSignIn logic to throw exception only if the session is invalid

--- applications/dashboard/settings/class.hooks.php
+++ applications/dashboard/settings/class.hooks.php
@@ -664,13 +664,15 @@ class DashboardHooks extends Gdn_Plugin {
      * @throws Exception if the user is banned.
      */
     public function base_afterSignIn_handler() {
-        if ($ban = Gdn::session()->getPermissions()->getBan([])) {
-            throw new ClientException($ban['msg'], 401, $ban);
-        } else if (!Gdn::session()->isValid()) {
-            if (!Gdn::session()->getPermissions()->has('Garden.SignIn.Allow')) {
-                throw new PermissionException('Garden.SignIn.Allow');
+        if (!Gdn::session()->isValid()) {
+            if ($ban = Gdn::session()->getPermissions()->getBan([])) {
+                throw new ClientException($ban['msg'], 401, $ban);
             } else {
-                throw new ClientException('The session could not be started', 401);
+                if (!Gdn::session()->getPermissions()->has('Garden.SignIn.Allow')) {
+                    throw new PermissionException('Garden.SignIn.Allow');
+                } else {
+                    throw new ClientException('The session could not be started', 401);
+                }
             }
         }
     }

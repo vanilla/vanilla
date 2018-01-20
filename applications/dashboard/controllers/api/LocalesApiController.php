03e267d62a67903865e72405d2d093f2d6c9d6b3
Todd Burry <todd@vanillaforums.com>
2018-01-20 11:49:48 -0500

    Add the /api/v2/locales/:locale/translations endpoint

---
+++ applications/dashboard/controllers/api/LocalesApiController.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @author Todd Burry <todd@vanillaforums.com>
+ * @copyright 2009-2018 Vanilla Forums Inc.
+ * @license GPLv2
+ */
+
+use Garden\Container\Container;
+use Garden\Web\Data;
+use Vanilla\Web\Controller;
+
+/**
+ * Endpoint for getting translations.
+ */
+class LocalesApiController extends Controller {
+    /**
+     * @var \Gdn_Locale
+     */
+    private $locale;
+
+    /**
+     * LocalesApiController constructor.
+     *
+     * @param \Gdn_Locale $locale
+     */
+    public function __construct(\Gdn_Locale $locale) {
+        $this->locale = $locale;
+    }
+
+    /**
+     *
+     */
+    public function get_translations($locale, array $query = []) {
+        $this->permission();
+
+        $in = $this->schema([
+            'js:b?' => 'Whether or not to serve as javascript.'
+        ], 'in');
+        $out = $this->schema([':o'], 'out');
+
+        $query = $in->validate($query);
+
+        $this->locale->set($locale);
+
+        $translations = (array)$this->locale->getDefinitions();
+
+        if (!empty($query['js'])) {
+            return $this->dumpJavascript($translations);
+        }
+
+        if (empty($translations)) {
+            $translations = (object)[];
+        }
+        return new Data($translations);
+    }
+
+    private function dumpJavascript(array $translations) {
+        $js = 'gdn.translations = '.json_encode($translations, JSON_UNESCAPED_SLASHES | JSON_PARTIAL_OUTPUT_ON_ERROR | JSON_UNESCAPED_UNICODE).';';
+        $r = new Data($js, ['CONTENT_TYPE' => 'application/javascript; charset=utf-8']);
+        return $r;
+    }
+}

c7ddc776b397a1ffc69c8bb01e2d85290265d192
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2018-01-22 11:29:23 -0500

    Add follow button if we're not on the category "home"

--- applications/vanilla/views/categories/all.php
+++ applications/vanilla/views/categories/all.php
@@ -1,6 +1,10 @@
 <?php if (!defined('APPLICATION')) exit();
 include_once $this->fetchViewLocation('helper_functions', 'categories');
-echo '<h1 class="H HomepageTitle">'.$this->data('Title').'</h1>';
+$title = $this->data('Title');
+if(!is_null($this->Category)) {
+    $title .= followButton(true);
+}
+echo '<h1 class="H HomepageTitle">'.$title.'</h1>';
 if ($description = $this->description()) {
     echo wrap($description, 'div', ['class' => 'P PageDescription']);
 }

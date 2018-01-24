057b737e9f4d7bd1e402aeb2c0e8c2f037900ab5
Adam Charron <adam@charrondev.com>
2018-01-24 11:24:43 -0500

    Spread out tests into multiple functions

--- tests/Library/Vanilla/QuillRendererTest.php
+++ tests/Library/Vanilla/QuillRendererTest.php
@@ -11,17 +11,43 @@ use PHPUnit\Framework\TestCase;
 use Vanilla\QuillRenderer;
 
 class QuillRendererTest extends TestCase {
-    public function testRenderDelta() {
-        $fixturePath = realpath("../../fixtures/editor-rendering");
-        $testDirectories = array_diff(scandir($fixturePath), ['..', '.']);
-        $renderer = new QuillRenderer();
 
-        foreach($testDirectories as $dir) {
-            $input = file_get_contents(realpath($fixturePath . "/" . $dir . "/input.json"));
-            $expectedOutput = trim(file_get_contents(realpath($fixturePath . "/" . $dir . "/output.html")));
+    /** @var QuillRenderer */
+    private $renderer;
 
-            $output = $renderer->renderDelta($input);
-            $this->assertEquals($expectedOutput, $output);
-        }
+    public function __construct(?string $name = null, array $data = [], string $dataName = '') {
+        parent::__construct($name, $data, $dataName);
+
+        $this->renderer = new QuillRenderer();
+    }
+
+    private function renderFixtureDirectory($dirname) {
+        $fixturePath = realpath("../../fixtures/editor-rendering/".$dirname);
+
+        $input = file_get_contents(realpath($fixturePath . "/input.json"));
+        $expectedOutput = trim(file_get_contents(realpath($fixturePath . "/output.html")));
+
+        $output = $this->renderer->renderDelta($input);
+        $this->assertEquals($expectedOutput, $output);
+    }
+
+    public function testRenderParagraphs() {
+        $this->renderFixtureDirectory("paragraphs");
+    }
+
+    public function testRenderHeadings() {
+        $this->renderFixtureDirectory("headings");
+    }
+
+    public function testRenderInline() {
+        $this->renderFixtureDirectory("inline-formatting");
+    }
+
+    public function testRenderLists() {
+        $this->renderFixtureDirectory("lists");
+    }
+
+    public function testRenderEverything() {
+        $this->renderFixtureDirectory("everything");
     }
 }

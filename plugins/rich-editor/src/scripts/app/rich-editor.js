9863c978ac3d164670b54081b2c41aa5596d40e5
Adam Charron <adam@charrondev.com>
2018-01-25 13:52:17 -0500

    Remove double license

--- plugins/rich-editor/src/scripts/app/rich-editor.js
+++ plugins/rich-editor/src/scripts/app/rich-editor.js
@@ -1,7 +1,7 @@
 /*
  * @author Adam (charrondev) Charron <adam.c@vanillaforums.com>
  * @copyright 2009-2018 Vanilla Forums Inc.
- * @license @license https://opensource.org/licenses/GPL-2.0 GPL-2.0
+ * @license https://opensource.org/licenses/GPL-2.0 GPL-2.0
  */
 
 import events from "@core/events";

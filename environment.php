9d9d6dab9c309e8b71800ee7fe424a20a8849a0f
Lincoln Russell <lincoln@icrontic.com>
2018-01-25 16:36:48 -0500

    2.5.1

--- environment.php
+++ environment.php
@@ -16,7 +16,7 @@ if (!defined('APPLICATION')) {
     define('APPLICATION', 'Vanilla');
 }
 if (!defined('APPLICATION_VERSION')) {
-    define('APPLICATION_VERSION', '2.5p2');
+    define('APPLICATION_VERSION', '2.5.1');
 }
 if (!defined('DS')) {
     define('DS', DIRECTORY_SEPARATOR);

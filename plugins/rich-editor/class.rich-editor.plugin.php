470da83e958bdf0fbe5379106390cb48acb58bce
Adam Charron <adam@charrondev.com>
2018-01-31 11:38:16 -0500

    Fix merger conflicts

---
+++ plugins/rich-editor/class.rich-editor.plugin.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @author Adam (charrondev) Charron <adam.c@vanillaforums.com>
+ * @copyright 2009-2018 Vanilla Forums Inc.
+ * @license https://opensource.org/licenses/GPL-2.0 GPL-2.0
+ */
+
+class RichEditorPlugin extends Gdn_Plugin {
+
+    /**
+     * Add the style script to the head
+     *
+     * @param Gdn_Controller $sender
+     * @return void
+     */
+    public function base_render_before($sender) {
+        if (inSection("Dashboard")) {
+            return;
+        }
+
+        $sender->addCssFile("//cdn.quilljs.com/1.3.4/quill.bubble.css");
+        $sender->addDefinition("editor", "RichEditor");
+    }
+
+    /**
+     * Attach editor anywhere 'BodyBox' is used.
+     *
+     * It is not being used for editing a posted reply, so find another event to hook into.
+     *
+     * @param Gdn_Form $sender
+     */
+    public function gdn_form_beforeBodyBox_handler($sender, $args) {
+        require_once Gdn::controller()->fetchViewLocation("helper_functions", "", "plugins/rich-editor");
+        $view = renderEditorShell();
+
+        $args['BodyBox'] .= $view;
+
+        $sender->setValue('Format', 'Rich');
+    }
+}

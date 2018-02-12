eb6baacabdea02d96328ed61dfbe6e698f2dab66
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2018-02-12 17:18:46 -0500

    Added hover effect to placeholder video

--- plugins/rich-editor/views/richeditorstyles/index.php
+++ plugins/rich-editor/views/richeditorstyles/index.php
@@ -488,8 +488,8 @@ class MyThemeNameThemeHooks extends Gdn_Plugin {
         <button class="embedVideo-playButton iconButton" style="background-image: url('https://images.pexels.com/photos/31459/pexels-photo.jpg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb')">
             <svg class="embedVideo-playIcon" xmlns="http://www.w3.org/2000/svg" viewBox="-1 -1 24 24">
                 <title>Play Video</title>
-                <path class="embedVideo-playIconPath" style="fill: currentColor; stroke-width: .3;" d="M11,0A11,11,0,1,0,22,11,11,11,0,0,0,11,0Zm0,20.308A9.308,9.308,0,1,1,20.308,11,9.308,9.308,0,0,1,11,20.308Z"/>
-                <polygon class="embedVideo-playIconPath" style="fill: currentColor; stroke-width: .3;" points="8.609 6.696 8.609 15.304 16.261 11 8.609 6.696"/>
+                <path class="embedVideo-playIconPath embedVideo-playIconPath-circle" style="fill: currentColor; stroke-width: .3;" d="M11,0A11,11,0,1,0,22,11,11,11,0,0,0,11,0Zm0,20.308A9.308,9.308,0,1,1,20.308,11,9.308,9.308,0,0,1,11,20.308Z"/>
+                <polygon class="embedVideo-playIconPath embedVideo-playIconPath-triangle" style="fill: currentColor; stroke-width: .3;" points="8.609 6.696 8.609 15.304 16.261 11 8.609 6.696"/>
             </svg>
         </button>
     </div>

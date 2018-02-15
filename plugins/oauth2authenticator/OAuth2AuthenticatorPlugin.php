5272faf5b284e0d6e6bf4e6ae2913122b28675d6
DaazKu <daazku@gmail.com>
2018-02-15 15:02:59 -0500

    WIP

---
+++ plugins/oauth2authenticator/OAuth2AuthenticatorPlugin.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * @author Alexandre (DaazKu) Chouinard <alexandre.c@vanillaforums.com>
+ * @copyright 2009-2018 Vanilla Forums Inc.
+ * @license https://opensource.org/licenses/GPL-2.0 GPL-2.0
+ */

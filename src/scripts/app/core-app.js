7d6497ba24798cc1d3b28cef29e998a69376f1ac
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2018-02-16 15:09:02 -0500

    Added new core-app entry point

---
+++ src/scripts/app/core-app.js
@@ -0,0 +1,10 @@
+/**
+ * @author Stephane LaFlèche <stephane.l@vanillaforums.com>
+ * @copyright 2009-2018 Vanilla Forums Inc.
+ * @license GPLv2
+ */
+
+import * as utility from "@core/utility";
+
+utility.log("core-app loaded");
+

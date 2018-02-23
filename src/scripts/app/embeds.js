91392035f738da19bb9e6170648663c0a369e564
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2018-02-23 12:58:42 -0500

    Added code to truncate link embeds

--- src/scripts/app/embeds.js
+++ src/scripts/app/embeds.js
@@ -4,8 +4,42 @@
  * @license https://opensource.org/licenses/GPL-2.0 GPL-2.0
  */
 
-
 import { delegateEvent } from "@core/dom-utility";
+import { onResize } from "@core/events";
+import { setData, getData } from "@core/dom-utility";
+import shave from 'shave';
+
+/**
+ * Truncate embeds in container
+ *
+ * @param {Element} container - Element containing embeds
+ */
+export function truncateEmbeds(container = document.body) {
+    const embeds = container.querySelectorAll('.embedLink-excerpt');
+    embeds.forEach(el => {
+        let untruncatedText = getData(el, 'untruncatedText');
+
+        if (!untruncatedText) {
+            untruncatedText = el.innerHTML;
+            setData(el, 'untruncatedText', untruncatedText);
+        } else {
+            el.innerHTML = untruncatedText;
+        }
+        shaveEmbedLink(el);
+    });
+}
+
+/**
+ * Shave a link embed
+ *
+ * @param {Element} excerpt - The embed.
+ */
+export function shaveEmbedLink(excerpt) {
+    const maxHeight = parseInt(getComputedStyle(excerpt)['max-height'], 10);
+    if(maxHeight && maxHeight > 0) {
+        shave(excerpt, maxHeight);
+    }
+}
 
 /**
  * Handle a click on a video.
@@ -21,7 +55,16 @@ function handlePlayVideo() {
     container.innerHTML = `<iframe frameborder="0" allow="autoplay; encrypted-media" class="embedVideo-iframe" src="${playButton.dataset.url}" allowfullscreen></iframe>`;
 }
 
+
+/**
+ * Init handler for embeds
+ */
 export function setupEmbeds() {
     delegateEvent('click', '.js-playVideo', handlePlayVideo);
-}
+    truncateEmbeds();
 
+    // Resize
+    onResize(() => {
+        truncateEmbeds();
+    }, "truncate-embedlinks");
+}

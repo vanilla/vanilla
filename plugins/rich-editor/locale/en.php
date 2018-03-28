513c7c679fbf685997ec43fe5855da522409a5aa
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2018-03-28 08:56:34 -0400

    Fixed translations for rich editor

--- plugins/rich-editor/locale/en.php
+++ plugins/rich-editor/locale/en.php
@@ -1,7 +1,7 @@
 <?php
 $Definition['richEditor.description.title'] = 'Rich Text Editor.';
 $Definition['richEditor.description.paragraphMenu'] = 'To edit a paragraph\'s style, hit tab to get to the paragraph menu. From there you will be able to pick one style. Nothing defaults to paragraph.';
-$Definition['richEditor.description.inlineMenu'] = 'A inline formatting menu will show up when you select text. Hit tab to get into that menu.';
+$Definition['richEditor.description.inlineMenu'] = 'An inline formatting menu will show up when you select text. Hit tab to get into that menu.';
 $Definition['richEditor.menu.blockquote'] = 'Blockquote';
 $Definition['richEditor.menu.bold'] = 'Bold';
 $Definition['richEditor.menu.code'] = 'Inline Code Block';
@@ -13,4 +13,4 @@ $Definition['richEditor.menu.link'] = 'Link';
 $Definition['richEditor.menu.paragraph'] = 'Paragraph Style Menu';
 $Definition['richEditor.menu.spoiler'] = 'Spoiler';
 $Definition['richEditor.menu.subheading'] = 'Subheading';
-$Definition['richEditor.menu.strike'] = 'strike';
+$Definition['richEditor.menu.strike'] = 'Strike Through';

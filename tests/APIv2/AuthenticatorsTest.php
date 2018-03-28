3dc799f453566d211c6d8211a3ba9d17773d47d5
DaazKu <daazku@gmail.com>
2018-03-28 11:01:35 -0400

    Add Authenticators unit tests

---
+++ tests/APIv2/AuthenticatorsTest.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * @author Alexandre (DaazKu) Chouinard <alexandre.c@vanillaforums.com>
+ * @copyright 2009-2018 Vanilla Forums Inc.
+ * @license https://opensource.org/licenses/GPL-2.0 GPL-2.0
+ */
+
+namespace VanillaTests\APIv2;
+
+
+use Vanilla\Authenticator\Authenticator;
+use Vanilla\Models\AuthenticatorModel;
+
+class AuthenticatorsTest extends AbstractAPIv2Test {
+
+    /** @var Authenticator[] */
+    private static $authenticators;
+
+    /** @var string */
+    private $baseUrl;
+
+    /**
+     * AuthenticatorsTest constructor.
+     *
+     * @param null $name
+     * @param array $data
+     * @param string $dataName
+     */
+    public function __construct($name = null, array $data = [], $dataName = '') {
+        $this->baseUrl = '/authenticators';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function setupBeforeClass() {
+        parent::setupBeforeClass();
+
+        /** @var AuthenticatorModel $authenticatorModel */
+        $authenticatorModel = self::container()->get('AuthenticatorModel');
+        self::$authenticators = $authenticatorModel->getAuthenticators();
+
+
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setUp() {
+        if (!self::$authenticators) {
+            $this->markTestSkipped('No Authenticator found.');
+        }
+    }
+
+    /**
+     * @param array $record
+     */
+    public function assertIsAuthenticator(array $record) {
+        $this->assertInternalType('array', $record);
+
+        $this->assertArrayHasKey('authenticatorID', $record);
+        $this->assertArrayHasKey('type', $record);
+        $this->assertArrayHasKey('resourceUrl', $record);
+        $this->assertArrayHasKey('ui', $record);
+        $this->assertInternalType('array', $record['ui']);
+        $this->assertArrayHasKey('isActive', $record);
+        $this->assertInternalType('bool', $record['isActive']);
+    }
+
+    /**
+     * Test GET /authenticators/:id
+     */
+    public function testGetAuthenticators() {
+        $id = self::$authenticators[0]->getID();
+
+        $response = $this->api()->get($this->baseUrl.'/'.$id);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $body = $response->getBody();
+        $this->assertIsAuthenticator($body);
+    }
+
+    /**
+     * Test GET /authenticators
+     */
+    public function testListAuthenticators() {
+        $response = $this->api()->get($this->baseUrl);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $body = $response->getBody();
+
+        $this->assertInternalType('array', $body);
+        $this->assertCount(count(self::$authenticators), $body);
+
+        foreach ($body as $record) {
+            $this->assertIsAuthenticator($record);
+        }
+    }
+
+    /**
+     * Test PATCH /authenticators/:id
+     */
+    public function testPatchAuthenticator() {
+        $id = self::$authenticators[0]->getID();
+
+        $response = $this->api()->patch($this->baseUrl.'/'.$id, [
+            'isActive' => !self::$authenticators[0]->isActive(),
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $body = $response->getBody();
+        $this->assertIsAuthenticator($body);
+    }
+}

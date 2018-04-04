1204b6f87f4e15a39d70acdf8ac4588ea49434ac
Adam Charron <adam@charrondev.com>
2018-04-04 15:49:33 -0400

    Rebuild

---
+++ src/scripts/legacy/index.ts
@@ -0,0 +1,21 @@
+/**
+ * Bundled entrypoint for gradually replacing global.js
+ *
+ * @author Adam Charron <adam.c@vanillaforums.com>
+ * @copyright 2009-2018 Vanilla Forums Inc.
+ * @license GPLv2
+ */
+
+import { onContent, onReady } from "@core/application";
+import { initialize } from "./atwho";
+
+// Initialize legacy @mentions for all BodyBox elements.
+//
+// Also assign this window function to the global scope.
+// The advanced editor calls this function directly when in wysiwyg format, as it needs to
+// handle an iframe, and the editor instance needs to be referenced.
+if ($.fn.atwho) {
+    onReady(() => initialize(".BodyBox,.js-bodybox"));
+    onContent(() => initialize(".BodyBox,.js-bodybox"));
+    window.gdn.atCompleteInit = initialize;
+}

4f23b2d4664f75fe5849ca608d7b621eb6a6e02a
Todd Burry <todd@vanillaforums.com>
2018-04-04 11:39:08 -0400

    Tweak transient key management in the APIv2 client

--- src/scripts/apiv2.ts
+++ src/scripts/apiv2.ts
@@ -4,30 +4,47 @@
  * @license GPLv2
  */
 
-import axios from "axios";
-import { formatUrl, getMeta } from "@core/application";
+import axios, {AxiosResponse} from "axios";
+import { formatUrl, getMeta, setMeta } from "@core/application";
 
 /**
  * Add the transient key to every request.
  *
  * @see {AxiosTransformer}
  *
- * @param {FormData} data - The data from the request.
+ * @param {FormData} data The data from the request.
+ * @param {any} headers The request header config.
  *
  * @returns {string|Buffer|ArrayBuffer|FormData|Stream} - Must
  */
-function addTransientKey(data) {
-    data.append("transientKey", getMeta("TransientKey"));
+function addTransientKey(data, headers: any) {
+    headers.post['X-Transient-Key'] = getMeta('TransientKey');
     return data;
 }
 
-const dataTransformers = [
+/**
+ * Intercept all responses and do some data processing.
+ *
+ * - Refresh the CSRF token.
+ *
+ * @param {AxiosResponse} response The response to handle.
+ * @returns {AxiosResponse} Returns the response.
+ */
+function handleResponse(response: AxiosResponse) {
+    if ('x-csrf-token' in response.headers) {
+        setMeta('TransientKey', response.headers['x-csrf-token']);
+    }
+    return response;
+}
+
+const requestTransformers = [
     addTransientKey,
 ];
 
-const instance = axios.create({
+const api = axios.create({
     baseURL: formatUrl("/api/v2/"),
-    transformRequest: dataTransformers,
+    transformRequest: requestTransformers,
 });
+api.interceptors.response.use(handleResponse);
 
-export default instance;
+export default api;

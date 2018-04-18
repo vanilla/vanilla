42cab2c70fdc8b990224d3e8b718637f1051013a
initvector <ryan.p@vanillaforums.com>
2018-04-18 15:21:40 -0400

    Pass discussion to CommentModel::canEdit in PostController::comment

--- applications/vanilla/controllers/class.postcontroller.php
+++ applications/vanilla/controllers/class.postcontroller.php
@@ -643,7 +643,7 @@ class PostController extends VanillaController {
         // Check permissions
         if ($Discussion && $Editing) {
             // Make sure that content can (still) be edited.
-            if (!$this->CommentModel::canEdit($this->Comment)) {
+            if (!CommentModel::canEdit($this->Comment, $editTimeout, $Discussion)) {
                 throw permissionException('Vanilla.Comments.Edit');
             }
 

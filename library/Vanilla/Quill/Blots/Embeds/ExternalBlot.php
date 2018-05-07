af94d1242fae35159ebcd73ecc900780db97e707
initvector <ryan.p@vanillaforums.com>
2018-05-07 16:34:01 -0400

    Add ExternalBlot::addScripts

--- library/Vanilla/Quill/Blots/Embeds/ExternalBlot.php
+++ library/Vanilla/Quill/Blots/Embeds/ExternalBlot.php
@@ -7,6 +7,8 @@
 namespace Vanilla\Quill\Blots\Embeds;
 
 use Gdn;
+use Gdn_Controller;
+use HeadModule;
 use Vanilla\Embeds\EmbedManager;
 
 class ExternalBlot extends AbstractBlockEmbedBlot {
@@ -25,6 +27,23 @@ class ExternalBlot extends AbstractBlockEmbedBlot {
     }
 
     /**
+     * Add client-side scripts to the current controller.
+     *
+     * @param array $scripts
+     */
+    private function addScripts(array $scripts) {
+        /** @var Gdn_Controller $controller */
+        $controller = Gdn::getContainer()->get(Gdn_Controller::class);
+        $head = $controller->getHead();
+
+        if ($head instanceof HeadModule) {
+            foreach ($scripts as $script) {
+                $head->addScript($script, 'text/javascript', false, ['defer' => 'true']);
+            }
+        }
+    }
+
+    /**
      * @inheritDoc
      */
     protected static function getInsertKey(): string {
@@ -37,6 +56,11 @@ class ExternalBlot extends AbstractBlockEmbedBlot {
     protected function renderContent(array $data): string {
         try {
             $rendered = $this->embedManager->renderData($data);
+
+            $scripts = $this->embedManager->getScripts($data['type'] ?? '');
+            if ($scripts) {
+                $this->addScripts($scripts);
+            }
         } catch (\Exception $e) {
             $rendered = '';
         }

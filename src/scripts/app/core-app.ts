699443c60201e6459174f13d001f360e68f102f6
Adam Charron <adam@charrondev.com>
2018-05-07 20:29:43 -0400

    Add oncontent call

--- src/scripts/app/core-app.ts
+++ src/scripts/app/core-app.ts
@@ -6,11 +6,14 @@
 
 import initEmbeds from "./user-content/embeds";
 import initSpoilers from "./user-content/spoilers";
-import { onReady } from "@core/application";
+import { onReady, onContent } from "@core/application";
 import { convertToSafeEmojiCharacters } from "@core/dom";
 
-onReady(() => {
+function init() {
     convertToSafeEmojiCharacters(document.body);
     initEmbeds();
     initSpoilers();
-});
+}
+
+onReady(init);
+onContent(init);

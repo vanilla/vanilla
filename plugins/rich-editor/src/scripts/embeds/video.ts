c87fa6879926e83df3546bfc6be443f07fbcafe2
Adam Charron <adam@charrondev.com>
2018-05-08 16:02:56 -0400

    Fix vimeo/youtube embed url

--- plugins/rich-editor/src/scripts/embeds/video.ts
+++ plugins/rich-editor/src/scripts/embeds/video.ts
@@ -49,7 +49,7 @@ export async function renderer(node: HTMLElement, data: IEmbedData) {
     )}</title><path class="embedVideo-playIconPath embedVideo-playIconPath-circle" style="fill: currentColor; stroke-width: .3;" d="M11,0A11,11,0,1,0,22,11,11,11,0,0,0,11,0Zm0,20.308A9.308,9.308,0,1,1,20.308,11,9.308,9.308,0,0,1,11,20.308Z"/><polygon class="embedVideo-playIconPath embedVideo-playIconPath-triangle" style="fill: currentColor; stroke-width: .3;" points="8.609 6.696 8.609 15.304 16.261 11 8.609 6.696"/></svg>`;
 
     // TODO: Change the data-url here so that the embeds actually work.
-    ratioContainer.innerHTML = `<button type="button" data-url="${data.url}" aria-label="${
+    ratioContainer.innerHTML = `<button type="button" data-url="${data.attributes.embedUrl}" aria-label="${
         data.name
     }" class="embedVideo-playButton iconButton js-playVideo" style="background-image: url(${
         data.photoUrl

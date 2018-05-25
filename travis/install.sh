c024cb9a5e921c77fea660a47d711c5d1f418223
Adam Charron <adam@charrondev.com>
2018-05-25 17:16:41 -0400

    Try new caching strategy

--- travis/install.sh
+++ travis/install.sh
@@ -2,20 +2,14 @@
 
 cd $TRAVIS_BUILD_DIR
 
-# Travis will get it's cache files which can prevent a clone.
-if [ -f "$TRAVIS_BUILD_DIR/vanilla/applications/dashboard/node_modules" ]; then
-    mv "$TRAVIS_BUILD_DIR/vanilla/" "$TRAVIS_BUILD_DIR/vanilla-temp/"
+if [ ! -f "$TRAVIS_BUILD_DIR/vanilla" ]; then
+    mkdir "$TRAVIS_BUILD_DIR/vanilla"
 fi
 
-
-git clone --depth=50 --branch=$TRAVIS_PULL_REQUEST_BRANCH https://github.com/vanilla/vanilla vanilla
-
-# Travis will get it's cache files which can prevent a clone.
-if [ -f "$TRAVIS_BUILD_DIR/vanilla-temp" ]; then
-    mv "$TRAVIS_BUILD_DIR/vanilla-temp/applications/dashboard/node_modules" \ "$TRAVIS_BUILD_DIR/vanilla/applications/dashboard/node_modules"
-    mv "$TRAVIS_BUILD_DIR/vanilla-temp/tests/node_modules" \
-    "$TRAVIS_BUILD_DIR/vanilla/tests/node_modules"
-fi
+cd "$TRAVIS_BUILD_DIR/vanilla"
+git init
+git remote add origin https://github.com/vanilla/vanilla
+git pull origin $TRAVIS_PULL_REQUEST_BRANCH
 
 # Symlink in the editor plugin
 cd "$TRAVIS_BUILD_DIR/vanilla/plugins"

79bf180bad998eb2b11d6e1d7c7a633999bc5fa7
Adam Charron <adam@charrondev.com>
2018-05-25 17:11:30 -0400

    Try to bring back module caching

--- travis/install.sh
+++ travis/install.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 
 cd $TRAVIS_BUILD_DIR
-ls
+
+# Travis will get it's cache files which can prevent a clone.
+if [ -f "$TRAVIS_BUILD_DIR/vanilla/applications/dashboard/node_modules" ]; then
+    mv "$TRAVIS_BUILD_DIR/vanilla/" "$TRAVIS_BUILD_DIR/vanilla-temp/"
+fi
+
 
 git clone --depth=50 --branch=$TRAVIS_PULL_REQUEST_BRANCH https://github.com/vanilla/vanilla vanilla
+
+# Travis will get it's cache files which can prevent a clone.
+if [ -f "$TRAVIS_BUILD_DIR/vanilla-temp" ]; then
+    mv "$TRAVIS_BUILD_DIR/vanilla-temp/applications/dashboard/node_modules" \ "$TRAVIS_BUILD_DIR/vanilla/applications/dashboard/node_modules"
+    mv "$TRAVIS_BUILD_DIR/vanilla-temp/tests/node_modules" \
+    "$TRAVIS_BUILD_DIR/vanilla/tests/node_modules"
+fi
 
 # Symlink in the editor plugin
 cd "$TRAVIS_BUILD_DIR/vanilla/plugins"

6dc40e2039d7b70736f536669bc83d624fafd04c
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2018-06-12 17:19:38 -0400

    Added draft variables for new CSS variable standard

--- applications/dashboard/Gruntfile.js
+++ applications/dashboard/Gruntfile.js
@@ -178,6 +178,7 @@ module.exports = function (grunt) {
         src: [
             'design/admin.css',
             'design/style.css',
+            'design/core-variables.css',
             'design/style-compat.css'
         ],
         options: {

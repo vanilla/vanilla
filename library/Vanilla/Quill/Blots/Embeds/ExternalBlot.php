7bd299aec070452d5890d87cf93d4beb6e4904bd
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2018-06-27 12:35:05 -0400

    Add responsive class for embeds

--- library/Vanilla/Quill/Blots/Embeds/ExternalBlot.php
+++ library/Vanilla/Quill/Blots/Embeds/ExternalBlot.php
@@ -40,7 +40,7 @@ class ExternalBlot extends AbstractBlockEmbedBlot {
         $data = $value['data'] ?? $value;
         $type = $data['type'] ?? '';
         try {
-            $rendered = "<div class='js-embed'>".$this->embedManager->renderData($data)."</div>";
+            $rendered = "<div class='js-embed embedResponsive'>".$this->embedManager->renderData($data)."</div>";
         } catch (\Exception $e) {
             $rendered = ''; // Silently fail.
         }

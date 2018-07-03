a32ad6bf45b84fd6db4b34e50e7df7d53e739cb7
Adam Charron <adam@charrondev.com>
2018-07-03 10:59:09 -0400

    Fix comment box clearing when debug is enabled

--- plugins/rich-editor/src/scripts/components/editor/Editor.tsx
+++ plugins/rich-editor/src/scripts/components/editor/Editor.tsx
@@ -32,6 +32,7 @@ export default class Editor extends React.Component<IProps, IState> {
     private hasUploadPermission: boolean;
     private quill: Quill;
     private quillMountRef: React.RefObject<HTMLDivElement> = React.createRef();
+    private allowPasteListener = true;
 
     constructor(props) {
         super(props);
@@ -135,8 +136,10 @@ export default class Editor extends React.Component<IProps, IState> {
         const form = this.quill.container.closest("form");
         if (form) {
             form.addEventListener("X-ClearCommentForm", () => {
+                this.allowPasteListener = false;
                 this.quill.setContents([]);
                 this.quill.setSelection(null as any, Quill.sources.USER);
+                this.allowPasteListener = true;
             });
         }
     }
@@ -151,14 +154,16 @@ export default class Editor extends React.Component<IProps, IState> {
         if (debug()) {
             const { bodybox } = this.props;
             bodybox.addEventListener("paste", event => {
-                event.stopPropagation();
-                event.preventDefault();
+                if (this.allowPasteListener) {
+                    event.stopPropagation();
+                    event.preventDefault();
 
-                // Get pasted data via clipboard API
-                const clipboardData = event.clipboardData || window.clipboardData;
-                const pastedData = clipboardData.getData("Text");
-                const delta = JSON.parse(pastedData);
-                this.quill.setContents(delta);
+                    // Get pasted data via clipboard API
+                    const clipboardData = event.clipboardData || window.clipboardData;
+                    const pastedData = clipboardData.getData("Text");
+                    const delta = JSON.parse(pastedData);
+                    this.quill.setContents(delta);
+                }
             });
         }
     }

f804083a3c445befeb900359073b8949fa0a869f
Adam Charron <adam@charrondev.com>
2018-07-04 16:23:29 -0400

    Add NullBlot

---
+++ library/Vanilla/Quill/Blots/NullBlot.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @author Adam (charrondev) Charron <adam.c@vanillaforums.com>
+ * @copyright 2009-2018 Vanilla Forums Inc.
+ * @license https://opensource.org/licenses/GPL-2.0 GPL-2.0
+ */
+
+namespace Vanilla\Quill\Blots;
+
+use Vanilla\Quill\Blots\Embeds\EmojiBlot;
+use Vanilla\Quill\Formats;
+use Vanilla\Quill\BlotGroup;
+use Vanilla\Quill\FormattableTextTrait;
+use Vanilla\Quill\Renderer;
+
+/**
+ * A blot for non-matching operations. This is to prevent crashes when bad input is introduced. It renders nothing
+ * and acts as if it doesn't exist.
+ */
+class NullBlot extends AbstractBlot {
+
+    /**
+     * @inheritDoc
+     */
+    public static function matches(array $operations): bool {
+        return true;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function __construct(array $currentOperation, array $previousOperation, array $nextOperation) {
+        parent::__construct($currentOperation, $previousOperation, $nextOperation);
+        $this->content = "";
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function render(): string {
+        return "";
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function shouldClearCurrentGroup(BlotGroup $group): bool {
+        return false;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function hasConsumedNextOp(): bool {
+        return false;
+    }
+}

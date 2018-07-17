4a2c7c9918fa8416c62b37da46b80c8181102ad8
Chris Chabilall <chris.c@vanillaforums.com>
2018-07-17 09:05:41 -0400

    Add discussionsApiController_beforeGet event

--- applications/vanilla/controllers/api/DiscussionsApiController.php
+++ applications/vanilla/controllers/api/DiscussionsApiController.php
@@ -223,7 +223,7 @@ class DiscussionsApiController extends AbstractApiController {
         $out = $this->schema($this->discussionSchema(), 'out');
 
         $query = $in->validate($query);
-        
+
         $this->getEventManager()->fireFilter('discussionsApiController_beforeGet', $this, $id, $query);
 
         $row = $this->discussionByID($id);

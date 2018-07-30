ac7f1c5b9faef82f663f817cba479220c12648da
Adam Charron <adam@charrondev.com>
2018-07-30 15:04:02 -0400

    Flesh out formatting methods

--- plugins/rich-editor/src/scripts/quill/Formatter.ts
+++ plugins/rich-editor/src/scripts/quill/Formatter.ts
@@ -13,8 +13,28 @@ import getStore from "@dashboard/state/getStore";
 import { IStoreState } from "@rich-editor/@types/store";
 import { getIDForQuill, rangeContainsBlot, disableAllBlotsInRange } from "@rich-editor/quill/utility";
 import CodeBlot from "@rich-editor/quill/blots/inline/CodeBlot";
+import CodeBlockBlot from "@rich-editor/quill/blots/blocks/CodeBlockBlot";
+import BlockquoteLineBlot from "@rich-editor/quill/blots/blocks/BlockquoteBlot";
+import SpoilerLineBlot from "@rich-editor/quill/blots/blocks/SpoilerBlot";
+import HeadingBlot from "quill/formats/header";
+import { Blot } from "quill/core";
 
 export default class Formatter {
+    private static INLINE_FORMAT_NAMES = [
+        BoldBlot.blotName,
+        ItalicBlot.blotName,
+        StrikeBlot.blotName,
+        CodeBlot.blotName,
+        LinkBlot.blotName,
+    ];
+
+    private static BLOCK_FORMAT_NAMES = [
+        CodeBlockBlot.blotName,
+        BlockquoteLineBlot.blotName,
+        SpoilerLineBlot.blotName,
+        HeadingBlot.blotName,
+    ];
+
     private store = getStore<IStoreState>();
     constructor(private quill: Quill) {}
 
@@ -50,12 +70,33 @@ export default class Formatter {
             this.quill.format(LinkBlot.blotName, linkValue, Quill.sources.USER);
         }
     };
-    public paragraph = () => {};
-    public h2 = () => {};
-    public h3 = () => {};
-    public codeBlock = () => {};
-    public blockquote = () => {};
-    public spoiler = () => {};
+    public paragraph = () => {
+        Formatter.BLOCK_FORMAT_NAMES.forEach(name => {
+            this.quill.format(name, false, Quill.sources.API);
+        });
+        this.quill.update(Quill.sources.USER);
+    };
+    public h2 = () => {
+        this.quill.format(HeadingBlot.blotName, 2, Quill.sources.USER);
+    };
+    public h3 = () => {
+        this.quill.format(HeadingBlot.blotName, 3, Quill.sources.USER);
+    };
+    public codeBlock = () => {
+        const line = this.quill.getLine(this.instanceState.lastGoodSelection.index)[0] as Blot;
+        const index = line.offset(this.quill.scroll);
+        const length = index + line.length();
+
+        // Code cannot have any inline formattings inside of it.
+        this.quill.removeFormat(index, length, Quill.sources.API);
+        this.quill.format(CodeBlockBlot.blotName, true, Quill.sources.USER);
+    };
+    public blockquote = () => {
+        this.quill.format(BlockquoteLineBlot.blotName, true, Quill.sources.USER);
+    };
+    public spoiler = () => {
+        this.quill.format(SpoilerLineBlot.blotName, true, Quill.sources.USER);
+    };
 
     private handleBooleanFormat(formatKey) {
         const isEnabled = this.formats[formatKey] === true;

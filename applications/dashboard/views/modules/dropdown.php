4defbac3c8b5c3efe367424a40d87214c8af37a7
alexander.kim <kim.alexander.ca@gmail.com>
2018-07-31 12:56:34 -0400

    applying styling standards

--- applications/dashboard/views/modules/dropdown.php
+++ applications/dashboard/views/modules/dropdown.php
@@ -3,7 +3,7 @@
 $dropdown = $this;
 $trigger = $dropdown->getTrigger();
 ?><span class="ToggleFlyout <?php echo $dropdown->getCssClass(); ?>"><?php
-    if ( $trigger['type'] ?? '' === 'button') :
+    if (($trigger['type'] ?? '') === 'button') :
     ?><span class="Button-Options">
         <span class="OptionsTitle" title="<?php echo t('Options'); ?>">
             <?php echo $trigger['text']; ?>
@@ -11,46 +11,46 @@ $trigger = $dropdown->getTrigger();
         <?php echo sprite('SpFlyoutHandle', 'Arrow'); ?>
     </span>
     <?php else :
-        $text = $trigger['text'];
-        $url = $trigger['url'];
-        $icon = $trigger['icon'];
-        $cssClass = $trigger['cssClass'];
-        $attributes = $trigger['attributes'];
+        $text = $trigger['text'] ?? false;
+        $url = $trigger['url'] ?? false;
+        $icon = $trigger['icon'] ?? false;
+        $cssClass = $trigger['cssClass'] ?? false;
+        $attributes = $trigger['attributes'] ?? false;
         $alert = !empty($dropdown->data('DashboardCount', '')) ? wrap($dropdown->data('DashboardCount', ''), 'span', ['class' => 'Alert']) : '';
         echo anchor($icon.$text.$alert, $url, $cssClass, $attributes);
     endif; ?>
     <ul class="Flyout MenuItems list-reset <?php echo $dropdown->getListCssClass(); ?>" role="menu" aria-labelledby="<?php echo $dropdown->getTriggerId(); ?>">
         <?php foreach($dropdown->getItems() as $item) {
-            if ( $item['type'] ?? '' == 'group') { ?>
+            if (($item['type'] ?? '') == 'group') { ?>
                 <li role="presentation" class="dropdown-header <?= $item['cssClass'] ?? ''; ?>">
-                    <?php if ($iIcon = $item['icon'] ?? false) {
+                    <?php if ($iIcon = ($item['icon'] ?? false)) {
                         echo icon($iIcon);
                     }
                     echo $item['text'] ?? '';
-                    if ($iBadge = $item['badge'] ?? false) {
+                    if ($iBadge = ($item['badge'] ?? false)) {
                         echo badge($iBadge);
                     } ?>
                 </li>
             <?php } ?>
-            <?php  if ($item['type'] ?? '' == 'link') { ?>
+            <?php  if (($item['type'] ?? '') == 'link') { ?>
                 <li role="presentation" <?php
-                    if ($ilistItemCssClass = $item['listItemCssClass'] ?? false || empty($item['icon'])) {
+                    if ($ilistItemCssClass = ($item['listItemCssClass'] ?? false) || empty($item['icon'])) {
                        ?>class="<?php
                         echo trim($ilistItemCssClass.(empty($item['icon']) ? ' no-icon' : '')); ?>"<?php
                     } ?>>
                     <a role="menuitem" class="dropdown-menu-link <?php echo val('cssClass', $item); ?>" tabindex="-1" href="<?= url($item['url'] ?? ''); ?>" <?= attribute($item['attributes'] ?? []) ?>><?php
-                        if ($iIcon = $item['icon'] ?? false) {
+                        if ($iIcon = ($item['icon'] ?? false)) {
                             echo icon($iIcon);
                         }
                         echo $item['text'] ?? '';
-                        if ($iBadge = $item['badge'] ?? false) {
+                        if ($iBadge = ($item['badge'] ?? false)) {
                             echo ' '.wrap($iBadge, 'span', ['class' => 'Alert']);
                         }
                         ?></a>
                 </li>
             <?php }
-            if ($item['type'] ?? '' == 'divider') { ?>
-                <li role="presentation" <?php if ($iCssClass = $item['cssClass'] ?? false ) { ?> class="<?= $iCssClass; ?>"<?php } ?>>
+            if (($item['type'] ?? '') == 'divider') { ?>
+                <li role="presentation" <?php if ($iCssClass = ($item['cssClass'] ?? false)) { ?> class="<?= $iCssClass; ?>"<?php } ?>>
                     <hr />
                 </li>
             <?php }

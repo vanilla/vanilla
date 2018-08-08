781d0353f64833e62a369323f516645ca8d511d7
Adam Charron <adam@charrondev.com>
2018-08-08 11:57:59 -0400

    Add comments

--- plugins/rich-editor/locale/en.php
+++ plugins/rich-editor/locale/en.php
@@ -1,11 +1,12 @@
 <?php
+// Long Descriptions
 $Definition['richEditor.description.title'] = 'Rich Text Editor.';
 $Definition['richEditor.description.paragraphMenu'] = 'To edit a paragraph\'s style, hit tab to get to the paragraph menu. From there you will be able to pick one style. Nothing defaults to paragraph.';
 $Definition['richEditor.description.inlineMenu'] = 'An inline formatting menu will show up when you select text. Hit tab to get into that menu.';
 $Definition['richEditor.description.embed'] = 'Some elements, such as rich link embeds, images, loading indicators, and error messages may get inserted into the editor. You may navigate to these using the arrow keys inside of the editor and delete them with the delete or backspace key.';
 $Definition['richEditor.externalEmbed.description'] = 'This is an embed external element. It can be deleted using the delete key or the backspace key. To view the full element, press the preview button below.';
 
-// Formatting Meus
+// Formatting Menus
 $Definition['Inline Level Formatting Menu'] = 'Inline Level Formatting Menu';
 $Definition['Format as Bold'] = 'Format as Bold';
 $Definition['Bold'] = 'Bold';

d05b24e11db8276a1b38c28d5dce3885fb43b1af
alexander.kim <kim.alexander.ca@gmail.com>
2018-08-13 15:34:00 -0400

    Implement few static variables to avoid unnecessary calls to port() and host() functions
    Fixes: https://github.com/vanilla/vanilla/issues/7617

--- library/core/class.request.php
+++ library/core/class.request.php
@@ -87,6 +87,22 @@ class Gdn_Request implements RequestInterface {
     protected $_RequestArguments;
 
     /**
+    /*  Holds domain plus port as a string to avoid multiple calculations
+     * https://github.com/vanilla/vanilla/issues/7617
+     *
+     * @var string ex: vanilla.local:8080
+     */
+    public static $host = false;
+
+    /**
+    /*  Holds url() results to avoid recalls vs same path
+     * https://github.com/vanilla/vanilla/issues/7617
+     *
+     * @var array
+     */
+    public static $urls = [];
+
+    /**
      * Instantiate a new instance of the {@link Gdn_Request} class.
      */
     public function __construct() {
@@ -1554,6 +1570,10 @@ class Gdn_Request implements RequestInterface {
      */
     public function url($path = '', $withDomain = false, $ssl = null) {
         static $allowSSL = null;
+        $staticKey = ($withDomain?'1-':'0-').($ssl?'1-':'0-').json_encode($path);
+        if ($r = (self::$urls[$staticKey] ?? false)) {
+            return $r;
+        }
         if ($allowSSL === null) {
             $allowSSL = c('Garden.AllowSSL', false);
         }
@@ -1591,6 +1611,7 @@ class Gdn_Request implements RequestInterface {
         }
 
         if (substr($path, 0, 2) == '//' || in_array(strpos($path, '://'), [4, 5])) { // Accounts for http:// and https:// - some querystring params may have "://", and this would cause things to break.
+            self::$urls[$staticKey] = ''.$path;
             return $path;
         }
 
@@ -1609,10 +1630,15 @@ class Gdn_Request implements RequestInterface {
         // Having en empty string in here will prepend a / in front of the URL on implode.
         $parts = [''];
         if ($withDomain !== '/') {
-            $port = $this->port();
-            $host = $this->host();
-            if (!in_array($port, [80, 443]) && (strpos($host, ':'.$port) === false)) {
-                $host .= ':'.$port;
+            if (!empty(self::$host)) {
+                $host = self::$host;
+            } else {
+                $port = $this->port();
+                $host = $this->host();
+                if (!in_array($port, [80, 443]) && (strpos($host, ':'.$port) === false)) {
+                    $host .= ':'.$port;
+                }
+                self::$host = $host;
             }
 
             if ($withDomain === '//') {
@@ -1655,6 +1681,7 @@ class Gdn_Request implements RequestInterface {
             $result .= $hash;
         }
 
+        self::$urls[$staticKey] = ''.$result;
         return $result;
     }
 

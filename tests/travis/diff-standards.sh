526e83ef5468f2b2fb18a44e875af57b6fc80f67
Adam Charron <adam@charrondev.com>
2018-08-16 14:38:10 -0400

    Tweak diff standards scriptp

--- tests/travis/diff-standards.sh
+++ tests/travis/diff-standards.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 if [ -z "$1" ]; then
-    echo "A branch is required." >&2
-    exit 1
+    echo "No branch provided. Linting against default branch master"
+    echo ""
+    DIFF_BRANCH="master"
+else
+    echo "Linting compared to provided branch $1"
+    DIFF_BRANCH=$1
 fi
-DIFF_BRANCH=$1
 
 if [ -z "$2" ]; then
     BUILD_DIR=$(pwd)
@@ -17,7 +20,8 @@ PHPCS_DIFF_FILENAME=$BUILD_DIR/phpcs-diff.json
 cd $BUILD_DIR
 
 # Begin folding in Travis.
-echo "travis_fold:start:coding_standards"
+[ -n "$TRAVIS" ] && echo "travis_fold:start:coding_standards"
+
 echo "Verify changed code against coding standards."
 
 # Without this, Travis only has a ref to the current branch in a shallow clone. Other branches cannot be compared.
@@ -25,10 +29,11 @@ echo "Updating available refs..."
 git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
 git fetch --all
 
-GIT_DIFF=$(git diff HEAD $DIFF_BRANCH '*.php')
+GIT_DIFF=$(git diff $DIFF_BRANCH '*.php')
 if [ -z "$GIT_DIFF" ]; then
     echo "No PHP file changes detected."
-    echo "travis_fold:end:coding_standards"
+
+    [ -n "$TRAVIS" ] && echo "travis_fold:end:coding_standards"
     exit
 fi
 echo "Exporting branch diff of $DIFF_BRANCH to $GIT_DIFF_FILENAME..."
@@ -37,10 +42,11 @@ echo -n "$GIT_DIFF" > $GIT_DIFF_FILENAME
 
 echo "Exporting full PHP_CodeSniffer scan of changed files to $PHPCS_DIFF_FILENAME..."
 rm -f $PHPCS_DIFF_FILENAME
-./vendor/bin/phpcs --standard=./vendor/vanilla/standards/code-sniffer/Vanilla --report=json $(git diff --name-only HEAD $DIFF_BRANCH -- '*.php') > $PHPCS_DIFF_FILENAME
+./vendor/bin/phpcs --standard=./vendor/vanilla/standards/code-sniffer/Vanilla --report=json $(git diff --name-only $DIFF_BRANCH -- '*.php') > $PHPCS_DIFF_FILENAME
 
 echo "Comparing results of PHP_CodeSniffer scan with changed lines from branch diff..."
+echo ""
 ./vendor/bin/diffFilter --phpcs $GIT_DIFF_FILENAME $PHPCS_DIFF_FILENAME
 
 # End folding in Travis.
-echo "travis_fold:end:coding_standards"
+[ -n "$TRAVIS" ] && echo "travis_fold:end:coding_standards"

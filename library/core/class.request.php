b9f42d7beab4b41e044a15a11cf4900cda767791
alexander.kim <kim.alexander.ca@gmail.com>
2018-08-17 09:03:04 -0400

    Update according to code review commant

--- library/core/class.request.php
+++ library/core/class.request.php
@@ -1568,9 +1568,9 @@ class Gdn_Request implements RequestInterface {
      *    2.1   Added the // option to $WithDomain.
      *    2.2   Added the / option to $WithDomain.
      */
-    public function url($path = '', $withDomain = false, $ssl = null) {
+    public function url(string $path = '', $withDomain = false, $ssl = null) {
         static $allowSSL = null;
-        $staticKey = ($withDomain ? '1-' : '0-').($ssl ? '1-' : '0-').json_encode($path);
+        $staticKey = ($withDomain ? '1-' : '0-').($ssl ? '1-' : '0-').$path;
         if ($r = (self::$urls[$staticKey] ?? false)) {
             return $r;
         }

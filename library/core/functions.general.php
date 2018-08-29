41f1605d36fd2631b121fb7b34f363ac3dd43e3e
initvector <ryan.p@vanillaforums.com>
2018-08-29 18:24:50 -0400

    Move jsonEncodeChecked into StringUtils

--- library/core/functions.general.php
+++ library/core/functions.general.php
@@ -2332,34 +2332,7 @@ if (!function_exists('jsonEncodeChecked')) {
      * @throws Exception
      */
     function jsonEncodeChecked($value, $options = null) {
-         if ($options === null) {
-            $options = JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES;
-        }
-        $encoded = json_encode($value, $options);
-        $errorMessage = null;
-        switch (json_last_error()) {
-            case JSON_ERROR_NONE:
-                // Do absolutely nothing since all went well!
-                break;
-            case JSON_ERROR_UTF8:
-                $errorMessage = 'Malformed UTF-8 characters, possibly incorrectly encoded';
-                break;
-            case JSON_ERROR_RECURSION:
-                $errorMessage = 'One or more recursive references in the value to be encoded.';
-                break;
-            case JSON_ERROR_INF_OR_NAN:
-                $errorMessage = 'One or more NAN or INF values in the value to be encoded';
-                break;
-            case JSON_ERROR_UNSUPPORTED_TYPE:
-                $errorMessage = 'A value of a type that cannot be encoded was given.';
-                break;
-            default:
-                $errorMessage = 'An unknown error has occurred.';
-        }
-        if ($errorMessage !== null) {
-            throw new Exception("JSON encoding error: {$errorMessage}", 500);
-        }
-        return $encoded;
+        return \Vanilla\Utility\StringUtils::jsonEncodeChecked($value, $options);
     }
 }
 

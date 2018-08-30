97592273e48ca55360e29351afb5011597bbd619
alexander.kim <kim.alexander.ca@gmail.com>
2018-08-30 09:08:35 -0400

    Add new empty Controller class
    Fixes: #7682

---
+++ library/Garden/Controller.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Alexander Kim <alexander.k@vanillaforums.com>
+ * @copyright 2009-2018 Vanilla Forums Inc.
+ * @license GPL v2
+ */
+
+namespace Garden;
+
+/**
+ * Base Class Controller.
+ */
+class Controller {
+
+}

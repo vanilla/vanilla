3b98b774841843d5ba9c5ee9b57c8e3aab34038c
Adam Charron <adam@charrondev.com>
2018-09-11 11:28:52 -0400

    Migrate install script to PHP

--- library/setup/ComposerHelper.php
+++ library/setup/ComposerHelper.php
@@ -14,6 +14,10 @@ use Composer\Factory;
  * Contains helper methods for Vanilla's composer integration.
  */
 class ComposerHelper {
+
+    const NODE_ARGS_ENV = "VANILLA_BUILD_NODE_ARGS";
+    const DISABLE_VALIDATION_ENV = "VANILLA_BUILD_DISABLE_CODE_VALIDATION";
+
     /**
      * Clear the addon manager cache.
      */
@@ -30,6 +34,34 @@ class ComposerHelper {
             if (file_exists($path)) {
                 unlink($path);
             }
+        }
+    }
+
+    public static function postUpdate(Event $event) {
+        printf("\nInstalling core node_modules\n");
+        passthru('yarn install --pure-lockfile', $installReturn);
+
+        if ($installReturn !== 0) {
+            printf("Installing core node_modules failed\n");
+            exit($installReturn);
+        }
+
+        $nodeArgs = getenv(self::NODE_ARGS_ENV) ?: "";
+        $disableValidationFlag = getenv(self::DISABLE_VALIDATION_ENV) ? "--low-memory" : "";
+
+        $vanillaRoot = realpath(__DIR__ . "/../../");
+        $buildScript = realpath($vanillaRoot . "/build/scripts/build.ts");
+        $tsNodeRegister = realpath($vanillaRoot . "/node_modules/ts-node/register");
+        $tsConfig = realpath($vanillaRoot . "/build/tsconfig.json");
+        $buildCommand = "TS_NODE_PROJECT=$tsConfig node $nodeArgs -r $tsNodeRegister $buildScript $disableValidationFlag";
+
+        printf("\nBuilding frontend assets\n");
+        printf("\n$buildCommand\n");
+        passthru($buildCommand, $buildResult);
+
+        if ($buildResult !== 0) {
+            printf("The build failed with code $buildResult");
+            exit($buildResult);
         }
     }
 

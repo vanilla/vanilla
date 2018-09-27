023e15584ca85152fb79141d094b5942607ad273
Todd Burry <todd@vanillaforums.com>
2018-09-27 16:15:33 -0400

    Don’t index addons with invalid keys (redux)

--- library/Vanilla/AddonManager.php
+++ library/Vanilla/AddonManager.php
@@ -346,9 +346,7 @@ class AddonManager {
             try {
                 $addon = new Addon($subdir);
                 $key = $addon->getKey();
-                if (!static::validateKey($key)) {
-                    trigger_error("The $type in $subdir has an invalid key: $key.", E_USER_WARNING);
-                } elseif (!array_key_exists($key, $addons)) {
+                if (!array_key_exists($key, $addons)) {
                     $addons[$key] = $addon;
                 } else {
                     \Logger::error('Duplicate addon: {key}', [
@@ -396,9 +394,7 @@ class AddonManager {
             $paths = glob(PATH_ROOT."$subdir/*", GLOB_ONLYDIR | GLOB_NOSORT);
             foreach ($paths as $path) {
                 $basename = basename($path);
-                if (!AddonManager::validateKey($basename)) {
-                    trigger_error("The $type in $subdir/$basename has an invalid key: $basename.", E_USER_WARNING);
-                } elseif (!array_key_exists($basename, $result)) {
+                if (!array_key_exists($basename, $result)) {
                     $result[$basename] = substr($path, $strlen);
                 } else {
                     \Logger::error('Duplicate addon: {basename}', [

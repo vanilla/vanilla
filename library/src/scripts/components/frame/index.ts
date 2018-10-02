71fac669df826df3da2fc8289296e0cc7bb3493c
Adam Charron <adam@charrondev.com>
2018-10-02 00:46:58 -0400

    Add barrel for frame component

---
+++ library/src/scripts/components/frame/index.ts
@@ -0,0 +1,11 @@
+/**
+ * @author Adam (charrondev) Charron <adam.c@vanillaforums.com>
+ * @copyright 2009-2018 Vanilla Forums Inc.
+ * @license Proprietary
+ */
+
+export { default as Frame } from "./Frame";
+export { default as FrameBody } from "./FrameBody";
+export { default as FrameFooter } from "./FrameFooter";
+export { default as FrameHeader } from "./FrameHeader";
+export { default as FramePanel } from "./FramePanel";

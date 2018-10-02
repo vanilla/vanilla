7812bfc7d79b2fd9c8e00e5d65796d3f9c0a9af1
Adam Charron <adam@charrondev.com>
2018-10-02 11:50:00 -0400

    Use real backlink

--- library/src/scripts/components/BackLink.tsx
+++ library/src/scripts/components/BackLink.tsx
@@ -1,25 +1,22 @@
-/**
+/*
  * @author Stéphane LaFlèche <stephane.l@vanillaforums.com>
  * @copyright 2009-2018 Vanilla Forums Inc.
- * @license GPL-2.0-only
+ * @license Proprietary
  */
 
 import * as React from "react";
 import classNames from "classnames";
-import { t } from "../application";
-import { Link } from "react-router-dom";
+import { t } from "@library/application";
 import { leftChevron } from "@library/components/Icons";
+import { Link, RouteComponentProps, withRouter } from "react-router-dom";
 
-interface IProps {
-    url?: string;
+interface IProps extends RouteComponentProps<{}> {
+    url?: string | null;
     title?: string;
     className?: string;
 }
 
-/**
- * A link button for navigating backwards. Uses a back arrow icon.
- */
-export default class BackLink extends React.Component<IProps> {
+export class BackLink extends React.Component<IProps> {
     public static defaultProps = {
         title: t("Back"),
     };
@@ -31,6 +28,7 @@ export default class BackLink extends React.Component<IProps> {
                         to={this.props.url}
                         aria-label={this.props.title}
                         title={this.props.title}
+                        onClick={this.clickHandler}
                         className="backLink-link"
                     >
                         {leftChevron("backLink-icon")}
@@ -41,4 +39,16 @@ export default class BackLink extends React.Component<IProps> {
             return null;
         }
     }
+
+    /**
+     * If we can do an actual back action on the history object we should.
+     * Otherwise fallback to the default behaviour.
+     */
+    private clickHandler = (event: React.MouseEvent) => {
+        event.preventDefault();
+        event.stopPropagation();
+        this.props.history.goBack();
+    };
 }
+
+export default withRouter(BackLink);

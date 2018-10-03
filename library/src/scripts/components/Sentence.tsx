aa2116e89768d83e3e8e2c80428ca35f9cbd0028
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2018-10-03 11:50:45 -0400

    Fixed props error

--- library/src/scripts/components/Sentence.tsx
+++ library/src/scripts/components/Sentence.tsx
@@ -105,7 +105,7 @@ export default class Sentence extends React.Component<ISentence> {
                             className={word.className}
                             children={text.children}
                             counter={childCounter}
-                            recursiveChildClass={this.props.childClass}
+                            recursiveChildClass={this.props.recursiveChildClass}
                         />
                     </span>
                 );

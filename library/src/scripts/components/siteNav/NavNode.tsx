db30ea001ba07de7318bee45ac7c937e51ed077d
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2018-10-11 14:20:10 -0400

    Added a few new optional props to Button component Added accessibility and toggeable functionality to SiteNav

--- library/src/scripts/components/siteNav/NavNode.tsx
+++ library/src/scripts/components/siteNav/NavNode.tsx
@@ -7,7 +7,7 @@
 import * as React from "react";
 import classNames from "classnames";
 import { NavLink } from "react-router-dom";
-import { rightTriangle } from "@library/components/Icons";
+import { rightTriangle, downTriangle } from "@library/components/Icons";
 import Button from "@library/components/forms/Button";
 import { t } from "@library/application";
 import Paragraph from "@library/components/Paragraph";
@@ -15,14 +15,49 @@ import Paragraph from "@library/components/Paragraph";
 interface IProps {
     name: string;
     className?: string;
+    titleID?: string;
     children: any[];
     counter: number;
     url: string;
+    openRecursive?: boolean;
 }
 
-export default class NavNode extends React.Component<IProps> {
+interface IState {
+    open: boolean;
+}
+
+export default class NavNode extends React.Component<IProps, IState> {
+    public constructor(props) {
+        super(props);
+        this.state = {
+            open: props.openRecursive,
+        };
+        this.open = this.open.bind(this);
+        this.close = this.close.bind(this);
+        this.toggle = this.toggle.bind(this);
+    }
+
+    public open(recursive?) {
+        this.setState({
+            open: true,
+        });
+    }
+
+    public close() {
+        this.setState({
+            open: true,
+        });
+    }
+
+    public toggle() {
+        this.setState({
+            open: !this.state.open,
+        });
+    }
+
     public render() {
         const hasChildren = this.props.children && this.props.children.length > 0;
+        const topLevel = this.props.counter === 1;
         const childrenContents =
             hasChildren &&
             this.props.children.map((child, i) => {
@@ -31,22 +66,38 @@ export default class NavNode extends React.Component<IProps> {
                         {...child}
                         key={"navNode-" + this.props.counter + "-" + i}
                         counter={this.props.counter! + 1}
+                        openRecursive={this.props.openRecursive}
                     />
                 );
             });
 
         return (
-            <li className={classNames("navNode", this.props.className)}>
+            <li
+                role={topLevel ? "tree" : "group"}
+                className={classNames("navNode", this.props.className)}
+                aria-labelledby={this.props.titleID}
+                aria-expanded={this.state.open}
+            >
                 {hasChildren && (
-                    <Button>
-                        <Paragraph>{t("Toggle Category")}</Paragraph>
-                        {rightTriangle()}
+                    <Button
+                        tabIndex={-1}
+                        ariaHidden={true}
+                        title={t("Toggle Category")}
+                        ariaLabel={t("Toggle Category")}
+                        onClick={this.toggle}
+                    >
+                        {this.state.open ? downTriangle() : rightTriangle()}
                     </Button>
                 )}
-                <NavLink to={this.props.url} activeClassName="isCurrent">
+                <NavLink tabIndex={0} to={this.props.url} activeClassName="isCurrent">
                     <span className="navNode-label">{this.props.name}</span>
                 </NavLink>
-                {hasChildren && <ul className="navNode-children">{childrenContents}</ul>}
+                {hasChildren &&
+                    this.state.open && (
+                        <ul className="navNode-children" role="group">
+                            {childrenContents}
+                        </ul>
+                    )}
             </li>
         );
     }
--- library/src/scripts/components/siteNav/SiteNav.tsx
+++ library/src/scripts/components/siteNav/SiteNav.tsx
@@ -8,6 +8,7 @@ import * as React from "react";
 import { t } from "../../application";
 import classNames from "classnames";
 import NavNode from "@library/components/siteNav/NavNode";
+import { getRequiredID } from "@library/componentIDs";
 
 interface IProps {
     name: string;
@@ -15,25 +16,130 @@ interface IProps {
     children: any[];
 }
 
+export interface IState {
+    id: string;
+}
+
 /**
  * Recursive component to generate site nav
  * No need to set "counter". It will be set automatically. Kept optional to not need to call it on the top level. Used for React's "key" values
  */
-export default class SiteNav extends React.Component<IProps> {
+export default class SiteNav extends React.Component<IProps, IState> {
+    public constructor(props) {
+        super(props);
+        this.state = {
+            id: getRequiredID(props, "siteNav"),
+        };
+    }
+
+    public get titleID() {
+        return this.state.id + "-title";
+    }
+
     public render() {
         const content =
             this.props.children && this.props.children.length > 0
                 ? this.props.children.map((child, i) => {
-                      return <NavNode {...child} key={`navNode-${i}`} counter={i} />;
+                      return <NavNode {...child} key={`navNode-${i}`} counter={i} titleID={this.titleID} />;
                   })
                 : null;
         return (
-            <nav
-                aria-label={`${t("Category navigation from folder: ")}\"${this.props.name}\"`}
-                className={classNames("siteNavigation", this.props.className)}
-            >
+            <nav onKeyDownCapture={this.handleKeyDown} className={classNames("siteNavigation", this.props.className)}>
+                <h2 id={this.titleID} className="sr-only">{`${t("Category navigation from folder: ")}\"${
+                    this.props.name
+                }\"`}</h2>
                 {content}
             </nav>
         );
     }
+
+    private handleKeyDown = event => {
+        const currentFocussedElement = document.activeElement;
+        switch (`${event.controlKey ? "Control+" : ""}${event.shiftKey ? "Shift+" : ""}event.code`) {
+            case "RightArrow":
+                event.preventDefault();
+                event.stopPropagation();
+                /*
+                    If a row is focused, and it is collapsed, expands the current row.
+                    If a row is focused, and it is expanded, focuses the first cell in the row.
+                    If a cell is focused, moves one cell to the right.
+                    If focus is on the right most cell, focus does not move.
+                 */
+                break;
+            case "LeftArrow":
+                event.preventDefault();
+                event.stopPropagation();
+                break;
+            /*
+                    If a row is focused, and it is expanded, collapses the current row.
+                    If a row is focused, and it is collapsed, moves to the parent row (if there is one).
+                    If a cell in the first column is focused, focuses the row.
+                    If a cell in a different column is focused, moves focus one cell to the left.
+                 */
+            case "DownArrow":
+                event.preventDefault();
+                event.stopPropagation();
+                break;
+            /*
+                    Moves focus one row or one cell down, depending on whether a row or cell is currently focused.
+                    If focus is on the bottom row, focus does not move.
+                 */
+            case "UpArrow":
+                event.preventDefault();
+                event.stopPropagation();
+                break;
+            /*
+                    Moves focus one row or one cell up, depending on whether a row or cell is currently focused.
+                    If focus is on the top row, focus does not move.
+                 */
+            case "Shift+Tab":
+                event.preventDefault();
+                event.stopPropagation();
+                break;
+            /*
+                    Moves focus to the next interactive widget in the current row.
+                    If there are no more interactive widgets in the current row, moves focus out of the treegrid.
+                 */
+            case "Home":
+                event.preventDefault();
+                event.stopPropagation();
+                break;
+            /*
+                    If a cell is focused, moves focus to the previous interactive widget in the current row.
+                    If a row is focused, moves focus out of the treegrid.
+                 */
+            case "End":
+                event.preventDefault();
+                event.stopPropagation();
+                break;
+            /*
+                If a row is focused, moves to the first row.
+                If a cell is focused, moves focus to the first cell in the row containing focus.
+             */
+            case "Control+Home":
+                event.preventDefault();
+                event.stopPropagation();
+                break;
+            /*
+                If a row has focus, moves focus to the first row.
+                If a cell has focus, moves focus to the cell in the first row in the same column as the cell that had focus.
+             */
+            case "Control+End":
+                event.preventDefault();
+                event.stopPropagation();
+                break;
+            /*
+                If a row has focus, moves focus to the last row.
+                If a cell has focus, moves focus to the cell in the last row in the same column as the cell that had focus.
+             */
+            case "Enter":
+                event.preventDefault();
+                event.stopPropagation();
+                break;
+            /*
+                Performs default action associated with row or cell that has focus, e.g. opens message or navigate to link.
+                If focus is on the cell with the expand/collapse button, and there is no other action, will toggle expansion of the current row.
+            */
+        }
+    };
 }

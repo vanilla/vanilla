8dec284ac36f79ffb93824468f40d4952dda070f
Patrick Kelly <patrick.k@vanillaforums.com>
2018-10-24 12:12:52 -0400

    Add more comments to embed.js.

--- js/embed.js
+++ js/embed.js
@@ -237,7 +237,7 @@ window.vanilla.embed = function(host) {
 
         if (embed_type == 'comments') {
             result = '//' + host + '/discussion/embed/'
-            // Break the cache.
+            // Break the cache. /embed/ gets cached, looks like you are not logged in.
             + '&c=' + new Date().getTime()
             + '&vanilla_identifier=' + encodeURIComponent(foreign_id)
             + '&vanilla_url=' + encodeURIComponent(foreign_url);

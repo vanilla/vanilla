23e9085f0b7708b52345e00c6ccffd0f304441bc
igraziatto <isis.graziatto@gmail.com>
2018-10-24 17:20:01 -0400

    handle themeoptions customizations using afterCustomStyles event

--- class.keystone.themehooks.php
+++ class.keystone.themehooks.php
@@ -73,6 +73,44 @@ class KeystoneThemeHooks extends Gdn_Plugin {
     }
 
     /**
+     * Add custom toggles "hasHeroBanner", "hasFeatureSearchbox", "panelToLeft" to Theme Options
+     *
+     *
+     * @param settingsController $sender
+     *
+     * @return void
+     */
+    public function settingsController_afterCustomStyles_handler($sender,$args) {
+        $form = $sender->Form;
+        $hasHeroImagePlugin = $sender->Data["hasHeroImagePlugin"];
+        $hasAdvancedSearch = $sender->Data["hasAdvancedSearch"];
+        echo '<section>';
+        echo    '<h2 class="subheading">'.t("Options").'</h2>';
+
+        //Only render these fields if hasHeroImagePlugin == true
+        if($hasHeroImagePlugin) {
+            echo    '<li class="form-group">';
+            echo        $sender->Form->toggle("ThemeOptions.Options.hasHeroBanner", t("Integrate Hero Image plugin"), [], t("Displays \"Hero Image\" plugin below the header. \"Hero Image\" plugin needs to be enabled for this option to work properly.") );
+            echo    '</li>';
+
+            //Only render this field if AdvancedSearchPlugin == true
+            if($hasAdvancedSearch) {
+                echo    '<li class="form-group">';
+                echo        $sender->Form->toggle("ThemeOptions.Options.hasFeatureSearchbox", t("Integrate searchbox with Hero Image plugin"), [], t("Change searchbox's position to display over Hero Banner. \"Hero Image\" plugin needs to be enabled for this option to work properly.") );
+                echo    '</li>';
+            }
+        }
+
+        echo    '<li class="form-group">';
+        echo        $sender->Form->toggle("ThemeOptions.Options.panelToLeft", t("Panel to the left"), [], t("Change the main panel's position to the left side.") );
+        echo    '</li>';
+        echo '</section>';
+        echo '<div class="form-footer js-modal-footer">';
+        echo    $form->button('Save');
+        echo '</div>';
+    }
+
+    /**
      * Unset ThemeOptions.Options config related to HeroImagePlugin
      *
      *

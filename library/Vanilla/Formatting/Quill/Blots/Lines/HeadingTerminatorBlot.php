e44f6524c4cdf3eaea405fd554e1bf7bd563e367
alexander.kim <kim.alexander.ca@gmail.com>
2018-10-25 17:02:48 -0400

    some spacing

--- library/Vanilla/Formatting/Quill/Blots/Lines/HeadingTerminatorBlot.php
+++ library/Vanilla/Formatting/Quill/Blots/Lines/HeadingTerminatorBlot.php
@@ -30,7 +30,7 @@ class HeadingTerminatorBlot extends AbstractLineTerminatorBlot {
      * @throws \Exception
      */
     public function getGroupOpeningTag(): string {
-        return "<h".$this->getHeadingLevel().">";
+        return "<h" . $this->getHeadingLevel() . ' id="' . $this->getReferenceId() . '">';
     }
 
     /**
@@ -80,10 +80,30 @@ class HeadingTerminatorBlot extends AbstractLineTerminatorBlot {
      * @return int
      * @throws \Exception if the level is not a valid integer.
      */
-    private function getHeadingLevel(): int {
+    public function getHeadingLevel(): int {
         $defaultLevel = 2;
         // Heading attributes generally live in the next operation.
         // For empty headings there is only one operation, so it could be in the current op.
         return $this->currentOperation["attributes"]["header"] ?? $defaultLevel;
+    }
+
+    /**
+     * Get the internal reference  for the blot.
+     *
+     * @return string Interdoc reference. Ex: #my-title, #hello-heading
+     */
+    public function getReference(): string {
+        $ref = '#' . urlencode(str_replace(' ', '-', $this->getText()));
+        return $ref;
+    }
+
+    /**
+     * Get heading text with html tags stripped out.
+     *
+     * @return string
+     */
+    public function getText(): string {
+        $text = strip_tags($this->render());
+        return $text;
     }
 }

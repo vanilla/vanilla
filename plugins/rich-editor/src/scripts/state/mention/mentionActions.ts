4ad9c4f94039703adc35ca8fc6391d258d52aede
Adam Charron <adam@charrondev.com>
2018-10-29 18:40:21 -0400

    Remove forced assertions

--- plugins/rich-editor/src/scripts/state/mention/mentionActions.ts
+++ plugins/rich-editor/src/scripts/state/mention/mentionActions.ts
@@ -56,14 +56,17 @@ function loadUsers(username: string) {
         if (exactLookup != null) {
             switch (exactLookup.status) {
                 case LoadStatus.SUCCESS:
-                    return dispatch(actions.loadUsersSuccess(username, exactLookup.data!));
+                    if (exactLookup.data) {
+                        return dispatch(actions.loadUsersSuccess(username, exactLookup.data));
+                    }
                 case LoadStatus.LOADING:
                     // Already handled
                     return;
                 case LoadStatus.ERROR:
                     // Previously failed.
-                    dispatch(actions.loadUsersFailure(username, exactLookup.error!));
-                    return;
+                    if (exactLookup.error) {
+                        return dispatch(actions.loadUsersFailure(username, exactLookup.error!));
+                    }
             }
         }
 
@@ -72,11 +75,11 @@ function loadUsers(username: string) {
         if (partialLookup != null) {
             switch (partialLookup.status) {
                 case LoadStatus.SUCCESS: {
-                    if (partialLookup.data!.length < USER_LIMIT) {
+                    if (partialLookup.data && partialLookup.data.length < USER_LIMIT) {
                         // The previous match already found the maximum amount of users that the server had
                         // Return the previous results.
                         return dispatch(
-                            actions.loadUsersSuccess(username, filterSuggestions(partialLookup.data!, username)),
+                            actions.loadUsersSuccess(username, filterSuggestions(partialLookup.data, username)),
                         );
                     }
                 }

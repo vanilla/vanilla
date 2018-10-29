2f73984e0052af3609666ad911fc321ca62aec66
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2018-10-28 20:12:35 -0400

    added "radio buttons as tabs" component

---
+++ library/src/scripts/components/radioButtonsAsTabs/RadioButtonTab.tsx
@@ -0,0 +1,64 @@
+/*
+ * @author Stéphane LaFlèche <stephane.l@vanillaforums.com>
+ * @copyright 2009-2018 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ */
+
+import * as React from "react";
+import classNames from "classnames";
+
+interface IRadioButtonTab {
+    groupID: string;
+    label: string;
+    checked?: boolean;
+    className?: string;
+    setData: (data: any) => void;
+    data: any;
+}
+
+interface IState {
+    checked: boolean;
+}
+
+/**
+ * Implement what looks like a tab, but what is semantically radio button. To be used in the RadioButtonsAsTabs component
+ */
+// export default class RadioButtonTab extends React.Component<IRadioButtonTab> {
+export default class RadioButtonTab extends React.Component<IRadioButtonTab> {
+    // public render() {
+
+    public static defaultProps = {
+        checked: false,
+    };
+
+    public constructor(props) {
+        super(props);
+        this.state = {
+            checked: this.props.checked,
+        };
+    }
+
+    private onClick = e => {
+        this.props.setData(this.props.data);
+        this.setState({
+            checked: true,
+        });
+    };
+
+    //{groupID: string, setData: (data: any) => void, selectedTab: ISearchDomain}
+    public render() {
+        return (
+            <label className={classNames("radioButtonsAsTabs-tab", this.props.className)}>
+                <input
+                    className="radioButtonsAsTabs-input sr-only"
+                    type="radio"
+                    onClick={this.onClick}
+                    checked={this.props.checked}
+                    name={this.props.groupID}
+                    value={this.props.label}
+                />
+                <span className="radioButtonsAsTabs-label">{this.props.label}</span>
+            </label>
+        );
+    }
+}
---
+++ library/src/scripts/components/radioButtonsAsTabs/RadioButtonsAsTabs.tsx
@@ -0,0 +1,61 @@
+/*
+ * @author Stéphane LaFlèche <stephane.l@vanillaforums.com>
+ * @copyright 2009-2018 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ */
+
+import { uniqueIDFromPrefix } from "@library/componentIDs";
+import { classNames as className } from "react-select/lib/utils";
+import { ISearchDomain } from "@knowledge/modules/search/components/AdvancedSearch";
+import RadioButtonTab from "./RadioButtonTab";
+import { ReactNode } from "react";
+import { t } from "@library/application";
+
+interface IProps {
+    prefix: string;
+    accessibleTitle: string; // Describe what these buttons represent. Hidden from view, for screen readers
+    children: ReactNode;
+    selectedTab: ISearchDomain;
+    className?: string;
+    setData: (data: any) => void;
+}
+
+/**
+ * Implement what looks like tabs, but what is semantically radio buttons.
+ */
+export default class RadioButtonsAsTabs extends React.Component<IProps> {
+    private groupID;
+
+    public constructor(props) {
+        super(props);
+        this.groupID = uniqueIDFromPrefix(this.props.prefix);
+    }
+
+    public render() {
+        // const content = this.props.children.map((child, index) => {
+        //     return child.render(this.groupID, this.props.setData, this.props.selectedTab);
+        // });
+
+        return (
+            <fieldset
+                className={className("inputBlock radioButtonsAsTabs _searchBarAdvanced-searchIn", this.props.className)}
+            >
+                <legend className="sr-only">{this.props.accessibleTitle}</legend>
+                <div className="radioButtonsAsTabs-tabs">
+                    <RadioButtonTab
+                        groupID={this.groupID}
+                        label={t("Articles")}
+                        setData={this.props.setData}
+                        data={ISearchDomain.ARTICLES}
+                    />
+                    <RadioButtonTab
+                        groupID={this.groupID}
+                        label={t("Everywhere")}
+                        setData={this.props.setData}
+                        data={ISearchDomain.ARTICLES}
+                    />
+                </div>
+            </fieldset>
+        );
+    }
+}

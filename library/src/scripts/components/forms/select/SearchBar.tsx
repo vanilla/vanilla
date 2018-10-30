8f0767e3ae4cb2a6a701541f6372e088715143e5
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2018-10-30 10:15:24 -0400

    Added new compose icon Renamed "menu" to "actions" to be more generic Refactored bigSearch to be searchControl to be more generic Refactored clear token and search's clear icons and alignment

--- library/src/scripts/components/forms/select/SearchBar.tsx
+++ library/src/scripts/components/forms/select/SearchBar.tsx
@@ -18,6 +18,7 @@ import menuList from "@library/components/forms/select/overwrites/menuList";
 import menu from "@library/components/forms/select/overwrites/menu";
 import selectContainer from "@library/components/forms/select/overwrites/selectContainer";
 import doNotRender from "@library/components/forms/select/overwrites/doNotRender";
+import { ReactNode } from "react";
 
 export interface IComboBoxOption {
     value: string;
@@ -33,6 +34,9 @@ interface IProps extends IOptionalComponentID {
     options?: any[];
     loadOptions?: any[];
     setQuery: (value) => void;
+    isBigInput?: boolean;
+    noHeading: boolean;
+    children: ReactNode;
 }
 
 interface IState {
@@ -45,10 +49,13 @@ interface IState {
 export default class BigSearch extends React.Component<IProps> {
     public static defaultProps = {
         disabled: false,
+        isBigInput: false,
+        noHeading: false,
+        children: t("Search"),
     };
 
     private id: string;
-    private prefix = "bigSearch";
+    private prefix = "searchBar";
     private searchButtonID: string;
     private searchInputID: string;
 
@@ -72,7 +79,7 @@ export default class BigSearch extends React.Component<IProps> {
 
         /** The children to be rendered inside the indicator. */
         const componentOverwrites = {
-            Control: this.bigSearchControl,
+            Control: this.searchControl,
             IndicatorSeparator: doNotRender,
             DropdownIndicator: doNotRender,
             ClearIndicator: clearIndicator,
@@ -132,22 +139,34 @@ export default class BigSearch extends React.Component<IProps> {
      * Note that this is NOT a real react component and it needs to be defined here because we need to access the props from the plugin
      * @param props
      */
-    private bigSearchControl = props => {
+    private searchControl = props => {
         const id = uniqueIDFromPrefix("searchInputBlock");
         const labelID = id + "-label";
 
         return (
-            <form className="bigSearch-form" onSubmit={this.preventFormSubmission}>
-                <Heading depth={1} className="bigSearch-heading">
-                    <label className="searchInputBlock-label" htmlFor={this.searchInputID}>
-                        {t("Search")}
-                    </label>
-                </Heading>
-                <div className="bigSearch-content">
-                    <div className={`${this.prefix}-valueContainer inputText isLarge isClearable`}>
+            <form className="searchBar-form" onSubmit={this.preventFormSubmission}>
+                {!this.props.noHeading && (
+                    <Heading depth={1} className="searchBar-heading">
+                        <label className="searchBar-label" htmlFor={this.searchInputID}>
+                            {this.props.children}
+                        </label>
+                    </Heading>
+                )}
+                <div className="searchBar-content">
+                    <div
+                        className={classNames(
+                            `${this.prefix}-valueContainer`,
+                            "suggestedTextInput-inputText",
+                            "inputText",
+                            "isClearable",
+                            {
+                                isLarge: this.props.isBigInput,
+                            },
+                        )}
+                    >
                         <components.Control {...props} />
                     </div>
-                    <Button type="submit" id={this.searchButtonID} className="buttonPrimary bigSearch-submitButton">
+                    <Button type="submit" id={this.searchButtonID} className="buttonPrimary searchBar-submitButton">
                         {t("Search")}
                     </Button>
                 </div>

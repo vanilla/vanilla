30f8946e13650f94a521eadad9881502d2c8f1df
alexander.kim <kim.alexander.ca@gmail.com>
2018-10-30 12:36:43 -0400

    Sphinx trait for Knowledge base project

--- library/Garden/SphinxTrait.php
+++ library/Garden/SphinxTrait.php
@@ -7,14 +7,11 @@
 
 namespace Garden;
 
-use Gdn;
-
 /**
- * For classes that want translation to incorporate.
+ * For classes that want Sphinx search to incorporate.
  *
  */
 trait SphinxTrait {
-
     /**
      * Check if a SphinxClient is available.  If not try to make it available using $apiDir.
      *
@@ -54,73 +51,23 @@ trait SphinxTrait {
         $client = new \SphinxClient();
         $client->setServer($sphinxHost, $sphinxPort);
 
-        // Set some defaults.
+        // Set some defaults
         $client->setMatchMode(SPH_MATCH_EXTENDED2);
         $client->setSortMode(SPH_SORT_TIME_SEGMENTS, 'DateInserted');
         $client->setMaxQueryTime(5000);
-        $client->setFieldWeights(['name' => 3, 'body' => 1]);
+
         return $client;
     }
 
-    public static function sphinxSearch(\SphinxClient $sphinx, string $query, array $indexes): array {
-        $search = $sphinx->query($query, implode(' ', $indexes));
-        echo(__CLASS__.':'.__METHOD__.':'.__LINE__."\n");
-        die(print_r($search));
-        $results = self::sphinxGetDocuments($search);
-        $total = $search['total'] ?? 0;
-        $searchTerms = $search['words'];
-
-        if (is_array($searchTerms)) {
-            $searchTerms = array_keys($searchTerms);
-        } else {
-            $searchTerms = [];
-        }
-
-        return [
-            'SearchResults' => $results,
-            'RecordCount' => $total,
-            'SearchTerms' => $searchTerms
-        ];
-    }
-
+    /**
+     * Return sphinx index name: ex vanilla_dev_KnowledgeArticle
+     *
+     * @param string $index Search index name. Ex: KnowledgeArticle
+     * @return string
+     */
     public static function sphinxIndexName(string $index): string {
         $prefix = str_replace(['-'], '_', c('Database.Name')) . '_';
         return $prefix . $index;
     }
 
-    public static function sphinxGetDocuments($search): array {
-        $result = [];
-
-        // Loop through the matches to figure out which IDs we have to grab.
-        $iDs = [];
-        if (!is_array($search) || !isset($search['matches'])) {
-            return [];
-        }
-
-        foreach ($search['matches'] as $documentID => $info) {
-            print_r($documentID);
-            print_r($info);
-            $iD = (int) ($documentID / 10);
-            $type = $documentID % 10;
-
-            $iDs[$type][] = $iD;
-        }
-        die();
-
-        // Join them with the search results.
-        $result = [];
-        foreach ($search['matches'] as $documentID => $info) {
-            $row = $documents[$documentID] ?? false;
-            if ($row === false) {
-                continue;
-            }
-            $row['Relevance'] = $info['weight'];
-            $row['Score'] = $info['attrs']['score'];
-            $row['Count'] = $info['attrs']['@count'] ?? 1;
-            $row['sort'] = $info['attrs']['sort'] ?? false;
-
-            $result[] = $row;
-        }
-        return $result;
-    }
 }

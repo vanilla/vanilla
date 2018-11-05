eb697d1ced45d570bdfe0edd4aad69f8cbf44d93
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2018-11-05 14:54:15 -0500

    Added missing include

--- library/src/scripts/components/icons/revisions.tsx
+++ library/src/scripts/components/icons/revisions.tsx
@@ -7,6 +7,7 @@
 import React from "react";
 import classNames from "classnames";
 import { t } from "@library/application";
+import { checkCompact } from "./common";
 
 const currentColorFill = {
     fill: "currentColor",

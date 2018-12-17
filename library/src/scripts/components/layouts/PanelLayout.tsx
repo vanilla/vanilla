50d2d1113e0c6790828bca1a5823f0ce510ab463
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2018-12-17 08:45:15 -0500

    Removed unused component Refactored page loader to be fixed position Fixed padding issues with panel layout

--- library/src/scripts/components/layouts/PanelLayout.tsx
+++ library/src/scripts/components/layouts/PanelLayout.tsx
@@ -15,6 +15,7 @@ interface IPanelLayoutProps extends IDeviceProps {
     toggleMobileMenu?: (isOpen: boolean) => void;
     contentTag?: string;
     growMiddleBottom?: boolean;
+    topPadding?: boolean;
 }
 
 /**
@@ -76,6 +77,7 @@ export default class PanelLayout extends CompoundComponent<IPanelLayoutProps> {
     public static defaultProps = {
         contentTag: "div",
         growMiddleBottom: false,
+        topPadding: true,
     };
 
     public render() {
@@ -98,6 +100,7 @@ export default class PanelLayout extends CompoundComponent<IPanelLayoutProps> {
             { noBreadcrumbs: !children.breadcrumbs },
             this.props.className,
             { inheritHeight: this.props.growMiddleBottom },
+            { hasTopPadding: this.props.topPadding },
         );
 
         // If applicable, set semantic tag, like "article"

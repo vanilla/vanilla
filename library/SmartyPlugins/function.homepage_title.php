8c062746b06d4448a9e8bcddb7eca8930d22011f
Ryan <initvector@users.noreply.github.com>
2019-01-02 11:39:05 -0500

    Fix doc block on Smarty homepage title function

--- library/SmartyPlugins/function.homepage_title.php
+++ library/SmartyPlugins/function.homepage_title.php
@@ -1,12 +1,11 @@
 <?php
 /**
+ * Returns Homepage Title from config
+ *
  * @copyright 2009-2019 Vanilla Forums Inc.
  * @license GPL-2.0-only
  * @package vanilla-smarty
  * @since 2.0
- *
- * Returns Homepage Title from config
- *
  * @return string
  */
 function smarty_function_homepage_title() {

249960ccfdb692c73b80dfd4a36c33fe8bf7f68c
Adam Charron <adam@charrondev.com>
2019-01-17 20:38:46 -0500

    Add file upload button

--- plugins/rich-editor/src/scripts/components/editor/pieces/EditorUploadButton.tsx
+++ plugins/rich-editor/src/scripts/components/editor/pieces/EditorUploadButton.tsx
@@ -8,10 +8,12 @@ import React, { MouseEvent, ChangeEvent } from "react";
 import EmbedInsertionModule from "@rich-editor/quill/EmbedInsertionModule";
 import { withEditor, IWithEditorProps } from "@rich-editor/components/context";
 import { isFileImage } from "@library/utility";
-import { image } from "@library/components/icons/editorIcons";
+import { image, attachment } from "@library/components/icons/editorIcons";
 
 interface IProps extends IWithEditorProps {
     disabled?: boolean;
+    type: "file" | "image";
+    allowedMimeTypes: string[];
 }
 
 export class EditorUploadButton extends React.Component<IProps, {}> {
@@ -26,16 +28,40 @@ export class EditorUploadButton extends React.Component<IProps, {}> {
                 disabled={this.props.disabled}
                 onClick={this.onFakeButtonClick}
             >
-                {image()}
+                {this.icon}
                 <input
                     ref={this.inputRef}
                     onChange={this.onInputChange}
                     className="richEditor-upload"
                     type="file"
-                    accept="image/gif, image/jpeg, image/jpg, image/png"
+                    accept={this.inputAccepts}
                 />
             </button>
         );
+    }
+
+    private isMimeTypeImage = (mimeType: string) => mimeType.startsWith("image/");
+
+    private get icon(): React.ReactNode {
+        switch (this.props.type) {
+            case "file":
+                return attachment();
+            case "image":
+                return image();
+        }
+    }
+
+    private get inputAccepts(): string {
+        switch (this.props.type) {
+            case "file": {
+                const types = this.props.allowedMimeTypes.filter(type => !this.isMimeTypeImage(type));
+                return types.join(", ");
+            }
+            case "image": {
+                const types = this.props.allowedMimeTypes.filter(this.isMimeTypeImage);
+                return types.join(",");
+            }
+        }
     }
 
     private onFakeButtonClick = (event: MouseEvent<any>) => {
@@ -44,15 +70,19 @@ export class EditorUploadButton extends React.Component<IProps, {}> {
         }
     };
 
-    private onInputChange = (event: ChangeEvent<any>) => {
+    private onInputChange = () => {
         // Grab the first file.
         const file =
             this.inputRef && this.inputRef.current && this.inputRef.current.files && this.inputRef.current.files[0];
         const embedInsertion =
             this.props.quill && (this.props.quill.getModule("embed/insertion") as EmbedInsertionModule);
 
-        if (file && isFileImage(file) && embedInsertion) {
-            embedInsertion.createImageEmbed(file);
+        if (file && embedInsertion) {
+            if (this.props.type === "image" && isFileImage(file)) {
+                embedInsertion.createImageEmbed(file);
+            } else {
+                embedInsertion.createFileEmbed(file);
+            }
         }
     };
 }

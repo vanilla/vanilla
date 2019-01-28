64a5fb9f51cd936a6c3883ebcb43712ca1ccdb98
Chris Chabilall <chris.c@vanillaforums.com>
2019-01-28 17:01:33 -0500

    More refactors

--- plugins/Akismet/class.akismet.php
+++ plugins/Akismet/class.akismet.php
@@ -312,21 +312,27 @@ class Akismet {
     }
 
     /**
-     *    Defaults to 80
+     * Defaults to 80
+     *
+     * @param string $apiPort The apiPort.
      */
     public function setAPIPort($apiPort) {
         $this->apiPort = $apiPort;
     }
 
     /**
-     *    Defaults to rest.akismet.com
+     * Defaults to rest.akismet.com
+     *
+     * @param string $akismetServer The akismetServer.
      */
     public function setAkismetServer($akismetServer) {
         $this->akismetServer = $akismetServer;
     }
 
     /**
-     *    Defaults to '1.1'
+     * Defaults to '1.1'
+     *
+     * @param string $akismetVersion The akismetVersion.
      */
     public function setAkismetVersion($akismetVersion) {
         $this->akismetVersion = $akismetVersion;
@@ -334,13 +340,16 @@ class Akismet {
 }
 
 /**
- *    Utility class used by Akismet
+ * Utility class used by Akismet
  *
- *  This class is used by Akismet to do the actual sending and receiving of data.  It opens a connection to a remote host, sends some data and the reads the response and makes it available to the calling program.
+ * This class is used by Akismet to do the actual sending and receiving of data.
+ * It opens a connection to a remote host, sends some data and the reads the response and makes it available to the calling program.
  *
- *  The code that makes up this class originates in the Akismet WordPress plugin, which is {@link http://akismet.com/download/ available on the Akismet website}.
+ * The code that makes up this class originates in the Akismet WordPress plugin,
+ * which is {@link http://akismet.com/download/ available on the Akismet website}.
  *
- *    N.B. It is not necessary to call this class directly to use the Akismet class.  This is included here mainly out of a sense of completeness.
+ * N.B. It is not necessary to call this class directly to use the Akismet class.
+ * This is included here mainly out of a sense of completeness.
  *
  * @package    akismet
  * @name        SocketWriteRead
@@ -358,10 +367,12 @@ class SocketWriteRead {
     private $errorString;
 
     /**
-     * @param    string $host The host to send/receive data.
-     * @param    int $port The port on the remote host.
-     * @param    string $request The data to send.
-     * @param    int $responseLength The amount of data to read.  Defaults to 1160 bytes.
+     * Constructor
+     *
+     * @param string $host The host to send/receive data.
+     * @param int $port The port on the remote host.
+     * @param string $request The data to send.
+     * @param int $responseLength The amount of data to read.  Defaults to 1160 bytes.
      */
     public function __construct($host, $port, $request, $responseLength = 1160) {
         $this->host = $host;
@@ -373,9 +384,11 @@ class SocketWriteRead {
     }
 
     /**
-     *  Sends the data to the remote host.
+     * Send
      *
-     * @throws    An exception is thrown if a connection cannot be made to the remote host.
+     * Sends the data to the remote host.
+     *
+     * @throws Exception Exception is thrown if a connection cannot be made to the remote host.
      */
     public function send() {
         $this->response = '';
@@ -400,16 +413,16 @@ class SocketWriteRead {
     /**
      *  Returns the server response text
      *
-     * @return    string
+     * @return string
      */
     public function getResponse() {
         return $this->response;
     }
 
     /**
-     *    Returns the error number
+     * Returns the error number
      *
-     *    If there was no error, 0 will be returned.
+     * If there was no error, 0 will be returned.
      *
      * @return int
      */
@@ -418,9 +431,9 @@ class SocketWriteRead {
     }
 
     /**
-     *    Returns the error string
+     * Returns the error string
      *
-     *    If there was no error, an empty string will be returned.
+     * If there was no error, an empty string will be returned.
      *
      * @return string
      */

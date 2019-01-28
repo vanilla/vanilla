02d0dd4d211babb83b48f5f924a362b0637bb12e
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2019-01-28 16:04:29 -0500

    Created new component for displaying lists of links

---
+++ library/src/scripts/components/NavLinks.tsx
@@ -0,0 +1,48 @@
+/**
+ * @author Adam Charron <adam.c@vanillaforums.com>
+ * @copyright 2009-2019 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ */
+
+import React, { Component } from "react";
+import Heading from "@library/components/Heading";
+import classNames from "classnames";
+import { NavArticle } from "@knowledge/modules/navigation/NavigationSelector";
+import SmartLink from "@library/components/navigation/SmartLink";
+
+// export interface NavLink {
+//     to: string;
+//     name: string;
+// }
+// links: NavLink[];
+
+interface IProps {
+    classNames?: string;
+    title: string;
+    articles: NavArticle[];
+}
+
+/**
+ * Component for displaying lists in "tiles"
+ */
+export default class NavLinks extends Component<IProps> {
+    public render() {
+        if (this.props.articles.length !== 0) {
+            const contents = this.props.articles.map(item => {
+                return (
+                    <li>
+                        <SmartLink to={item.url} className="navLinks-link" title={item.name} />
+                    </li>
+                );
+            });
+            return (
+                <section className={classNames("navLinks", this.props.classNames)}>
+                    <Heading title={this.props.title} className="navLinks-title" />
+                    <ul className="navLinks-links">{contents}</ul>
+                </section>
+            );
+        } else {
+            return null;
+        }
+    }
+}
---
+++ library/src/scripts/components/NavLinksWithHeadings.tsx
@@ -0,0 +1,49 @@
+/**
+ * @author Adam Charron <adam.c@vanillaforums.com>
+ * @copyright 2009-2019 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ */
+
+import React, { Component } from "react";
+
+import { t } from "@library/application";
+import Heading from "@library/components/Heading";
+import classNames from "classnames";
+import { IHelpData } from "@knowledge/modules/navigation/NavigationSelector";
+import NavLinks from "@library/components/NavLinks";
+import ScreenReaderContent from "@library/components/ScreenReaderContent";
+
+interface IProps {
+    title: string; // For accessibility, title of group
+    classNames?: string;
+    linkGroups: IHelpData;
+}
+
+/**
+ * Component for displaying lists in "tiles"
+ */
+export default class NavLinksWithHeadings extends Component<IProps> {
+    public render() {
+        const ungrouped = this.props.linkGroups.ungroupedArticles || [];
+        const grouped = this.props.linkGroups.groups || [];
+
+        if (ungrouped.length !== 0 || grouped.length !== 0) {
+            const ungroupedContent = <NavLinks articles={ungrouped} title={t("Overview")} />;
+            const groupedContent = grouped.map(group => {
+                return <NavLinks articles={group.articles} title={group.category.name} />;
+            });
+
+            return (
+                <nav className={classNames("navLinksWithHeadings", this.props.classNames)}>
+                    <ScreenReaderContent>
+                        <Heading title={this.props.title} />
+                    </ScreenReaderContent>
+                    {ungroupedContent}
+                    {groupedContent}
+                </nav>
+            );
+        } else {
+            return null;
+        }
+    }
+}

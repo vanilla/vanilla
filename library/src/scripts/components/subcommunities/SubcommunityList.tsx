614190fd94f86708d8a120b3fa9e22151dd9cd71
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2019-02-21 16:19:22 -0500

    Added styles for subcomunity list Removed empty SASS files.

--- library/src/scripts/components/subcommunities/SubcommunityList.tsx
+++ library/src/scripts/components/subcommunities/SubcommunityList.tsx
@@ -8,6 +8,7 @@ import * as React from "react";
 import classNames from "classnames";
 import Paragraph from "@library/components/Paragraph";
 import SubcommunityTile from "@library/components/subcommunities/SubcommunityTile";
+import { subcommunityListClasses } from "@library/styles/subcommunityListStyles";
 
 interface ISubcommunity {
     icon: string;
@@ -24,6 +25,7 @@ interface IProps {
     hiddenTitle?: boolean;
     emptyMessage: string;
     fallbackIcon: React.ReactNode;
+    theme?: object;
 }
 
 /**
@@ -31,20 +33,21 @@ interface IProps {
  */
 export default class SubcommunityList extends React.Component<IProps> {
     public render() {
-        const { className, items } = this.props;
+        const { className, items, theme } = this.props;
+        const classes = subcommunityListClasses(theme);
 
         if (items.length === 0) {
             return (
-                <div className={classNames("subcommunityList", className, "isEmpty")}>
+                <div className={classNames("subcommunityList", className, "isEmpty", classes.root)}>
                     <Paragraph className="subcommunityList-emptyMessage">{this.props.emptyMessage}</Paragraph>
                 </div>
             );
         } else {
             return (
-                <div className={classNames("subcommunityList", className)}>
-                    <ul className="subcommunityList-items">
+                <div className={classNames("subcommunityList", className, classes.root)}>
+                    <ul className={classNames("subcommunityList-items", classes.items)}>
                         {items.map((subcommunity, i) => (
-                            <li key={i} className="subcommunityList-item">
+                            <li key={i} className={classNames("subcommunityList-item", classes.item)}>
                                 <SubcommunityTile
                                     icon={subcommunity.icon}
                                     title={subcommunity.name}

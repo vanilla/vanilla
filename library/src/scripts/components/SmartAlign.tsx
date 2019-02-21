6f2650b72a2d334da3e96f9413de399bd3b08976
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2019-02-21 15:40:05 -0500

    Added smart align styles

--- library/src/scripts/components/SmartAlign.tsx
+++ library/src/scripts/components/SmartAlign.tsx
@@ -5,22 +5,24 @@
 
 import React from "react";
 import classNames from "classnames";
+import { smartAlignClasses } from "@library/styles/smartAlignStyles";
 
 interface IProps {
     className?: string;
     children: React.ReactNode;
     outerTag?: string;
     innerTag?: string;
+    theme?: object;
 }
 
 export default class SmartAlign extends React.Component<IProps> {
     public render() {
         const Outer = this.props.outerTag ? `${this.props.outerTag}` : "div";
         const Inner = this.props.outerTag ? `${this.props.innerTag}` : "div";
-
+        const classes = smartAlignClasses(this.props.theme);
         return (
-            <Outer className={classNames("smartAlign-outer", this.props.className)}>
-                <Inner className="smartAlign-inner">{this.props.children}</Inner>
+            <Outer className={classNames("smartAlign-outer", this.props.className, classes.root)}>
+                <Inner className={classNames("smartAlign-inner", classes.inner)}>{this.props.children}</Inner>
             </Outer>
         );
     }

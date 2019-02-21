0915d54c8052e0cee35d47b54818f6fc72a8ee1b
Adam Charron <adam@charrondev.com>
2019-02-21 00:56:46 -0500

    Move replaceButProtectCodeBlocks to FormatUtil

--- library/core/class.format.php
+++ library/core/class.format.php
@@ -12,8 +12,6 @@
  */
 
 use Garden\EventManager;
-use Vanilla\Formatting\Formats;
-use Vanilla\Formatting\Html;
 use \Vanilla\Formatting;
 
 /**
@@ -967,7 +965,7 @@ class Gdn_Format {
      * @since 2.1
      */
     public static function plainText($body, $format = 'Html', $collapse = false) {
-        if (strcasecmp($format, \Vanilla\Formatting\Formats\RichFormat::FORMAT_KEY) === 0) {
+        if (strcasecmp($format, Formatting\Formats\RichFormat::FORMAT_KEY) === 0) {
             return self::getFormatService()
                 ->getFormatter(Formats\RichFormat::FORMAT_KEY)
                 ->renderPlainText($body);
@@ -1008,7 +1006,7 @@ class Gdn_Format {
      * @since 2.1
      */
     public static function excerpt($body, $format = 'Html', $collapse = false) {
-        if (strcasecmp($format, \Vanilla\Formatting\Formats\RichFormat::FORMAT_KEY) === 0) {
+        if (strcasecmp($format, Formatting\Formats\RichFormat::FORMAT_KEY) === 0) {
             return self::getFormatService()
                 ->getFormatter(Formats\RichFormat::FORMAT_KEY)
                 ->renderExcerpt($body);
@@ -1780,49 +1778,16 @@ EOT;
     }
 
     /**
-     * Do a preg_replace, but don't affect things inside <code> tags.
-     *
-     * The three parameters are identical to the ones you'd pass
-     * preg_replace.
-     *
-     * @param mixed $search The value being searched for, just like in
-     *              preg_replace or preg_replace_callback.
-     * @param mixed $replace The replacement value, just like in
-     *              preg_replace or preg_replace_callback.
-     * @param mixed $subject The string being searched.
-     * @param bool $isCallback If true, do preg_replace_callback. Do
-     *             preg_replace otherwise.
-     * @return string
+     * @deprecated 2.9 Use Formatting\FormatUtil::replaceButProtectCodeBlocks
      */
     public static function replaceButProtectCodeBlocks($search, $replace, $subject, $isCallback = false) {
-        // Take the code blocks out, replace with a hash of the string, and
-        // keep track of what substring got replaced with what hash.
-        $codeBlockContents = [];
-        $codeBlockHashes = [];
-        $subject = preg_replace_callback(
-            '/<code.*?>.*?<\/code>/is',
-            function ($matches) use (&$codeBlockContents, &$codeBlockHashes) {
-                // Surrounded by whitespace to try to prevent the characters
-                // from being picked up by $Pattern.
-                $replacementString = ' '.sha1($matches[0]).' ';
-                $codeBlockContents[] = $matches[0];
-                $codeBlockHashes[] = $replacementString;
-                return $replacementString;
-            },
-            $subject
+        deprecated(__FUNCTION__, 'FormatUtil::replaceButProtectCodeBlocks');
+        return Formatting\FormatUtil::replaceButProtectCodeBlocks(
+            (string) $search,
+            (string) $replace,
+            (string) $subject,
+            (bool) $isCallback
         );
-
-        // Do the requested replacement.
-        if ($isCallback) {
-            $subject = preg_replace_callback($search, $replace, $subject);
-        } else {
-            $subject = preg_replace($search, $replace, $subject);
-        }
-
-        // Put back the code blocks.
-        $subject = str_replace($codeBlockHashes, $codeBlockContents, $subject);
-
-        return $subject;
     }
 
     /**
@@ -2260,7 +2225,7 @@ EOT;
      * @return string
      */
     public static function quoteEmbed($body, string $format): string {
-        if (strcasecmp($format, \Vanilla\Formatting\Formats\RichFormat::FORMAT_KEY) === 0) {
+        if (strcasecmp($format, Formatting\Formats\RichFormat::FORMAT_KEY) === 0) {
             if (is_array($body)) {
                 $body = json_encode($body);
             }

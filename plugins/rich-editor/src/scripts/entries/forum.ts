4da8c02665faa0fec46ffc1b3dd39d5607ba3b20
Ryan <initvector@users.noreply.github.com>
2019-03-14 15:12:32 -0400

    Move registering editor reducer outside of onReady

--- plugins/rich-editor/src/scripts/entries/forum.ts
+++ plugins/rich-editor/src/scripts/entries/forum.ts
@@ -8,8 +8,8 @@ import editorReducer from "@rich-editor/state/editorReducer";
 import { registerReducer } from "@library/state/reducerRegistry";
 import { onReady, onContent } from "@library/application";
 
+registerReducer("editor", editorReducer);
 onReady(() => {
-    registerReducer("editor", editorReducer);
     void setupEditor();
 });
 onContent(setupEditor);

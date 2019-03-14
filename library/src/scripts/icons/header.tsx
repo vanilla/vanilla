312e5ff9170cd0af5bf611c4725e87469f2283af
Adam Charron <adam@charrondev.com>
2019-03-14 16:55:06 -0400

    Remove duplicate icons

--- library/src/scripts/icons/header.tsx
+++ library/src/scripts/icons/header.tsx
@@ -5,63 +5,8 @@
  */
 
 import React from "react";
-import { t } from "../dom/appUtils";
+import { t } from "@library/utility/appUtils";
 import classNames from "classnames";
-
-const currentColorFill = {
-    fill: "currentColor",
-};
-
-export function rightChevron(className?: string) {
-    const title = `>`;
-    return (
-        <svg
-            className={classNames("icon", "icon-chevronRight", className)}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            aria-hidden="true"
-        >
-            <title>{title}</title>
-            <path
-                fill="currentColor"
-                d="M15.4955435,6.92634039 C15.4955435,6.82589384 15.4509005,6.73660802 15.3839362,6.66964366 L14.8258998,6.11160728 C14.7589354,6.04464291 14.6584889,6 14.5692031,6 C14.4799172,6 14.3794707,6.04464291 14.3125063,6.11160728 L9.11160728,11.3125063 C9.04464291,11.3794707 9,11.4799172 9,11.5692031 C9,11.6584889 9.04464291,11.7589354 9.11160728,11.8258998 L14.3125063,17.0267989 C14.3794707,17.0937632 14.4799172,17.1384061 14.5692031,17.1384061 C14.6584889,17.1384061 14.7589354,17.0937632 14.8258998,17.0267989 L15.3839362,16.4687625 C15.4509005,16.4017981 15.4955435,16.3013516 15.4955435,16.2120657 C15.4955435,16.1227799 15.4509005,16.0223334 15.3839362,15.955369 L10.9977702,11.5692031 L15.3839362,7.18303712 C15.4509005,7.11607276 15.4955435,7.01562621 15.4955435,6.92634039 Z"
-                transform="matrix(-1 0 0 1 25 .5)"
-            />
-        </svg>
-    );
-}
-
-const leftChefronPath =
-    "M14.9,7.7l-4.4,4.4,4.4,4.4a.5.5,0,0,1,0,.6l-.6.6a.5.5,0,0,1-.6,0L8.5,12.5a.5.5,0,0,1,0-.6l5.2-5.2a.5.5,0,0,1,.6,0s.676.543.7.7A.325.325,0,0,1,14.9,7.7Z";
-
-export function leftChevron(className?: string) {
-    const title = `<`;
-    return (
-        <svg
-            className={classNames("icon", "icon-chevronLeft", className)}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            aria-hidden="true"
-        >
-            <title>{title}</title>
-            <path d={leftChefronPath} style={currentColorFill} />
-        </svg>
-    );
-}
-
-export function leftChevronCompact(className?: string) {
-    const title = `<`;
-    return (
-        <svg
-            className={classNames("icon-chevronLeftCompact", className)}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 6.606 11.2"
-        >
-            <title>{title}</title>
-            <path d={leftChefronPath} transform="translate(-8.4 -6.6)" style={currentColorFill} />
-        </svg>
-    );
-}
 
 export function close(className?: string, noPadding: boolean = false) {
     const title = t("Close");
@@ -117,62 +62,6 @@ export function check(className?: string) {
     );
 }
 
-export function dropDownMenu(className?: string) {
-    const title = `…`;
-    return (
-        <svg
-            className={classNames("icon", "icon-dropDownMenu", className)}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            aria-hidden="true"
-        >
-            <title>{title}</title>
-            <circle cx="5.7" cy="12" r="2" fill="currentColor" />
-            <circle cx="18.3" cy="12" r="2" fill="currentColor" />
-            <circle cx="12" cy="12" r="2" fill="currentColor" />
-        </svg>
-    );
-}
-
-export function categoryIcon(className?: string) {
-    const title = t("Folder");
-    return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className={classNames("icon", "icon-categoryIcon", className)}
-            viewBox="0 0 14.25 12.75"
-            role="img"
-            aria-label={title}
-        >
-            <title>{title}</title>
-            <path
-                d="M9.369,3.164H14.75a.375.375,0,0,1,.375.375V13a.375.375,0,0,1-.375.375H1.25A.375.375,0,0,1,.875,13V1A.375.375,0,0,1,1.25.625H8a.375.375,0,0,1,.343.223ZM1.625,1.375v11.25h12.75V3.914H9.125a.375.375,0,0,1-.343-.223L7.756,1.375Z"
-                transform="translate(-0.875 -0.625)"
-                fill="currentColor"
-            />
-        </svg>
-    );
-}
-
-export function checkCompact(className?: string) {
-    const title = `✓`;
-    return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className={classNames("icon", "icon-selectedCategory", className)}
-            viewBox="0 0 16.8 13"
-            role="img"
-            aria-label={title}
-        >
-            <title>{title}</title>
-            <polygon
-                points="12.136 3.139 13.25 4.253 6.211 11.292 2.75 7.83 3.863 6.717 6.211 9.064 12.136 3.139"
-                fill="currentColor"
-            />
-        </svg>
-    );
-}
-
 export function help(className?: string) {
     const title = t("Help");
     return (
@@ -223,22 +112,6 @@ export function download(className?: string) {
             <path
                 d="M6.483,10.462h.005a.5.5,0,0,1,.707.01l3.954,4.066V3.887a.5.5,0,0,1,.5-.5h.621a.5.5,0,0,1,.5.5V14.533l4.035-4.073h0a.5.5,0,0,1,.707,0l.437.437a.5.5,0,0,1,0,.707h0l-5.6,5.6a.5.5,0,0,1-.707,0h0l-5.6-5.6a.5.5,0,0,1,0-.707h0ZM20.25,19.5V17.25a.75.75,0,0,1,1.5,0v3A.75.75,0,0,1,21,21H3a.75.75,0,0,1-.75-.75v-3a.75.75,0,0,1,1.5,0V19.5Z"
                 fill="currentColor"
-            />
-        </svg>
-    );
-}
-
-export function plusCircle(className?: string) {
-    return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 14 14"
-            className={classNames("icon", "icon-plusCircle", className)}
-            aria-hidden="true"
-        >
-            <path
-                fill="currentColor"
-                d="M14,7A7,7,0,1,1,7,0,7,7,0,0,1,14,7Zm-3.727.79a.339.339,0,0,0,.34-.338h0v-.9a.339.339,0,0,0-.339-.339H7.79V3.727a.339.339,0,0,0-.338-.34h-.9a.339.339,0,0,0-.339.339h0V6.21H3.727a.339.339,0,0,0-.34.338h0v.9a.339.339,0,0,0,.339.339H6.21v2.483a.339.339,0,0,0,.338.34h.9a.339.339,0,0,0,.339-.339h0V7.79Z"
             />
         </svg>
     );
@@ -452,10 +325,4 @@ export function userWarning(className?: string) {
             />
         </svg>
     );
-}
-
-{
-    /* <svg class="embedLoader-icon embedLoader-warningIcon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                            <path d="M11.651,3.669,2.068,21.75H21.234Zm.884-1,10,18.865A1,1,0,0,1,21.649,23h-20a1,1,0,0,1-.884-1.468l10-18.865a1,1,0,0,1,1.768,0Zm.231,13.695H10.547L10.2,10h2.9Zm-2.535,2.354a1.24,1.24,0,0,1,.363-.952,1.493,1.493,0,0,1,1.056-.34,1.445,1.445,0,0,1,1.039.34,1.26,1.26,0,0,1,.353.952,1.223,1.223,0,0,1-.366.944A1.452,1.452,0,0,1,11.65,20a1.5,1.5,0,0,1-1.042-.34A1.206,1.206,0,0,1,10.231,18.716Z" style="fill: currentColor;"/>
-                        </svg> */
 }

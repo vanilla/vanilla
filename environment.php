ae9d6b8d7b4bda0dcf4081be831fc924da915eec
dannimajdalani <dani.m@vanillaforums.com>
2019-03-25 10:13:40 -0400

    Update version 2019.005

--- environment.php
+++ environment.php
@@ -16,7 +16,7 @@ if (!defined('APPLICATION')) {
     define('APPLICATION', 'Vanilla');
 }
 if (!defined('APPLICATION_VERSION')) {
-    define('APPLICATION_VERSION', '2.8.1+2019.004');
+    define('APPLICATION_VERSION', '2.8.1+2019.005');
 }
 if (!defined('DS')) {
     define('DS', DIRECTORY_SEPARATOR);

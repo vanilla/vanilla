84f77aea58fb0aec2f798dd99d9fa2f22b22bcf2
Chris Chabilall <chris.c@vanillaforums.com>
2019-04-04 14:30:09 -0400

    Started google sign in class

---
+++ plugins/googlesignin/class.googlesignin.plugin.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * @copyright 2009-2019 Vanilla Forums Inc.
+ * @license Proprietary
+ */
+
+/**
+ * Class GoogleSignInPlugin
+ *
+ * Expose the functionality of the core class Gdn_OAuth2 to create SSO workflows.
+ */
+
+class GoogleSignInPlugin extends Gdn_OAuth2 {
+    /**
+     * Set the key for saving OAuth settings in GDN_UserAuthenticationProvider
+     */
+    public function __construct() {
+        $this->setProviderKey('googlesignin');
+        $this->settingsView = 'plugins/settings/googlesignin';
+    }
+
+    /**
+     * Create a controller to deal with plugin settings in dashboard.
+     *
+     * @param Gdn_Controller $sender.
+     * @param Gdn_Controller $args.
+     */
+    public function settingsEndpoint($sender, $args) {
+        $sender->permission('Garden.Settings.Manage');
+        $model = new Gdn_AuthenticationProviderModel();
+
+        /* @var Gdn_Form $form */
+        $form = new Gdn_Form();
+        $form->setModel($model);
+        $sender->Form = $form;
+
+        if (!$form->authenticatedPostBack()) {
+            $provider = $this->provider();
+            $form->setData($provider);
+        } else {
+
+            $form->setFormValue('AuthenticationKey', $this->getProviderKey());
+
+            $sender->Form->validateRule('AssociationKey', 'ValidateRequired', 'You must provide a unique AccountID.');
+            $sender->Form->validateRule('AssociationSecret', 'ValidateRequired', 'You must provide a Secret');
+
+            // To satisfy the AuthenticationProviderModel, create a BaseUrl.
+            $baseUrlParts = parse_url($form->getValue('AuthorizeUrl'));
+            $baseUrl = (val('scheme', $baseUrlParts) && val('host', $baseUrlParts)) ? val('scheme', $baseUrlParts).'://'.val('host', $baseUrlParts) : null;
+            if ($baseUrl) {
+                $form->setFormValue('BaseUrl', $baseUrl);
+                $form->setFormValue('SignInUrl', $baseUrl); // kludge for default provider
+            }
+            if ($form->save()) {
+                $sender->informMessage(t('Saved'));
+            }
+        }
+
+        // Set up the form.
+        $formFields = [
+            'AssociationKey' =>  ['LabelCode' => 'Client ID', 'Description' => 'Unique ID of the authentication application.'],
+            'AssociationSecret' =>  ['LabelCode' => 'Secret', 'Description' => 'Secret provided by the authentication provider.'],
+        ];
+
+        $formFields['IsDefault'] = ['LabelCode' => 'Make this connection your default signin method.', 'Control' => 'checkbox'];
+
+        $sender->setData('_Form', $formFields);
+
+        $sender->setHighlightRoute();
+        if (!$sender->data('Title')) {
+            $sender->setData('Title', sprintf(t('%s Settings'), 'Google Sign-In'));
+        }
+
+        $view = ($this->settingsView) ? $this->settingsView : 'plugins/googlesignin';
+
+        // Create and send the possible redirect URLs that will be required by the authenticating server and display them in the dashboard.
+        // Use Gdn::Request instead of convience function so that we can return http and https.
+        $redirectUrls = Gdn::request()->url('/entry/'. $this->getProviderKey(), true, true);
+        $sender->setData('redirectUrls', $redirectUrls);
+
+        $sender->render('settings', '', $view);
+    }
+
+
+}

2017e7375b2481743553f765d27a0b84d6c09306
alexander.kim <kim.alexander.ca@gmail.com>
2019-04-05 14:15:26 -0400

    Add new TickApiController to register basic pageviews

---
+++ applications/dashboard/controllers/TickApiController.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @copyright 2009-2019 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ */
+
+use Garden\Web\Data;
+use Gdn_Statistics as Statistics;
+
+/**
+ * API Controller for site analytics.
+ */
+class TickApiController extends AbstractApiController {
+
+    /** @var Statistics */
+    private $statistics;
+
+    /**
+     * TickApiController constructor.
+     * @param Gdn_Statistics $statisticss
+     */
+    public function __construct(Statistics $statistics) {
+        $this->statistics = $statistics;
+    }
+
+    /**
+     * Collect an analytics tick.
+     * @return Data
+     */
+    public function post(): Data {
+        $this->statistics->tick();
+        $this->statistics->fireEvent("AnalyticsTick");
+        return new Data('');
+    }
+}

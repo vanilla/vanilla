976c8707f62a61b672ade3106e864b7abf156608
Adam Charron <adam@charrondev.com>
2019-04-05 14:03:31 -0400

    Make admin script more robust

--- applications/vanilla/src/scripts/entries/admin.ts
+++ applications/vanilla/src/scripts/entries/admin.ts
@@ -6,28 +6,28 @@
 import { onReady, onContent } from "@library/utility/appUtils";
 
 onReady(handleImageUploadInputDisplay);
-
+onContent(handleImageUploadInputDisplay);
 
 function handleImageUploadInputDisplay() {
     const imageUploadEnabled = document.getElementById("Form_ImageUpload-dot-Limits-dot-Enabled") as HTMLInputElement;
 
     if (imageUploadEnabled) {
         const displayClass = "dimensionsDisabled";
-        const imageUploadDimensions = [].slice.call(document.getElementsByClassName("ImageUploadLimitsDimensions"));
+        const imageUploadDimensions = Array.from(document.getElementsByClassName("ImageUploadLimitsDimensions"));
 
         if (imageUploadEnabled.checked) {
-            imageUploadDimensions.forEach((input) => {
+            imageUploadDimensions.forEach(input => {
                 input.classList.remove(displayClass);
             });
         }
 
         imageUploadEnabled.addEventListener("click", () => {
             if (imageUploadEnabled.checked) {
-                imageUploadDimensions.forEach((input) => {
+                imageUploadDimensions.forEach(input => {
                     input.classList.remove(displayClass);
                 });
             } else {
-                imageUploadDimensions.forEach((input) => {
+                imageUploadDimensions.forEach(input => {
                     input.classList.add(displayClass);
                 });
             }

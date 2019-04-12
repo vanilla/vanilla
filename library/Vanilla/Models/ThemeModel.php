cfa3f3a8e066e2afe70c321ef5641ea801e3dc35
Ryan <initvector@users.noreply.github.com>
2019-04-12 18:42:32 -0400

    Add support for variable providers to ThemeModel

--- library/Vanilla/Models/ThemeModel.php
+++ library/Vanilla/Models/ThemeModel.php
@@ -9,11 +9,15 @@ namespace Vanilla\Models;
 use Gdn_Session;
 use Vanilla\Database\Operation\CurrentUserFieldProcessor;
 use Vanilla\Database\Operation\CurrentDateFieldProcessor;
+use Vanilla\Theme\VariablesProviderInterface;
 
 /**
  * Handle custom themes.
  */
 class ThemeModel extends PipelineModel {
+
+    /** @var VariablesProviderInterface[] */
+    private $variableProviders;
 
     /** @var Gdn_Session */
     private $session;
@@ -36,5 +40,23 @@ class ThemeModel extends PipelineModel {
         $userProcessor->setInsertFields(["insertUserID", "updateUserID"])
             ->setUpdateFields(["updateUserID"]);
         $this->addPipelineProcessor($userProcessor);
+    }
+
+    /**
+     * Add a theme-variable provider.
+     *
+     * @param VariablesProviderInterface $provider
+     */
+    public function addVariableProvider(VariablesProviderInterface $provider) {
+        $this->variableProviders[] = $provider;
+    }
+
+    /**
+     * Get all configured theme-variable providers.
+     *
+     * @return array
+     */
+    public function getVariableProviders(): array {
+        return $this->variableProviders;
     }
 }

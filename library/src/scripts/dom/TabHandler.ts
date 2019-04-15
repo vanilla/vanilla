cac9b59c6d2c7da8718c67c561a496d59db85fcc
Adam Charron <adam@charrondev.com>
2019-04-15 17:36:13 -0400

    Add useTabKeyboardHandler hook.

--- library/src/scripts/dom/TabHandler.ts
+++ library/src/scripts/dom/TabHandler.ts
@@ -7,6 +7,81 @@
 
 import tabbable from "tabbable";
 import { logError } from "../utility/utils";
+import { useCallback } from "react";
+
+export function useTabKeyboardHandler(
+    root: Element | null = document.documentElement!,
+    excludedElements: Element[] = [],
+    excludedRoots: Element[] = [],
+): React.KeyboardEventHandler | undefined {
+    const makeTabHandler = () => {
+        if (!root) {
+            return;
+        }
+        return new TabHandler(root, excludedElements, excludedRoots);
+    };
+
+    /**
+     * Handle shift tab key presses.
+     *
+     * - Focuses the previous element in the modal.
+     * - Loops if we are at the beginning
+     *
+     * @param event The react event.
+     */
+    const handleShiftTab = useCallback((event: React.KeyboardEvent) => {
+        const tabHandler = makeTabHandler();
+        if (!tabHandler) {
+            return;
+        }
+        const nextElement = tabHandler.getNext(undefined, true);
+        if (nextElement) {
+            event.preventDefault();
+
+            event.stopPropagation();
+            nextElement.focus();
+        }
+    }, []);
+
+    /**
+     * Handle tab key presses.
+     *
+     * - Focuses the next element in the modal.
+     * - Loops if we are at the end.
+     *
+     * @param event The react event.
+     */
+    const handleTab = useCallback((event: React.KeyboardEvent) => {
+        const tabHandler = makeTabHandler();
+        if (!tabHandler) {
+            return;
+        }
+        const previousElement = tabHandler.getNext();
+        if (previousElement) {
+            event.preventDefault();
+            event.stopPropagation();
+            previousElement.focus();
+        }
+    }, []);
+
+    /**
+     * Handle tab keyboard presses.
+     */
+    const handleTabbing = useCallback(
+        (event: React.KeyboardEvent) => {
+            const tabKey = 9;
+
+            if (event.shiftKey && event.keyCode === tabKey) {
+                handleShiftTab(event);
+            } else if (!event.shiftKey && event.keyCode === tabKey) {
+                handleTab(event);
+            }
+        },
+        [handleShiftTab, handleTab],
+    );
+
+    return handleTabbing;
+}
 
 /**
  * A class for handling tabbing inside of a container with various exclusions.

b1287cd15da2817fe578d40147ae1e20e0d6963c
Ryan <initvector@users.noreply.github.com>
2019-04-23 16:23:29 -0400

    Add function for custom background style to Splash

--- library/src/scripts/splash/Splash.tsx
+++ library/src/scripts/splash/Splash.tsx
@@ -20,6 +20,7 @@ interface IProps extends IDeviceProps {
     action?: React.ReactNode;
     title?: string; // Often the message to display isn't the real H1
     className?: string;
+    backgroundImage?: string;
 }
 
 /**
@@ -34,7 +35,12 @@ export class Splash extends React.Component<IProps> {
 
         return (
             <div className={classNames(className, classes.root)}>
-                <div className={classes.outerBackground} />
+                <div
+                    className={classNames(
+                        classes.outerBackground,
+                        this.props.backgroundImage && classes.customBackground(this.props.backgroundImage),
+                    )}
+                />
                 <Container>
                     <div className={classes.innerContainer}>
                         <PanelWidgetHorizontalPadding>

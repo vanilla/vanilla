aeecbf7f071494804268de6f003e9cbe95ae4537
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2019-04-23 16:32:39 -0400

    Removed commented code

--- library/src/scripts/headers/mebox/pieces/compactSearchStyles.ts
+++ library/src/scripts/headers/mebox/pieces/compactSearchStyles.ts
@@ -78,11 +78,6 @@ export const compactSearchClasses = useThemeCache(() => {
         alignItems: "center",
         flexWrap: "nowrap",
         height: unit(headerVariables.sizing.height),
-        // $nest: {
-        //     ["& ." + searchBarClasses().content]: {
-        //         minHeight: "initial",
-        //     },
-        // },
     },
         mediaQueries.oneColumn({
         height: unit(headerVariables.sizing.mobile.height),

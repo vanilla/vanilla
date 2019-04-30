bc5f2812cb3249c2607407085e3a133f42127a1f
Adam Charron <adam@charrondev.com>
2019-04-30 12:17:03 -0400

    Set version to release/3.0-rc1

--- environment.php
+++ environment.php
@@ -16,7 +16,7 @@ if (!defined('APPLICATION')) {
     define('APPLICATION', 'Vanilla');
 }
 if (!defined('APPLICATION_VERSION')) {
-    define('APPLICATION_VERSION', '2.8.1+2019.006');
+    define('APPLICATION_VERSION', '3.0-rc1');
 }
 if (!defined('DS')) {
     define('DS', DIRECTORY_SEPARATOR);

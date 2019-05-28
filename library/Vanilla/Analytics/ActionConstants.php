94e8df298194ae79719f470fb8c07e87c7126ed9
Ryan <initvector@users.noreply.github.com>
2019-05-28 17:57:13 -0400

    Add Analytics Client interface and stub

---
+++ library/Vanilla/Analytics/ActionConstants.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @copyright 2009-2019 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ */
+
+namespace Vanilla\Analytics;
+
+/**
+ * Action constants for analytics operations.
+ */
+class ActionConstants {
+    public const GET_CONFIG = "@@analytics/GET_CONFIG";
+    public const GET_EVENT_DEFAULTS = "@@analytics/GET_EVENT_DEFAULTS";
+}

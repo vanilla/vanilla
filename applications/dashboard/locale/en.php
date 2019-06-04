e6bbedbb8b71969da521c6f01a576b7789ce7cde
Todd Burry <todd@vanillaforums.com>
2019-06-04 15:45:30 -0400

    Reduce line length for the lint police

--- applications/dashboard/locale/en.php
+++ applications/dashboard/locale/en.php
@@ -162,7 +162,8 @@ $Definition['Check out the new community forum I\'ve just set up.'] = 'Hi Pal!
 
 Check out the new community forum I\'ve just set up. It\'s a great place for us to chat with each other online.';
 $Definition['Large images will be scaled down.'] = 'Large images will be scaled down to a max width of %spx and a max height of %spx.';
-$Definition['Locales allow you to support other languages on your site.'] = 'Locales allow you to support other languages on your site. Enable and disable locales you want to make available here.';
+$Definition['Locales allow you to support other languages on your site.'] =
+    'Locales allow you to support other languages on your site. Enable and disable locales you want to make available here.';
 $Definition['Test Email Message'] = '<p>This is a test email message.</p>'.
     '<p>You can configure the appearance of your forum\'s emails by navigating to the Email page in the dashboard.</p>';
 $Definition['oauth2Instructions'] = '<p>Configure your forum to connect with an OAuth2 application by putting your unique Client ID, Client Secret, and required endpoints. You will probably need to provide your SSO application with an allowed callback URL, in part, to validate requests. The callback url for this forum is <code>%s</code></p>';

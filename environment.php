6ad969442c929a26a395fe4376ad6a2ebe598552
Adam Charron <adam@charrondev.com>
2019-06-05 10:15:09 -0400

    Bump version to 2019.007

--- environment.php
+++ environment.php
@@ -18,7 +18,7 @@ if (!defined('APPLICATION')) {
 if (!defined('APPLICATION_VERSION')) {
     // Rules for the versioning
     // {OSS version}-{Cloud release version}-{? SNAPSHOT if it's a dev build}
-    define('APPLICATION_VERSION', '3.0-2019.007-SNAPSHOT');
+    define('APPLICATION_VERSION', '3.0-2019.007');
 }
 if (!defined('DS')) {
     define('DS', DIRECTORY_SEPARATOR);

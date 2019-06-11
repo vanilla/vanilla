64f2185475301645d7ad4fc691c064287db6cc2e
Adam Charron <adam@charrondev.com>
2019-06-11 15:44:46 -0400

    Fix sudo

--- tests/travis/circleci-setup.sh
+++ tests/travis/circleci-setup.sh
@@ -3,20 +3,20 @@
 set -e
 set -x
 
-# Make some working directories.
-mkdir "$DIR/nginx"
-mkdir "$DIR/nginx/sites-enabled"
-
-# Build the default nginx config files.
-tpl "$TEMPLATES/nginx/nginx.conf.tpl" "$DIR/nginx/nginx.conf"
-tpl "$TEMPLATES/nginx/fastcgi.conf.tpl" "$DIR/nginx/fastcgi.conf"
-tpl "$TEMPLATES/nginx/sites-enabled/default-site.conf.tpl" "$DIR/nginx/sites-enabled/default-site.conf"
-
 # Start php-fpm
 php-fpm --daemonize
 
+# Make some working directories.
+sudo mkdir "$DIR/nginx"
+sudo mkdir "$DIR/nginx/sites-enabled"
+
+# Build the default nginx config files.
+sudo tpl "$TEMPLATES/nginx/nginx.conf.tpl" "$DIR/nginx/nginx.conf"
+sudo tpl "$TEMPLATES/nginx/fastcgi.conf.tpl" "$DIR/nginx/fastcgi.conf"
+sudo tpl "$TEMPLATES/nginx/sites-enabled/default-site.conf.tpl" "$DIR/nginx/sites-enabled/default-site.conf"
+
 # Copy the config changer that will allow Vanilla to use a different config file per host.
-cp "$TEMPLATES/vanilla/conf/bootstrap.before.php" "$ROOT/conf"
+sudo cp "$TEMPLATES/vanilla/conf/bootstrap.before.php" "$ROOT/conf"
 
 # Start nginx.
 nginx -c "$DIR/nginx/nginx.conf"

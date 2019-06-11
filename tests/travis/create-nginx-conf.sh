76a222386a5e77bf2a719cc62115fe86b1f93828
Adam Charron <adam@charrondev.com>
2019-06-11 15:54:31 -0400

    rename config

--- tests/travis/create-nginx-conf.sh
+++ tests/travis/create-nginx-conf.sh
@@ -3,9 +3,6 @@
 set -e
 set -x
 
-# Start php-fpm
-php-fpm --daemonize
-
 DIR=$(realpath $(dirname "$0"))
 TEMPLATES="$DIR/templates"
 ROOT=$(realpath "$DIR/../..")
@@ -13,7 +10,7 @@ PORT=9000
 SERVER="/tmp/php.sock"
 
 function tpl {
-    sudo sed \
+    sed \
         -e "s|{DIR}|$DIR|g" \
         -e "s|{ROOT}|$ROOT|g" \
         -e "s|{PORT}|$PORT|g" \
@@ -22,8 +19,8 @@ function tpl {
 }
 
 # Make some working directories.
-sudo mkdir "$DIR/nginx"
-sudo mkdir "$DIR/nginx/sites-enabled"
+mkdir "$DIR/nginx"
+mkdir "$DIR/nginx/sites-enabled"
 
 # Build the default nginx config files.
 tpl "$TEMPLATES/nginx/nginx.conf.tpl" "$DIR/nginx/nginx.conf"
@@ -31,7 +28,4 @@ tpl "$TEMPLATES/nginx/fastcgi.conf.tpl" "$DIR/nginx/fastcgi.conf"
 tpl "$TEMPLATES/nginx/sites-enabled/default-site.conf.tpl" "$DIR/nginx/sites-enabled/default-site.conf"
 
 # Copy the config changer that will allow Vanilla to use a different config file per host.
-sudo cp "$TEMPLATES/vanilla/conf/bootstrap.before.php" "$ROOT/conf"
-
-# Start nginx.
-nginx -c "$DIR/nginx/nginx.conf"
+cp "$TEMPLATES/vanilla/conf/bootstrap.before.php" "$ROOT/conf"

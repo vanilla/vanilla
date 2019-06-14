4aed1dd260c4229308ed79bb81e7ba1dde9d14c6
egarciajulia <eduardo.garciajulia@vanillaforums.com>
2019-06-14 09:31:53 -0400

    add a line to separate container rules

--- bootstrap.php
+++ bootstrap.php
@@ -350,6 +350,7 @@ $dic->setInstance(Garden\Container\Container::class, $dic)
     ->rule(\Vanilla\Analytics\Client::class)
     ->setShared(true)
     ->addAlias(\Vanilla\Contracts\Analytics\ClientInterface::class)
+
     ->rule(Vanilla\Scheduler\SchedulerInterface::class)
     ->setClass(Vanilla\Scheduler\DummyScheduler::class)
     ->addCall('addDriver', [Vanilla\Scheduler\Driver\LocalDriver::class])

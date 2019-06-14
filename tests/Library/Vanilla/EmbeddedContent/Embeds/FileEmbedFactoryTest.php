60f2d99ebbce135e49b7787e98fa6589c1c3b8b8
Adam Charron <adam@charrondev.com>
2019-06-14 15:57:01 -0400

    Layout example file embed factory

---
+++ tests/Library/Vanilla/EmbeddedContent/Embeds/FileEmbedFactoryTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @author Adam Charron <adam.c@vanillaforums.com>
+ * @copyright 2009-2019 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ */
+
+//
+// {
+//    "url": "https://dev.vanilla.localhost/uploads/150/LKE0S2FWLFUP.zip",
+//    "type": "file",
+//    "attributes": {
+//        "mediaID": 62,
+//        "name": "___img_onload_prompt(1)_ (2).zip",
+//        "path": "150/LKE0S2FWLFUP.zip",
+//        "type": "application/zip",
+//        "size": 41,
+//        "active": 1,
+//        "insertUserID": 4,
+//        "dateInserted": "2019-06-14 14:09:38",
+//        "foreignID": 4,
+//        "foreignTable": "embed",
+//        "imageWidth": null,
+//        "imageHeight": null,
+//        "thumbWidth": null,
+//        "thumbHeight": null,
+//        "thumbPath": null,
+//        "foreignType": "embed",
+//        "url": "https://dev.vanilla.localhost/uploads/150/LKE0S2FWLFUP.zip"
+//    }
+// }

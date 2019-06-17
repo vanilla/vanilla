89caa5cff5b6755cfed236d54d0348b58d6101d7
alexander.kim <kim.alexander.ca@gmail.com>
2019-06-17 12:33:27 -0400

    output container.html page through GDN_Controller to apply all standard checks and headers apply CSP 'frame-ancestors' header for Gdn_Controller
    Closes: https://github.com/vanilla/vanilla-patches/issues/551

--- library/Vanilla/Web/ContentSecurityPolicy/DefaultContentSecurityPolicyProvider.php
+++ library/Vanilla/Web/ContentSecurityPolicy/DefaultContentSecurityPolicyProvider.php
@@ -30,6 +30,7 @@ class DefaultContentSecurityPolicyProvider implements ContentSecurityPolicyProvi
     public function getPolicies(): array {
         $policies = [];
         $policies = array_merge($policies, $this->getScriptSources());
+        $policies = array_merge($policies, $this->getFrameAncestors());
 
         return $policies;
     }
@@ -43,6 +44,20 @@ class DefaultContentSecurityPolicyProvider implements ContentSecurityPolicyProvi
             $scriptSrcPolicies[] = new Policy(Policy::SCRIPT_SRC, implode(' ', $whitelist));
         }
 
+        return $scriptSrcPolicies;
+    }
+
+    /**
+    * @return Policy[]
+    */
+    private function getFrameAncestors(): array {
+        $scriptSrcPolicies[] = new Policy(Policy::FRAME_ANCESTORS, '\'self\'');
+        if ($whitelist = $this->config->get('Garden.TrustedDomains', false)) {
+            $trusteddDomains = is_string($whitelist) ? explode("\n", $whitelist) : [];
+            if(count($trusteddDomains) > 0) {
+                $scriptSrcPolicies[] = new Policy(Policy::FRAME_ANCESTORS, implode(' ', $trusteddDomains));
+            }
+        }
         return $scriptSrcPolicies;
     }
 }

32b54ac1a3e75a1cd9930f7b6b97e5f2fa207379
alexander.kim <kim.alexander.ca@gmail.com>
2019-07-04 13:34:20 -0400

    remove space according to th code standards

--- applications/dashboard/models/class.usermodel.php
+++ applications/dashboard/models/class.usermodel.php
@@ -3860,7 +3860,7 @@ class UserModel extends Gdn_Model {
         // If CountInvitations is null (ie. never been set before) or it is a new month since the DateSetInvitations
         if ((empty($user->CountInvitations) && $user->CountInvitations !== 0 )
             || is_null($user->DateSetInvitations)
-            || date( "m Y", strtotime($user->DateSetInvitations)) !== date('m Y')) {
+            || date("m Y", strtotime($user->DateSetInvitations)) !== date('m Y')) {
             // Reset CountInvitations and DateSetInvitations
             $this->SQL->put(
                 $this->Name,

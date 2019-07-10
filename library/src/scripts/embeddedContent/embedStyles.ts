180cea7bc5efebe3cbe62d267a66d768527009f9
Adam Charron <adam@charrondev.com>
2019-07-10 13:48:39 -0400

    Fix bad type

--- library/src/scripts/embeddedContent/embedStyles.ts
+++ library/src/scripts/embeddedContent/embedStyles.ts
@@ -3,13 +3,12 @@
  * @license GPL-2.0-only
  */
 
-import { useThemeCache, styleFactory, componentThemeVariables, variableFactory } from "@library/styles/styleUtils";
-import { globalVariables } from "@library/styles/globalStyleVars";
-import { px, percent } from "csx";
-import { userSelect, borders, IBordersSameAllSidesStyles, unit, colorOut } from "@library/styles/styleHelpers";
-import { shadowOrBorderBasedOnLightness, shadowHelper } from "@library/styles/shadowHelpers";
-import { formElementsVariables } from "@library/forms/formElementStyles";
 import { EmbedContainerSize } from "@library/embeddedContent/EmbedContainer";
+import { globalVariables } from "@library/styles/globalStyleVars";
+import { shadowHelper, shadowOrBorderBasedOnLightness } from "@library/styles/shadowHelpers";
+import { borders, colorOut, unit, userSelect } from "@library/styles/styleHelpers";
+import { styleFactory, useThemeCache, variableFactory } from "@library/styles/styleUtils";
+import { percent, px } from "csx";
 import { NestedCSSProperties } from "typestyle/lib/types";
 
 export const embedContainerVariables = useThemeCache(() => {
@@ -20,7 +19,7 @@ export const embedContainerVariables = useThemeCache(() => {
         bg: globalVars.mainColors.bg,
     });
 
-    const border: IBordersSameAllSidesStyles = makeThemeVars("border", {
+    const border = makeThemeVars("border", {
         style: "none",
         width: 0,
         radius: px(4),

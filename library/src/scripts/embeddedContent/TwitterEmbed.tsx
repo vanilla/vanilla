c6386f8b5261226da61fd764f247594106bcba3a
Adam Charron <adam@charrondev.com>
2019-07-16 11:09:01 -0400

    Fix twitter embed flashing

--- library/src/scripts/embeddedContent/TwitterEmbed.tsx
+++ library/src/scripts/embeddedContent/TwitterEmbed.tsx
@@ -6,9 +6,10 @@
 import { ensureScript } from "@vanilla/dom-utils";
 import { EmbedContent } from "@library/embeddedContent/EmbedContent";
 import { IBaseEmbedProps } from "@library/embeddedContent/embedService";
-import React, { useEffect } from "react";
 import { twitterEmbedClasses } from "@library/embeddedContent/twitterEmbedStyles";
+import { visibility } from "@library/styles/styleHelpers";
 import classNames from "classnames";
+import React, { useEffect, useState } from "react";
 
 interface IProps extends IBaseEmbedProps {
     statusID: string;
@@ -20,24 +21,48 @@ const TWITTER_SCRIPT = "https://platform.twitter.com/widgets.js";
  * A class for rendering Twitter embeds.
  */
 export function TwitterEmbed(props: IProps): JSX.Element {
+    const [twitterLoaded, setTwitterLoaded] = useState(false);
     const classes = twitterEmbedClasses();
 
     useEffect(() => {
-        void convertTwitterEmbeds().then(props.onRenderComplete);
-    }, []);
+        void convertTwitterEmbeds().then(() => {
+            // We need to track the load status for the internal representation.
+            // Otherwise we end up with a flash of the URL next to the rendered tweet.
+            setTwitterLoaded(true);
+        });
+    }, [setTwitterLoaded, props.onRenderComplete]);
+
+    useEffect(() => {
+        // Don't count our tweet as rendered until the tweet is fully loaded.
+        props.onRenderComplete && props.onRenderComplete();
+    }, [twitterLoaded]);
 
     return (
-        <EmbedContent type={props.embedType} inEditor={props.inEditor} isSmall>
-            <div
-                className={classNames("js-twitterCard", classes.card)}
-                data-tweeturl={props.url}
-                data-tweetid={props.statusID}
+        <>
+            {!twitterLoaded && (
+                <div className={classNames("embedLinkLoader")}>
+                    <a href={props.url} className="embedLinkLoader-link" rel="nofollow">
+                        {props.url} <span aria-hidden="true" className="embedLinkLoader-loader" />
+                    </a>
+                </div>
+            )}
+            <EmbedContent
+                type={props.embedType}
+                inEditor={props.inEditor}
+                isSmall
+                className={twitterLoaded ? undefined : visibility().displayNone}
             >
-                <a href={props.url} className="tweet-url" rel="nofollow">
-                    {props.url}
-                </a>
-            </div>
-        </EmbedContent>
+                <div
+                    className={classNames("js-twitterCard", classes.card)}
+                    data-tweeturl={props.url}
+                    data-tweetid={props.statusID}
+                >
+                    <a href={props.url} className="tweet-url" rel="nofollow">
+                        {props.url}
+                    </a>
+                </div>
+            </EmbedContent>
+        </>
     );
 }
 
@@ -65,15 +90,15 @@ async function renderTweet(contentElement: HTMLElement) {
     ) {
         contentElement.classList.add("js-twitterCardPreload");
 
-        // Render the embed.
-        const options = { conversation: "none" };
-        await window.twttr.widgets.createTweet(statusID, contentElement, options);
-
         // Remove a url if there is one around.
         const url = contentElement.querySelector(".tweet-url");
         if (url) {
             url.remove();
         }
+
+        // Render the embed.
+        const options = { conversation: "none" };
+        await window.twttr.widgets.createTweet(statusID, contentElement, options);
 
         // Fade it in.
         contentElement.classList.add("js-twitterCardLoaded");

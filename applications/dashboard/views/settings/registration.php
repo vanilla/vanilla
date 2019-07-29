6fba8cbc936bed33bb680cf5ac7117c6f234f137
igraziatto <isis.graziatto@gmail.com>
2019-07-29 10:45:29 -0400

    fix invitation interface

--- applications/dashboard/views/settings/registration.php
+++ applications/dashboard/views/settings/registration.php
@@ -75,11 +75,7 @@ echo $this->Form->errors(); ?>
         <?php echo $this->Form->label('Invitation target', 'Garden.Registration.InviteTarget'); ?>
     </div>
     <div class="input-wrap invite-url-code">
-        <?php
-            echo '<div class="community-domain padded-right">';
-            echo    Gdn::request()->urlDomain().(Gdn::request()->getRoot() !== '' ? Gdn::request()->getRoot().'/' : '/');
-            echo '</div>';
-            echo $this->Form->textBox('Garden.Registration.InviteTarget', ['value' => $this->InviteTarget]); ?>
+        <?php echo $this->Form->textBox('Garden.Registration.InviteTarget', ['value' => $this->InviteTarget]); ?>
     </div>
 </div>
 <div id="InvitationExpiration" class="form-group">

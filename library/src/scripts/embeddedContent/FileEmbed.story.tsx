9c52d6c3ba7db9c2bb445d91b9dc460a6e8aead1
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2019-08-01 11:16:12 -0400

    Removed unused imports

--- library/src/scripts/embeddedContent/FileEmbed.story.tsx
+++ library/src/scripts/embeddedContent/FileEmbed.story.tsx
@@ -1,15 +1,13 @@
 /**
- * @author Stéphane LaFlèche <stephane.l@vanillaforums.com>
+ * @author Adam Charron <adam.c@vanillaforums.com>
  * @copyright 2009-2019 Vanilla Forums Inc.
  * @license GPL-2.0-only
  */
 
-import { EmbedContainer, EmbedContainerSize } from "@library/embeddedContent/EmbedContainer";
 import { StoryHeading } from "@library/storybook/StoryHeading";
 import { storiesOf } from "@storybook/react";
 import React from "react";
 import { FileEmbed } from "@library/embeddedContent/FileEmbed";
-import DateTime from "@library/content/DateTime";
 import { formatUrl } from "@library/utility/appUtils";
 
 const reactionsStory = storiesOf("Embeds", module);
--- library/src/scripts/embeddedContent/LinkEmbed.story.tsx
+++ library/src/scripts/embeddedContent/LinkEmbed.story.tsx
@@ -1,16 +1,12 @@
 /**
- * @author Stéphane LaFlèche <stephane.l@vanillaforums.com>
+ * @author Adam Charron <adam.c@vanillaforums.com>
  * @copyright 2009-2019 Vanilla Forums Inc.
  * @license GPL-2.0-only
  */
 
-import { EmbedContainer, EmbedContainerSize } from "@library/embeddedContent/EmbedContainer";
 import { StoryHeading } from "@library/storybook/StoryHeading";
 import { storiesOf } from "@storybook/react";
 import React from "react";
-import { FileEmbed } from "@library/embeddedContent/FileEmbed";
-import DateTime from "@library/content/DateTime";
-import { formatUrl } from "@library/utility/appUtils";
 import { LinkEmbed } from "@library/embeddedContent/LinkEmbed";
 
 const reactionsStory = storiesOf("Embeds", module);

8f712514ef411bc37fce0fd5c6180d03b1bac4e8
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2019-08-02 15:11:51 -0400

    Fixed button story

--- library/src/scripts/embeddedContent/Components.story.tsx
+++ library/src/scripts/embeddedContent/Components.story.tsx
@@ -4,87 +4,82 @@
  * @license GPL-2.0-only
  */
 
-import { EmbedContainer, EmbedContainerSize } from "@library/embeddedContent/EmbedContainer";
 import { StoryHeading } from "@library/storybook/StoryHeading";
 import { storiesOf } from "@storybook/react";
 import React from "react";
 import { StoryTiles } from "@library/storybook/StoryTiles";
 import Button from "@library/forms/Button";
-import { StoryTile } from "@library/storybook/StoryTile";
 import { StoryParagraph } from "@library/storybook/StoryParagraph";
+import { StoryTileAndTextCompact } from "@library/storybook/StoryTileAndTextCompact";
+import { ButtonTypes, buttonUtilityClasses } from "@library/forms/buttonStyles";
+import { ComposeIcon } from "@library/icons/titleBar";
+import { globalVariables } from "@library/styles/globalStyleVars";
 
 const reactionsStory = storiesOf("Components", module);
 
 reactionsStory.add("Buttons", () => {
+    const globalVars = globalVariables();
+    const classes = buttonUtilityClasses();
     return (
         <>
             <StoryHeading depth={1}>Buttons</StoryHeading>
             <StoryTiles>
                 <>
-                    <StoryTile>
-                        <Button>Standard</Button>
-                    </StoryTile>
-                    <StoryTile>
-                        <Button>Text</Button>
-                    </StoryTile>
-                    <StoryTile>
-                        <Button>Text Primary</Button>
-                    </StoryTile>
-                    <StoryTile>
-                        <Button>Icon</Button>
-                    </StoryTile>
-                    <StoryTile>
-                        <Button>Icon_Compact</Button>
-                    </StoryTile>
-                    <StoryTile>
-                        <Button>Compact</Button>
-                    </StoryTile>
-                    <StoryTile>
+                    <StoryTileAndTextCompact>
+                        <Button baseClass={ButtonTypes.STANDARD}>Standard</Button>
+                    </StoryTileAndTextCompact>
+                    <StoryTileAndTextCompact>
+                        <Button baseClass={ButtonTypes.PRIMARY}>Primary</Button>
+                    </StoryTileAndTextCompact>
+                    <StoryTileAndTextCompact text={"Mostly for modal footer"}>
+                        <Button baseClass={ButtonTypes.TEXT}>Text</Button>
+                    </StoryTileAndTextCompact>
+                    <StoryTileAndTextCompact text={"Mostly for modal footer call to action"}>
+                        <Button baseClass={ButtonTypes.TEXT_PRIMARY}>Text Primary</Button>
+                    </StoryTileAndTextCompact>
+                    <StoryTileAndTextCompact
+                        text={`Standard icon button - ${globalVars.buttonIcon.size} x ${
+                            globalVars.buttonIcon.size
+                        } pixels`}
+                    >
+                        <Button baseClass={ButtonTypes.ICON}>
+                            <ComposeIcon />
+                        </Button>
+                    </StoryTileAndTextCompact>
+                    <StoryTileAndTextCompact
+                        text={`Compact icon button - ${globalVars.icon.sizes.default} x ${
+                            globalVars.icon.sizes.default
+                        } pixels`}
+                    >
+                        <Button baseClass={ButtonTypes.ICON_COMPACT}>
+                            <ComposeIcon />
+                        </Button>
+                    </StoryTileAndTextCompact>
+
+                    <StoryTileAndTextCompact type={"titleBar"} text={"For TitleBar call to action (login)"}>
+                        <Button baseClass={ButtonTypes.TRANSPARENT}>Transparent</Button>
+                    </StoryTileAndTextCompact>
+
+                    <StoryTileAndTextCompact type={"titleBar"} text={"For TitleBar call to action (register)"}>
+                        <Button baseClass={ButtonTypes.TRANSLUCID}>Translucid</Button>
+                    </StoryTileAndTextCompact>
+                    <StoryTileAndTextCompact
+                        text={
+                            "Only basic styles applied so you can customize your own without fighting existing styles. Use sparingly"
+                        }
+                    >
+                        <Button baseClass={ButtonTypes.CUSTOM}>Custom</Button>
+                    </StoryTileAndTextCompact>
+
+                    <StoryHeading>Icon with text</StoryHeading>
+                    <Button type={ButtonTypes.PRIMARY}>
                         <>
-                            <Button>Primary</Button>
-                            <StoryParagraph>Uses primary color as BG</StoryParagraph>
+                            <ComposeIcon />
+                            <span className={classes.buttonIconLeftMargin}>Example with icon</span>
                         </>
-                    </StoryTile>
-                    <StoryTile>
-                        <>
-                            <Button>Compact Primary</Button>
-                            <StoryParagraph>Uses primary color as BG, with less padding</StoryParagraph>
-                        </>
-                    </StoryTile>
-                    <StoryTile>
-                        <>
-                            <Button>Transparent</Button>
-                            <StoryParagraph>No background color</StoryParagraph>
-                        </>
-                    </StoryTile>
-                    <StoryTile>
-                        <>
-                            <Button>Translucid</Button>
-                            <StoryParagraph>Fake transparency of the text with colors</StoryParagraph>
-                        </>
-                    </StoryTile>
-                    <StoryTile>
-                        <>
-                            <Button>Inverted</Button>
-                            <StoryParagraph>Inverted colors</StoryParagraph>
-                        </>
-                    </StoryTile>
-                    <StoryTile>
-                        <>
-                            <Button>Cutstom</Button>
-                            <StoryParagraph>No special styling, for special case buttons.</StoryParagraph>
-                        </>
-                    </StoryTile>
+                    </Button>
                 </>
             </StoryTiles>
         </>
     );
-});
-
-reactionsStory.add("Modals", () => {
-    return <StoryHeading depth={1}>Modals</StoryHeading>;
-});
-
-reactionsStory.add("Examples", () => {
-    return <StoryHeading depth={1}>Modal Examples</StoryHeading>;
 });

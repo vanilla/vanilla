7191f9b3c510c8058d1ee118bbfc7019709e7159
Adam Charron <adam@charrondev.com>
2019-08-14 13:23:40 -0400

    Add route instance caching to RouteHandler

--- library/src/scripts/routing/RouteHandler.tsx
+++ library/src/scripts/routing/RouteHandler.tsx
@@ -1,7 +1,6 @@
 /**
- * @author Adam (charrondev) Charron <adam.c@vanillaforums.com>
  * @copyright 2009-2019 Vanilla Forums Inc.
- * @license Proprietary
+ * @license GPL-2.0-only
  */
 
 import React from "react";
@@ -17,8 +16,35 @@ type LoadFunction = () => Promise<any>;
  * Class for managing routing and matching a particular page.
  */
 export default class RouteHandler<GeneratorProps> {
+    /**
+     * Local cache of react nodes caching a route.
+     * This is used to allow multiple routes to share the same component instance if their key matches.
+     *
+     * Example:
+     * <ResourceEditorPage /> has 2 allowed URL formats. /resource/:id/edit & /resource/editor
+     *
+     * We may want to change the URL from one 2 the other at some point of the lifecycle,
+     * but we don't want the component to be umounted/remounted.
+     *
+     * In this case by passing the same `key` when constructing the route handler,
+     * the same component instance will be re-used for both routes.
+     */
+    private static routeCache: { [key: string]: React.ReactNode } = {};
+
+    /**
+     * Clear the cached routes on the static RouteHandler.
+     */
+    public static clearRouteCache() {
+        RouteHandler.routeCache = {};
+    }
+
+    /** A react-loadable instance. */
     public loadable;
+
+    /** A react node representing the route of the component. */
     public route: React.ReactNode;
+
+    /** Key to identify the route. Components with the same key share the same instance. */
     private key: string;
 
     public constructor(
@@ -33,9 +59,21 @@ export default class RouteHandler<GeneratorProps> {
             loader: componentPromise,
         });
         this.key = key || path;
-        this.route = <Route exact path={this.path} component={this.loadable} key={this.key} />;
+
+        const cachedRoute = RouteHandler.routeCache[this.key];
+        if (cachedRoute) {
+            this.route = cachedRoute;
+        } else {
+            this.route = <Route exact path={this.path} component={this.loadable} key={this.key} />;
+            RouteHandler.routeCache[this.key] = this.route;
+        }
     }
 
+    /**
+     * A component representing a link to the component.
+     *
+     * - Preloads the loadable for the component on hover.
+     */
     public Link = (props: Omit<NavLinkProps, "to"> & { data: GeneratorProps }) => {
         return (
             <Hoverable duration={50} onHover={this.preload}>
@@ -44,7 +82,10 @@ export default class RouteHandler<GeneratorProps> {
         );
     };
 
+    /**
+     * Call this to preload the route.
+     */
     public preload = () => {
-        (this.loadable as LoadableComponent).preload();
+        return (this.loadable as LoadableComponent).preload();
     };
 }

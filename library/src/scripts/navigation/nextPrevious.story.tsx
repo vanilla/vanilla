32ffde50c2b90bda5d718e224a12e4bd9c8e97a0
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2019-08-29 16:41:56 -0400

    Added embed errors and next/previous component

---
+++ library/src/scripts/navigation/nextPrevious.story.tsx
@@ -0,0 +1,51 @@
+/**
+ * @author Stéphane LaFlèche <stephane.l@vanillaforums.com>
+ * @copyright 2009-2019 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ */
+
+import { StoryHeading } from "@library/storybook/StoryHeading";
+import { storiesOf } from "@storybook/react";
+import React from "react";
+import { StoryContent } from "@library/storybook/StoryContent";
+import { t } from "@library/utility/appUtils";
+import { globalVariables } from "@library/styles/globalStyleVars";
+import NextPrevious from "@library/navigation/NextPrevious";
+
+const story = storiesOf("Site Nav", module);
+
+story.add("Next/Previous", () => {
+    return (
+        <StoryContent>
+            <StoryHeading depth={1}>Next/Previous</StoryHeading>
+            <StoryHeading>Both next and previous</StoryHeading>
+            <NextPrevious
+                accessibleTitle={t("More Articles")}
+                prevItem={{
+                    name: "The last article",
+                    url: "#",
+                }}
+                nextItem={{
+                    name: "The next article",
+                    url: "#",
+                }}
+            />
+            <StoryHeading>Only previous</StoryHeading>
+            <NextPrevious
+                accessibleTitle={t("More Articles")}
+                prevItem={{
+                    name: "The last article",
+                    url: "#",
+                }}
+            />
+            <StoryHeading>Only next</StoryHeading>
+            <NextPrevious
+                accessibleTitle={t("More Articles")}
+                nextItem={{
+                    name: "The next article",
+                    url: "#",
+                }}
+            />
+        </StoryContent>
+    );
+});

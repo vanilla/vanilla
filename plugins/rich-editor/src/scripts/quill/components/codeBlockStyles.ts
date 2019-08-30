3eef19cf2b3f9359f949ee8b574248ccd57b3edc
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2019-08-30 13:28:08 -0400

    Added code block styles

--- plugins/rich-editor/src/scripts/quill/components/codeBlockStyles.ts
+++ plugins/rich-editor/src/scripts/quill/components/codeBlockStyles.ts
@@ -6,36 +6,96 @@
 
 import { useThemeCache, styleFactory, variableFactory } from "@library/styles/styleUtils";
 import { globalVariables } from "@library/styles/globalStyleVars";
-import { IButtonType } from "@library/forms/styleHelperButtonInterface";
-import { generateButtonStyleProperties } from "@library/forms/styleHelperButtonGenerator";
 import { cssRule } from "typestyle";
-import { colorOut } from "@library/styles/styleHelpersColors";
-import { borders, singleBorder } from "@library/styles/styleHelpersBorders";
-import { absolutePosition, margins, paddings, unit, userSelect } from "@library/styles/styleHelpers";
+import { colorOut, modifyColorBasedOnLightness } from "@library/styles/styleHelpersColors";
 import { em, percent } from "csx";
+import { paddings } from "@library/styles/styleHelpersfPadding";
 
 export const codeBlockVariables = useThemeCache(() => {
     const globalVars = globalVariables();
     const makeThemeVars = variableFactory("blockQuote");
 
+    const borderRadius = makeThemeVars("borderRadius", {
+        default: 0,
+    });
+
     const fonts = makeThemeVars("fonts", {
         size: em(0.85),
+        families: `Menlo, Monaco, Consolas, "Courier New", monospace`,
     });
 
     const border = makeThemeVars("border", {
-        radius: 0,
+        radius: borderRadius.default,
     });
 
-    const colors = makeThemeVars("colors", {});
+    const colors = makeThemeVars("colors", {
+        bg: globalVars.mixBgAndFg(0.2),
+        fg: modifyColorBasedOnLightness(globalVars.mainColors.fg, 0.2),
+    });
+
+    const inline = makeThemeVars("inline", {
+        border: {
+            color: globalVars.mixBgAndFg(0.5),
+            radius: 0,
+        },
+        paddings: {
+            vertical: em(0.2),
+            horizontal: em(0.4),
+        },
+    });
+
+    const block = makeThemeVars("block", {
+        border: {
+            radius: borderRadius.default,
+        },
+        paddings: {
+            all: globalVars.userContent.font.sizes.default,
+        },
+    });
 
     return {
         fonts,
+        border,
         colors,
+        inline,
+        block,
     };
 });
 
 export const codeBlockCSS = useThemeCache(() => {
     const globalVars = globalVariables();
     const vars = codeBlockVariables();
-    cssRule(".blockquote", {});
+    cssRule(".userContent", {
+        $nest: {
+            ".code": {
+                position: "relative",
+                display: "inline",
+                verticalAlign: "middle",
+                lineHeight: "inherit",
+                fontSize: vars.fonts.size,
+                fontFamily: vars.fonts.families,
+                maxWidth: percent(100),
+                margin: 0,
+                color: colorOut(vars.colors.fg),
+                backgroundColor: colorOut(vars.colors.bg),
+                border: 0,
+                overflowX: "auto",
+                flexShrink: 0,
+            },
+            ".codeInline": {
+                display: "inline",
+                whiteSpace: "normal",
+                ...paddings(vars.inline.paddings),
+                borderRadius: vars.inline.border.radius,
+            },
+            ".codeBlock": {
+                display: "block",
+                wordWrap: "normal",
+                lineHeight: globalVars.lineHeights.code,
+                whiteSpace: "pre",
+                ...paddings(vars.block.paddings),
+                borderRadius: vars.block.border.radius,
+            },
+        },
+    });
 });

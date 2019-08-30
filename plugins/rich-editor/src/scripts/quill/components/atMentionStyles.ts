c773399837b44f7c273e9a2611e994819cc5c2ed
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2019-08-30 15:18:50 -0400

    Renamed "Site Nav" in storybook to "Navigation" Preparing atMention styles for conversion to JS

--- plugins/rich-editor/src/scripts/quill/components/atMentionStyles.ts
+++ plugins/rich-editor/src/scripts/quill/components/atMentionStyles.ts
@@ -16,24 +16,76 @@ import { percent } from "csx";
 
 export const atMentionVariables = useThemeCache(() => {
     const globalVars = globalVariables();
-    const makeThemeVars = variableFactory("spoiler");
+    const makeThemeVars = variableFactory("atMention");
 
     const font = makeThemeVars("font", {
         size: globalVars.fonts.size.large,
     });
 
+    const avatar = makeThemeVars("avatar", {
+        width: 30,
+        margin: 10,
+    });
+
+    // As in the <mark/> tag
+    const mark = makeThemeVars("mark", {
+        weight: globalVars.fonts.weights.semiBold,
+    });
+
+    const link = makeThemeVars("link", {
+        weight: globalVars.fonts.weights.semiBold,
+    });
+
+    const user = makeThemeVars("user", {
+        padding: {
+            vertical: 3,
+            horizontal: 6,
+        },
+    });
+
+    const selected = makeThemeVars("selected", {
+        bg: globalVars.mainColors.bg,
+    });
+
+    const sizing = makeThemeVars("sizing", {
+        width: 200,
+        maxHeight: (avatar.width + user.padding.vertical * 2) * 7.5,
+    });
+
     return {
         font,
+        avatar,
+        mark,
+        link,
+        user,
+        selected,
+        sizing,
     };
 });
 
 export const atMentionCSS = useThemeCache(() => {
     const globalVars = globalVariables();
     const vars = atMentionVariables();
-    // cssRule(".blockQuote", {
-    //     $nest: {
-    //         "& .": {
-    //         },
-    //     },
-    // });
+    cssRule(".atMentionList", {});
+    cssRule(".atMentionList-suggestion", {});
+    cssRule(".atMentionList-items", {
+        $nest: {
+            "&.atMentionList-items": {},
+            "&.isHidden": {},
+        },
+    });
+    cssRule(".atMentionList-item", {
+        $nest: {
+            "&.atMentionList-item": {},
+            "&.isActive .atMentionList-suggestion": {},
+        },
+    );
+    cssRule(".atMentionList-suggestion", {});
+    cssRule(".atMentionList-user, .atMentionList-user", {});
+    cssRule(".atMentionList-photoWrap", {});
+    cssRule(".atMentionList-photo", {});
+    cssRule(".atMentionList-userName", {});
+    cssRule(".atMentionList-mark", {});
+    cssRule(".atMentionList-photo", {});
+    cssRule(".atMention", {});
 });

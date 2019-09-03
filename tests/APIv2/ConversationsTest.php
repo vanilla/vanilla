d8390d54d4f1525521b5f1113e573609b32f14d2
Dani <31856281+dannimajdalani@users.noreply.github.com>
2019-09-03 16:00:13 -0400

    Update tests/APIv2/ConversationsTest.php
    Co-Authored-By: Ryan <initvector@users.noreply.github.com>

--- tests/APIv2/ConversationsTest.php
+++ tests/APIv2/ConversationsTest.php
@@ -159,7 +159,7 @@ class ConversationsTest extends AbstractAPIv2Test {
      * @expectedExceptionCode 403
      * @expectedExceptionMessage The site is not configured for moderating conversations.
      */
-    public function testSiteModeration() {
+    public function testSiteModerationDisabled() {
         $this->api()->get($this->baseUrl, ['insertUserID' => self::$userIDs[0]]);
     }
 

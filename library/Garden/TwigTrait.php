91329a3cd2eab073b9cd69da0ccc663bcea6902b
Adam Charron <adam@charrondev.com>
2019-09-10 15:49:02 -0400

    Cleanup \Garden\TwigTrait

--- library/Garden/TwigTrait.php
+++ library/Garden/TwigTrait.php
@@ -12,22 +12,34 @@ namespace Garden;
  *
  */
 trait TwigTrait {
+
+    /** @var string The path to look for twig views in. */
     protected static $twigDefaultFolder = PATH_ROOT;
+
     /**
      * Twig templating environment initialize
      *
      * @return \Twig\Environment
      */
     protected static function twigInit() {
-        $loader = new \Twig_Loader_Filesystem(self::$twigDefaultFolder);
-        if (\Gdn::config('Debug') === true) {
-            $twigEnv = new \Twig\Environment($loader, ['debug' => true]);
-            $twigEnv->addExtension(new \Twig\Extension\DebugExtension());
-        } else {
-            $twigEnv = new \Twig\Environment($loader, [
-                'cache' => PATH_CACHE . '/twig',
-            ]);
+        $loader = new \Twig\Loader\FilesystemLoader(self::$twigDefaultFolder);
+
+        $isDebug = \Gdn::config('Debug') === true;
+        $envArgs = [
+            'cache' => PATH_CACHE . '/twig',
+            'debug' => $isDebug,
+            // Automatically controlled by the debug value.
+            // This causes twig to check the FS timestamp before going to cache.
+            // It will rebuild that file's cache if an update had occured.
+            // 'auto_reload' => $isDebug
+            'strict_variables' => $isDebug, // Surface template errors in debug mode.
+        ];
+        $environment = new \Twig\Environment($loader, $envArgs);
+
+        if ($isDebug) {
+            $environment->addExtension(new \Twig\Extension\DebugExtension());
         }
-        return $twigEnv;
+
+        return $environment;
     }
 }

6627ca4cf575be9926acd6d171a25cb2b5c969bd
Adam Charron <adam@charrondev.com>
2019-09-17 10:08:21 -0400

    Additional splash cleanup

--- library/src/scripts/splash/Splash.tsx
+++ library/src/scripts/splash/Splash.tsx
@@ -42,12 +42,11 @@ interface IProps extends IDeviceProps {
  */
 export class Splash extends React.Component<IProps> {
     public render() {
-        const { action, className } = this.props;
-        const styleOverwrite = this.props.styleOverwrite ? this.props.styleOverwrite : ({} as ISplashStyleOverwrite);
+        const { action, className, title } = this.props;
+        const styleOverwrite = this.props.styleOverwrite || {};
 
         const classes = splashClasses(styleOverwrite);
-        const title = this.props.title;
-        const vars = splashVariables({});
+        const vars = splashVariables(styleOverwrite);
 
         return (
             <div className={classNames(className, classes.root)}>

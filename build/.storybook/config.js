2c0bcc9ac84ed17be55576b963199f69fbdab172
Adam Charron <adam@charrondev.com>
2019-09-26 13:31:07 -0400

    Adjust diff threshold again

--- build/.storybook/config.js
+++ build/.storybook/config.js
@@ -37,7 +37,7 @@ function loadStories() {
 addParameters({
     chromatic: {
         delay: 1500, // Add a slight delay to ensure everything has rendered properly.
-        diffThreshold: 0.8, // Default is 0.67. Lower numbers are more accurate.
+        diffThreshold: 0.2, // Default is 0.67. Lower numbers are more accurate.
                             // Set to prevent diffs like this https://www.chromaticqa.com/snapshot?appId=5d5eba16c782b600204ba187&id=5d8cef8dbc622e00202a6edd
                             // From triggering
     }

2ed085d578ba5fd5c9dc4b641653fea234fd7af9
Chris Chabilall <chris.c@vanillaforums.com>
2019-10-07 13:14:07 -0400

    Added Mock objects for site-section implementation

---
+++ tests/fixtures/src/MockSiteSectionProvider.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * @copyright 2009-2019 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ */
+
+
+namespace VanillaTests\Fixtures;
+
+use Vanilla\Contracts\Site\SiteSectionInterface;
+use Vanilla\Contracts\Site\SiteSectionProviderInterface;
+
+/**
+ * Mock site-section-provider.
+ */
+class MockSiteSectionProvider implements SiteSectionProviderInterface {
+
+    /**
+     * @var array $siteSections
+     */
+    private $siteSections = [];
+
+    /**
+     * MockSiteSectionProvider constructor.
+     */
+    public function __construct() {
+        $this->siteSections = self::createSiteSections();
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    public function getAll(): array{
+        return $this->siteSections;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getByID(int $id): ?SiteSectionInterface{
+
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getByBasePath(string $basePath): ?SiteSectionInterface{
+
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getForLocale(string $localeKey): array {
+
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getCurrentSiteSection(): SiteSectionInterface{
+
+    }
+
+    /**
+     * Create site-sections to a section group.
+
+     * @return array
+     */
+    public static function createSiteSections() {
+        $locales = ["en", "fr", "es", "ru"];
+        $siteSections = [];
+        foreach ($locales as $locale) {
+
+            $siteSectionPath = uniqid("folder").'/';
+            $siteSectionName = uniqid("siteSectionName");
+            $sectionGroup = "subcommunities-group-1";
+            $siteSectionID = "subcommunities-group-1";
+
+            $siteSections[] = new MockSiteSection(
+                $siteSectionName,
+                $locale,
+                $siteSectionPath,
+                $siteSectionID,
+                $sectionGroup
+            );
+        }
+
+        return $siteSections;
+    }
+
+}

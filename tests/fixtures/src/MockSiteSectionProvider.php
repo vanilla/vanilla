baa6c7ad988c09f27f3bf0968cef793b5f1ad876
Chris Chabilall <chris.c@vanillaforums.com>
2019-10-07 13:55:52 -0400

    fixed spacing

--- tests/fixtures/src/MockSiteSectionProvider.php
+++ tests/fixtures/src/MockSiteSectionProvider.php
@@ -4,7 +4,6 @@
  * @license GPL-2.0-only
  */
 
-
 namespace VanillaTests\Fixtures;
 
 use Vanilla\Contracts\Site\SiteSectionInterface;
@@ -31,53 +30,47 @@ class MockSiteSectionProvider implements SiteSectionProviderInterface {
     /**
      * @inheritdoc
      */
-    public function getAll(): array{
+    public function getAll(): array {
         return $this->siteSections;
     }
 
     /**
      * @inheritdoc
      */
-    public function getByID(int $id): ?SiteSectionInterface{
-
+    public function getByID(int $id): ?SiteSectionInterface {
     }
 
     /**
      * @inheritdoc
      */
-    public function getByBasePath(string $basePath): ?SiteSectionInterface{
-
+    public function getByBasePath(string $basePath): ?SiteSectionInterface {
     }
 
     /**
      * @inheritdoc
      */
     public function getForLocale(string $localeKey): array {
-
     }
 
     /**
      * @inheritdoc
      */
-    public function getCurrentSiteSection(): SiteSectionInterface{
-
+    public function getCurrentSiteSection(): SiteSectionInterface {
     }
 
     /**
      * Create site-sections to a section group.
-
      * @return array
      */
     public static function createSiteSections() {
         $locales = ["en", "fr", "es", "ru"];
         $siteSections = [];
-        foreach ($locales as $locale) {
 
+        foreach ($locales as $locale) {
             $siteSectionPath = $locale.'/';
             $siteSectionName = uniqid("siteSectionName");
             $sectionGroup = "subcommunities-group-1";
             $siteSectionID = "subcommunities-group-1";
-
             $siteSections[] = new MockSiteSection(
                 $siteSectionName,
                 $locale,

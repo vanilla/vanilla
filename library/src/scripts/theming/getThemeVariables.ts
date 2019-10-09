6e13ee4693004b40d9e0eb113c0f8bcb5b334c73
Adam Charron <adam@charrondev.com>
2019-10-09 14:57:24 -0400

    Allow truly dynamic redux reducers

--- library/src/scripts/theming/getThemeVariables.ts
+++ library/src/scripts/theming/getThemeVariables.ts
@@ -3,11 +3,10 @@
  * @license GPL-2.0-only
  */
 
-import { getDeferredStoreState } from "@library/redux/getStore";
-import { ICoreStoreState } from "@library/redux/reducerRegistry";
+import getStore from "@library/redux/getStore";
 
 export function getThemeVariables() {
-    const state = getDeferredStoreState<ICoreStoreState, null>(null);
+    const state = getStore().getState();
     if (state !== null) {
         const assets = state.theme.assets.data || {};
         const variables = assets.variables ? assets.variables.data : {};

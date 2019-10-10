87c448f973cea23942400eefc5d1c20de6c2274b
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2019-10-10 09:21:03 -0400

    Styling for tooltip "box" and positioning of box based on window position

--- library/src/scripts/toolTip/ToolTip.tsx
+++ library/src/scripts/toolTip/ToolTip.tsx
@@ -14,7 +14,7 @@ import { PanelWidgetHorizontalPadding } from "@library/layout/PanelLayout";
 import { splashClasses, splashVariables } from "@library/splash/splashStyles";
 import { t } from "@library/utility/appUtils";
 import classNames from "classnames";
-import React, { cloneElement, ReactElement } from "react";
+import React, { cloneElement, ReactElement, useState } from "react";
 import Tooltip, { useTooltip, TooltipPopup } from "@reach/tooltip";
 import { ColorValues } from "@library/styles/styleHelpersColors";
 import { url } from "csx";
@@ -22,17 +22,18 @@ import ReactDOM from "react-dom";
 import { mountModal } from "@library/modal/Modal";
 import { ConvertDiscussionModal } from "@knowledge/articleDiscussion/ConvertDiscussionModal";
 import Portal from "@reach/portal";
+import { toolTipClasses } from "@library/toolTip/toolTipStyles";
 
-const toolTipPosition = (triggerRect, tooltipRect) => {
-    const triggerCenter = triggerRect.left + triggerRect.width / 2;
-    const left = triggerCenter - tooltipRect.width / 2;
-    const maxLeft = window.innerWidth - tooltipRect.width - 2;
-    const maxBottom = window.innerHeight - tooltipRect.height - 2 + 10;
-
+const trianglePosition = (triggerRect, hasOverflow) => {
     return {
+        left: triggerRect && triggerRect.left - 10 + triggerRect.width / 2,
+        top: hasOverflow ? triggerRect.top - 10 + 2 + window.scrollY : triggerRect.bottom + window.scrollY,
         position: "absolute",
-        left: Math.min(Math.max(2, left), maxLeft) + window.scrollX,
-        top: triggerRect.bottom + 8 + window.scrollY,
+        width: 0,
+        height: 0,
+        borderLeft: "10px solid transparent",
+        borderRight: "10px solid transparent",
+        borderBottom: "10px solid black",
     };
 };
 
@@ -45,44 +46,49 @@ function TriangleTooltip(props: { children: React.ReactNode; label: string; aria
     // destructure off what we need to position the triangle
     const { isVisible, triggerRect } = tooltip;
 
+    const [hasOverflow, setHasOverflow] = useState(false);
+    const classes = toolTipClasses();
+
+    const toolTipPosition = (triggerRect, tooltipRect) => {
+        const triangleHeight = 10;
+        const borderWidth = 2;
+
+        const triggerCenter = triggerRect.left + triggerRect.width / 2;
+        const left = triggerCenter - tooltipRect.width / 2;
+        const maxLeft = window.innerWidth - tooltipRect.width - 2;
+        const maxBottom = window.innerHeight - tooltipRect.height - borderWidth + triangleHeight;
+        const hasOverflow = triggerRect.bottom + tooltipRect.height + borderWidth + triangleHeight > maxBottom;
+
+        setHasOverflow(hasOverflow);
+
+        return {
+            position: "absolute",
+            left: Math.min(Math.max(2, left), maxLeft) + window.scrollX,
+            top: hasOverflow
+                ? triggerRect.top - triangleHeight - tooltipRect.height - borderWidth + window.scrollY
+                : triggerRect.bottom + triangleHeight + window.scrollY,
+        };
+    };
+
     return (
         <>
             {cloneElement(children as any, trigger)}
-            {isVisible && (
-                // The Triangle. We position it relative to the trigger, not the popup
-                // so that collisions don't have a triangle pointing off to nowhere.
-                // Using a Portal may seem a little extreme, but we can keep the
-                // positioning logic simpler here instead of needing to consider
-                // the popup's position relative to the trigger and collisions
-                <Portal>
-                    <div
-                        aria-hidden={true}
-                        style={{
-                            position: "absolute",
-                            left: triggerRect && triggerRect.left - 10 + triggerRect.width / 2,
-                            top: triggerRect && triggerRect.bottom + window.scrollY,
-                            width: 0,
-                            height: 0,
-                            borderLeft: "10px solid transparent",
-                            borderRight: "10px solid transparent",
-                            borderBottom: "10px solid black",
-                        }}
-                    />
-                </Portal>
-            )}
+            {/*{isVisible && triggerRect && (*/}
+            {/*    // The Triangle. We position it relative to the trigger, not the popup*/}
+            {/*    // so that collisions don't have a triangle pointing off to nowhere.*/}
+            {/*    // Using a Portal may seem a little extreme, but we can keep the*/}
+            {/*    // positioning logic simpler here instead of needing to consider*/}
+            {/*    // the popup's position relative to the trigger and collisions*/}
+            {/*    <Portal>*/}
+            {/*        <div aria-hidden={true} style={trianglePosition(triggerRect, hasOverflow)} />*/}
+            {/*    </Portal>*/}
+            {/*)}*/}
             <TooltipPopup
                 {...tooltip}
                 label={label}
                 ariaLabel={ariaLabel ? ariaLabel : label}
-                style={{
-                    background: "black",
-                    color: "white",
-                    border: "none",
-                    borderRadius: "3px",
-                    padding: "0.5em 1em",
-                    maxWidth: "205px",
-                }}
                 position={toolTipPosition}
+                className={classes.box}
             />
         </>
     );
--- library/src/scripts/toolTip/toolTipStyles.ts
+++ library/src/scripts/toolTip/toolTipStyles.ts
@@ -6,25 +6,49 @@
 
 import { globalVariables } from "@library/styles/globalStyleVars";
 import { styleFactory, useThemeCache, variableFactory } from "@library/styles/styleUtils";
+import { colorOut } from "@library/styles/styleHelpersColors";
+import { borders, paddings, unit } from "@library/styles/styleHelpers";
+import { shadowHelper } from "@library/styles/shadowHelpers";
 
-export const tooltipsVariables = useThemeCache(() => {
+export const tooltipVariables = useThemeCache(() => {
     const makeThemeVars = variableFactory("toolTips");
     const globalVars = globalVariables();
 
     // Main colors
-    const colors = makeThemeVars("colors", {});
+    const sizes = makeThemeVars("sizes", {
+        default: 205,
+    });
+
+    const border = makeThemeVars("radius", {
+        radius: 6,
+    });
 
     return {
-        colors,
+        sizes,
+        border,
     };
 });
 
 export const toolTipClasses = useThemeCache(() => {
     const style = styleFactory("toolTip");
     const globalVars = globalVariables();
-    const root = style({});
+    const vars = tooltipVariables();
+    const shadow = shadowHelper();
+
+    const box = style({
+        fontSize: unit(globalVars.fonts.size.medium),
+        width: unit(vars.sizes.default),
+        color: colorOut(globalVars.mainColors.fg),
+        backgroundColor: colorOut(globalVars.mainColors.bg),
+        lineHeight: globalVars.lineHeights.base,
+        ...borders(vars.border),
+        ...paddings({
+            all: globalVars.fonts.size.medium,
+        }),
+        ...shadow.dropDown(),
+    });
 
     return {
-        root,
+        box,
     };
 });

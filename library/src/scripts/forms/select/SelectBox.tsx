bad6b92bbedfa017297db33f9e94f5334227c6b8
Adam Charron <adam@charrondev.com>
2019-10-15 16:32:18 -0400

    Rollback selectbox and languages dropdown changes

--- library/src/scripts/forms/select/SelectBox.tsx
+++ library/src/scripts/forms/select/SelectBox.tsx
@@ -6,47 +6,37 @@
 
 import React from "react";
 import { t } from "@library/utility/appUtils";
-//import { has, map } from "lodash";
 import { ButtonTypes } from "@library/forms/buttonStyles";
 import { getRequiredID } from "@library/utility/idUtils";
 import { dropDownClasses } from "@library/flyouts/dropDownStyles";
 import { selectBoxClasses } from "@library/forms/select/selectBoxStyles";
 import DropDown, { FlyoutType } from "@library/flyouts/DropDown";
 import DropDownItemButton from "@library/flyouts/items/DropDownItemButton";
-import DropDownItemLink from "@library/flyouts/items/DropDownItemLink";
 import { metasClasses } from "@library/styles/metasStyles";
-import { LocaleDisplayer, ILocale } from "@vanilla/i18n";
 import classNames from "classnames";
 import { CheckCompactIcon, DownTriangleIcon } from "@library/icons/common";
 
 export interface ISelectBoxItem {
-    [x: string]: string | undefined;
     name: string;
+    content?: React.ReactNode;
     className?: string;
-    lang?: string;
-    url?: string;
+    onClick?: () => void;
+    selected?: boolean;
 }
 
 interface IProps {
     className?: string;
     id?: string;
     children: ISelectBoxItem[];
-    renderAbove?: boolean; // Adjusts the flyout position vertically
-    renderLeft?: boolean; // Adjusts the flyout position horizontally
     buttonClassName?: string;
     buttonBaseClass?: ButtonTypes;
     widthOfParent?: boolean;
     openAsModal?: boolean;
-    localeInfo?: ILocale[];
-    languageSelect?: boolean;
-    currentLocale?: string;
+    selectedIndex: number;
 }
 
 export interface ISelfLabelledProps extends IProps {
     label: string;
-    onClick?: () => {};
-    selected?: boolean;
-    outdated?: boolean;
 }
 
 export interface IExternalLabelledProps extends IProps {
@@ -74,10 +64,7 @@ export default class SelectBox extends React.Component<ISelfLabelledProps | IExt
         this.state = {
             id: getRequiredID(props, "selectBox-"),
             selectedIndex: props.selectedIndex,
-            selectedItem:
-                props.selectedItem || props.languageSelect
-                    ? props.children[props.selectedIndex]
-                    : this.getLanguageName(props.currentLocale),
+            selectedItem: props.children[props.selectedIndex],
         };
     }
 
@@ -93,97 +80,57 @@ export default class SelectBox extends React.Component<ISelfLabelledProps | IExt
         });
     };
 
-    public getLanguageName = (val: string) => {
-        if (this.props.localeInfo !== undefined) {
-            let name = this.props.localeInfo.map(l => {
-                if (l.localeKey === val) return l.displayNames[`${val}`];
-            });
-            return name[0];
-        } else {
-            return "";
-        }
-    };
-
     public render() {
-        const checkURL = "url" in this.props.children[0];
-        const url = "/";
-        const { localeInfo, currentLocale } = this.props;
         const classes = selectBoxClasses();
         const classesDropDown = dropDownClasses();
-        const classesMetas = metasClasses();
         const selectItems = this.props.children.map((child, i) => {
             const selected = this.state.selectedIndex === i;
-
-            if (!checkURL) {
-                return (
-                    <DropDownItemButton
-                        key={this.props.id + "-item" + i}
-                        className={classNames({ isSelected: child.selected })}
-                        name={child.name}
-                        onClick={this.handleClick.bind(this, child, i)}
-                        disabled={i === this.state.selectedIndex}
-                        clickData={child}
-                        index={i}
-                        current={selected}
-                        lang={child.lang}
-                        buttonClassName={classNames(
-                            "dropDownItem-button",
-                            "selectBox-buttonItem",
-                            classesDropDown.action,
-                            classes.buttonItem,
-                            {
-                                isInModal: this.props.openAsModal,
-                            },
-                        )}
-                    >
-                        <span className={classNames("selectBox-checkContainer", "sc-only", classes.checkContainer)}>
-                            {selected && <CheckCompactIcon className={"selectBox-selectedIcon"} />}
-                            {!selected && (
-                                <span className={classNames("selectBox-spacer", classes.spacer)} aria-hidden={true}>
-                                    {` `}
-                                </span>
-                            )}
-                        </span>
-                        <span className={classNames("selectBox-itemLabel", classes.itemLabel)}>{child.name}</span>
-                        {child.outdated && (
-                            <span
-                                className={classNames("selectBox-outdated", classesMetas.metaStyle, classes.outdated)}
-                            >
-                                {t("(Outdated)")}
+            return (
+                <DropDownItemButton
+                    key={this.props.id + "-item" + i}
+                    className={classNames({ isSelected: child.selected })}
+                    name={child.name}
+                    onClick={() => {
+                        this.handleClick.bind(this, child, i);
+                        child.onClick && child.onClick();
+                    }}
+                    disabled={i === this.state.selectedIndex}
+                    clickData={child}
+                    index={i}
+                    current={selected}
+                    buttonClassName={classNames(
+                        "dropDownItem-button",
+                        "selectBox-buttonItem",
+                        classesDropDown.action,
+                        classes.buttonItem,
+                        {
+                            isInModal: this.props.openAsModal,
+                        },
+                    )}
+                >
+                    <span className={classNames("selectBox-checkContainer", "sc-only", classes.checkContainer)}>
+                        {selected && <CheckCompactIcon className={"selectBox-selectedIcon"} />}
+                        {!selected && (
+                            <span className={classNames("selectBox-spacer", classes.spacer)} aria-hidden={true}>
+                                {` `}
                             </span>
                         )}
-                    </DropDownItemButton>
-                );
-            } else {
-                return (
-                    <DropDownItemLink
-                        key={i}
-                        name={
-                            localeInfo !== undefined
-                                ? localeInfo.map(l => {
-                                      if (l.localeKey === child.locale) {
-                                          return l.displayNames[`${child.locale}`];
-                                      }
-                                  })
-                                : { url }
-                        }
-                        to={child.translationStatus !== "not-translated" ? child.url : "/"}
-                        className={classNames({ isSelected: child.selected })}
-                        lang={child.locale}
-                        onClick={this.handleClick.bind(this, child, i)}
-                    />
-                );
-            }
+                    </span>
+                    <span className={classNames("selectBox-itemLabel", classes.itemLabel)}>
+                        {child.content || child.name}
+                    </span>
+                    {/* {child.outdated && (
+                        <span className={classNames("selectBox-outdated", classesMetas.metaStyle, classes.outdated)}>
+                            {t("(Outdated)")}
+                        </span>
+                    )} */}
+                </DropDownItemButton>
+            );
         });
-
         const buttonContents =
             this.state.selectedItem && this.state.selectedItem.name ? (
                 <React.Fragment>
-                    {!this.props.languageSelect
-                        ? this.state.selectedItem.name
-                        : currentLocale !== undefined
-                        ? this.getLanguageName(currentLocale)
-                        : ""}
+                    {this.state.selectedItem.content || this.state.selectedItem.name}
                     <DownTriangleIcon className={classNames("selectBox-buttonIcon", classes.buttonIcon)} />
                 </React.Fragment>
             ) : null;
@@ -194,38 +141,24 @@ export default class SelectBox extends React.Component<ISelfLabelledProps | IExt
             >
                 {"label" in this.props && <span className="selectBox-label sr-only">{this.props.label}</span>}
                 <div className="selectBox-content">
-                    {!checkURL ? (
-                        <DropDown
-                            id={this.state.id}
-                            className={classNames(
-                                "selectBox-dropDown",
-                                "dropDownItem-verticalPadding",
-                                classesDropDown.verticalPadding,
-                            )}
-                            name={"label" in this.props ? this.props.label : this.state.selectedItem.name}
-                            buttonContents={buttonContents}
-                            buttonClassName={classNames(this.props.buttonClassName, "selectBox-toggle", classes.toggle)}
-                            contentsClassName={classNames({ isParentWidth: this.props.widthOfParent })}
-                            buttonBaseClass={this.props.buttonBaseClass}
-                            renderAbove={this.props.renderAbove}
-                            renderLeft={this.props.renderLeft}
-                            openAsModal={this.props.openAsModal}
-                            flyoutType={FlyoutType.LIST}
-                            selfPadded={true}
-                        >
-                            {selectItems}
-                        </DropDown>
-                    ) : (
-                        <DropDown
-                            buttonContents={buttonContents}
-                            buttonClassName={classNames(this.props.buttonClassName, "selectBox-toggle", classes.toggle)}
-                            contentsClassName={classNames({ isParentWidth: this.props.widthOfParent })}
-                            buttonBaseClass={this.props.buttonBaseClass}
-                            flyoutType={FlyoutType.LIST}
-                        >
-                            {selectItems}
-                        </DropDown>
-                    )}
+                    <DropDown
+                        id={this.state.id}
+                        className={classNames(
+                            "selectBox-dropDown",
+                            "dropDownItem-verticalPadding",
+                            classesDropDown.verticalPadding,
+                        )}
+                        name={"label" in this.props ? this.props.label : this.state.selectedItem.name}
+                        buttonContents={buttonContents}
+                        buttonClassName={classNames(this.props.buttonClassName, "selectBox-toggle", classes.toggle)}
+                        contentsClassName={classNames({ isParentWidth: this.props.widthOfParent })}
+                        buttonBaseClass={this.props.buttonBaseClass}
+                        openAsModal={this.props.openAsModal}
+                        flyoutType={FlyoutType.LIST}
+                        selfPadded={true}
+                    >
+                        {selectItems}
+                    </DropDown>
                 </div>
             </div>
         );
--- library/src/scripts/layout/LanguagesDropDown.tsx
+++ library/src/scripts/layout/LanguagesDropDown.tsx
@@ -16,26 +16,22 @@ interface IState {
     id: string;
 }
 
-export interface ILanguageProps extends ISelectBoxItem {
-    [x: string]: any;
-    lang: string;
-    outdated?: boolean;
+interface ILanguageItem {
+    locale: string;
+    url: string;
 }
 
 export interface ILanguageDropDownProps {
     id?: string;
-    data: ILanguageProps[];
+    data: ILanguageItem[];
     titleID?: string; // set when it comes with a heading
     widthOfParent?: boolean;
-    selected: any;
     className?: string;
     buttonClassName?: string;
     buttonBaseClass?: ButtonTypes;
     renderLeft?: boolean;
     openAsModal?: boolean;
-    localeInfo?: ILocale[];
-    currentLocale?: any;
-    languageSelect?: boolean;
+    currentLocale?: string;
 }
 
 /**
@@ -44,38 +40,38 @@ export interface ILanguageDropDownProps {
 export default class LanguagesDropDown extends React.Component<ILanguageDropDownProps, IState> {
     public render() {
         const showPicker = this.props.data && this.props.data.length > 1;
-        if (showPicker) {
-            let foundIndex = false;
-            const processedChildren = this.props.data.map(language => {
-                const selected = language.locale === this.props.selected;
-                language.selected = selected;
-                if (selected) {
-                    foundIndex = selected;
-                }
-                return language;
-            });
-            if (!foundIndex) {
-                processedChildren[0].selected = true;
-            }
-            return (
-                <SelectBox
-                    describedBy={this.props.titleID!}
-                    //label={!this.props.titleID ? t("Locale") : null}
-                    widthOfParent={!!this.props.widthOfParent}
-                    className={classNames("languagesDropDown", this.props.className)}
-                    renderLeft={this.props.renderLeft}
-                    buttonClassName={this.props.buttonClassName}
-                    buttonBaseClass={this.props.buttonBaseClass}
-                    openAsModal={this.props.openAsModal}
-                    localeInfo={this.props.localeInfo}
-                    currentLocale={this.props.currentLocale}
-                    languageSelect={true}
-                >
-                    {processedChildren}
-                </SelectBox>
-            );
-        } else {
+        if (!showPicker) {
             return null;
         }
+
+        let selectedIndex = 0;
+        const selectBoxItems: ISelectBoxItem[] = this.props.data.map((data, index) => {
+            const isSelected = data.locale === this.props.currentLocale;
+            if (isSelected) {
+                selectedIndex = index;
+            }
+            return {
+                selected: isSelected,
+                name: data.locale,
+                content: <LocaleDisplayer displayLocale={data.locale} localeContent={data.locale} />,
+                onClick: () => {
+                    window.location.href = data.url;
+                },
+            };
+        });
+
+        return (
+            <SelectBox
+                describedBy={this.props.titleID!}
+                widthOfParent={!!this.props.widthOfParent}
+                className={classNames("languagesDropDown", this.props.className)}
+                buttonClassName={this.props.buttonClassName}
+                buttonBaseClass={this.props.buttonBaseClass}
+                openAsModal={this.props.openAsModal}
+                selectedIndex={selectedIndex}
+            >
+                {selectBoxItems}
+            </SelectBox>
+        );
     }
 }

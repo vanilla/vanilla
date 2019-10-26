a2dffa257016d9777113a74a8c3a87ef09bcafa0
Todd Burry <todd@vanillaforums.com>
2019-10-26 09:33:12 -0400

    Centralize PHP version checks
    In anticipation of the most wonderful time of the year when we upgrade our version requirements, I’m trying to consolidate the places that need to be updated to as few files as possible.
    
    This change:
    
    1. Moves our version requirement to the earliest version check in `environment.php`.
    2. Makes use of the constants whenever version checking.

--- applications/dashboard/settings/class.hooks.php
+++ applications/dashboard/settings/class.hooks.php
@@ -387,13 +387,13 @@ class DashboardHooks extends Gdn_Plugin {
      */
     public function settingsController_render_before($sender) {
         // Set this in your config to dismiss our upgrade warnings. Not recommended.
-        if (c('Vanilla.WarnedMeToUpgrade') === 'PHP 7.0') {
+        if (c('Vanilla.WarnedMeToUpgrade') === ENVIRONMENT_PHP_NEXT_VERSION) {
             return;
         }
 
         $phpVersion = phpversion();
-        if (version_compare($phpVersion, '7.1') < 0) {
-            $upgradeMessage = ['Content' => 'We recommend using at least PHP 7.1. Support for PHP '.htmlspecialchars($phpVersion).' may be dropped in upcoming releases.', 'AssetTarget' => 'Content', 'CssClass' => 'WarningMessage'];
+        if (version_compare($phpVersion, ENVIRONMENT_PHP_NEXT_VERSION) < 0) {
+            $upgradeMessage = ['Content' => 'We recommend using at least PHP '.ENVIRONMENT_PHP_NEXT_VERSION.'. Support for PHP '.htmlspecialchars($phpVersion).' may be dropped in upcoming releases.', 'AssetTarget' => 'Content', 'CssClass' => 'WarningMessage'];
             $messageModule = new MessageModule($sender, $upgradeMessage);
             $sender->addModule($messageModule);
         }

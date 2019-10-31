4ef760f5bf38145eeac00b982b557e35bd2d3184
Adam Charron <adam@charrondev.com>
2019-10-30 22:42:36 -0400

    Setup initila fast refresh config

--- build/scripts/Builder.ts
+++ build/scripts/Builder.ts
@@ -128,7 +128,6 @@ ${chalk.yellowBright("$Configuration['HotReload']['Enabled'] = true;")}`);
         const devServerOptions: DevServerConfiguration = {
             host: this.options.devIp,
             port: 3030,
-            hot: true,
             open: false,
             https: false,
             disableHostCheck: true,

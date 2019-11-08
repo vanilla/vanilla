a33b33c1fe669249d291ef2381e4742cad324bdd
Adam Charron <adam@charrondev.com>
2019-11-08 17:44:29 -0500

    Fix optional chaining

--- packages/vanilla-babel-preset/index.js
+++ packages/vanilla-babel-preset/index.js
@@ -48,6 +48,7 @@ module.exports = api => {
             resolve("@babel/plugin-proposal-object-rest-spread"),
             resolve("@babel/plugin-syntax-dynamic-import"),
             resolve("@babel/plugin-proposal-nullish-coalescing-operator"),
+            resolve("@babel/plugin-proposal-optional-chaining"),
             ...runtimePlugins,
         ],
     };

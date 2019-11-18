052d869dac24ad3bd5bfe46d29d1a2020e211035
alexander.kim <kim.alexander.ca@gmail.com>
2019-11-18 13:08:50 -0500

    update SiteTestTrait

--- tests/SiteTestTrait.php
+++ tests/SiteTestTrait.php
@@ -9,6 +9,8 @@ namespace VanillaTests;
 
 use Garden\Container\Container;
 use Garden\EventManager;
+use PHPUnit\Framework\AssertionFailedError;
+use PHPUnit\Framework\TestCase;
 
 /**
  * Allow a class to test against
@@ -25,14 +27,29 @@ trait SiteTestTrait {
     protected static $siteInfo;
 
     /**
+     * @var array
+     */
+    private static $symLinkedAddons;
+
+    /**
      * @var array The addons to install. Restored on teardownAfterClass();
      */
     protected static $addons = ['vanilla', 'conversations', 'stubcontent'];
 
     /**
+     * Get the names of addons to install.
+     *
+     * @return string[] Returns an array of addon names.
+     */
+    protected static function getAddons(): array {
+        return static::$addons;
+    }
+
+    /**
      * Install the site.
      */
     public static function setupBeforeClass() {
+        self::symlinkAddonFixtures();
         static::bootstrapBeforeClass();
 
         $dic = self::$container;
@@ -43,7 +60,7 @@ trait SiteTestTrait {
         $installer->uninstall();
         $result = $installer->install([
             'site' => ['title' => EventManager::classBasename(get_called_class())],
-            'addons' => static::$addons
+            'addons' => static::getAddons(),
         ]);
 
         // Start Authenticators
@@ -58,5 +75,54 @@ trait SiteTestTrait {
     public static function teardownAfterClass() {
         self::$addons = ['vanilla', 'conversations', 'stubcontent'];
         static::bootstrapAfterClass();
+        self::unSymlinkAddonFixtures();
+    }
+
+    /**
+     * Symlink all addon fixtures.
+     */
+    private static function symlinkAddonFixtures(): void {
+        self::mapAddonFixtures(function (string $path, string $dest): void {
+            if (file_exists($dest)) {
+                if (realpath($dest) !== realpath($path)) {
+                    throw new AssertionFailedError("Cannot symlink addon fixture: $path");
+                }
+            } else {
+                self::$symLinkedAddons[$path] = $dest;
+
+                symlink($path, $dest);
+            }
+        });
+    }
+
+    /**
+     * Remove symlinks to all addon fixtures.
+     */
+    private static function unSymlinkAddonFixtures(): void {
+        self::mapAddonFixtures(function (string $path, string $dest): void {
+            if (isset(self::$symLinkedAddons[$path]) && file_exists($dest) && realpath($dest) === realpath($path)) {
+                unlink($dest);
+            }
+        });
+        self::$symLinkedAddons = [];
+    }
+
+    /**
+     * Run a callback on all test addon fixtures.
+     *
+     * @param callable $callback
+     */
+    private static function mapAddonFixtures(callable $callback): void {
+        $testAddonPaths = array_merge(
+            glob(PATH_ROOT . '/tests/addons/*', GLOB_ONLYDIR),
+            glob(PATH_ROOT . '/plugins/*/tests/plugins/*', GLOB_ONLYDIR)
+        );
+        foreach ($testAddonPaths as $path) {
+            $dirname = basename($path);
+
+            $dest = PATH_ROOT . "/plugins/$dirname";
+
+            $callback($path, $dest);
+        }
     }
 }

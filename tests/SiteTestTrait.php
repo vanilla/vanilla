b24bdeb8964c30de0b2794b483dd2c7ff86630ad
alexander.kim <kim.alexander.ca@gmail.com>
2019-11-18 14:28:03 -0500

    add enabledLocales to SiteTestTrait

--- tests/SiteTestTrait.php
+++ tests/SiteTestTrait.php
@@ -9,8 +9,6 @@ namespace VanillaTests;
 
 use Garden\Container\Container;
 use Garden\EventManager;
-use PHPUnit\Framework\AssertionFailedError;
-use PHPUnit\Framework\TestCase;
 
 /**
  * Allow a class to test against
@@ -27,11 +25,6 @@ trait SiteTestTrait {
     protected static $siteInfo;
 
     /**
-     * @var array
-     */
-    private static $symLinkedAddons;
-
-    /**
      * @var array The addons to install. Restored on teardownAfterClass();
      */
     protected static $addons = ['vanilla', 'conversations', 'stubcontent'];
@@ -40,19 +33,9 @@ trait SiteTestTrait {
     protected static $enabledLocales = [];
 
     /**
-     * Get the names of addons to install.
-     *
-     * @return string[] Returns an array of addon names.
-     */
-    protected static function getAddons(): array {
-        return static::$addons;
-    }
-
-    /**
      * Install the site.
      */
     public static function setupBeforeClass() {
-        self::symlinkAddonFixtures();
         static::bootstrapBeforeClass();
 
         $dic = self::$container;
@@ -63,7 +46,7 @@ trait SiteTestTrait {
         $installer->uninstall();
         $result = $installer->install([
             'site' => ['title' => EventManager::classBasename(get_called_class())],
-            'addons' => static::getAddons(),
+            'addons' => static::$addons
         ]);
 
         self::preparelocales();
@@ -78,7 +61,7 @@ trait SiteTestTrait {
      * Create locale directory and locale definitions.php
      */
     public static function preparelocales() {
-        foreach(self::$enabledLocales as $localeKey => $locale) {
+        foreach(static::$enabledLocales as $localeKey => $locale) {
             $localeDir = PATH_ROOT."/locales/test_$localeKey";
             if (!(file_exists($localeDir) && is_dir($localeDir))) {
                 mkdir($localeDir);
@@ -117,54 +100,5 @@ TEMPLATE;
     public static function teardownAfterClass() {
         self::$addons = ['vanilla', 'conversations', 'stubcontent'];
         static::bootstrapAfterClass();
-        self::unSymlinkAddonFixtures();
-    }
-
-    /**
-     * Symlink all addon fixtures.
-     */
-    private static function symlinkAddonFixtures(): void {
-        self::mapAddonFixtures(function (string $path, string $dest): void {
-            if (file_exists($dest)) {
-                if (realpath($dest) !== realpath($path)) {
-                    throw new AssertionFailedError("Cannot symlink addon fixture: $path");
-                }
-            } else {
-                self::$symLinkedAddons[$path] = $dest;
-
-                symlink($path, $dest);
-            }
-        });
-    }
-
-    /**
-     * Remove symlinks to all addon fixtures.
-     */
-    private static function unSymlinkAddonFixtures(): void {
-        self::mapAddonFixtures(function (string $path, string $dest): void {
-            if (isset(self::$symLinkedAddons[$path]) && file_exists($dest) && realpath($dest) === realpath($path)) {
-                unlink($dest);
-            }
-        });
-        self::$symLinkedAddons = [];
-    }
-
-    /**
-     * Run a callback on all test addon fixtures.
-     *
-     * @param callable $callback
-     */
-    private static function mapAddonFixtures(callable $callback): void {
-        $testAddonPaths = array_merge(
-            glob(PATH_ROOT . '/tests/addons/*', GLOB_ONLYDIR),
-            glob(PATH_ROOT . '/plugins/*/tests/plugins/*', GLOB_ONLYDIR)
-        );
-        foreach ($testAddonPaths as $path) {
-            $dirname = basename($path);
-
-            $dest = PATH_ROOT . "/plugins/$dirname";
-
-            $callback($path, $dest);
-        }
     }
 }

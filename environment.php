0b0c6512bd12826f3a1609b7ac42254934cfc58e
Ryan <initvector@users.noreply.github.com>
2019-11-25 10:14:15 -0500

    Increment Vanilla version

--- environment.php
+++ environment.php
@@ -22,7 +22,7 @@ if (!defined('APPLICATION')) {
 if (!defined('APPLICATION_VERSION')) {
     // Rules for the versioning
     // {OSS version}-{Cloud release version}-{? SNAPSHOT if it's a dev build}
-    define('APPLICATION_VERSION', '3.5-2019.018-SNAPSHOT');
+    define('APPLICATION_VERSION', '4.0-2019.018-SNAPSHOT');
 }
 if (!defined('DS')) {
     define('DS', DIRECTORY_SEPARATOR);

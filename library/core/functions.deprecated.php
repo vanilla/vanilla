1957691dabf61c8c6e3047512f9035f9f1d10b79
Todd Burry <todd@vanillaforums.com>
2019-12-01 13:52:54 -0500

    Remove deprecated arrayMergeRecursiveDistinct()
    Deprecated 2018-09-18.

--- library/core/functions.deprecated.php
+++ library/core/functions.deprecated.php
@@ -37,48 +37,6 @@ if (!function_exists('arrayInArray')) {
     }
 }
 
-/**
- * The array_merge_recursive function does indeed merge arrays, but it converts values with duplicate
- * keys to arrays rather than overwriting the value in the first array with the duplicate
- * value in the second array, as array_merge does. I.e., with array_merge_recursive,
- * this happens (documented behavior):
- *
- * array_merge_recursive(array('key' => 'org value'), array('key' => 'new value'));
- *     => array('key' => array('org value', 'new value'));
- *
- * array_merge_recursive_distinct does not change the datatypes of the values in the arrays.
- * Matching keys' values in the second array overwrite those in the first array, as is the
- * case with array_merge, i.e.:
- *
- * array_merge_recursive_distinct(array('key' => 'org value'), array('key' => 'new value'));
- *     => array('key' => array('new value'));
- *
- * Parameters are passed by reference, though only for performance reasons. They're not
- * altered by this function.
- *
- * @param array $array1
- * @param mixed $array2
- * @return array
- * @author daniel@danielsmedegaardbuus.dk
- * @deprecated
- */
-function &arrayMergeRecursiveDistinct(array &$array1, &$array2 = null) {
-    deprecated('arrayMergeRecursiveDistinct');
-    $merged = $array1;
-
-    if (is_array($array2)) {
-        foreach ($array2 as $key => $val) {
-            if (is_array($array2[$key])) {
-                $merged[$key] = is_array($merged[$key]) ? arrayMergeRecursiveDistinct($merged[$key], $array2[$key]) : $array2[$key];
-            } else {
-                $merged[$key] = $val;
-            }
-        }
-    }
-
-    return $merged;
-}
-
 if (!function_exists('arrayValue')) {
     /**
      * Get the value associated with a {@link $Needle} key in a {@link $Haystack} array.

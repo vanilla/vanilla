2001a10c8c1275ffb6f89752513b9fc79374c06f
alexander.kim <kim.alexander.ca@gmail.com>
2019-12-02 15:28:13 -0500

    test

--- library/Vanilla/Contracts/Models/UserProviderInterface.php
+++ library/Vanilla/Contracts/Models/UserProviderInterface.php
@@ -11,12 +11,4 @@ namespace Vanilla\Contracts\Models;
  * Interface for modelling user expansion.
  */
 interface UserProviderInterface extends FragmentProviderInterface {
-    /**
-     * Populate records with fragments based on various ID fields.
-     *
-     * @param array $records The records to populate.
-     * @param array $columnNames The column names to check for. These should end with `ID`.
-     *        The resulting values will be populated on a field without the ID suffix.
-     */
-    public function expandUsers(array &$records, array $columnNames): void;
 }

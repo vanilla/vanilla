697c6e3f2fc0d63ced44c750a1a674e407987b23
egarciajulia <eduardo.garciajulia@vanillaforums.com>
2019-12-03 15:56:00 -0500

    add missing chunkhash for some css

--- build/scripts/configs/makeBaseConfig.ts
+++ build/scripts/configs/makeBaseConfig.ts
@@ -170,7 +170,7 @@ ${chalk.green(aliases)}`;
     if (options.mode === BuildMode.PRODUCTION) {
         config.plugins.push(
             new MiniCssExtractPlugin({
-                filename: "[name].min.css",
+                filename: "[name].min.css?[chunkhash]",
             }),
         );
     }

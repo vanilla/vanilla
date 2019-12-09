04edcb8b19a102c08e73fd5b72da07141c286515
Adam Charron <adam@charrondev.com>
2019-12-09 17:39:34 -0500

    Add file docblock

--- tests/Library/Vanilla/Web/PageTest.php
+++ tests/Library/Vanilla/Web/PageTest.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @author Adam Charron <adam.c@vanillaforums.com>
+ * @copyright 2009-2019 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ */
 
 namespace VanillaTests\Library\Vanilla\Web;
 

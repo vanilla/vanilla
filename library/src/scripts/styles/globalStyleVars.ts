71aa3790ef374700e4b1273f728ea9266b4468bd
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2019-12-10 16:31:39 -0500

    Added new global variable

--- library/src/scripts/styles/globalStyleVars.ts
+++ library/src/scripts/styles/globalStyleVars.ts
@@ -194,6 +194,10 @@ export const globalVariables = useThemeCache(() => {
         size: fonts.size.medium * lineHeights.base,
     });
 
+    const fullScreenModalTitle = makeThemeVars("fullScreenModalTitle", {
+        gap: 52,
+    });
+
     const animation = makeThemeVars("animation", {
         defaultTiming: ".15s",
         defaultEasing: "ease-out",
@@ -383,6 +387,7 @@ export const globalVariables = useThemeCache(() => {
         separator,
         userContentHyphenation,
         findColorMatch,
+        fullScreenModalTitle,
     };
 });
 

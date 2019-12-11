2d1a45f03f63bb515d8ab7af094af06b167cabe6
Richard Flynn <richardflynn@gmail.com>
2019-12-11 12:49:07 -0500

    Add tests to functions.general wip
    Fixed linting issues for ArrayHasValueTest.php

--- tests/Library/Core/ArrayHasValueTest.php
+++ tests/Library/Core/ArrayHasValueTest.php
@@ -7,34 +7,33 @@
 
 namespace VanillaTests\Library\Core;
 
-
 use PHPUnit\Framework\TestCase;
-
 
 /**
  * Tests for arrayHasValue().
  */
- class ArrayHasValueTest extends TestCase {
+
+class ArrayHasValueTest extends TestCase {
 
      /**
       * Test {@link arrayHasValue()} against several scenarios.
       *
       * @param array $testArray Array to search.
-      * @param $testValue Value to search for.
+      * @param mixed $testValue Value to search for.
       * @param bool $expected Expected result.
       * @dataProvider provideHasValueArrays
       */
-     public function testArrayHasValue(array $testArray, $testValue, bool $expected) {
+    public function testArrayHasValue(array $testArray, $testValue, bool $expected) {
          $actual = arrayHasValue($testArray, $testValue);
          $this->assertSame($expected, $actual);
-     }
+    }
 
      /**
       * Provide test data for {@link arrayHasValue()}.
       *
       * @return array Returns an array of test data.
       */
-     public function provideHasValueArrays() {
+    public function provideHasValueArrays() {
          $r = [
              'doesHaveValueStr' => [
                  ['0' => 'x', '1' => 'y', '2' => 'z'],
@@ -69,5 +68,5 @@ use PHPUnit\Framework\TestCase;
          ];
 
          return $r;
-     }
+    }
 }

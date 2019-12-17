9d411b0a0bf55d58efbe715b113a59f6d5634fb9
Richard Flynn <richardflynn@gmail.com>
2019-12-17 15:53:10 -0500

    Update test for safeUnlink()

--- tests/Library/Core/SafeUnlinkTest.php
+++ tests/Library/Core/SafeUnlinkTest.php
@@ -16,11 +16,33 @@ use PHPUnit\Framework\TestCase;
 class SafeUnlinkTest extends TestCase {
 
     /**
+     * Tests {@link safeUnlink()} against two scenarios.
+     */
+    public static function setUpBeforeClass(): void {
+        parent::setUpBeforeClass();
+        \Gdn_FileSystem::removeFolder(PATH_ROOT.'/tests/cache/safe-unlink');
+        touchFolder(PATH_ROOT.'/tests/cache/safe-unlink');
+    }
+
+    /**
      * Test {@link safeUnlink()} by trying to delete a nonexistent file.
      */
     public function testSafeUnlinkPhantom() {
-        $actual = safeUnlink(PATH_ROOT.'/tests/cache/safe-unlink/phantom-file');
-        $expected = false;
-        $this->assertSame($expected, $actual);
+        $filename = PATH_ROOT.'/tests/cache/safe-unlink/phantom-file';
+        $this->assertFileNotExists($filename);
+        $actual = safeUnlink($filename);
+        $this->assertFalse($actual);
+    }
+
+    /**
+     * Test where folder is deleted.
+     */
+    public function testSafeUnlinkFolderExists() {
+        $filename = PATH_ROOT . '/tests/cache/safe-unlink/test-file.txt';
+        file_put_contents($filename, 'foo');
+        $this->assertFileExists($filename);
+        $actual = safeUnlink($filename);
+        $this->assertTrue($actual);
+        $this->assertFileNotExists($filename);
     }
 }

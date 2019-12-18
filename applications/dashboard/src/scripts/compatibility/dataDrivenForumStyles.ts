849b5938e8793e78ca826ffdb29a2db5f167e7ce
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2019-12-18 13:49:00 -0500

    Added missing dynamic CSS file

---
+++ applications/dashboard/src/scripts/compatibility/dataDrivenForumStyles.ts
@@ -0,0 +1,21 @@
+/**
+ * Compatibility styles, using the color variables.
+ *
+ * @copyright 2009-2019 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ */
+
+import { useThemeCache } from "@vanilla/library/src/scripts/styles/styleUtils";
+import { cssRule } from "typestyle";
+import { globalVariables } from "@vanilla/library/src/scripts/styles/globalStyleVars";
+import { colorOut } from "@vanilla/library/src/scripts/styles/styleHelpersColors";
+
+// To use compatibility styles, set '$colorFromDynamicTheme : true;' in custom.scss
+// $Configuration['Feature']['DeferredLegacyScripts']['Enabled'] = true;
+export const compatibilityStyles = useThemeCache(() => {
+    const vars = globalVariables();
+    const mainColors = vars.mainColors;
+    cssRule("body", {
+        backgroundColor: colorOut(mainColors.bg),
+    });
+});

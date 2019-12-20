7dc59b77e596db3115bbc2140290617537b988b5
Chris Chabilall <chris.c@vanillaforums.com>
2019-12-20 15:51:25 -0500

    Add base for tabs and editor

---
+++ library/src/scripts/sectioning/TabStyles.ts
@@ -0,0 +1,107 @@
+/**
+ * @copyright 2009-2019 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ */
+
+import { globalVariables } from "@library/styles/globalStyleVars";
+import { styleFactory, useThemeCache, variableFactory } from "@library/styles/styleUtils";
+import { colorOut, unit, fonts, paddings, borders, negative, srOnly, IFont } from "@library/styles/styleHelpers";
+import { userSelect } from "@library/styles/styleHelpers";
+import { layoutVariables } from "@library/layout/panelLayoutStyles";
+import { formElementsVariables } from "@library/forms/formElementStyles";
+
+export const tabsVariables = useThemeCache(() => {
+    const globalVars = globalVariables();
+    const makeVars = variableFactory("onlineTabs");
+
+    const colors = makeVars("colors", {
+        bg: globalVars.mainColors.bg,
+        fg: globalVars.mainColors.fg,
+        state: {
+            border: {
+                color: globalVars.mixPrimaryAndBg(0.5),
+            },
+            fg: globalVars.mainColors.primary,
+        },
+        selected: {
+            bg: globalVars.mainColors.primary.desaturate(0.3).fade(0.05),
+            fg: globalVars.mainColors.fg,
+        },
+    });
+
+    const border = makeVars("border", {
+        width: globalVars.border.width,
+        color: globalVars.border.color,
+        radius: globalVars.border.radius,
+        style: globalVars.border.style,
+        active: {
+            color: globalVars.mixPrimaryAndBg(0.5),
+        },
+    });
+
+    return {
+        colors,
+        border,
+    };
+});
+
+export const tabClasses = useThemeCache(() => {
+    const vars = tabsVariables();
+    const style = styleFactory("tabs");
+    const mediaQueries = layoutVariables().mediaQueries();
+    const formElementVariables = formElementsVariables();
+
+    const root = style({
+        display: "block",
+    });
+
+    const tabsHandles = style("tabsHandles", {
+        display: "flex",
+        position: "relative",
+        flexWrap: "nowrap",
+        alignItems: "center",
+        justifyContent: "stretch",
+        width: "100%",
+    });
+
+    const tab = style(
+        "tab",
+        {
+            ...userSelect(),
+            position: "relative",
+            flexWrap: "nowrap",
+            alignItems: "center",
+            justifyContent: "center",
+            $nest: {
+                "& + &": {
+                    marginLeft: unit(negative(vars.border.width)),
+                },
+                "&:hover, &:focus, &:active": {
+                    color: colorOut(vars.colors.state.fg),
+                },
+            },
+        },
+        mediaQueries.oneColumnDown({
+            flexGrow: 0,
+            $nest: {
+                label: {
+                    minHeight: unit(formElementVariables.sizing.height),
+                    lineHeight: unit(formElementVariables.sizing.height),
+                },
+            },
+        }),
+    );
+
+    const panels = style;
+
+    const isActive = style("isActive", {
+        backgroundColor: colorOut(vars.colors.selected.bg),
+    });
+
+    return {
+        root,
+        tabsHandles,
+        tab,
+        isActive,
+    };
+});

26922e50a26a9d2d429c94d5781856800d3b8500
Richard Flynn <richardflynn@gmail.com>
2019-12-31 09:41:32 -0500

    Add getter for the blacklisted fields

--- library/Vanilla/Web/ApiFilterMiddleware.php
+++ library/Vanilla/Web/ApiFilterMiddleware.php
@@ -77,4 +77,13 @@ class ApiFilterMiddleware {
             unset($this->blacklist[$key]);
         }
     }
+
+    /**
+     * Gets the array of blacklisted fields.
+     *
+     * @return array Returns an array of the blacklisted fields.
+     */
+    public function getBlacklistFields() {
+        return $this->blacklist;
+    }
 }

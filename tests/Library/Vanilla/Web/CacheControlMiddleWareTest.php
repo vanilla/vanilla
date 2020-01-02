64c6e98e771f989f7add4f26f4da3c7412ec0b5b
Richard Flynn <richardflynn@gmail.com>
2020-01-02 16:58:01 -0500

    Add tests for __invoke()

--- tests/Library/Vanilla/Web/CacheControlMiddleWareTest.php
+++ tests/Library/Vanilla/Web/CacheControlMiddleWareTest.php
@@ -7,8 +7,12 @@
 
 namespace VanillaTests\Library\Vanilla\Web;
 
+use Garden\Web\Data;
+use Garden\Web\RequestInterface;
+use Gdn_Session;
 use PHPUnit\Framework\TestCase;
 use Vanilla\Web\CacheControlMiddleware;
+use VanillaTests\Fixtures\Request;
 
 /**
  * Test for functions of CacheControlMiddleWare
@@ -41,5 +45,102 @@ class CacheControlMiddleWareTest extends TestCase {
         $actual = CacheControlMiddleware::getHttp10Headers('');
         $expected = [];
         $this->assertSame($expected, $actual);
+    }
+
+    /**
+     * Test __invoke().
+     *
+     * @param int $userID The userID code.
+     * @param array $headers Array of http headers.
+     * @param mixed $expected The expected result.
+     * @dataProvider provideTestInvokeArrays
+     */
+    public function testInvoke(int $userID, array $headers, $expected) {
+        $testSession = new Gdn_Session();
+        $testSession->UserID = $userID;
+        $testRequest = new Request('/', 'GET', [1, 2, 3]);
+        $testObject = new CacheControlMiddleware($testSession);
+        $modifiedObject = $testObject($testRequest, function (RequestInterface $request) use ($headers) {
+            $r = new Data([]);
+
+            foreach ($headers as $key => $value) {
+                $r->setHeader($key, $value);
+            }
+
+            return $r;
+        });
+        $actual = $modifiedObject->getHeaders();
+
+        if ($actual['Expires'] !== 'Sat, 01 Jan 2000 00:00:00 GMT') {
+            unset($actual['Expires']);
+        }
+
+        $this->assertSame($expected, $actual);
+    }
+
+    /**
+     * Provide test data for testInvoke().
+     *
+     * @return array Returns an array of test data.
+     */
+    public function provideTestInvokeArrays() {
+        $r = [
+            'userIDZeroAndPublicCache' => [
+                0,
+                ['Cache-Control' => CacheControlMiddleware::PUBLIC_CACHE],
+                [
+                    'Cache-Control' => 'public, max-age=120',
+                    'Vary' => 'Accept-Encoding, Cookie',
+                ],
+            ],
+            'userIDZeroAndNoCache' => [
+                0,
+                ['Cache-Control' => CacheControlMiddleware::NO_CACHE],
+                [
+                    'Cache-Control' => 'private, no-cache, max-age=0, must-revalidate',
+                    'Expires' => 'Sat, 01 Jan 2000 00:00:00 GMT',
+                    'Pragma' => 'no-cache',
+                ],
+            ],
+            'userIDZeroAndNoCacheControlHeaderAtAll' => [
+                0,
+                ['Foo' => 'bar'],
+                [
+                    'Foo' => 'bar',
+                    'Cache-Control' => 'public, max-age=120',
+                    'Vary' => 'Accept-Encoding, Cookie'
+                ],
+            ],
+            'userID1AndPublicCache' => [
+                1,
+                ['Cache-Control' => CacheControlMiddleware::PUBLIC_CACHE],
+                [
+                    'Cache-Control' => 'public, max-age=120',
+                    'Vary' => 'Accept-Encoding, Cookie',
+                ],
+            ],
+            'userID1AndNoCache' => [
+                1,
+                ['Cache-Control' => CacheControlMiddleware::NO_CACHE],
+                [
+                    'Cache-Control' => 'private, no-cache, max-age=0, must-revalidate',
+                    'Expires' => 'Sat, 01 Jan 2000 00:00:00 GMT',
+                    'Pragma' => 'no-cache',
+                ],
+            ],
+            'userID1AndNoCacheControlHeaderAtAll' => [
+                1,
+                ['Foo' => 'bar'],
+                [
+                    'Foo' => 'bar',
+                    'Cache-Control' => 'private, no-cache, max-age=0, must-revalidate',
+                    'Expires' => 'Sat, 01 Jan 2000 00:00:00 GMT',
+                    'Pragma' => 'no-cache',
+                ],
+            ],
+
+        ];
+
+        return $r;
     }
 }

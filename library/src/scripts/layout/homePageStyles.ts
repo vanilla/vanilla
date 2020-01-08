568a83331863bd09dcc5b2e9e48c8c36764fa2aa
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2020-01-08 07:58:23 -0500

    Clean up code

--- library/src/scripts/layout/homePageStyles.ts
+++ library/src/scripts/layout/homePageStyles.ts
@@ -3,19 +3,8 @@
  * @license GPL-2.0-only
  */
 
-import { globalVariables } from "@library/styles/globalStyleVars";
-import { styleFactory, useThemeCache, variableFactory } from "@library/styles/styleUtils";
-import { percent, viewHeight } from "csx";
-import { cssRule, forceRenderStyles } from "typestyle";
-import {
-    colorOut,
-    background,
-    fontFamilyWithDefaults,
-    margins,
-    paddings,
-    fonts,
-    IBackground,
-} from "@library/styles/styleHelpers";
+import { useThemeCache, variableFactory } from "@library/styles/styleUtils";
+import { IBackground } from "@library/styles/styleHelpers";
 
 export const homePageVariables = useThemeCache(() => {
     const makeThemeVars = variableFactory("homePage");

bffad7b22dc2a209674309813ccd49f6d8bdb6fc
NeenaDev <neena.dev@vanillaforums.com>
2020-01-08 10:00:45 -0500

    add value and oncahge props to editor comp

--- library/src/scripts/textEditor/TextEditor.tsx
+++ library/src/scripts/textEditor/TextEditor.tsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
-import Editor from "@monaco-editor/react";
+import { ControlledEditor, ControlledEditorOnChange, DiffEditorDidMount } from "@monaco-editor/react";
 import { ToggleIcon } from "@library/icons/common";
 import textEditorClasses from "./textEditorStyles";
 
 export interface ITextEditorProps {
     language: string;
+    value?: string;
+    onChange?: ControlledEditorOnChange;
+    editorDidMount?: DiffEditorDidMount;
 }
 export default function TextEditor(props: ITextEditorProps) {
-    const { language } = props;
+    const { language, value } = props;
     const [intialTheme, setTheme] = useState("dark");
     const [isEditorReady, setIsEditorReady] = useState(false);
     const classes = textEditorClasses();
@@ -25,11 +28,12 @@ export default function TextEditor(props: ITextEditorProps) {
             <button onClick={toggleTheme} className={classes.themeToggleIcon} disabled={!isEditorReady}>
                 <ToggleIcon />
             </button>
-            <Editor
+            <ControlledEditor
                 theme={intialTheme}
                 language={language}
                 editorDidMount={handleEditorDidMount}
                 options={{ lineNumbers: "on" }}
+                value={value}
             />
         </div>
     );

5f46a996b58f47e65bafd8ba264d3c3cf84ce2a6
alexander.kim <kim.alexander.ca@gmail.com>
2020-01-13 16:11:35 -0500

    update styles of Tiles components to reflect mockups when 2,3,4 columns
    Relates to: https://github.com/vanilla/knowledge/issues/1468

--- library/src/scripts/features/tiles/tiles.story.tsx
+++ library/src/scripts/features/tiles/tiles.story.tsx
@@ -98,6 +98,53 @@ formsStory.add("Tiles", () =>
                     title={"Our Games"}
                     emptyMessage={"No subcommunities found"}
                 />
+                <StoryContent>
+                    <StoryHeading>As Tiles - 4 columns centered</StoryHeading>
+                </StoryContent>
+                <Tiles
+                    columns={4}
+                    alignment={TileAlignment.CENTER}
+                    items={[
+                        {
+                            name: "Development",
+                            description: "Processes and guidance for developers.",
+                            url: "https://staff.vanillaforums.com/kb/dev",
+                            icon: "https://us.v-cdn.net/5022541/uploads/341/G35SLM2LBY4G.png",
+                        },
+                        {
+                            name: "Success",
+                            description: "Information for CSMs about troubleshooting & working with Vanilla.",
+                            url: "https://staff.vanillaforums.com/kb/success",
+                            icon: "https://us.v-cdn.net/5022541/uploads/466/WCXDHD4UMW3K.png",
+                        },
+                        {
+                            name: "Internal Testing",
+                            description: "Knowledge for us in internal tests. Don't put anything important here.",
+                            url: "https://staff.vanillaforums.com/kb/testing",
+                            icon: "https://us.v-cdn.net/5022541/uploads/048/66SQHHGSZT2R.png",
+                        },
+                        {
+                            name: "Information Security",
+                            description: "Internal company security practices.",
+                            url: "https://staff.vanillaforums.com/kb/infosec",
+                            icon: "https://us.v-cdn.net/5022541/uploads/346/B6QMAFIQAXLI.png",
+                        },
+                        {
+                            name: "Development",
+                            description: "Processes and guidance for developers.",
+                            url: "https://staff.vanillaforums.com/kb/dev",
+                            icon: "https://us.v-cdn.net/5022541/uploads/341/G35SLM2LBY4G.png",
+                        },
+                        {
+                            name: "Information Security",
+                            description: "Internal company security practices.",
+                            url: "https://staff.vanillaforums.com/kb/infosec",
+                            icon: "https://us.v-cdn.net/5022541/uploads/346/B6QMAFIQAXLI.png",
+                        },
+                    ]}
+                    title={"Our Games"}
+                    emptyMessage={"No subcommunities found"}
+                />
             </>
         );
     })(),

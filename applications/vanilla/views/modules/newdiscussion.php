b98e249ec146e2891e11d245d303784b79de5deb
Richard Flynn <richardflynn@gmail.com>
2020-01-14 10:53:28 -0500

    Change  default parameter in buttongGroup() call
    Used the instance property DefaultButton instead of making a call to config settings.

--- applications/vanilla/views/modules/newdiscussion.php
+++ applications/vanilla/views/modules/newdiscussion.php
@@ -2,11 +2,10 @@
 echo '<div class="BoxButtons BoxNewDiscussion">';
 
 $Css = 'Button Primary Action NewDiscussion BigButton';
-$default = c('Vanilla.DefaultNewButton');
 
 foreach ($this->getButtonGroups() as $buttonGroup) {
 //    $Css .= (count($buttonGroup) == 1) ? ' BigButton' : '';
-    echo buttonGroup($buttonGroup, $Css, $default, $this->reorder);
+    echo buttonGroup($buttonGroup, $Css, $this->DefaultButton, $this->reorder);
 }
 
 Gdn::controller()->fireEvent('AfterNewDiscussionButton');

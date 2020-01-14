cecff3c9d0d60764750886a94daa228e27ac2cc6
Richard Flynn <richardflynn@gmail.com>
2020-01-14 12:48:16 -0500

    Update newdiscussion.php
    Changed the $Css logic back to the way it originally was.

--- applications/vanilla/views/modules/newdiscussion.php
+++ applications/vanilla/views/modules/newdiscussion.php
@@ -1,11 +1,11 @@
 <?php if (!defined('APPLICATION')) exit();
 echo '<div class="BoxButtons BoxNewDiscussion">';
 
-$Css = 'Button Primary Action NewDiscussion BigButton';
+$Css = 'Button Primary Action NewDiscussion';
+$Css .= strpos($this->CssClass, 'Big') !== FALSE ? ' BigButton' : '';
 
 foreach ($this->getButtonGroups() as $buttonGroup) {
-//    $Css .= (count($buttonGroup) == 1) ? ' BigButton' : '';
-    echo buttonGroup($buttonGroup, $Css, $this->DefaultButton, $this->reorder);
+    echo buttonGroup($buttonGroup, $Css, $this->DefaultButton, $this->reorder)."\n";
 }
 
 Gdn::controller()->fireEvent('AfterNewDiscussionButton');

cb4e17c62013e6a455aafe801c6abee8813f6f01
Adam Charron <adam@charrondev.com>
2020-01-14 11:52:46 -0500

    Add extra tests

--- bootstrap.php
+++ bootstrap.php
@@ -96,8 +96,8 @@ $dic->setInstance(Garden\Container\Container::class, $dic)
     ->setShared(true)
     ->setConstructorArgs([
         [
-            Addon::TYPE_ADDON => ['/applications', '/plugins', '/addons'],
-            Addon::TYPE_THEME => ['/themes', '/addons'],
+            Addon::TYPE_ADDON => ['/addons', '/applications', '/plugins'],
+            Addon::TYPE_THEME => ['/addons', '/themes'],
             Addon::TYPE_LOCALE => '/locales'
         ],
         PATH_CACHE

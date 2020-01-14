40f4ddc4b4c5d60b65fcf1fb991d745bc8db79a2
Richard Flynn <richardflynn@gmail.com>
2020-01-14 13:55:05 -0500

    Update copyright year

--- applications/vanilla/modules/class.newdiscussionmodule.php
+++ applications/vanilla/modules/class.newdiscussionmodule.php
@@ -2,7 +2,7 @@
 /**
  * New Discussion module
  *
- * @copyright 2009-2019 Vanilla Forums Inc.
+ * @copyright 2009-2020 Vanilla Forums Inc.
  * @license GPL-2.0-only
  * @package Vanilla
  * @since 2.0

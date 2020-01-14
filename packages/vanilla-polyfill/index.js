68236a53434ef14515391c3872dad40fc887b014
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2020-01-14 11:12:04 -0500

    Bumped up specificity and fixed CSS prop for logo Added polyfills for "append" and "prepend" for our dear friend IE 11

--- packages/vanilla-polyfill/index.js
+++ packages/vanilla-polyfill/index.js
@@ -8,6 +8,8 @@
  * @license GPL-2.0-only
  */
 
+"use strict";
+
 import "core-js/stable";
 import "@webcomponents/webcomponentsjs";
 
@@ -112,3 +114,63 @@ function polyfillStringNormalize() {
         return this;
     }
 }
+
+
+/**
+ * Polyfill "append" for ie11.
+ */
+
+// Source: https://github.com/jserz/js_piece/blob/master/DOM/ParentNode/append()/append().md
+(function (arr) {
+    arr.forEach(function (item) {
+        if (item.hasOwnProperty('append')) {
+            return;
+        }
+        Object.defineProperty(item, 'append', {
+            configurable: true,
+            enumerable: true,
+            writable: true,
+            value: function append() {
+                var argArr = Array.prototype.slice.call(arguments),
+                    docFrag = document.createDocumentFragment();
+
+                argArr.forEach(function (argItem) {
+                    var isNode = argItem instanceof Node;
+                    docFrag.appendChild(isNode ? argItem : document.createTextNode(String(argItem)));
+                });
+
+                this.appendChild(docFrag);
+            }
+        });
+    });
+})([Element.prototype, Document.prototype, DocumentFragment.prototype]);
+
+/**
+ * Polyfill "prepend" for ie11.
+ */
+// Source: https://github.com/jserz/js_piece/blob/master/DOM/ParentNode/prepend()/prepend().md
+
+(function (arr) {
+    arr.forEach(function (item) {
+        if (item.hasOwnProperty('prepend')) {
+            return;
+        }
+        Object.defineProperty(item, 'prepend', {
+            configurable: true,
+            enumerable: true,
+            writable: true,
+            value: function prepend() {
+                var argArr = Array.prototype.slice.call(arguments),
+                    docFrag = document.createDocumentFragment();
+
+                argArr.forEach(function (argItem) {
+                    var isNode = argItem instanceof Node;
+                    docFrag.appendChild(isNode ? argItem : document.createTextNode(String(argItem)));
+                });
+
+                this.insertBefore(docFrag, this.firstChild);
+            }
+        });
+    });
+})([Element.prototype, Document.prototype, DocumentFragment.prototype]);
+

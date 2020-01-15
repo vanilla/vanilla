20c279ba68a0b457c0b2fe10d48762ac8a065f31
Adam Charron <adam@charrondev.com>
2020-01-14 21:49:08 -0500

    Move keystone, boilerplate, foundation to addons/themes
    - Much of boilerplate moved out to packages/theme-core
    - Foundation and keystone moved to new directory.
    - Keystone & Foundation now depends on theme-core instead of boilerplate

---
+++ addons/themes/keystone/src/js/index.js
@@ -0,0 +1,7 @@
+/*
+ * @author Isis (igraziatto) Graziatto <isis.g@vanillaforums.com>
+ * @copyright 2009-2018 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ */
+
+import "../../../theme-boilerplate/src/js/index";
---
+++ addons/themes/theme-boilerplate/setup/src/index.js
@@ -0,0 +1,7 @@
+/*!
+ * @author Isis (igraziatto) Graziatto <isis.g@vanillaforums.com>
+ * @copyright 2009-2018 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ */
+
+import "../../node_modules/@vanillaforums/theme-boilerplate/src/js/index";

cf4c7837bb48ec6c93fc88ec5968965ecc174d3f
alexander.kim <kim.alexander.ca@gmail.com>
2020-01-15 09:53:38 -0500

    prettier

--- library/src/scripts/features/tiles/tiles.story.tsx
+++ library/src/scripts/features/tiles/tiles.story.tsx
@@ -8,148 +8,150 @@ import { StoryHeading } from "@library/storybook/StoryHeading";
 import { storiesOf } from "@storybook/react";
 import React, { useState } from "react";
 import Tiles, { TileAlignment } from "./Tiles";
-import {layoutVariables} from "@library/layout/panelLayoutStyles";
+import { layoutVariables } from "@library/layout/panelLayoutStyles";
 import Container from "@library/layout/components/Container";
 
 const formsStory = storiesOf("Home Page", module);
 
-formsStory.add("Tiles", () =>
-    (() => {
-        return (
-            <Container>
-                <StoryContent>
-                    <StoryHeading depth={1}>Sub community List</StoryHeading>
-                    <StoryHeading>As Tiles - 2 columns left aligned</StoryHeading>
-                </StoryContent>
-                <Tiles
-                    columns={2}
-                    alignment={TileAlignment.LEFT}
-                    items={[
-                        {
-                            name: "Development",
-                            description: "Processes and guidance for developers.",
-                            url: "https://staff.vanillaforums.com/kb/dev",
-                            icon: "https://us.v-cdn.net/5022541/uploads/341/G35SLM2LBY4G.png",
-                        },
-                        {
-                            name: "Success",
-                            description: "Information for CSMs about troubleshooting & working with Vanilla.",
-                            url: "https://staff.vanillaforums.com/kb/success",
-                            icon: "https://us.v-cdn.net/5022541/uploads/466/WCXDHD4UMW3K.png",
-                        },
-                        {
-                            name: "Internal Testing",
-                            description: "Knowledge for us in internal tests. Don't put anything important here.",
-                            url: "https://staff.vanillaforums.com/kb/testing",
-                            icon: "https://us.v-cdn.net/5022541/uploads/048/66SQHHGSZT2R.png",
-                        },
-                        {
-                            name: "Information Security",
-                            description: "Internal company security practices.",
-                            url: "https://staff.vanillaforums.com/kb/infosec",
-                            icon: "https://us.v-cdn.net/5022541/uploads/346/B6QMAFIQAXLI.png",
-                        },
-                        {
-                            name: "Information Security",
-                            description: "Internal company security practices.",
-                            url: "https://staff.vanillaforums.com/kb/infosec",
-                            icon: "https://us.v-cdn.net/5022541/uploads/346/B6QMAFIQAXLI.png",
-                        },
-                    ]}
-                    title={"Our Games"}
-                    emptyMessage={"No subcommunities found"}
-                />
-                <StoryContent>
-                    <StoryHeading>As Tiles - 3 columns centered</StoryHeading>
-                </StoryContent>
-                <Tiles
-                    columns={3}
-                    alignment={TileAlignment.CENTER}
-                    items={[
-                        {
-                            name: "Development",
-                            description: "Processes and guidance for developers.",
-                            url: "https://staff.vanillaforums.com/kb/dev",
-                            icon: "https://us.v-cdn.net/5022541/uploads/341/G35SLM2LBY4G.png",
-                        },
-                        {
-                            name: "Success",
-                            description: "Information for CSMs about troubleshooting & working with Vanilla.",
-                            url: "https://staff.vanillaforums.com/kb/success",
-                            icon: "https://us.v-cdn.net/5022541/uploads/466/WCXDHD4UMW3K.png",
-                        },
-                        {
-                            name: "Internal Testing",
-                            description: "Knowledge for us in internal tests. Don't put anything important here.",
-                            url: "https://staff.vanillaforums.com/kb/testing",
-                            icon: "https://us.v-cdn.net/5022541/uploads/048/66SQHHGSZT2R.png",
-                        },
-                        {
-                            name: "Information Security",
-                            description: "Internal company security practices.",
-                            url: "https://staff.vanillaforums.com/kb/infosec",
-                            icon: "https://us.v-cdn.net/5022541/uploads/346/B6QMAFIQAXLI.png",
-                        },
-                        {
-                            name: "Information Security",
-                            description: "Internal company security practices.",
-                            url: "https://staff.vanillaforums.com/kb/infosec",
-                            icon: "https://us.v-cdn.net/5022541/uploads/346/B6QMAFIQAXLI.png",
-                        },
-                    ]}
-                    title={"Our Games"}
-                    emptyMessage={"No subcommunities found"}
-                />
-                <StoryContent>
-                    <StoryHeading>As Tiles - 4 columns centered</StoryHeading>
-                </StoryContent>
-                <Tiles
-                    columns={4}
-                    alignment={TileAlignment.CENTER}
-                    items={[
-                        {
-                            name: "Development",
-                            description: "Processes and guidance for developers.",
-                            url: "https://staff.vanillaforums.com/kb/dev",
-                            icon: "https://us.v-cdn.net/5022541/uploads/341/G35SLM2LBY4G.png",
-                        },
-                        {
-                            name: "Success",
-                            description: "Information for CSMs about troubleshooting & working with Vanilla.",
-                            url: "https://staff.vanillaforums.com/kb/success",
-                            icon: "https://us.v-cdn.net/5022541/uploads/466/WCXDHD4UMW3K.png",
-                        },
-                        {
-                            name: "Internal Testing",
-                            description: "Knowledge for us in internal tests. Don't put anything important here.",
-                            url: "https://staff.vanillaforums.com/kb/testing",
-                            icon: "https://us.v-cdn.net/5022541/uploads/048/66SQHHGSZT2R.png",
-                        },
-                        {
-                            name: "Information Security",
-                            description: "Internal company security practices.",
-                            url: "https://staff.vanillaforums.com/kb/infosec",
-                            icon: "https://us.v-cdn.net/5022541/uploads/346/B6QMAFIQAXLI.png",
-                        },
-                        {
-                            name: "Development",
-                            description: "Processes and guidance for developers.",
-                            url: "https://staff.vanillaforums.com/kb/dev",
-                            icon: "https://us.v-cdn.net/5022541/uploads/341/G35SLM2LBY4G.png",
-                        },
-                        {
-                            name: "Information Security",
-                            description: "Internal company security practices.",
-                            url: "https://staff.vanillaforums.com/kb/infosec",
-                            icon: "https://us.v-cdn.net/5022541/uploads/346/B6QMAFIQAXLI.png",
-                        },
-                    ]}
-                    title={"Our Games"}
-                    emptyMessage={"No subcommunities found"}
-                />
-            </Container>
-        );
-    })(),
+formsStory.add(
+    "Tiles",
+    () =>
+        (() => {
+            return (
+                <Container>
+                    <StoryContent>
+                        <StoryHeading depth={1}>Sub community List</StoryHeading>
+                        <StoryHeading>As Tiles - 2 columns left aligned</StoryHeading>
+                    </StoryContent>
+                    <Tiles
+                        columns={2}
+                        alignment={TileAlignment.LEFT}
+                        items={[
+                            {
+                                name: "Development",
+                                description: "Processes and guidance for developers.",
+                                url: "https://staff.vanillaforums.com/kb/dev",
+                                icon: "https://us.v-cdn.net/5022541/uploads/341/G35SLM2LBY4G.png",
+                            },
+                            {
+                                name: "Success",
+                                description: "Information for CSMs about troubleshooting & working with Vanilla.",
+                                url: "https://staff.vanillaforums.com/kb/success",
+                                icon: "https://us.v-cdn.net/5022541/uploads/466/WCXDHD4UMW3K.png",
+                            },
+                            {
+                                name: "Internal Testing",
+                                description: "Knowledge for us in internal tests. Don't put anything important here.",
+                                url: "https://staff.vanillaforums.com/kb/testing",
+                                icon: "https://us.v-cdn.net/5022541/uploads/048/66SQHHGSZT2R.png",
+                            },
+                            {
+                                name: "Information Security",
+                                description: "Internal company security practices.",
+                                url: "https://staff.vanillaforums.com/kb/infosec",
+                                icon: "https://us.v-cdn.net/5022541/uploads/346/B6QMAFIQAXLI.png",
+                            },
+                            {
+                                name: "Information Security",
+                                description: "Internal company security practices.",
+                                url: "https://staff.vanillaforums.com/kb/infosec",
+                                icon: "https://us.v-cdn.net/5022541/uploads/346/B6QMAFIQAXLI.png",
+                            },
+                        ]}
+                        title={"Our Games"}
+                        emptyMessage={"No subcommunities found"}
+                    />
+                    <StoryContent>
+                        <StoryHeading>As Tiles - 3 columns centered</StoryHeading>
+                    </StoryContent>
+                    <Tiles
+                        columns={3}
+                        alignment={TileAlignment.CENTER}
+                        items={[
+                            {
+                                name: "Development",
+                                description: "Processes and guidance for developers.",
+                                url: "https://staff.vanillaforums.com/kb/dev",
+                                icon: "https://us.v-cdn.net/5022541/uploads/341/G35SLM2LBY4G.png",
+                            },
+                            {
+                                name: "Success",
+                                description: "Information for CSMs about troubleshooting & working with Vanilla.",
+                                url: "https://staff.vanillaforums.com/kb/success",
+                                icon: "https://us.v-cdn.net/5022541/uploads/466/WCXDHD4UMW3K.png",
+                            },
+                            {
+                                name: "Internal Testing",
+                                description: "Knowledge for us in internal tests. Don't put anything important here.",
+                                url: "https://staff.vanillaforums.com/kb/testing",
+                                icon: "https://us.v-cdn.net/5022541/uploads/048/66SQHHGSZT2R.png",
+                            },
+                            {
+                                name: "Information Security",
+                                description: "Internal company security practices.",
+                                url: "https://staff.vanillaforums.com/kb/infosec",
+                                icon: "https://us.v-cdn.net/5022541/uploads/346/B6QMAFIQAXLI.png",
+                            },
+                            {
+                                name: "Information Security",
+                                description: "Internal company security practices.",
+                                url: "https://staff.vanillaforums.com/kb/infosec",
+                                icon: "https://us.v-cdn.net/5022541/uploads/346/B6QMAFIQAXLI.png",
+                            },
+                        ]}
+                        title={"Our Games"}
+                        emptyMessage={"No subcommunities found"}
+                    />
+                    <StoryContent>
+                        <StoryHeading>As Tiles - 4 columns centered</StoryHeading>
+                    </StoryContent>
+                    <Tiles
+                        columns={4}
+                        alignment={TileAlignment.CENTER}
+                        items={[
+                            {
+                                name: "Development",
+                                description: "Processes and guidance for developers.",
+                                url: "https://staff.vanillaforums.com/kb/dev",
+                                icon: "https://us.v-cdn.net/5022541/uploads/341/G35SLM2LBY4G.png",
+                            },
+                            {
+                                name: "Success",
+                                description: "Information for CSMs about troubleshooting & working with Vanilla.",
+                                url: "https://staff.vanillaforums.com/kb/success",
+                                icon: "https://us.v-cdn.net/5022541/uploads/466/WCXDHD4UMW3K.png",
+                            },
+                            {
+                                name: "Internal Testing",
+                                description: "Knowledge for us in internal tests. Don't put anything important here.",
+                                url: "https://staff.vanillaforums.com/kb/testing",
+                                icon: "https://us.v-cdn.net/5022541/uploads/048/66SQHHGSZT2R.png",
+                            },
+                            {
+                                name: "Information Security",
+                                description: "Internal company security practices.",
+                                url: "https://staff.vanillaforums.com/kb/infosec",
+                                icon: "https://us.v-cdn.net/5022541/uploads/346/B6QMAFIQAXLI.png",
+                            },
+                            {
+                                name: "Development",
+                                description: "Processes and guidance for developers.",
+                                url: "https://staff.vanillaforums.com/kb/dev",
+                                icon: "https://us.v-cdn.net/5022541/uploads/341/G35SLM2LBY4G.png",
+                            },
+                            {
+                                name: "Information Security",
+                                description: "Internal company security practices.",
+                                url: "https://staff.vanillaforums.com/kb/infosec",
+                                icon: "https://us.v-cdn.net/5022541/uploads/346/B6QMAFIQAXLI.png",
+                            },
+                        ]}
+                        title={"Our Games"}
+                        emptyMessage={"No subcommunities found"}
+                    />
+                </Container>
+            );
+        })(),
     {
         chromatic: {
             viewports: [1200],

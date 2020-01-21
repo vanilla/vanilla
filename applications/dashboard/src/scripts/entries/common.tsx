ba9291c31f2b7d40ddbd27a66d62f16f545f4079
alexander.kim <kim.alexander.ca@gmail.com>
2020-01-21 10:15:37 -0500

    remove unnecessary comments

--- applications/dashboard/src/scripts/entries/common.tsx
+++ applications/dashboard/src/scripts/entries/common.tsx
@@ -5,8 +5,6 @@
 
 import React from "react";
 import {onReady} from "@library/utility/appUtils";
-// The forum section needs these legacy scripts that have been moved into the bundled JS so it could be refactored.
-// Other sections should not need this yet.
 import { addComponent } from "@library/utility/componentRegistry";
 import Toast from "@library/features/toaster/Toast";
 import {ThemePreviewToast} from "@library/features/toaster/themePreview/ThemePreviewToast";

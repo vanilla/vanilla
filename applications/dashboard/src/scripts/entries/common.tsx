2101313ca9dadd39d6b7cfb6f4f89a1d4e6d83d4
alexander.kim <kim.alexander.ca@gmail.com>
2020-01-21 10:21:48 -0500

    prettier

--- applications/dashboard/src/scripts/entries/common.tsx
+++ applications/dashboard/src/scripts/entries/common.tsx
@@ -4,24 +4,24 @@
  */
 
 import React from "react";
-import {onReady} from "@library/utility/appUtils";
+import { onReady } from "@library/utility/appUtils";
 import { addComponent } from "@library/utility/componentRegistry";
 import Toast from "@library/features/toaster/Toast";
-import {ThemePreviewToast} from "@library/features/toaster/themePreview/ThemePreviewToast";
-import {mountModal} from "@library/modal/Modal";
-import {themePreviewToastReducer} from "@library/features/toaster/themePreview/ThemePreviewToastReducer";
-import {registerReducer} from "@library/redux/reducerRegistry";
-import {AppContext} from "@library/AppContext";
+import { ThemePreviewToast } from "@library/features/toaster/themePreview/ThemePreviewToast";
+import { mountModal } from "@library/modal/Modal";
+import { themePreviewToastReducer } from "@library/features/toaster/themePreview/ThemePreviewToastReducer";
+import { registerReducer } from "@library/redux/reducerRegistry";
+import { AppContext } from "@library/AppContext";
 
 // Routing
 
 registerReducer("themePreviewToaster", themePreviewToastReducer);
 addComponent("toaster", Toast);
 onReady(() => {
-    const themePreviewContainer = document.body
+    const themePreviewContainer = document.body;
     mountModal(
         <AppContext noWrap noTheme>
-           <ThemePreviewToast />
-        </AppContext>
-    )
-})
+            <ThemePreviewToast />
+        </AppContext>,
+    );
+});

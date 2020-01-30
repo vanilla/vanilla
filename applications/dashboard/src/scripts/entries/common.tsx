26694860108f000ccd3c376f4853b23843a57b98
Adam Charron <adam@charrondev.com>
2020-01-30 12:05:43 -0500

    Fix convert to discussion being broken
    - When we added the theme preview card, we used mountModal.
    - mountModal tries takes over the entire modal container.
    - The actual Modal component did not have the unmounting behaviour of mountModal, so it would try to render into the container before clearing it.
    
    The fix
    
    - Extract a new function mountPortal, that cleans up the mounting container and attaches to the end of the document.
    - Update Modal to use mountPortal (now cleans the container properly).
    - Update theme preview card to use mountPortal

--- applications/dashboard/src/scripts/entries/common.tsx
+++ applications/dashboard/src/scripts/entries/common.tsx
@@ -8,20 +8,22 @@ import { onReady } from "@library/utility/appUtils";
 import { addComponent } from "@library/utility/componentRegistry";
 import Toast from "@library/features/toaster/Toast";
 import { ThemePreviewToast } from "@library/features/toaster/themePreview/ThemePreviewToast";
-import { mountModal } from "@library/modal/Modal";
 import { themePreviewToastReducer } from "@library/features/toaster/themePreview/ThemePreviewToastReducer";
 import { registerReducer } from "@library/redux/reducerRegistry";
 import { AppContext } from "@library/AppContext";
+import { mountPortal } from "@vanilla/react-utils";
+
+const PREVIEW_CONTAINER = "previewContainer";
 
 // Routing
 
 registerReducer("themePreviewToaster", themePreviewToastReducer);
 addComponent("toaster", Toast);
 onReady(() => {
-    const themePreviewContainer = document.body;
-    mountModal(
+    mountPortal(
         <AppContext noWrap noTheme>
             <ThemePreviewToast />
         </AppContext>,
+        PREVIEW_CONTAINER,
     );
 });

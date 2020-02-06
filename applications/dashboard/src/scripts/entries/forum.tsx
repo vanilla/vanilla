1f5a48fdee4fd6e36774f51cce869f06a06bfc96
Adam Charron <adam@charrondev.com>
2020-02-06 17:30:39 -0500

    Prevent duplicate mounting of theme provider

--- applications/dashboard/src/scripts/entries/forum.tsx
+++ applications/dashboard/src/scripts/entries/forum.tsx
@@ -41,18 +41,19 @@ Router.addRoutes([
 
 applySharedPortalContext(props => {
     return (
-        <AppContext variablesOnly={!getMeta("themeFeatures.DataDrivenTheme", false)} errorComponent={ErrorPage}>
+        <AppContext
+            variablesOnly={
+                !getMeta("themeFeatures.DataDrivenTheme", false) && !getMeta("themeFeatures.SharedMasterView", false)
+            }
+            errorComponent={ErrorPage}
+        >
             {props.children}
         </AppContext>
     );
 });
 
 // Routing
-addComponent("App", () => (
-    <AppContext variablesOnly>
-        <Router disableDynamicRouting />
-    </AppContext>
-));
+addComponent("App", () => <Router disableDynamicRouting />);
 
 addComponent("title-bar-hamburger", TitleBarHamburger);
 

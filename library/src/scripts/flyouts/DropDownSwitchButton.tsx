00f41d9075acad72a5244b16894817146fdd8548
Chris Chabilall <chris.c@vanillaforums.com>
2020-02-17 11:55:12 -0500

    added accessibility

--- library/src/scripts/flyouts/DropDownSwitchButton.tsx
+++ library/src/scripts/flyouts/DropDownSwitchButton.tsx
@@ -5,6 +5,7 @@ import ButtonLoader from "@library/loaders/ButtonLoader";
 import { CheckCompactIcon } from "@library/icons/common";
 import { dropdownSwitchButtonClasses } from "@library/flyouts/dropDownSwitchButtonStyles";
 import DropDownItemButton from "@library/flyouts/items/DropDownItemButton";
+import { t } from "@vanilla/i18n/src";
 
 export interface IButtonSwitch {
     status: boolean;
@@ -20,22 +21,22 @@ export default function DropDownSwitchButton(props: IButtonSwitch) {
     const classes = dropdownSwitchButtonClasses();
 
     const { isLoading, status, onClick, label } = props;
+    const checkStatus = status ? (
+        <>
+            <CheckCompactIcon aria-hidden={true} />
+            <span className={visibilityClasses.visuallyHidden}>{t("on")}</span>
+        </>
+    ) : (
+        <>
+            <span className={visibilityClasses.visuallyHidden}>{t("off")}</span>
+        </>
+    );
 
     const content = (
         <>
             <span className={classes.itemLabel}>{label}</span>
-            <span className={classes.checkContainer}>
-                {isLoading ? (
-                    <ButtonLoader />
-                ) : (
-                    <CheckCompactIcon
-                        aria-hidden={true}
-                        className={classNames(
-                            { [visibilityClasses.visuallyHidden]: !status },
-                            "selectBox-isSelectedIcon",
-                        )}
-                    />
-                )}
+            <span className={classNames(classes.checkContainer, "sc-only")}>
+                {isLoading ? <ButtonLoader /> : checkStatus}
             </span>
         </>
     );

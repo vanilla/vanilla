7c5b8ccf91e3615a8302859ae5453f41e2840c39
NeenaDev <neena.dev@vanillaforums.com>
2020-02-18 14:46:29 -0500

    update story

--- library/src/scripts/theming/addTheme.story.tsx
+++ library/src/scripts/theming/addTheme.story.tsx
@@ -9,15 +9,16 @@ import React from "react";
 import { StoryContent } from "@library/storybook/StoryContent";
 import { AddTheme } from "./AddTheme";
 import { PlusIcon } from "@library/icons/common";
+import { manageThemingClasses } from "@themingapi/theming-ui-settings/manageThemingStyles";
 
 const story = storiesOf("Theme", module);
-
+const classes = manageThemingClasses();
 story.add("Add Theme", () => {
     return (
         <>
             <StoryContent>
                 <StoryHeading depth={1}>Add Theme</StoryHeading>
-                <AddTheme onAdd={<PlusIcon />} />
+                <AddTheme className={classes.gridItem} onAdd={<PlusIcon />} />
             </StoryContent>
         </>
     );

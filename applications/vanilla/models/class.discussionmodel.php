a2b7ca88700ec81bb51e6c7fdccf2a96d6179eb8
dannimajdalani <dani.m@vanillaforums.com>
2020-02-21 08:44:52 -0500

    fix deleting posts in spam queue

--- applications/vanilla/models/class.discussionmodel.php
+++ applications/vanilla/models/class.discussionmodel.php
@@ -3059,11 +3059,12 @@ class DiscussionModel extends Gdn_Model implements FormatFieldInterface {
             $this->setUserBookmarkCount($user->UserID);
         }
 
-        $dataObject = (object)$data;
-        $this->calculate($dataObject);
-        $discussionEvent = $this->eventFromRow((array)$dataObject, DiscussionEvent::ACTION_DELETE);
-        $this->getEventManager()->dispatch($discussionEvent);
-
+        if ($data) {
+            $dataObject = (object)$data;
+            $this->calculate($dataObject);
+            $discussionEvent = $this->eventFromRow((array)$dataObject, DiscussionEvent::ACTION_DELETE);
+            $this->getEventManager()->dispatch($discussionEvent);
+        }
         return true;
     }
 

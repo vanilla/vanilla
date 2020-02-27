d5468ad23dfd78f3a7877705050a33030a8229df
Adam Charron <adam@charrondev.com>
2020-02-27 14:09:08 -0500

    Adjust checkbox states to match mockups

--- library/src/scripts/forms/Checkbox.tsx
+++ library/src/scripts/forms/Checkbox.tsx
@@ -18,6 +18,8 @@ interface IProps extends IOptionalComponentID {
     onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
     label: string;
     isHorizontal?: boolean;
+    fakeFocus?: boolean;
+    defaultChecked?: boolean;
 }
 
 export default function CheckBox(props: IProps) {
@@ -29,28 +31,23 @@ export default function CheckBox(props: IProps) {
     return (
         <label className={classNames(props.className, classes.root, { isHorizontal })}>
             <input
-                className={classes.input}
+                className={classNames(classes.input, props.fakeFocus && "focus-visible")}
                 aria-labelledby={labelID}
                 type="checkbox"
                 onChange={props.onChange}
                 checked={props.checked}
+                defaultChecked={props.defaultChecked}
                 disabled={props.disabled}
                 tabIndex={0}
             />
             <span className={classes.iconContainer} aria-hidden="true">
-                <span className={classes.state}>
-                    <svg
-                        className={classNames(classes.checkIcon)}
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 10 10"
-                    >
-                        <title>{t("✓")}</title>
-                        <path
-                            fill="currentColor"
-                            d="M10,2.7c0-0.2-0.1-0.3-0.2-0.4L8.9,1.3c-0.2-0.2-0.6-0.2-0.9,0L3.8,5.6L1.9,3.7c-0.2-0.2-0.6-0.2-0.9,0L0.2,4.6c-0.2,0.2-0.2,0.6,0,0.9l3.2,3.2c0.2,0.2,0.6,0.2,0.9,0l5.5-5.5C9.9,3,10,2.8,10,2.7z"
-                        />
-                    </svg>
-                </span>
+                <svg className={classNames(classes.checkIcon)} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10">
+                    <title>{t("✓")}</title>
+                    <path
+                        fill="currentColor"
+                        d="M10,2.7c0-0.2-0.1-0.3-0.2-0.4L8.9,1.3c-0.2-0.2-0.6-0.2-0.9,0L3.8,5.6L1.9,3.7c-0.2-0.2-0.6-0.2-0.9,0L0.2,4.6c-0.2,0.2-0.2,0.6,0,0.9l3.2,3.2c0.2,0.2,0.6,0.2,0.9,0l5.5-5.5C9.9,3,10,2.8,10,2.7z"
+                    />
+                </svg>
             </span>
             {props.label && (
                 <span id={labelID} className={classes.label}>

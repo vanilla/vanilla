022a6394cafd7b0c4414969b6850c3f5385aff1d
Richard Flynn <richardflynn@gmail.com>
2020-03-27 16:37:51 -0400

    Add split merge en.php file

---
+++ plugins/SplitMerge/locale/en.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Richard Flynn <richard.flynn@vanillaforums.com>
+ * @copyright 2009-2020 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ */
+// phpcs:ignoreFile
+
+$Definition['Create New Discussion'] = 'Create New Discussion';
+$Definition['Leave a redirect link from the old discussions.'] = 'Leave a redirect link from the old discussions.';
+$Definition['New Discussion Topic'] = 'New Discussion Topic';
+$Definition['Split %s to new discussion'] = 'You have chosen to split %s into a new discussion.';

a64c81b031ae109edcb72030bcaee98e6a933368
Adam Charron <adam@charrondev.com>
2020-03-30 18:51:33 -0400

    Allow differentiating between klduged variable providers and non-kludged ones

---
+++ library/Vanilla/Theme/KludgedVariablesProviderInterface.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Adam Charron <adam.c@vanillaforums.com>
+ * @copyright 2009-2020 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ */
+
+namespace Vanilla\Theme;
+
+interface KludgedVariablesProviderInterface extends VariablesProviderInterface {
+}

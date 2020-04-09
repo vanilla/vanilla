ddb41321578982442615848c694f467c4da792d1
alexander.kim <kim.alexander.ca@gmail.com>
2020-04-09 08:33:26 -0400

    extend CurrentUserFieldProcessorTest to test operations in MODE_DEFAULT and MODE_IMPORT

--- tests/Library/Vanilla/Database/CurrentUserFieldProcessorTest.php
+++ tests/Library/Vanilla/Database/CurrentUserFieldProcessorTest.php
@@ -53,4 +53,77 @@ class CurrentUserFieldProcessorTest extends TestCase {
             [Operation::TYPE_UPDATE, ["UpdateUserID" => self::CURRENT_USER_ID]],
         ];
     }
+
+    /**
+     * Verify processor adds/resets fields to current user when operations in default mode.
+     *
+     * @param string $type
+     * @param array $expectedSet
+     * @dataProvider provideUserFields
+     */
+    public function testUserFieldsModeDefault(string $type, array $expectedSet) {
+        $model = new BasicPipelineModel("Example");
+        $session = new Gdn_Session();
+        $session->UserID = self::CURRENT_USER_ID;
+        $processor = new CurrentUserFieldProcessor($session);
+        $model->addPipelineProcessor($processor);
+
+        $operation = new Operation();
+        $operation->setType($type);
+        $operation->setCaller($model);
+        $operation->setSet($expectedSet);
+        $model->doOperation($operation);
+        $set = $operation->getSet();
+
+        foreach ($set as $setField => $setValue) {
+            if (!array_key_exists($setField, $expectedSet)) {
+                // Throw a failure exception.
+                $this->fail("Unexpected field: {$setField}");
+            }
+            $this->assertEquals(self::CURRENT_USER_ID, $setValue);
+        }
+    }
+
+    /**
+     * Verify processor set user fields when operations in import mode.
+     *
+     * @param string $type
+     * @param array $expectedSet
+     * @dataProvider provideUserFields
+     */
+    public function testUserFieldsModeImport(string $type, array $expectedSet) {
+        $model = new BasicPipelineModel("Example");
+        $session = new Gdn_Session();
+        $session->UserID = self::CURRENT_USER_ID;
+        $processor = new CurrentUserFieldProcessor($session);
+        $model->addPipelineProcessor($processor);
+
+        $operation = new Operation();
+        $operation->setType($type);
+        $operation->setCaller($model);
+        $operation->setMode(Operation::MODE_IMPORT);
+        $operation->setSet($expectedSet);
+        $model->doOperation($operation);
+        $set = $operation->getSet();
+
+        foreach ($set as $setField => $setValue) {
+            if (!array_key_exists($setField, $expectedSet)) {
+                // Throw a failure exception.
+                $this->fail("Unexpected field: {$setField}");
+            }
+            $this->assertEquals($expectedSet[$setField], $setValue);
+        }
+    }
+
+    /**
+     * Generate type and "sets" pairs for testing if the current user fields should be set (or not).
+     *
+     * @return array
+     */
+    public function provideUserFields() {
+        return [
+            [Operation::TYPE_INSERT, ["InsertUserID" => 200]],
+            [Operation::TYPE_UPDATE, ["UpdateUserID" => 200]],
+        ];
+    }
 }

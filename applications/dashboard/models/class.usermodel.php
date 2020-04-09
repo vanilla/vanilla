ee756c9b3bea308f506b968f837a8b33166dbf15
dannimajdalani <dani.m@vanillaforums.com>
2020-04-09 10:51:21 -0400

    Fix user count when no keyword is supplied

--- applications/dashboard/models/class.usermodel.php
+++ applications/dashboard/models/class.usermodel.php
@@ -2952,12 +2952,13 @@ class UserModel extends Gdn_Model implements UserProviderInterface {
 
         // Check to see if the search exactly matches a role name.
         $roleID = false;
-        if (strtolower($keywords) == 'banned') {
-            $this->SQL->where('u.Banned >', 0);
-        } else {
-            $roleID = $this->SQL->getWhere('Role', ['Name' => $keywords])->value('RoleID');
+        if ($keywords !== "") {
+            if (strtolower($keywords) == 'banned') {
+                $this->SQL->where('u.Banned >', 0);
+            } else {
+                $roleID = $this->SQL->getWhere('Role', ['Name' => $keywords])->value('RoleID');
+            }
         }
-
         if (isset($where)) {
             $this->SQL->where($where, null, false);
         }

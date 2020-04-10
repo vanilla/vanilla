7c1c8b402caf59e173c7e69bdb37cbda4798ee5e
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2020-04-10 16:00:46 -0400

    Add on change for font url

--- library/src/scripts/forms/themeEditor/ThemeBuilderFontDeclaration.tsx
+++ library/src/scripts/forms/themeEditor/ThemeBuilderFontDeclaration.tsx
@@ -4,26 +4,34 @@
  * @license GPL-2.0-only
  */
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useThemeVariableField } from "@library/forms/themeEditor/ThemeBuilderContext";
 import { ThemeBuilderBlock } from "@library/forms/themeEditor/ThemeBuilderBlock";
 import { t } from "@vanilla/i18n/src";
 import { isAllowedUrl } from "@library/utility/appUtils";
 import { CustomFontFamilyName } from "@themingapi/theme/CustomFontFamilyName";
 import { CustomFontUrl } from "@themingapi/theme/CustomFontUrl";
+import { LoadStatus } from "@library/@types/api/core";
 
 export const fontURLKey = "global.fonts.customFontUrl";
 
 export function ThemeBuilderFontDeclaration() {
     const { generatedValue, initialValue, rawValue } = useThemeVariableField(fontURLKey);
-    const validURL = isAllowedUrl(generatedValue ?? rawValue);
+    const value = generatedValue ?? rawValue;
+    const [currentUrl, setCurrentUrl] = useState(initialValue);
+
+    let validURL = false;
+    useEffect(() => {
+        validURL = value ? isAllowedUrl(value) : false;
+    }, [generatedValue, rawValue]);
+
     return (
         <>
             <ThemeBuilderBlock
                 label={t("Font URL")}
                 info={t("You can upload a Custom Font in your Theming System. Just copy & paste the URL in the field.")}
             >
-                <CustomFontUrl />
+                <CustomFontUrl onChange={setCurrentUrl} />
             </ThemeBuilderBlock>
             {validURL && (
                 <ThemeBuilderBlock label={t("Font Name")}>

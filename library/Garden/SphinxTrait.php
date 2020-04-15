6f6bcf290a817427b34f1e032734d7643815e60d
alexander.kim <kim.alexander.ca@gmail.com>
2020-04-15 11:32:32 -0400

    SphinxClient converted from original sphinxapi.php to PSR-, PSR-4 friendly class to avoid unnecessary installation and deployment manipulations
    Relates to: https://github.com/vanilla/internal/issues/2354

--- library/Garden/SphinxTrait.php
+++ library/Garden/SphinxTrait.php
@@ -7,58 +7,24 @@
 
 namespace Garden;
 
+use Vanilla\Adapters\SphinxClient;
 /**
  * For classes that want Sphinx search to incorporate.
  *
  */
 trait SphinxTrait {
     /**
-     * Check if a SphinxClient is available.  If not try to make it available using $apiDir.
-     *
-     * $apiPAth is a kludge that allows to use Sphinx on PHP7 the correct version without compiling the php extension yourself
-     * Source of the class https://github.com/sphinxsearch/sphinx/blob/master/api/sphinxapi.php
-     * Make sure that it matches the sphinx version you are using.
-     *
-     * @param string $apiDir Directory that contains sphinxapi.php
-     *
-     * @return bool
-     */
-    public static function checkSphinxClient(string $apiDir = null): bool {
-        if (class_exists('SphinxClient')) {
-            return true;
-        }
-
-        $sphinxClientPath = rtrim($apiDir, '/').'/sphinxapi.php';
-        if (is_readable($sphinxClientPath)) {
-            require_once($sphinxClientPath);
-        }
-
-        return class_exists('SphinxClient');
-    }
-
-    /**
      * Get SphinxClient object
      *
      * @return SphinxClient
      */
-    public static function sphinxClient(): \SphinxClient {
-        if (!self::checkSphinxClient()) {
-            throw new \Exception('Sphinx client class not found.');
-        }
+    public static function sphinxClient(): SphinxClient {
         $sphinxHost = c('Plugins.Sphinx.Server', c('Database.Host', 'localhost'));
         $sphinxPort = c('Plugins.Sphinx.Port', 9312);
 
-        $client = new \SphinxClient();
+        $client = new SphinxClient();
         $client->setServer($sphinxHost, $sphinxPort);
-
-        // Set some defaults
-        if (method_exists($client, "setMatchMode")) {
-            $client->setMatchMode(SPH_MATCH_EXTENDED2);
-            $client->setSortMode(SPH_SORT_TIME_SEGMENTS, 'DateInserted');
-        } else {
-            // SPH_SORT_TIME_SEGMENTS is not a valid sort mode in Sphinx 3.2.1.
-            $client->setSortMode(SPH_SORT_RELEVANCE);
-        }
+        $client->setSortMode(SphinxClient::SORT_RELEVANCE);
         $client->setMaxQueryTime(5000);
 
         return $client;

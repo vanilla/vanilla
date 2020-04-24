e821af69bae7d4a1738591f9cb593bca338e2ce5
Ryan <initvector@users.noreply.github.com>
2020-04-24 13:50:07 -0400

    Add UserModel::getDefaultSSOIDs

--- applications/dashboard/models/class.usermodel.php
+++ applications/dashboard/models/class.usermodel.php
@@ -1508,6 +1508,20 @@ class UserModel extends Gdn_Model implements UserProviderInterface {
     }
 
     /**
+     * Get user authentication rows by user ID and provider.
+     *
+     * @param int[] $userIDs
+     * @param string $provider
+     * @return array
+     */
+    private function getUserAuthentications(array $userIDs, string $provider): array {
+        return $this->SQL->getWhere(
+            'UserAuthentication',
+            ['UserID' => $userIDs, 'ProviderKey' => $provider]
+        )->resultArray();
+    }
+
+    /**
      *
      *
      * @param array|bool $like
@@ -5515,5 +5529,28 @@ class UserModel extends Gdn_Model implements UserProviderInterface {
     public function setEmailUnique(bool $emailUnique) {
         $this->emailUnique = $emailUnique;
         return $this;
+    }
+
+    /**
+     * Given an array of user IDs
+     *
+     * @param array $userIDs
+     * @return array
+     */
+    public function getDefaultSSOIDs(array $userIDs): array {
+        $defaultProvider = Gdn_AuthenticationProviderModel::getDefault();
+        $result = array_combine($userIDs, array_pad([], count($userIDs), null));
+
+        if ($defaultProvider === false) {
+            return $result;
+        }
+
+        $connections = $this->getUserAuthentications($userIDs, $defaultProvider["AuthenticationKey"]);
+        $mapping = array_column($connections, "ForeignUserKey", "UserID");
+        foreach ($mapping as $userID => $ssoID) {
+            $result[$userID] = $ssoID;
+        }
+
+        return $result;
     }
 }

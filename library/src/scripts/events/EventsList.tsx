85a164c1817c59f029f3c69b613f8b85c470f0b8
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2020-05-06 16:04:30 -0400

    Copied changes from /feature/events-panel

--- library/src/scripts/events/EventsList.tsx
+++ library/src/scripts/events/EventsList.tsx
@@ -1,63 +1,73 @@
 /**
- * @author Adam Charron <adam.c@vanillaforums.com>
+ * @author Stéphane LaFlèche <stephane.l@vanillaforums.com>
  * @copyright 2009-2019 Vanilla Forums Inc.
  * @license GPL-2.0-only
  */
 
-import React, { Component } from "react";
+import React from "react";
 
 import { IEvent } from "@library/events/Event";
 import { eventsClasses } from "@library/events/eventStyles";
 import { Event } from "@library/events/Event";
 import classNames from "classnames";
 import { t } from "@vanilla/i18n";
-import { EventAttendance } from "@library/events/EventAttendanceDropDown";
-import { ISelectBoxItem } from "@library/forms/select/SelectBox";
+import { eventAttendanceOptions } from "@library/events/eventOptions";
 
 export interface IEventList {
-    headingLevel?: 2 | 3;
+    headingLevel?: 2 | 3 | 4;
     data: IEvent[];
     hideIfEmpty?: boolean;
     emptyMessage?: string;
+    compact?: boolean;
 }
 
 /**
- * Component for displaying an accessible nicely formatted time string.
+ * Component for displaying the list of events
  */
 export function EventsList(props: IEventList) {
-    const classes = eventsClasses();
+    const classes = eventsClasses({
+        compact: props.compact,
+    });
+
     if (!props.data || props.data.length === 0) {
         const { hideIfEmpty = false, emptyMessage = t("This category does not have any events.") } = props;
         return hideIfEmpty ? null : <p className={classes.empty}>{emptyMessage}</p>;
     }
-
-    const options = [
-        { name: t("Going"), value: EventAttendance.GOING },
-        { name: t("Maybe"), value: EventAttendance.MAYBE },
-        { name: t("Not going"), value: EventAttendance.NOT_GOING },
-    ] as ISelectBoxItem[];
+    const going = t("Going");
+    const maybe = t("Maybe");
 
     let longestCharCount = 0;
-    options.forEach(o => {
-        if (o.name && o.name.length > longestCharCount) {
-            longestCharCount = o.name.length;
+    if (props.compact) {
+        if (going.length > maybe.length) {
+            longestCharCount = going.length;
+        } else {
+            longestCharCount = maybe.length;
         }
-    });
+    } else {
+        eventAttendanceOptions.forEach(o => {
+            if (o.name && o.name.length > longestCharCount) {
+                longestCharCount = o.name.length;
+            }
+        });
+    }
 
     return (
-        <ul className={classes.list}>
-            {props.data.map((event, i) => {
-                return (
-                    <Event
-                        className={classNames({ isFirst: i === 0 })}
-                        headingLevel={props.headingLevel}
-                        {...event}
-                        key={i}
-                        longestCharCount={longestCharCount}
-                        attendanceOptions={options}
-                    />
-                );
-            })}
-        </ul>
+        <>
+            <ul className={classes.list}>
+                {props.data.map((event, i) => {
+                    return (
+                        <Event
+                            className={classNames({ isFirst: i === 0 })}
+                            headingLevel={props.headingLevel}
+                            {...event}
+                            key={i}
+                            longestCharCount={longestCharCount}
+                            attendanceOptions={eventAttendanceOptions}
+                            compact={props.compact}
+                        />
+                    );
+                })}
+            </ul>
+        </>
     );
 }

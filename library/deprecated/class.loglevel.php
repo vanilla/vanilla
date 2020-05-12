b6c53027de1f942e4969faa9896264cb39845b35
Todd Burry <todd@vanillaforums.com>
2020-05-12 17:59:19 -0400

    Deprecate our non-PSR loggers

---
+++ library/deprecated/class.loglevel.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * LogLevel.
+ *
+ * @copyright 2009-2019 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ * @package Core
+ * @since 2.2
+ */
+
+/**
+ * Describes log levels. These are ordered from most severe to least severe.
+ *
+ * These constants have been moved to the Logger class and will be removed shortly.
+ * @deprecated
+ */
+class LogLevel extends \Psr\Log\LogLevel {
+}

6f6dd7307bc5aeb612a9b5c6031666aa94726e7d
igraziatto <isis.graziatto@gmail.com>
2020-06-02 14:26:35 -0400

    Make the GuestModule more flexible

--- applications/dashboard/views/modules/guest.php
+++ applications/dashboard/views/modules/guest.php
@@ -1,24 +1,27 @@
 <?php if (!defined('APPLICATION')) exit(); ?>
 <div class="Box GuestBox">
-    <h4><?php echo t('Howdy, Stranger!'); ?></h4>
+    <h4 class="GuestBox-title">
+        <?php echo t('Howdy, Stranger!'); ?>
+    </h4>
+    <p class="GuestBox-message">
+        <?php echo t($this->MessageCode, $this->MessageDefault); ?>
+    </p>
 
-    <p><?php echo t($this->MessageCode, $this->MessageDefault); ?></p>
-
-    <p><?php $this->fireEvent('BeforeSignInButton'); ?></p>
+    <p class="GuestBox-beforeSignInButton">
+        <?php $this->fireEvent('BeforeSignInButton'); ?>
+    </p>
 
     <?php
-    $signInUrl = signInUrl($this->_Sender->SelfUrl);
+        if ($this->data('signInUrl')) {
+            echo '<div class="P">';
+            echo anchor(t('Sign In'), $this->data('signInUrl'), 'Button Primary'.(signInPopup() ? ' SignInPopup' : ''), ['rel' => 'nofollow']);
 
-    if ($signInUrl) {
-        echo '<div class="P">';
+            if ($this->data('registerUrl')) {
+                echo ' '.anchor(t('Register', t('Apply for Membership', 'Register')), $this->data('registerUrl'), 'Button ApplyButton', ['rel' => 'nofollow']);
+            }
 
-        echo anchor(t('Sign In'), signInUrl($this->_Sender->SelfUrl), 'Button Primary'.(signInPopup() ? ' SignInPopup' : ''), ['rel' => 'nofollow']);
-        $Url = registerUrl($this->_Sender->SelfUrl);
-        if (!empty($Url))
-            echo ' '.anchor(t('Register', t('Apply for Membership', 'Register')), $Url, 'Button ApplyButton', ['rel' => 'nofollow']);
-
-        echo '</div>';
-    }
+            echo '</div>';
+        }
     ?>
     <?php $this->fireEvent('AfterSignInButton'); ?>
 </div>

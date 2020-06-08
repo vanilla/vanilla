fcd39798f0f9e7dcb3aff7dcb5b54824a1aa6dd0
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2020-06-08 12:14:53 -0400

    Fixing block column styls

--- applications/dashboard/src/scripts/compatibilityStyles/blockColumnStyles.ts
+++ applications/dashboard/src/scripts/compatibilityStyles/blockColumnStyles.ts
@@ -5,12 +5,26 @@
  * @license GPL-2.0-only
  */
 import { cssOut } from "@dashboard/compatibilityStyles/index";
+import { forumVariables } from "@library/forms/forumStyleVars";
+import { userPhotoMixins } from "@library/headers/mebox/pieces/userPhotoStyles";
+import { absolutePosition, unit } from "@library/styles/styleHelpers";
+import { globalVariables } from "@library/styles/globalStyleVars";
+import { calc } from "csx";
 
 export function blockColumnCSS() {
+    const globalVars = globalVariables();
+    const forumVars = forumVariables();
+    const userPhotoSizing = forumVars.userPhoto.sizing;
+    const mixins = userPhotoMixins(forumVars.userPhoto);
     // Reworked placement of BlockColumn, because they were misaligned and also causing false positives on the accessibility tests.
     cssOut(`.BlockColumn .Block.Wrap`, {
         display: "flex",
         flexWrap: "wrap",
+        flexDirection: "column",
+        overflow: "hidden",
+        justifyContent: "space-between",
+        alignItems: "flex-end",
+        minHeight: unit(userPhotoSizing.medium),
     });
 
     cssOut(
@@ -39,6 +53,7 @@ export function blockColumnCSS() {
         .DataTable .Block.Wrap .Meta
         `,
         {
+            width: calc(`100% - ${unit(userPhotoSizing.medium + 12)}`),
             margin: 0,
         },
     );
@@ -53,9 +68,24 @@ export function blockColumnCSS() {
         .DataTable tbody td.FirstUser .PhotoWrap
     `,
         {
-            position: "relative",
-            top: "auto",
-            left: "auto",
+            ...mixins.root,
+            ...absolutePosition.topLeft(),
+            width: unit(userPhotoSizing.medium),
+            height: unit(userPhotoSizing.medium),
+        },
+    );
+
+    cssOut(
+        `
+        .Groups .DataTable tbody td.LatestPost .PhotoWrap img,
+        .Groups .DataTable tbody td.LastUser .PhotoWrap img,
+        .Groups .DataTable tbody td.FirstUser .PhotoWrap img,
+        .DataTable tbody td.LatestPost .PhotoWrap img,
+        .DataTable tbody td.LastUser .PhotoWrap img,
+        .DataTable tbody td.FirstUser .PhotoWrap img
+    `,
+        {
+            ...mixins.photo,
         },
     );
 }

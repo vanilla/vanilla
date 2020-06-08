4127d41b97eaf25b2f9201c2acc8c80771ff2cf8
Todd Burry <todd@vanillaforums.com>
2020-06-08 08:00:04 -0400

    Add some smoke tests for the /resources endpoint

---
+++ tests/APIv2/ResourcesTest.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * @author Todd Burry <todd@vanillaforums.com>
+ * @copyright 2009-2020 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ */
+
+namespace VanillaTests\APIv2;
+
+use Vanilla\Utility\ArrayUtils;
+use Vanilla\Utility\StringUtils;
+
+/**
+ * Tests for the `/resources` endpoints.
+ */
+class ResourcesTest extends AbstractAPIv2Test {
+    /**
+     * @inheritDoc
+     */
+    public static function getAddons(): array {
+        return ['dashboard', 'vanilla'];
+    }
+
+    /**
+     * The index should return the resource for the main tables.
+     */
+    public function testIndex(): void {
+        $r = $this->api()->get('/resources')->getBody();
+        $expected = [
+            [
+                'recordType' => 'user',
+                'url' => 'http://vanilla.test/resourcestest/api/v2/resources/user',
+                'crawlable' => true,
+            ],
+            [
+                'recordType' => 'category',
+                'url' => 'http://vanilla.test/resourcestest/api/v2/resources/category',
+                'crawlable' => true,
+            ],
+            [
+                'recordType' => 'discussion',
+                'url' => 'http://vanilla.test/resourcestest/api/v2/resources/discussion',
+                'crawlable' => true,
+            ],
+            [
+                'recordType' => 'comment',
+                'url' => 'http://vanilla.test/resourcestest/api/v2/resources/comment',
+                'crawlable' => true,
+            ],
+        ];
+
+        usort($expected, ArrayUtils::sortCallback('recordType'));
+        usort($r, ArrayUtils::sortCallback('recordType'));
+
+        $this->assertSame($expected, $r);
+    }
+
+    /**
+     * A basic smoke test of the individual resource endpoints.
+     */
+    public function testResourceCrawlInspection(): void {
+        $resources = $this->api()->get('/resources', ['crawlable' => true])->getBody();
+        foreach ($resources as $row) {
+            ['url' => $url] = $row;
+            StringUtils::substringLeftTrim($url, $this->api()->getBaseUrl());
+            $r = $this->api()->get($url, ['expand' => 'crawl'])->getBody();
+            $this->assertIsInt($r['crawl']['count']);
+        }
+    }
+}

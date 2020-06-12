b3720d7e825082e0d653699e282cfc643a124dc0
Adam Charron <adam@charrondev.com>
2020-06-11 21:28:59 -0400

    Add a model with full record caching

---
+++ library/Vanilla/Models/ModelCache.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * @author Adam Charron <adam.c@vanillaforums.com>
+ * @copyright 2009-2020 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ */
+
+namespace Vanilla\Models;
+
+use Vanilla\InjectableInterface;
+
+/**
+ * Cache for records out of various models.
+ *
+ * Particularly good support for PipelineModel.
+ */
+class ModelCache implements InjectableInterface {
+
+    /** @var int When we hit this size of incrementing key, we reset from 0. */
+    const MAX_INCREMENTING_KEY = 1000000;
+
+    /** @var string */
+    const INCREMENTING_KEY_NAMESPACE = 'vanillaIncrementingKey';
+
+    const GLOBAL_DEFAULT_OPTIONS = [
+        \Gdn_Cache::FEATURE_EXPIRY => 600,
+    ];
+
+    /** @var string */
+    private $namespace;
+
+    /** @var number */
+    private $defaultCacheOptions;
+
+    /** @var int */
+    private $incrementingKey;
+
+    /** @var \Gdn_Cache */
+    private $cache;
+
+    /**
+     * Create a cache from a model.
+     *
+     * @param Model $model The model to create from.
+     * @param array|null $defaultCacheOptions Default options to apply for storing cache values.
+     *
+     * @return ModelCache
+     */
+    public static function fromModel(Model $model, ?array $defaultCacheOptions = []): ModelCache {
+        $cache = \Gdn::getContainer()->getArgs(ModelCache::class, [$model->getTable(), $defaultCacheOptions]);
+        return $cache;
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param string $namespace
+     * @param array|null $defaultCacheOptions Default options to apply for storing cache values.
+     */
+    public function __construct(string $namespace, ?array $defaultCacheOptions = []) {
+        $this->namespace = $namespace;
+        $this->defaultCacheOptions = array_merge(self::GLOBAL_DEFAULT_OPTIONS, $defaultCacheOptions ?? []);
+    }
+
+    /**
+     * Dependency Injection.
+     *
+     * @param \Gdn_Cache $cache
+     */
+    public function setDependencies(\Gdn_Cache $cache) {
+        $this->cache = $cache;
+    }
+
+    /**
+     * Create a cache key for some parameters.
+     *
+     * @param array $keyArgs Some arguments to generate the cache key from.
+     *
+     * @return string
+     */
+    public function createCacheKey(array $keyArgs): string {
+        $key = $this->namespace . '-' . $this->getIncrementingKey() . '-' . md5(json_encode($keyArgs));
+        return $key;
+    }
+
+    /**
+     * Try to get a cached record.
+     *
+     * If the record can't be found, we hydrate it with the $hydrate callable and return it.
+     *
+     * @param array $keyArgs The arguments to build the cache key.
+     * @param callable $hydrate A callable to hydrate the cache.
+     * @param array|null $cacheOptions Options for the cache storage.
+     *
+     * @return mixed
+     */
+    public function getCachedOrHydrate(array $keyArgs, callable $hydrate, ?array $cacheOptions = []) {
+        $key = $this->createCacheKey($keyArgs);
+        $result = $this->cache->get($key);
+
+        if ($result === \Gdn_Cache::CACHEOP_FAILURE) {
+            $result = $hydrate();
+            $options = array_merge($this->defaultCacheOptions, $cacheOptions);
+            $this->cache->store($key, serialize($result), $options);
+        } else {
+            $result = unserialize($result);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Invalidate all cached results for this cache.
+     */
+    public function invalidateAll() {
+        $this->rolloverIncrementingKey();
+    }
+
+    /**
+     * Create a pipeline processor for invalidating the entire cache on every record.
+     *
+     * @return ModelCacheInvalidationProcessor
+     */
+    public function createInvalidationProcessor(): ModelCacheInvalidationProcessor {
+        return new ModelCacheInvalidationProcessor($this);
+    }
+
+    /**
+     * @param \Gdn_Cache $cache
+     */
+    public function setCache(\Gdn_Cache $cache): void {
+        $this->cache = $cache;
+    }
+
+    /**
+     * Get an incrementing key that can be rolled over everytime the whole cache is invalidated.
+     *
+     * @return int
+     */
+    private function getIncrementingKey(): int {
+        if ($this->incrementingKey === null) {
+            $incrementKeyCacheKey = self::INCREMENTING_KEY_NAMESPACE . '-' . $this->namespace;
+            $result = $this->cache->get($incrementKeyCacheKey);
+
+            if ($result === \Gdn_Cache::CACHEOP_FAILURE) {
+                $result = 0;
+            }
+            $this->incrementingKey = $result;
+
+            $this->cache->store($incrementKeyCacheKey, $this->incrementingKey);
+        }
+
+        return $this->incrementingKey;
+    }
+
+    /**
+     * Update the incrementing key.
+     */
+    private function rolloverIncrementingKey(): void {
+        $key = $this->getIncrementingKey();
+
+        $newKey = $key + 1;
+        if ($newKey > self::MAX_INCREMENTING_KEY) {
+            // Restart from 0.
+            $newKey = 0;
+        }
+
+        $incrementKeyCacheKey = self::INCREMENTING_KEY_NAMESPACE . '-' . $this->namespace;
+        $this->incrementingKey = $newKey;
+        $this->cache->store($incrementKeyCacheKey, $newKey);
+    }
+}
---
+++ tests/Library/Vanilla/Models/ModelCacheTest.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * @author Adam Charron <adam.c@vanillaforums.com>
+ * @copyright 2009-2020 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ */
+
+namespace VanillaTests\Library\Vanilla\Models;
+
+use PHPUnit\Framework\TestCase;
+use Vanilla\Models\Model;
+use Vanilla\Models\ModelCache;
+use Vanilla\Models\PipelineModel;
+use VanillaTests\SiteTestTrait;
+
+/**
+ * Tests for the `Model` class.
+ */
+class ModelCacheTest extends TestCase {
+    use SiteTestTrait;
+
+    /**
+     * @var PipelineModel
+     */
+    private $model;
+
+    /**
+     * @var ModelCache
+     */
+    private $modelCache;
+
+    /**
+     * Install the site and set up a test table.
+     */
+    public static function setupBeforeClass(): void {
+        static::setupBeforeClassSiteTestTrait();
+
+        static::container()->call(function (
+            \Gdn_DatabaseStructure $st
+        ) {
+            $st->table('model')
+                ->primaryKey('modelID')
+                ->column('name', 'varchar(50)')
+                ->set()
+            ;
+        });
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setUp(): void {
+        $this->container()->call(function (
+            \Gdn_SQLDriver $sql
+        ) {
+            $sql->truncate('model');
+        });
+
+        $this->model = $this->container()->getArgs(PipelineModel::class, ['model']);
+        $this->modelCache = ModelCache::fromModel($this->model);
+
+        // Apply a specific cache.
+        $this->modelCache->setCache(new \Gdn_Dirtycache());
+    }
+
+    /**
+     * Test that we can get and invalidate from the cache.
+     */
+    public function testGetInvalidate() {
+        $this->insertOne('foo');
+        $this->insertOne('bar');
+
+        $hydrator = function () {
+            return $this->model->get();
+        };
+
+        $initialResult = $this->modelCache->getCachedOrHydrate([], $hydrator);
+
+        // Insert another. We have no automatic cache invalidation enabled.
+        $this->insertOne('bar2');
+
+        $secondResult = $this->modelCache->getCachedOrHydrate([], $hydrator);
+
+        $this->assertSame($initialResult, $secondResult);
+        $this->assertSame(['foo', 'bar'], array_column($secondResult, 'name'));
+
+        // Invalidate and see they are different.
+        $this->modelCache->invalidateAll();
+
+        $finalResult = $this->modelCache->getCachedOrHydrate([], $hydrator);
+        $this->assertSame(['foo', 'bar', 'bar2'], array_column($finalResult, 'name'));
+    }
+
+    /**
+     * Test that we can get and invalidate from the cache.
+     */
+    public function testAutoInvalidation() {
+        $fooID = $this->insertOne('foo');
+        $this->insertOne('bar');
+        $this->model->addPipelinePostProcessor($this->modelCache->createInvalidationProcessor());
+
+        $hydrator = function () {
+            return $this->model->get();
+        };
+
+        $result = $this->modelCache->getCachedOrHydrate([], $hydrator);
+        $this->assertSame(['foo', 'bar'], array_column($result, 'name'));
+
+        // Insert another. This should invalidate the cache.
+        $bar2ID = $this->insertOne('bar2');
+        $result = $this->modelCache->getCachedOrHydrate([], $hydrator);
+        $this->assertSame(['foo', 'bar', 'bar2'], array_column($result, 'name'));
+
+        // Delete an item.
+        $this->model->delete(['modelID' => $bar2ID]);
+        $result = $this->modelCache->getCachedOrHydrate([], $hydrator);
+        $this->assertSame(['foo', 'bar'], array_column($result, 'name'));
+
+        // Update an item.
+        $this->model->update(['name' => 'foo update'], ['modelID' => $fooID]);
+        $result = $this->modelCache->getCachedOrHydrate([], $hydrator);
+        $this->assertSame(['foo update', 'bar'], array_column($result, 'name'));
+    }
+
+    /**
+     * Insert a basic test row.
+     *
+     * @param string $name
+     * @return int
+     */
+    private function insertOne(string $name = 'foo'): int {
+        $id = $this->model->insert(['name' => $name]);
+        return $id;
+    }
+}

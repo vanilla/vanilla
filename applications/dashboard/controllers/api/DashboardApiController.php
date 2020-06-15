3f49b27b38b0a4f04e8b9d68d8b6cc82bc4bf7fe
Adam Charron <adam@charrondev.com>
2020-06-15 18:34:08 -0400

    Add API for loading dasboard HTML

--- applications/dashboard/controllers/api/DashboardApiController.php
+++ applications/dashboard/controllers/api/DashboardApiController.php
@@ -8,31 +8,88 @@
 namespace Vanilla\Dashboard\Api;
 
 use DashboardNavModule;
+use Garden\EventManager;
+use Garden\Schema\Schema;
+use Garden\Web\Data;
+use Garden\Web\RequestInterface;
+use Vanilla\Exception\PermissionException;
 use Vanilla\Web\Controller;
 
 /**
  * Contains information useful for building the dashboard.
  */
 class DashboardApiController extends Controller {
+
+    /** @var \Gdn_Locale */
+    private $locale;
+
+    /** @var EventManager */
+    private $eventManager;
+
+    /**
+     * Constructor.
+     *
+     * @param \Gdn_Locale $locale
+     * @param EventManager $eventManager
+     */
+    public function __construct(\Gdn_Locale $locale, EventManager $eventManager) {
+        $this->locale = $locale;
+        $this->eventManager = $eventManager;
+    }
+
+
+    /**
+     * Get the HTML for the legacy dashboard menu.
+     *
+     * @param RequestInterface $request The request.
+     * @param array $query Query parameters.
+     *
+     * @return array
+     */
+    public function get_menuLegacy(RequestInterface $request, array $query = []) {
+        $this->checkDashboardPermission();
+        $this->locale->set($query['locale']);
+
+        $in = Schema::parse([
+            "activeUrl:s",
+            "section:s?" => [
+                "enum" => ["moderation", "settings"],
+            ],
+            "locale:s",
+        ]);
+
+        $params = $in->validate($query);
+        $section = ucfirst($query['section'] ?? 'settings');
+
+        $activeUrl = $params['activeUrl'];
+        if (filter_var($activeUrl, FILTER_VALIDATE_URL)) {
+            $activeUrlPath = parse_url($activeUrl, PHP_URL_PATH);
+            $toReplace = $request->getAssetRoot();
+            $activeUrlPath = str_replace($toReplace, '', $activeUrlPath);
+        } else {
+            $activeUrlPath = $activeUrl;
+        }
+
+        $dashboardNavModule = new DashboardNavModule();
+        $dashboardNavModule->setCurrentSections([$section]);
+        $dashboardNavModule->setHighlightRoute($activeUrlPath);
+
+        // Getting menus will call any necessary events.
+        $dashboardNavModule->getMenus();
+
+        $result = $dashboardNavModule->toString();
+        return [
+            'html' => $result,
+        ];
+    }
+
     /**
      * Get the menus in the dashboard.
      *
      * @return array Returns the menus.
      */
     public function index_menus() {
-        // This is the array of permissions from the module.
-        // We just want to make sure the user has at least one of the permissions although if they don't then the menus would be empty.
-        $this->permission([
-            'Garden.Settings.Manage',
-            'Garden.Community.Manage',
-            'Garden.Moderation.Manage',
-            'Garden.Settings.View',
-            'Moderation.ModerationQueue.Manage',
-            'Garden.Users.Add',
-            'Garden.Users.Edit',
-            'Garden.Users.Delete',
-            'Garden.Users.Approve',
-        ]);
+        $this->checkDashboardPermission();
 
         $in = $this->schema([], 'in')->setDescription('List the dashboard menus.');
         $out = $this->schema([
@@ -72,5 +129,25 @@ class DashboardApiController extends Controller {
         $result = $out->validate($result);
 
         return $result;
+    }
+
+    /**
+     * This is the array of permissions from the module.
+     * We just want to make sure the user has at least one of the permissions although if they don't then the menus would be empty.
+     *
+     * @throws PermissionException If the user doesn't have enough permission.
+     */
+    private function checkDashboardPermission() {
+        $this->permission([
+            'Garden.Settings.Manage',
+            'Garden.Community.Manage',
+            'Garden.Moderation.Manage',
+            'Garden.Settings.View',
+            'Moderation.ModerationQueue.Manage',
+            'Garden.Users.Add',
+            'Garden.Users.Edit',
+            'Garden.Users.Delete',
+            'Garden.Users.Approve',
+        ]);
     }
 }
--- tests/APIv2/DashboardTest.php
+++ tests/APIv2/DashboardTest.php
@@ -7,10 +7,16 @@
 
 namespace VanillaTests\APIv2;
 
+use Garden\EventManager;
+
 /**
  * Tests for the /api/v2/dashboard endpoints.
  */
 class DashboardTest extends AbstractAPIv2Test {
+
+    private $calledInit = false;
+
+    private $calledLegacyInit = false;
 
     /**
      * A basic smoke test of the dashboard menus.
@@ -19,5 +25,47 @@ class DashboardTest extends AbstractAPIv2Test {
         $r = $this->api()->get('/dashboard/menus');
         $data = $r->getBody();
         $this->assertSame(3, count($data));
+    }
+
+    /**
+     * Handler for the modern init.
+     */
+    public function dashboardNavModule_init_handler() {
+        $this->calledInit = true;
+    }
+
+    /**
+     * Handler for the legacy init.
+     *
+     * @param mixed $sender
+     */
+    public function base_getAppSettingsMenuItems_handler($sender) {
+        $this->calledLegacyInit = true;
+        $adapter = $sender->EventArguments['SideMenu'];
+        $this->assertInstanceOf(\NestedCollectionAdapter::class, $adapter);
+    }
+
+    /**
+     * A basic smoke test of the dashboard menu html handler.
+     */
+    public function testHtmlMenuSmoke() {
+        /** @var EventManager $eventManager */
+        $eventManager = self::container()->get(EventManager::class);
+        $eventManager->bindClass($this);
+
+        $noActive = $this->api()->get('/dashboard/menu-legacy', [
+            'activeUrl' => 'https://test.com',
+            'locale' => 'en',
+        ])->getBody();
+
+        $this->assertNotNull($noActive['html']);
+        $this->assertTrue($this->calledInit);
+        $this->assertTrue($this->calledLegacyInit);
+
+        $actualActive = $this->api()->get('/dashboard/menu-legacy', [
+            'activeUrl' => \Gdn::request()->getSimpleUrl('/dashboard/settings/branding'),
+            'locale' => 'en',
+        ])->getBody();
+        $this->assertEquals($actualActive, $noActive);
     }
 }

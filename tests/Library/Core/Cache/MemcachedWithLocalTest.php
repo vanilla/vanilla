6ed9f68b6154c257dc3d012384e6bcfb4000fc1d
Todd Burry <todd@vanillaforums.com>
2020-06-15 08:33:22 -0400

    Add Memcached tests
    These tests will need an environment variable to be set up so presently won’t run in CI.

---
+++ tests/Library/Core/Cache/MemcachedWithLocalTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author Todd Burry <todd@vanillaforums.com>
+ * @copyright 2009-2020 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ */
+
+namespace VanillaTests\Library\Core\Cache;
+
+/**
+ * Test our memcached adaptor with the local cache enabled.
+ */
+class MemcachedWithLocalTest extends MemcachedTest {
+    /**
+     * {@inheritDoc}
+     */
+    public function __construct($name = null, array $data = [], $dataName = '') {
+        parent::__construct($name, $data, $dataName);
+        $this->skippedTests = self::SKIP_TTL;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function setUpBeforeClass(): void {
+        parent::setUpBeforeClass();
+        self::$memcached->setStoreDefault(\Gdn_Cache::FEATURE_LOCAL, true);
+    }
+}

3ba305ba9b988faa1e878ec43d80b8c4ab55996f
Todd Burry <todd@vanillaforums.com>
2020-06-15 16:05:38 -0400

    Add tests based on 1:1

--- tests/Library/Vanilla/Utility/DomUtilsTest.php
+++ tests/Library/Vanilla/Utility/DomUtilsTest.php
@@ -12,6 +12,7 @@ namespace VanillaTests\Library\Vanilla\Utility;
 use DOMDocument;
 use Vanilla\Formatting\Html\HtmlDocument;
 use Vanilla\Formatting\Html\DomUtils;
+use Vanilla\Utility\StringUtils;
 use VanillaTests\Library\Vanilla\Formatting\AssertsFixtureRenderingTrait;
 use VanillaTests\MinimalContainerTestCase;
 use VanillaTests\Library\Vanilla\Formatting\HtmlNormalizeTrait;
@@ -93,6 +94,65 @@ class DomUtilsTest extends MinimalContainerTestCase {
     }
 
     /**
+     * @param string $html
+     * @param string $expected
+     * @dataProvider provideStripImagesTests
+     */
+    public function testStripImages(string $html, string $expected): void {
+        $dom = new HtmlDocument($html);
+
+        // This assertion tests against bugs in the HtmlDocument class itself.
+        $this->assertHtmlStringEqualsHtmlString($html, $dom->getInnerHtml(), "The HtmlDocument didn't parse the string properly.");
+
+        DomUtils::stripImages($dom->getDom());
+        $actual = $dom->getInnerHtml();
+        $this->assertHtmlStringEqualsHtmlString($expected, $actual);
+    }
+
+    /**
+     * @param string $html
+     * @param string $expected
+     * @dataProvider provideStripImagesFixedTests
+     */
+    public function testStripImagesFixed(string $html): void {
+        $expected = 'a b c';
+        $dom = new HtmlDocument($html);
+
+        // This assertion tests against bugs in the HtmlDocument class itself.
+        $this->assertHtmlStringEqualsHtmlString($html, $dom->getInnerHtml(), "The HtmlDocument didn't parse the string properly.");
+
+        DomUtils::stripImages($dom->getDom());
+        $actual = $dom->getInnerHtml();
+        $this->assertHtmlStringEqualsHtmlString($expected, $actual);
+    }
+
+    public function provideStripImagesFixedTests(): array {
+        $r = [
+            'no strip' => ['a b c'],
+            'strip before' => ['<img src="a.png" /> a b c'],
+            'strip after' => [' a b <img src="http://example.com/a.png" /> c'],
+            'closing tag' => [' a b <img src="http://example.com/a.png" ></img> c'],
+        ];
+
+        return $r;
+    }
+
+    public function provideStripImagesTests(): array {
+        $r = $this->createProviderFromDirectory('domutils/strip-images');
+
+        $r += [
+            'no strip' => ['a', 'a'],
+            'strip before' => ['<img src="http://example.com/a.png" /> a', 'a'],
+            'strip after' => ['a <img src="http://example.com/a.png" />', 'a'],
+            'multiple image' => ['a <img src="/a.jpg" /> b <img src="/b.jpg" />', 'a b'],
+            'closing tag' => ['a <img src="/a.jpg" ></img>', 'a'],
+            'nested' => ['<div><div><p><img src="http://example.com/a.png" /></p> a</div></div>', '<div><div><p></p> a</div></div>'],
+        ];
+
+        return $r;
+    }
+
+    /**
      * @return array
      */
     public function groupProviders(): array {
@@ -133,7 +193,26 @@ class DomUtilsTest extends MinimalContainerTestCase {
         return [$expectedHtml, $result];
     }
 
-      /**
+    /**
+     * @param string $subdir
+     * @return array
+     */
+    protected function createProviderFromDirectory(string $subdir): array {
+        $provider = $this->createFixtureDataProvider($subdir);
+        $r = [];
+        foreach ($provider as $row) {
+            $dirname = $row[0];
+            $shortName = StringUtils::substringLeftTrim($dirname, PATH_ROOT . '/tests/fixtures/');
+
+            $input = file_get_contents($dirname . '/input.html');
+            $output = file_get_contents($dirname . '/output.html');
+
+            $r[$shortName] = [$input, $output];
+        }
+        return $r;
+    }
+
+    /**
      * Provide tests for `testStripImages()`.
      * @return array
      */

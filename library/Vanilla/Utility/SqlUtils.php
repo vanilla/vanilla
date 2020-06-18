b0b9e057b776e408c3b71cf5178cf9eec43cf5e0
Todd Burry <todd@vanillaforums.com>
2020-06-17 22:39:45 -0400

    Add a docblock

--- library/Vanilla/Utility/SqlUtils.php
+++ library/Vanilla/Utility/SqlUtils.php
@@ -16,6 +16,18 @@ final class SqlUtils {
     public const FEATURE_ALTER_TEXT_FIELD_LENGTHS = 'alterTextFieldLengths';
 
     /**
+     * Protect against alters of text field lengths.
+     *
+     * This method is meant to be run just before a call to `Gdn_DatabaseStructure::set()`. It ensures that existing
+     * text fields don't get altered. Why? Well for two reasons:
+     *
+     * 1. Sometimes a site needs a different size text field (usually larger).
+     * 2. It doesn't really hurt anything to have a different size text field on a site.
+     * 3. Altering tables is expensive, especially on the types of tables that have text fields. Best to avoid that.
+     *
+     * You can call this method directly, but it's also something that can be bound to the `"gdn_mySQLStructure_beforeSet"`
+     * event to act site-wide. Neat.
+     *
      * @param Gdn_DatabaseStructure $structure
      */
     public static function keepTextFieldLengths(Gdn_DatabaseStructure $structure): void {

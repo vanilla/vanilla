7601ea7f63d2db700e8eee70e95b8995a089cf1e
Todd Burry <todd@vanillaforums.com>
2020-06-24 21:57:53 -0400

    Make the toggles on the messages edit page consistent

--- applications/dashboard/views/message/edit.php
+++ applications/dashboard/views/message/edit.php
@@ -6,6 +6,7 @@
             echo t('Add Message');
         ?></h1>
 <?php
+/** @var \MessageController $this */
 echo $this->Form->open();
 echo $this->Form->errors();
 ?>
@@ -60,12 +61,10 @@ echo $this->Form->errors();
             </div>
         </li>
         <li class="form-group">
-            <div class="input-wrap no-label">
-                <?php echo $this->Form->checkBox('AllowDismiss', 'Allow users to dismiss this message', ['value' => '1']); ?>
-            </div>
+            <?php echo $this->Form->toggle('AllowDismiss', 'Dismissible', ['value' => '1'], 'Allow users to dismiss this message.'); ?>
         </li>
         <li class="form-group">
-            <?php echo $this->Form->toggle('Enabled', 'Enable this message', ['value' => '1']); ?>
+            <?php echo $this->Form->toggle('Enabled', 'Enabled', ['value' => '1'], 'An enabled message will be visible on the site.'); ?>
         </li>
     </ul>
 <?php echo $this->Form->close('Save');
